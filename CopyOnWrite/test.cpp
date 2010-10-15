8f64c2e3748bfa11469ee0183624480de35bcadd
ewartt <ewartt@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2010-10-15 10:06:10 +0000

    SIMD test !

--- CopyOnWrite/test.cpp
+++ CopyOnWrite/test.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <iterator>
+#include <emmintrin.h>
 using std::endl;
 using std::cout;
 
 #include "cow_vector.h"
 #include "aligned_allocator.h"
+
 
 // I could've just used a static_cast<...>, but this saves me repeating the type ;-)
 template<class T> const T constify(T foo) { return foo; }
@@ -53,4 +55,34 @@ int main()
         copy_on_write_vector<SIC, aligned_allocator<SIC, 8, false> > bar(10);
         copy_on_write_vector<SIC, aligned_allocator<SIC, 8, true> > bar2(10);
     }
+	
+    {
+		__m128d xmm0,xmm1;
+		
+		copy_on_write_vector<double, aligned_allocator<double, 16, true> > A(2);
+		A[0] = 0.1;
+		A[1] = 0.2;
+		
+		copy_on_write_vector<double, aligned_allocator<double, 16, true> > B(2);		
+		B[0] = 0.9;
+		B[1] = 0.8;
+		//load
+		xmm0 = _mm_load_pd(&A[0]);
+		xmm1 = _mm_load_pd(&B[0]);
+		//add
+		xmm0 = _mm_add_pd(xmm0,xmm1);
+		//store
+		_mm_store_pd(&A[0], xmm0);
+		
+		std::copy(A.begin(), A.end(), std::ostream_iterator<double>(cout, " ")); cout << endl;
+		
+    }
+	
+	
+	
+	
+	
+	
+	
 }
+
