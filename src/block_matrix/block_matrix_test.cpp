dc13271137dfa88036215c108db064fa384a4c57
bela <bela@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2010-11-04 20:36:20 +0000

    Adapting block_matrix to today's discussions

--- src/block_matrix/block_matrix_test.cpp
+++ src/block_matrix/block_matrix_test.cpp
@@ -4,38 +4,70 @@
 using namespace std;
 
 #include <boost/numeric/bindings/std/vector.hpp>
-#include <alps/numeric/detail/general_matrix_new.hpp>
+#include "general_matrix.hpp"
 typedef blas::general_matrix<double> Matrix;
 
 #include "block_matrix.h"
 
 int main()
 {
-    typedef U1 grp;
+    {
+        typedef U1 grp;
+        
+        Index<grp> physical;
+        physical.push_back(std::make_pair(1, 1));
+        physical.push_back(std::make_pair(-2, 1));
+        physical.push_back(std::make_pair(-1, 1));
+        
+        cout << physical << endl;
+        physical.sort();
+        cout << physical << endl;
+        
+        cout << physical.destination(0) << endl;
+        
+        physical.insert(make_pair(0,3));
+        cout << physical << endl;
+        
+        cout << physical.at(-1) << endl;
+    }
     
-    std::vector<grp::charge> c;
-    c.push_back(-1); c.push_back(0); c.push_back(1);
+    {
+        typedef U1 grp;
+        
+        Matrix foo(10,10);
+        block_matrix<Matrix, grp> test;
+        test.insert_block(boost::tuples::make_tuple(foo, 0, 0));
+        
+        cout << test.description() << endl;
+    }
     
-    std::vector<std::size_t> s(3, 2);
-    Index<grp> rows(c, s);
-    
-    c.clear();
-    c.push_back(1); c.push_back(0); c.push_back(-1);
-    s.clear();
-    s.push_back(2); s.push_back(3); s.push_back(4);
-    Index<grp> cols(c, s);
-    grp::get_map(cols.charges());
-    
-    block_matrix<Matrix, grp> m1(rows, cols), m2(transpose(cols), transpose(rows)), m3;
-    
-    cout << m1.description() << endl << m2.description() << endl;
-    match_blocks(m1, m2);
-    cout << m1.description() << endl << m2.description() << endl;
-    gemm(m1, m2, m3);
-    
-    block_matrix<Matrix, grp> U, S, V;
-    svd(m3, U, V, S);
-    
-    cout << S << endl;
-    cout << U << endl;
+    if (false)
+    {
+        typedef U1 grp;
+        
+        std::vector<grp::charge> c;
+        c.push_back(-1); c.push_back(0); c.push_back(1);
+        
+        std::vector<std::size_t> s(3, 2);
+        Index<grp> rows(c, s);
+        
+        c.clear();
+        c.push_back(1); c.push_back(0); c.push_back(-1);
+        s.clear();
+        s.push_back(2); s.push_back(3); s.push_back(4);
+        Index<grp> cols(c, s);
+        grp::get_map(cols.charges());
+        
+        block_matrix<Matrix, grp> m1(rows, cols), m2(transpose(cols), transpose(rows)), m3;
+        
+        cout << m1.description() << endl << m2.description() << endl;
+        cout << m1.description() << endl << m2.description() << endl;
+        gemm(m1, m2, m3);
+        
+        block_matrix<Matrix, grp> U, S, V;
+        svd(m3, U, V, S);
+        
+        cout << S << endl;
+        cout << U << endl;
+    }
 }
