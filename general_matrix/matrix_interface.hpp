30f834d5c46bf1922f801d0e1536bb851f1d2b41
hehn <hehn@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2010-11-10 15:39:11 +0000

    -Moved the hook functions to general_matrix.hpp -Moved blas hooks to general_matrix_blas.hpp

--- general_matrix/matrix_interface.hpp
+++ general_matrix/matrix_interface.hpp
@@ -2,159 +2,37 @@
 #define __ALPS_MATRIX_INTERFACE_HPP__
 
 #include "matrix_concept_check.hpp"
-#include <iostream>
 
 namespace blas
 {
 
 #define IMPLEMENT_FORWARDING(RET,NAME,ARGS,VARS) \
 template <typename MatrixType> \
-typename RET NAME ARGS \
+RET NAME ARGS \
 { \
     BOOST_CONCEPT_ASSERT((blas::Matrix<MatrixType>)); \
     return m.NAME VARS; \
 } \
 
-// IMPLEMENT_FORWARDING(typename MatrixType::size_type, num_rows,(MatrixType const& m),())
-
-template <typename MatrixType>
-typename MatrixType::size_type num_rows(MatrixType const& m)
-{
-    BOOST_CONCEPT_ASSERT((blas::Matrix<MatrixType>));
-    return m.num_rows();
-}
-
-template <typename MatrixType>
-typename MatrixType::size_type num_columns(MatrixType const& m)
-{
-    BOOST_CONCEPT_ASSERT((blas::Matrix<MatrixType>));
-    return m.num_columns();
-}
-
-template <typename MatrixType>
-void swap_rows(MatrixType& m, typename MatrixType::size_type i1, typename MatrixType::size_type i2)
-{
-    BOOST_CONCEPT_ASSERT((blas::Matrix<MatrixType>));
-    m.swap_rows(i1,i2);
-}
-
-template <typename MatrixType>
-void swap_columns(MatrixType& m, typename MatrixType::size_type i1, typename MatrixType::size_type i2)
-{
-    BOOST_CONCEPT_ASSERT((blas::Matrix<MatrixType>));
-    m.swap_columns(i1,i2);
-}
-
+// num_rows(), num_columns(), swap_rows(), swap_columns()
+IMPLEMENT_FORWARDING(typename MatrixType::size_type, num_rows, (MatrixType const& m), () )
+IMPLEMENT_FORWARDING(typename MatrixType::size_type, num_columns, (MatrixType const& m), () )
+IMPLEMENT_FORWARDING(void, swap_rows, (MatrixType& m, typename MatrixType::size_type i1, typename MatrixType::size_type i2), (i1,i2) )
+IMPLEMENT_FORWARDING(void, swap_columns, (MatrixType& m, typename MatrixType::size_type i1, typename MatrixType::size_type i2), (i1,i2) )
 
 //
 // Matrix Iterator Interface
 //
 
-template <typename MatrixType>
-std::pair<typename MatrixType::row_element_iterator, typename MatrixType::row_element_iterator> row(MatrixType& m, typename MatrixType::size_type i)
-{
-    BOOST_CONCEPT_ASSERT((blas::Matrix<MatrixType>));
-    return m.row(i);
-}
+#define ITERATOR_PAIR(ITERATOR) \
+std::pair<typename MatrixType::ITERATOR, typename MatrixType::ITERATOR>
 
-template <typename MatrixType>
-std::pair<typename MatrixType::const_row_element_iterator, typename MatrixType::const_element_row_iterator> row(MatrixType const& m, typename MatrixType::size_type i)
-{
-    BOOST_CONCEPT_ASSERT((blas::Matrix<MatrixType>));
-    return m.row(i);
-}
-
-template <typename MatrixType>
-std::pair<typename MatrixType::column_element_iterator, typename MatrixType::column_element_iterator> column(MatrixType& m, typename MatrixType::size_type j)
-{
-    BOOST_CONCEPT_ASSERT((blas::Matrix<MatrixType>));
-    return m.column(j);
-}
-
-template <typename MatrixType>
-std::pair<typename MatrixType::const_column_element_iterator, typename MatrixType::const_column_element_iterator> column(MatrixType const& m, typename MatrixType::size_type j)
-{
-    BOOST_CONCEPT_ASSERT((blas::Matrix<MatrixType>));
-    return m.column(j);
-}
-
-//
-// Operators
-//
-
-
-template <typename MatrixType>
-boost::enable_if<
-const MatrixType operator + (MatrixType a, MatrixType const& b)
-{
-    BOOST_CONCEPT_ASSERT((blas::Matrix<MatrixType>));
-    a += b;
-    return a;
-}
-
-template <typename MatrixType>
-const MatrixType operator - (MatrixType a, MatrixType const& b)
-{
-    BOOST_CONCEPT_ASSERT((blas::Matrix<MatrixType>));
-    a -= b;
-    return a;
-}
-
-template <typename MatrixType>
-const MatrixType operator * (MatrixType m, typename MatrixType::value_type const& t)
-{
-    return m*=t;
-}
-
-template <typename MatrixType>
-const MatrixType operator * (typename MatrixType::value_type const& t, MatrixType m)
-{
-    return m*=t;
-}
-    
-template<typename MatrixType, typename VectorType>
-const VectorType operator * (MatrixType const& m, VectorType const& v)
-{
-    BOOST_CONCEPT_ASSERT((blas::Matrix<MatrixType>));
-    //TODO BOOST_CONCEPT_ASSERT((blas::Vector<VectorType>));
-    assert( num_columns(m) == v.size() );
-    VectorType result(num_rows(m));
-    // Simple Matrix * Vector
-    for(typename MatrixType::size_type i = 0; i < num_rows(m); ++i)
-    {
-        for(typename MatrixType::size_type j=0; j < num_columns(m); ++j)
-        {
-            result(i) = m(i,j) * v(j);
-        }
-    }
-    return result;
-}
-    
-template<typename MatrixType>
-const MatrixType operator * (MatrixType const& m1, MatrixType const& m2)
-{
-    using detail::matrix_matrix_multiply;
-    return matrix_matrix_multiply(m1,m2);
-}
-
-    
-//    
-//template <typename MatrixType>
-//std::ostream& operator << (std::ostream& o, MatrixType const& m)
-//{
-//    BOOST_CONCEPT_ASSERT((blas::Matrix<MatrixType>));
-//    for(typename MatrixType::size_type i=0; i< num_rows(m); ++i)
-//    {
-//        for(typename MatrixType::size_type j=0; j < num_columns(m); ++j)
-//        {
-//            typename MatrixType::value_type value(m(i,j));
-//            o<<value<<" ";
-//        }
-//        o<<std::endl;
-//    }
-//    return o;
-//}
-//
+IMPLEMENT_FORWARDING( ITERATOR_PAIR(row_element_iterator), row, (MatrixType& m, typename MatrixType::size_type i), (i) )
+IMPLEMENT_FORWARDING( ITERATOR_PAIR(const_row_element_iterator), row, (MatrixType const& m, typename MatrixType::size_type i), (i) )
+IMPLEMENT_FORWARDING( ITERATOR_PAIR(column_element_iterator), column, (MatrixType& m, typename MatrixType::size_type j), (j) )
+IMPLEMENT_FORWARDING( ITERATOR_PAIR(const_column_element_iterator), column, (MatrixType const& m, typename MatrixType::size_type j), (j) )
+#undef ITERATOR_PAIR
+#undef IMPLEMENT_FORWARDING
 } //namespace blas
 
 #endif //__ALPS_MATRIX_INTERFACE_HPP__
