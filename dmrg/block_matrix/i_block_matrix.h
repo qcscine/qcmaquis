a47a2c75a3e345ec1d46a6d568c4cda48927d4f1
akosenko <akosenko@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2010-12-16 16:49:53 +0000

    Updated dense/block matrix interfaces

--- dmrg/block_matrix/i_block_matrix.h
+++ dmrg/block_matrix/i_block_matrix.h
@@ -1,7 +1,13 @@
 class i_block_matrix{
    public:
    virtual void* memory_pointer() = 0;
-
+//   virtual void* block_memory_pointer(int k) = 0;
+//   virtual size_t block_lda(int k) = 0;
+//   virtual bool is_coherent() = 0;
+//   virtual void set_coherency(bool coherent) = 0;
+//   virtual int num_blocks() = 0;
+//   virtual size_t block_size(int k) = 0;
+//   virtual breakdown* breakdown_set() = 0;
    virtual ~i_block_matrix(){}
 };
 
--- dmrg/dense_matrix/i_dense_matrix.h
+++ dmrg/dense_matrix/i_dense_matrix.h
@@ -1,6 +1,13 @@
 class i_dense_matrix{
    public:
    virtual void* memory_pointer() = 0;
-
+//   virtual void* block_memory_pointer(int i, int j) = 0;
+//   virtual size_t lda() = 0;
+//   virtual bool is_coherent() = 0;
+//   virtual void set_coherency(bool coherent) = 0;
+//   virtual size_t size() = 0;
+//   virtual size_t block_size() = 0;
+//   virtual breakdown* breakdown_set() = 0;
    virtual ~i_dense_matrix(){}
 };
+
