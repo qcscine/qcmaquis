11936022c67b6d607b70bda8ad699683144426b2
bela <bela@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-01-12 19:53:35 +0000

    Better ways to create the MPOs

--- dmrg/mpos/generate_mpo.h
+++ dmrg/mpos/generate_mpo.h
@@ -4,19 +4,42 @@
 #include "adjancency.h"
 #include "mp_tensors/mpo.h"
 
-#include "hamiltonians.h"
-
 #include <boost/tuple/tuple.hpp>
 #include <set>
 
 namespace mpos {
+    template<class Matrix, class SymmGroup> class MPOMaker;
+    
+    template<class Matrix, class SymmGroup>
+    class Hamiltonian
+    {
+    public:
+        typedef block_matrix<Matrix, SymmGroup> op_t;
+        typedef std::pair<op_t, op_t> op_pair;
+        
+        virtual ~Hamiltonian() { };
+        
+        virtual op_t get_identity() = 0;
+        virtual op_t get_free() = 0;
+        
+        virtual Index<SymmGroup> get_phys() = 0;
+        
+        virtual int num_2site_ops() = 0;
+        virtual op_pair get_2site_op(int) = 0;
+        
+        virtual int num_1site_ops() = 0;
+        virtual op_t get_1site_op(int) = 0;
+        
+        virtual void push_extra_terms(MPOMaker<Matrix, SymmGroup> &) { }
+    };
+    
     using namespace std;
     using namespace boost::tuples;
     
-    size_t next_free(std::vector<std::size_t> const & out_taken,
-                     std::vector<std::size_t> const & in_taken)
+    size_t next_free(vector<size_t> const & out_taken,
+                     vector<size_t> const & in_taken)
     {
-        for (std::size_t k = 0; true; ++k)
+        for (size_t k = 0; true; ++k)
         {
             if (count(out_taken.begin(), out_taken.end(), k) == 0 &&
                 count(in_taken.begin(), in_taken.end(), k) == 0)
@@ -26,7 +49,7 @@ namespace mpos {
     
     size_t next_free(set<size_t> const & s)
     {
-        for (std::size_t k = 2; true; ++k)
+        for (size_t k = 2; true; ++k)
             if (s.count(k) == 0)
                 return k;
     }
@@ -44,8 +67,100 @@ namespace mpos {
         > op_pairs;
         
     public:
-        static MPOTensor<Matrix, SymmGroup> as_bulk(vector<block> const & ops,
-                                                    size_t M)
+        MPOMaker(Adjacency const & adj_,
+                 Hamiltonian<Matrix, SymmGroup> & H_)
+        : adj(adj_)
+        , H(H_)
+        , used_dims(adj.size())
+        , prempo(adj.size())
+        , maximum(0)
+        {   
+            for (size_t p = 0; p < adj.size(); ++p)
+            {
+                prempo[p].push_back( make_tuple(0, 0, H.get_identity()) );
+                prempo[p].push_back( make_tuple(1, 1, H.get_identity()) );
+            }
+        }
+        
+        void add_bond_ops()
+        {
+            for (size_t p = 0; p < adj.size(); ++p) {
+                vector<size_t> neighs = adj[p];
+                for (vector<size_t>::iterator neigh = neighs.begin(); neigh != neighs.end(); ++neigh)
+                    for (int i = 0; i < H.num_2site_ops(); ++i)
+                    {
+                        pair<op_t, op_t> op = H.get_2site_op(i);
+                        
+                        vector<pair<size_t, op_t> > ops;
+                        ops.push_back( make_pair( p, op.first ) );
+                        ops.push_back( make_pair( *neigh, op.second ) );
+                        add_term(ops);
+                    }
+            }
+            
+            cout << "Maximum: " << maximum << endl;
+        }
+        
+        void add_term(vector<pair<size_t, op_t> > const & ops)
+        {
+            vector<size_t> positions;
+            for (typename vector<pair<size_t, op_t> >::const_iterator it = ops.begin();
+                 it != ops.end(); ++it)
+                positions.push_back( it->first );
+            size_t minp = *min_element(positions.begin(), positions.end());
+            size_t maxp = *max_element(positions.begin(), positions.end());
+            
+            size_t use_b;
+            for (use_b = 2; true; ++use_b) {
+                bool valid = true;
+                for (size_t p = minp; p < maxp; ++p)
+                    valid &= (used_dims[p].count(use_b) == 0);
+                if (valid)
+                    break;
+            }
+            maximum = max(use_b, maximum);
+
+            vector<bool> done(positions.size(), false);
+            for (typename vector<pair<size_t, op_t> >::const_iterator it = ops.begin();
+                 it != ops.end(); ++it)
+            {
+                size_t first_use_b = (it->first == minp ? 0 : use_b);
+                size_t second_use_b = (it->first == maxp ? 1 : use_b);
+                prempo[it->first].push_back( make_tuple(first_use_b, second_use_b, it->second) );
+                used_dims[it->first].insert(use_b);
+                done[it->first] = true;
+            }
+            
+            for (size_t p = minp; p <= maxp; ++p)
+                if (!done[p]) {
+                    prempo[p].push_back( make_tuple(use_b, use_b, H.get_free()) );
+                    used_dims[p].insert(use_b);
+                    done[p] = true;
+                }
+        }
+            
+        MPO<Matrix, SymmGroup> create_mpo()
+        {
+            MPO<Matrix, SymmGroup> r(adj.size());
+            for (size_t p = 1; p < adj.size() - 1; ++p)
+                r[p] = as_bulk(prempo[p], maximum+1);
+            r[0] = as_left(prempo[0], maximum+1);
+            r[adj.size()-1] = as_right(prempo[adj.size()-1], maximum+1);
+            
+            return r;
+        }
+        
+    private:
+        Adjacency const & adj;
+        Hamiltonian<Matrix, SymmGroup> & H;
+
+        vector<set<size_t> > used_dims;
+        vector<vector<block> > prempo;
+        
+        size_t maximum;
+        
+        MPOTensor<Matrix, SymmGroup> as_bulk(vector<block> const & ops,
+                                             size_t M)
         {
             MPOTensor<Matrix, SymmGroup> r(M, M);
             for (typename vector<block>::const_iterator it = ops.begin();
@@ -56,8 +171,8 @@ namespace mpos {
             return r;
         }
         
-        static MPOTensor<Matrix, SymmGroup> as_left(vector<block> const & ops,
-                                                    size_t M)
+        MPOTensor<Matrix, SymmGroup> as_left(vector<block> const & ops,
+                                             size_t M)
         {
             MPOTensor<Matrix, SymmGroup> r(1, M);
             for (typename vector<block>::const_iterator it = ops.begin();
@@ -70,8 +185,8 @@ namespace mpos {
             return r;
         }
         
-        static MPOTensor<Matrix, SymmGroup> as_right(vector<block> const & ops,
-                                                     size_t M)
+        MPOTensor<Matrix, SymmGroup> as_right(vector<block> const & ops,
+                                              size_t M)
         {
             MPOTensor<Matrix, SymmGroup> r(M, 1);
             for (typename vector<block>::const_iterator it = ops.begin();
@@ -81,52 +196,6 @@ namespace mpos {
                     continue;
                 r(get<0>(*it), 0) = get<2>(*it);
             }
-            return r;
-        }
-        
-        static MPO<Matrix, SymmGroup>
-        create_mpo(Adjacency const & adj,
-                   Hamiltonian<Matrix, SymmGroup> & H)
-        {
-            vector<set<size_t> > used_dims(adj.size()-1);
-            
-            vector<vector<block> > prempo(adj.size());
-            size_t maximum = 0;
-            
-            for (size_t p = 0; p < adj.size(); ++p)
-            {
-                prempo[p].push_back( make_tuple(0, 0, H.get_identity()) );
-                prempo[p].push_back( make_tuple(1, 1, H.get_identity()) );
-                
-                vector<size_t> neighs = adj[p];
-                for (vector<size_t>::iterator neigh = neighs.begin(); neigh != neighs.end(); ++neigh)
-                    for (int i = 0; i < H.num_2site_ops(); ++i)
-                    {
-                        std::pair<op_t, op_t> op = H.get_2site_op(i);
-                        
-                        size_t nfree = next_free(used_dims[p]);
-                        maximum = std::max(maximum, nfree);
-                        
-                        prempo[p].push_back( make_tuple(0, nfree, op.first) );
-                        used_dims[p].insert(nfree);
-                        
-                        for (size_t pp = p+1; pp < *neigh; ++pp) {
-                            prempo[pp].push_back( make_tuple(nfree, nfree, H.get_free()) );
-                            used_dims[pp].insert(nfree);
-                        }
-                        
-                        prempo[*neigh].push_back( make_tuple(nfree, 1, op.second) );
-                    }
-            }
-            
-            cout << "Maximum: " << maximum << endl;
-            
-            MPO<Matrix, SymmGroup> r(adj.size());
-            for (size_t p = 1; p < adj.size() - 1; ++p)
-                r[p] = as_bulk(prempo[p], maximum+1);
-            r[0] = as_left(prempo[0], maximum+1);
-            r[adj.size()-1] = as_right(prempo[adj.size()-1], maximum+1);
-            
             return r;
         }
     };
--- dmrg/mpos/hamiltonians.h
+++ dmrg/mpos/hamiltonians.h
@@ -2,271 +2,270 @@
 #define HAMILTONIANS_H
 
 #include "block_matrix/block_matrix.h"
+#include "mpos/generate_mpo.h"
 
 #include <vector>
 #include <utility>
 
-template<class Matrix, class SymmGroup>
-class Hamiltonian
-{
-public:
-    typedef block_matrix<Matrix, SymmGroup> op_t;
-    typedef std::pair<op_t, op_t> op_pair;
-    
-	virtual ~Hamiltonian() { };
-    
-    virtual op_t get_identity() = 0;
-    virtual op_t get_free() = 0;
-    
-    virtual Index<SymmGroup> get_phys() = 0;
-    
-    virtual int num_2site_ops() = 0;
-    virtual op_pair get_2site_op(int) = 0;
-    
-    virtual int num_1site_ops() = 0;
-    virtual op_t get_1site_op(int) = 0;
-};
-
-template<class Matrix>
-class Heisenberg : public Hamiltonian<Matrix, U1>
-{
-public:
-    typedef typename Hamiltonian<Matrix, U1>::op_t op_t;
-    typedef std::pair<op_t, op_t> op_pair;
-    typedef std::vector<op_pair> op_pairs;
-    
-    Heisenberg(double Jxy_, double Jz_) : Jxy(Jxy_), Jz(Jz_)
+namespace mpos {
+    template<class Matrix>
+    class Heisenberg : public Hamiltonian<Matrix, U1>
     {
-        block_matrix<Matrix, U1> ident, splus, sminus, sz;
+    public:
+        typedef typename Hamiltonian<Matrix, U1>::op_t op_t;
+        typedef std::pair<op_t, op_t> op_pair;
+        typedef std::vector<op_pair> op_pairs;
         
-        ident.insert_block(Matrix(1, 1, 1), -1, -1);
-        ident.insert_block(Matrix(1, 1, 1), 1, 1);
+        Heisenberg(double Jxy_, double Jz_) : Jxy(Jxy_), Jz(Jz_)
+        {
+            block_matrix<Matrix, U1> ident, splus, sminus, sz;
+            
+            ident.insert_block(Matrix(1, 1, 1), -1, -1);
+            ident.insert_block(Matrix(1, 1, 1), 1, 1);
+            
+            splus.insert_block(Matrix(1, 1, 1), -1, 1);
+            
+            sminus.insert_block(Matrix(1, 1, 1), 1, -1);
+            
+            sz.insert_block(Matrix(1, 1, 0.5), 1, 1);
+            sz.insert_block(Matrix(1, 1, -0.5), -1, -1);
+            
+            ops.push_back(make_pair(ident, ident));
+            ops.push_back(make_pair(Jxy/2*splus, sminus));
+            ops.push_back(make_pair(Jxy/2*sminus, splus));
+            ops.push_back(make_pair(Jz*sz, sz));
+        }
         
-        splus.insert_block(Matrix(1, 1, 1), -1, 1);
+        op_t get_identity() { return ops[0].first; }
+        op_t get_free() { return ops[0].first; }
         
-        sminus.insert_block(Matrix(1, 1, 1), 1, -1);
+        int num_2site_ops() { return ops.size()-1; }
+        op_pair get_2site_op(int i) { return ops[i+1]; }
         
-        sz.insert_block(Matrix(1, 1, 0.5), 1, 1);
-        sz.insert_block(Matrix(1, 1, -0.5), -1, -1);
+        int num_1site_ops() { return 0; }
+        op_t get_1site_op(int) { return op_t(); }
         
-        ops.push_back(make_pair(ident, ident));
-        ops.push_back(make_pair(Jxy/2*splus, sminus));
-        ops.push_back(make_pair(Jxy/2*sminus, splus));
-        ops.push_back(make_pair(Jz*sz, sz));
-    }
+        Index<U1> get_phys()
+        {
+            Index<U1> phys;
+            phys.insert(std::make_pair(1, 1));
+            phys.insert(std::make_pair(-1, 1));
+            return phys;
+        }
+        
+    private:
+        double Jxy, Jz;
+        op_pairs ops;
+    };
     
-    op_t get_identity() { return ops[0].first; }
-    op_t get_free() { return ops[0].first; }
-    
-    int num_2site_ops() { return ops.size()-1; }
-    op_pair get_2site_op(int i) { return ops[i+1]; }
-    
-    int num_1site_ops() { return 0; }
-    op_t get_1site_op(int) { return op_t(); }
-    
-    Index<U1> get_phys()
+    template<class Matrix>
+    class HCB : public Hamiltonian<Matrix, U1>
     {
-        Index<U1> phys;
-        phys.insert(std::make_pair(1, 1));
-        phys.insert(std::make_pair(-1, 1));
-        return phys;
-    }
-    
-private:
-    double Jxy, Jz;
-    op_pairs ops;
-};
-
-template<class Matrix>
-class HCB : public Hamiltonian<Matrix, U1>
-{
-    typedef typename Hamiltonian<Matrix, U1>::op_t op_t;
-    typedef std::pair<op_t, op_t> op_pair;
-    typedef std::vector<op_pair> op_pairs;
-    
-public:
-    HCB()
-    {   
-        block_matrix<Matrix, U1> create, destroy, ident, count;
+        typedef typename Hamiltonian<Matrix, U1>::op_t op_t;
+        typedef std::pair<op_t, op_t> op_pair;
+        typedef std::vector<op_pair> op_pairs;
         
-        ident.insert_block(Matrix(1, 1, 1), 0, 0);
-        ident.insert_block(Matrix(1, 1, 1), 1, 1);
-        
-        create.insert_block(Matrix(1, 1, 1), 0, 1);
-        destroy.insert_block(Matrix(1, 1, 1), 1, 0);
-        
-        count.insert_block(Matrix(1, 1, 1), 1, 1);
-        
-        double t = 1;
-        
+    public:
+        HCB()
+        {   
+            block_matrix<Matrix, U1> create, destroy, ident, count;
+            
+            ident.insert_block(Matrix(1, 1, 1), 0, 0);
+            ident.insert_block(Matrix(1, 1, 1), 1, 1);
+            
+            create.insert_block(Matrix(1, 1, 1), 0, 1);
+            destroy.insert_block(Matrix(1, 1, 1), 1, 0);
+            
+            count.insert_block(Matrix(1, 1, 1), 1, 1);
+            
+            double t = 1;
+            
 #define term(a,b) ops.push_back(make_pair(a, b))
-        term(ident, ident);
-        term(-t*create, destroy);
-        term(-t*destroy, create);
+            term(ident, ident);
+            term(-t*create, destroy);
+            term(-t*destroy, create);
 #undef term
-    }
+        }
+        
+        op_t get_identity() { return ops[0].first; }
+        op_t get_free() { return ops[0].first; }
+        
+        int num_2site_ops() { return ops.size()-1; }
+        op_pair get_2site_op(int i) { return ops[i+1]; }
+        
+        int num_1site_ops() { return 0; }
+        op_t get_1site_op(int) { return op_t(); }
+        
+        Index<U1> get_phys()
+        {
+            Index<U1> phys;
+            phys.insert(std::make_pair(0, 1));
+            phys.insert(std::make_pair(1, 1));
+            return phys;
+        }
+        
+    private:
+        op_pairs ops;
+    };
     
-    op_t get_identity() { return ops[0].first; }
-    op_t get_free() { return ops[0].first; }
-    
-    int num_2site_ops() { return ops.size()-1; }
-    op_pair get_2site_op(int i) { return ops[i+1]; }
-    
-    int num_1site_ops() { return 0; }
-    op_t get_1site_op(int) { return op_t(); }
-    
-    Index<U1> get_phys()
+    template<class Matrix>
+    class FreeFermions : public Hamiltonian<Matrix, U1>
     {
-        Index<U1> phys;
-        phys.insert(std::make_pair(0, 1));
-        phys.insert(std::make_pair(1, 1));
-        return phys;
-    }
-    
-private:
-    op_pairs ops;
-};
-
-template<class Matrix>
-class FreeFermions : public Hamiltonian<Matrix, U1>
-{
-    typedef typename Hamiltonian<Matrix, U1>::op_t op_t;
-    typedef std::pair<op_t, op_t> op_pair;
-    typedef std::vector<op_pair> op_pairs;
-    
-public:
-    FreeFermions()
-    {   
-        block_matrix<Matrix, U1> create, destroy;
-        create.insert_block(Matrix(1, 1, 1), 0, 1);
-        destroy.insert_block(Matrix(1, 1, 1), 1, 0);
+        typedef typename Hamiltonian<Matrix, U1>::op_t op_t;
+        typedef std::pair<op_t, op_t> op_pair;
+        typedef std::vector<op_pair> op_pairs;
         
-        double t = 1;
-        
+    public:
+        FreeFermions()
+        {   
+            block_matrix<Matrix, U1> create, destroy;
+            create.insert_block(Matrix(1, 1, 1), 0, 1);
+            destroy.insert_block(Matrix(1, 1, 1), 1, 0);
+            
+            double t = 1;
+            
 #define term(a,b) ops.push_back(make_pair(a, b))
-        term(-t*create, destroy);
-        term(-t*destroy, create);
+            term(-t*create, destroy);
+            term(-t*destroy, create);
 #undef term
-    }
-    
-    op_t get_identity()
-    { 
-        block_matrix<Matrix, U1> ident;
+        }
         
-        ident.insert_block(Matrix(1, 1, 1), 0, 0);
-        ident.insert_block(Matrix(1, 1, 1), 1, 1);
+        op_t get_identity()
+        { 
+            block_matrix<Matrix, U1> ident;
+            
+            ident.insert_block(Matrix(1, 1, 1), 0, 0);
+            ident.insert_block(Matrix(1, 1, 1), 1, 1);
+            
+            return ident;
+        }
         
-        return ident;
-    }
+        op_t get_free()
+        {
+            block_matrix<Matrix, U1> sign;
+            
+            sign.insert_block(Matrix(1, 1, 1), 0, 0);
+            sign.insert_block(Matrix(1, 1, -1), 1, 1);
+            
+            return sign;
+        }
+        
+        int num_2site_ops() { return ops.size(); }
+        op_pair get_2site_op(int i) { return ops[i]; }
+        
+        int num_1site_ops() { return 0; }
+        op_t get_1site_op(int) { return op_t(); }
+        
+        Index<U1> get_phys()
+        {
+            Index<U1> phys;
+            phys.insert(std::make_pair(0, 1));
+            phys.insert(std::make_pair(1, 1));
+            return phys;
+        }
+        
+    private:
+        op_pairs ops;
+    };
     
-    op_t get_free()
+    template<class Matrix>
+    class Spin1BlBq : public Hamiltonian<Matrix, U1>
     {
-        block_matrix<Matrix, U1> sign;
+    public:
+        typedef typename Hamiltonian<Matrix, U1>::op_t op_t;
+        typedef std::pair<op_t, op_t> op_pair;
+        typedef std::vector<op_pair> op_pairs;
         
-        sign.insert_block(Matrix(1, 1, 1), 0, 0);
-        sign.insert_block(Matrix(1, 1, -1), 1, 1);
-        
-        return sign;
-    }
-    
-    int num_2site_ops() { return ops.size(); }
-    op_pair get_2site_op(int i) { return ops[i]; }
-    
-    int num_1site_ops() { return 0; }
-    op_t get_1site_op(int) { return op_t(); }
-    
-    Index<U1> get_phys()
-    {
-        Index<U1> phys;
-        phys.insert(std::make_pair(0, 1));
-        phys.insert(std::make_pair(1, 1));
-        return phys;
-    }
-    
-private:
-    op_pairs ops;
-};
-
-template<class Matrix>
-class Spin1BlBq : public Hamiltonian<Matrix, U1>
-{
-public:
-    typedef typename Hamiltonian<Matrix, U1>::op_t op_t;
-    typedef std::pair<op_t, op_t> op_pair;
-    typedef std::vector<op_pair> op_pairs;
-    
-    Spin1BlBq(double Jbl_, double Jbq_) : Jbl(Jbl_), Jbq(Jbq_)
-    {   
-        block_matrix<Matrix, U1> ident, splus, sminus, sz, spp, smm, spm, smp, szz, szp, spz, szm, smz;
-        
-        ident.insert_block(Matrix(1, 1, 1), -1, -1);
-        ident.insert_block(Matrix(1, 1, 1), 0, 0);
-        ident.insert_block(Matrix(1, 1, 1), 1, 1);
-        
-        splus.insert_block(Matrix(1, 1, 1), -1, 0);
-        splus.insert_block(Matrix(1, 1, 1), 0, 1);
-        
-        sminus.insert_block(Matrix(1, 1, 1), 1, 0);
-        sminus.insert_block(Matrix(1, 1, 1), 0, -1);
-        
-        sz.insert_block(Matrix(1, 1, 1), 1, 1);
-        sz.insert_block(Matrix(1, 1, 0), 0, 0);
-        sz.insert_block(Matrix(1, 1, -1), -1, -1);
-        
-        gemm(splus, splus, spp);
-        gemm(sminus, sminus, smm);
-        gemm(splus, sminus, spm);
-        gemm(sminus, splus, smp);
-        gemm(sz, sz, szz);
-        gemm(sz, splus, szp);
-        gemm(splus, sz, spz);
-        gemm(sz, sminus, szm);
-        gemm(sminus, sz, smz);
-        
+        Spin1BlBq(double Jbl_, double Jbq_, double h0_) : Jbl(Jbl_), Jbq(Jbq_), h0(h0_)
+        {   
+            block_matrix<Matrix, U1> ident, splus, sminus, sz, spp, smm, spm, smp, szz, szp, spz, szm, smz;
+            
+            ident.insert_block(Matrix(1, 1, 1), -1, -1);
+            ident.insert_block(Matrix(1, 1, 1), 0, 0);
+            ident.insert_block(Matrix(1, 1, 1), 1, 1);
+            
+            splus.insert_block(Matrix(1, 1, 1), -1, 0);
+            splus.insert_block(Matrix(1, 1, 1), 0, 1);
+            
+            sminus.insert_block(Matrix(1, 1, 1), 1, 0);
+            sminus.insert_block(Matrix(1, 1, 1), 0, -1);
+            
+            sz.insert_block(Matrix(1, 1, 1), 1, 1);
+            sz.insert_block(Matrix(1, 1, 0), 0, 0);
+            sz.insert_block(Matrix(1, 1, -1), -1, -1);
+            
+            gemm(splus, splus, spp);
+            gemm(sminus, sminus, smm);
+            gemm(splus, sminus, spm);
+            gemm(sminus, splus, smp);
+            gemm(sz, sz, szz);
+            gemm(sz, splus, szp);
+            gemm(splus, sz, spz);
+            gemm(sz, sminus, szm);
+            gemm(sminus, sz, smz);
+            
 #define term(a,b) ops.push_back(make_pair(a, b))
-        
-        term(ident, ident);
-        term(Jbl*splus, sminus);
-        term(Jbl*sminus, splus);
-        term(Jbl*sz, sz);
-        
-        term(Jbq*spp, smm);
-        term(Jbq*smm, spp);
-        term(Jbq*szz, szz);
-        
-        term(Jbq*spm, smp);
-        term(Jbq*smp, spm);
-        
-        term(Jbq*spz, smz);
-        term(Jbq*szp, szm);
-        
-        term(Jbq*smz, spz);
-        term(Jbq*szm, szp);
-        
+            
+            term(ident, ident);
+            term(Jbl*splus, sminus);
+            term(Jbl*sminus, splus);
+            term(Jbl*sz, sz);
+            
+            term(Jbq*spp, smm);
+            term(Jbq*smm, spp);
+            term(Jbq*szz, szz);
+            
+            term(Jbq*spm, smp);
+            term(Jbq*smp, spm);
+            
+            term(Jbq*spz, smz);
+            term(Jbq*szp, szm);
+            
+            term(Jbq*smz, spz);
+            term(Jbq*szm, szp);
+            
 #undef term
-    }
-    
-    op_t get_identity() { return ops[0].first; }
-    op_t get_free() { return ops[0].first; }
-    
-    int num_2site_ops() { return ops.size()-1; }
-    op_pair get_2site_op(int i) { return ops[i+1]; }
-    
-    int num_1site_ops() { return 0; }
-    op_t get_1site_op(int) { return op_t(); }
-
-    Index<U1> get_phys()
-    {
-        Index<U1> phys;
-        phys.insert(std::make_pair(1, 1));
-        phys.insert(std::make_pair(0, 1));
-        phys.insert(std::make_pair(-1, 1));
-        return phys;
-    }
-    
-private:
-    double Jbl, Jbq;
-    op_pairs ops;
-};
+        }
+        
+        op_t get_identity() { return ops[0].first; }
+        op_t get_free() { return ops[0].first; }
+        
+        int num_2site_ops() { return ops.size()-1; }
+        op_pair get_2site_op(int i) { return ops[i+1]; }
+        
+        int num_1site_ops() { return 0; }
+        op_t get_1site_op(int) { return op_t(); }
+        
+        Index<U1> get_phys()
+        {
+            Index<U1> phys;
+            phys.insert(std::make_pair(1, 1));
+            phys.insert(std::make_pair(0, 1));
+            phys.insert(std::make_pair(-1, 1));
+            return phys;
+        }
+        
+        void push_extra_terms(MPOMaker<Matrix, U1> & mm)
+        {
+            cout << "Adding extra term." << endl;
+            std::vector<std::pair<std::size_t, op_t> > terms;
+            block_matrix<Matrix, U1> sz, szz;
+            
+            sz.insert_block(Matrix(1, 1, 1), 1, 1);
+            sz.insert_block(Matrix(1, 1, 0), 0, 0);
+            sz.insert_block(Matrix(1, 1, -1), -1, -1);
+            
+            gemm(sz, sz, szz);
+            
+            terms.push_back( std::make_pair(1, h0*sz) );
+            
+            mm.add_term(terms);
+        }
+            
+    private:
+        double Jbl, Jbq, h0;
+        op_pairs ops;
+    };
+}
 
 #endif
