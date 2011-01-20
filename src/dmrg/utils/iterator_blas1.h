75826d32e880d895f2863623bac5b6579736a126
bela <bela@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-01-20 11:03:53 +0000

    Adding axpy on pointers.

---
+++ src/dmrg/utils/iterator_blas1.h
@@ -0,0 +1,26 @@
+#ifndef ITERATOR_BLAS1_H
+#define ITERATOR_BLAS1_H
+
+#include <algorithm>
+
+template<class ForwardIterator, class T>
+void iterator_axpy(ForwardIterator in1, ForwardIterator in2,
+                         ForwardIterator out1, T val)
+{
+    std::transform(in1, in2, out1, std::bind2nd(std::multiplies<T>(), val));
+}
+
+inline void iterator_axpy(double const * in1, double const * in2,
+                          double * out1, double val)
+{
+    fortran_int_t one = 1, diff = in2-in1;
+    daxpy_(&diff, &val, in1, &one, out1, &one);
+}
+
+inline void iterator_axpy(std::complex<double> const * in1, std::complex<double> const * in2,
+                          std::complex<double> * out1, double val)
+{
+    throw std::runtime_error("Not implemented.");
+}
+
+#endif
