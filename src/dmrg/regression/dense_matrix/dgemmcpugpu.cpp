645ec8c58a84ab19b8ffa4d4906f6cc4606e4ae1
ewartt <ewartt@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-01-24 15:51:56 +0000

    mv files

--- src/dmrg/regression/dense_matrix/dgemmcpugpu.cpp
+++ src/dmrg/regression/dense_matrix/dgemmcpugpu.cpp
@@ -12,6 +12,9 @@
 #include <ostream>
 #include <fstream>
 #include <iomanip>
+#include <sstream>
+#include <string>
+
 
 #include <sys/time.h> 
 #include <unistd.h>
@@ -19,6 +22,11 @@
 using std::cout;
 using std::cerr;
 using std::endl;
+using std::string;
+
+
+
+#include "utils/timings.h"
 
 #define NDEBUG
 
@@ -28,81 +36,102 @@ using std::endl;
 #include "dense_matrix/dense_matrix_algorithms.h"
 #include "dense_matrix/matrix_algorithms.hpp"
 
-#include <boost/timer.hpp>
-
 #include <omp.h>
 
-
-#ifdef __mGPU__
-#include "dense_matrix/gpu/matrix_gpu.h"
-#include "dense_matrix/gpu/dense_matrix_gpu.h"
-#include "dense_matrix/gpu/matrix_gpu_functions.hpp"
-#else
 #include "dense_matrix/dense_matrix_blas.hpp"
-#endif
+#include "dense_matrix/gpu/matrix_gpu.h"
+#include "dense_matrix/gpu/matrix_gpu_functions.hpp"
 
 
-int main(int   argc, char * argv[])
+
+template <class T>
+inline std::string to_string (const T& t)
 {
-	//gpu::Simu Simulation;
-	
-	srand(0);
-	int NUM = atoi(argv[1]);
-	
-	blas::dense_matrix<float> A(2*NUM,3*NUM);
-	blas::dense_matrix<float> B(3*NUM,2*NUM);
-	blas::dense_matrix<float> C(2*NUM,3*NUM,0);
-	
-	for(int i=0; i< 2*NUM ;i++  )
-	{
-		for(int j=0; j< 3*NUM ; j++ )
-		{
-			A(i,j) = rand();
-		}
-	}
-
-	cout << A << endl;
-	
-	for(int i=0; i< 3*NUM ;i++  )
-	{
-		for(int j=0; j<2*NUM ; j++ )
-		{
-				B(i,j) = rand();
-		}
-	}
-
-	cout << B << endl;
-	
-	
-	struct timeval tp;
-	gettimeofday( &tp, NULL );
-		
-	double sec      = static_cast<double>( tp.tv_sec );
-	double usec = static_cast<double>( tp.tv_usec )/1E6;
-	double start = sec + usec;
-	
-	C = matrix_matrix_multiply(A,B);
-	
-	cout << C << endl;
-	
-	gettimeofday( &tp, NULL );
-	sec = static_cast<double>( tp.tv_sec );
-	usec = static_cast<double>( tp.tv_usec )/1E6;
-	double end = sec + usec;
-	
-	double time = end - start;
-	
-	std::ofstream out;
-	out.open("time.txt",std::ios::app);
-	out << std::setprecision (6) << time << " " <<  NUM << " " << omp_get_max_threads() <<endl;
-	out.close();
-	
-	cout << std::setprecision (6) << time << " " <<  NUM << " " << omp_get_max_threads() <<endl;
-	
-	out.open("resultatcpu.txt",std::ios::app);
-	out << C <<endl;
-	out.close();
-	
-	
-	
+	std::stringstream ss;
+	ss << t;
+	return ss.str();
 }
+
+
+int main( int argc, char* argv[])
+{
+	
+	gpu::Simu Simulation;
+	srand(0);
+	
+	float verif = 0 ;
+	float verif2 = 0 ;
+
+	
+	for (int NUM = 3 ; NUM <= 5000 ; NUM *= 2)
+	{
+		
+		blas::dense_matrix<float> A(NUM,NUM);
+		blas::dense_matrix<float> B(NUM,NUM);
+		blas::dense_matrix<float> C_CPU(NUM,NUM,0);
+		blas::dense_matrix<float> C_GPU(NUM,NUM,0);
+		
+		for(int i=0; i< NUM ;i++  )
+		{
+			for(int j=0; j< NUM ; j++ )
+			{
+				A(i,j) = rand();
+			}
+		}
+		
+		for(int i=0; i< NUM ;i++  )
+		{
+			for(int j=0; j< NUM ; j++ )
+			{
+				B(i,j) = rand();
+			}
+		}
+		
+		string size_n = to_string(NUM);
+		string name_CPU = "DGEMM CPU ";
+		string name_CPU_GPU = "DGEMM GPU ";
+		
+		Timer temps_CPU(name_CPU += size_n) ;
+		
+		temps_CPU.begin();	
+		C_CPU =  matrix_matrix_multiply(A,B);
+		temps_CPU.end();
+		
+		float GFLOP_CPU = NUM*NUM*NUM/temps_CPU.GetTime();
+		
+		
+		
+		Timer temps_CPU_GPU(name_CPU_GPU += size_n) ;
+		
+		temps_CPU_GPU.begin();	
+		gpu::matrix_matrix_multiply(A,B,C_GPU);
+		temps_CPU_GPU.end();	
+	
+		
+		float GFLOP_GPU = NUM*NUM*NUM/temps_CPU_GPU.GetTime();
+		
+		float split = GFLOP_GPU/(GFLOP_GPU+GFLOP_CPU);
+
+		
+		
+		cout << " split  " << split << endl;
+		
+		for(int i=0; i< NUM ;i++  )
+		{
+			for(int j=0; j< NUM ; j++ )
+			{
+				verif = (C_GPU(i,j) -  C_CPU(i,j) ) * (C_GPU(i,j) -  C_CPU(i,j) ) / C_CPU(i,j);
+			}
+		}
+		
+		
+		std::ofstream out;
+		out.open("time.txt",std::ios::app);
+		out << std::setprecision (6) <<  NUM << " " << temps_CPU.GetTime() << " " << temps_CPU_GPU.GetTime() << "   " << verif << omp_get_max_threads() <<endl;
+		out.close();
+		
+		
+	}	
+	return 0;
+}
+
