50f2c94c0a3ee972a926a6ba75fb40833f521a4b
bela <bela@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-02-10 21:37:31 +0000

    Making use of GMRES.

--- dmrg/mp_tensors/ietl_jacobi_davidson.h
+++ dmrg/mp_tensors/ietl_jacobi_davidson.h
@@ -21,16 +21,23 @@ solve_ietl_jcd(SiteProblem<Matrix, SymmGroup> & sp,
     typedef ietl::vectorspace<Vector> Vecspace;
     typedef boost::lagged_fibonacci607 Gen;
     
-    ietl::jcd_simple_solver<SiteProblem<Matrix, SymmGroup>, SingleSiteVS<Matrix, SymmGroup> > jcd_ss(sp, vs);
-//    ietl::jcd_cg_solver<SiteProblem<Matrix, SymmGroup>, SingleSiteVS<Matrix, SymmGroup> > jcd_cg(sp, vs);
-    ietl::jacobi_davidson<SiteProblem<Matrix, SymmGroup>, SingleSiteVS<Matrix, SymmGroup> > jd(sp, vs, ietl::Smallest);
+    // define this variable if your ALPS is too old
+#ifdef USE_OLD_CRAP
+    ietl::jcd_simple_solver<SiteProblem<Matrix, SymmGroup>, SingleSiteVS<Matrix, SymmGroup> >
+    jcd_gmres(sp, vs);
+#else
+    ietl::jcd_gmres_solver<SiteProblem<Matrix, SymmGroup>, SingleSiteVS<Matrix, SymmGroup> >
+    jcd_gmres(sp, vs, params.get<int>("ietl_jcd_gmres"));
+#endif
+    
+    ietl::jacobi_davidson<SiteProblem<Matrix, SymmGroup>, SingleSiteVS<Matrix, SymmGroup> >
+    jd(sp, vs, ietl::Smallest);
     
     double tol = params.get<double>("ietl_jcd_tol");
     ietl::basic_iteration<double> iter(100, tol, tol);
     
     std::pair<double, Vector> r0;
-//    r0 = jd.calculate_eigenvalue(initial, jcd_cg, iter);
-    r0 = jd.calculate_eigenvalue(initial, jcd_ss, iter);
+    r0 = jd.calculate_eigenvalue(initial, jcd_gmres, iter);
     
     zout << "JCD used " << iter.iterations() << " iterations." << endl;
     
