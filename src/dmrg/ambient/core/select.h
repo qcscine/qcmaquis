4083448d2c21aa20697f40fbd386273abf1aa604
akosenko <akosenko@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-02-23 20:26:02 +0000

    save

--- src/dmrg/ambient/core/select.h
+++ src/dmrg/ambient/core/select.h
@@ -5,11 +5,9 @@
 #include "ambient/ambient.h"
 
 namespace ambient {
-
     int parseout_id(const char* sql, char** id);
     void select(const char* sql);
     void retain(const char* sql);
-
 }
 
 #endif
--- src/dmrg/ambient/interface/core.hpp
+++ src/dmrg/ambient/interface/core.hpp
@@ -1,27 +1,20 @@
-namespace ambient { using namespace blas;
+#include "ambient/core/operation/operation.pp.hpp"
 
-    template <typename FL, typename FC, class T0, class T1, class T2>
-    void push(FL l_kernel, FC c_kernel, T0& arg0, T1& arg1, T2& arg2){
-        if(get_profile(arg0)->proxy) pin(arg0, arg2);
-        if(get_profile(arg1)->proxy) pin(arg1, arg2);
-        ambient::engine.push(new core::operation(l_kernel, &arg0, &arg1, &arg2),
-                             new core::operation(c_kernel, &arg0, &arg1, &arg2));
-    }
-    template <typename ST, typename FL, typename FC, class T0, class T1>
-    ST push(FL l_kernel, FC c_kernel, T0& arg0, T1& arg1){
-        void_pt* handle = new void_pt((ST*)NULL);
-        ST out(handle);
-        push(l_kernel, c_kernel, arg0, arg1, out);
-        return out;
-    }
-    #include "ambient/interface/push.pp.hpp" // all other variants
+namespace USER_NAMESPACE{ using namespace ambient;
+    template <typename T> 
+    void_pt& breakdown(const T& obj){ return breakdown(&obj); }
 
-    template <typename L, typename R>
-    void pin(L& proxy_object, const R& real_object){
-        void_pt* proxy = get_profile(proxy_object);
-        void_pt* real  = get_profile(real_object);
-        proxy->profile   = real->profile;
-        proxy->set_dim(real->get_dim());
-        real->imitate(proxy); // copy proxy settings to the actual profile
+    template <typename T> 
+    void_pt& breakdown(const T* obj){ 
+        void_pt** profile_ptr = const_cast<void_pt**>(&obj->profile);
+        return *(*profile_ptr = (void_pt*)(*profile_ptr)->dereference());
     }
+}
+
+namespace ambient { using namespace USER_NAMESPACE;
+
+    template <typename T> void_pt::void_pt(const T* ptr) : p_profile(){
+        breakdown_model(this, ptr);
+        this->regroup(); 
+    };
 }
