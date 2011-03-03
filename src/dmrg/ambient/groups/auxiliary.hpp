29191d5c57af8378cdd67e89ef1cbc9a053724eb
akosenko <akosenko@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-03-03 06:57:56 +0000

    save

--- src/dmrg/ambient/groups/auxiliary.hpp
+++ src/dmrg/ambient/groups/auxiliary.hpp
@@ -17,9 +17,12 @@ namespace ambient{ namespace groups{
 
     packet* recv(const packet_t& type, group* grp, void* memory);
     packet* recv(const packet_t& type, const char* grp, void* memory);
-
+    ambient_request* irecv(const packet_t& type, group* grp, void* memory);
+    ambient_request* irecv(const packet_t& type, const char* grp, void* memory);
     void send(packet* pack, group* grp, int dest = -1);
     void send(packet* pack, const char* grp, int dest = -1);
+    ambient_request* isend(packet* pack, group* grp, int dest = -1);
+    ambient_request* isend(packet* pack, const char* grp, int dest = -1);
 
 } }
 
--- src/dmrg/ambient/groups/packet_manager.h
+++ src/dmrg/ambient/groups/packet_manager.h
@@ -8,6 +8,16 @@ using namespace ambient::packets;
 
 namespace ambient{ namespace groups{
 
+    class group;
+
+    class ambient_request
+    {
+    public:
+        ambient_request(void* memory);
+        MPI_Request request;
+        void* memory;
+    };
+
     class packet_manager
     {
     private: 
@@ -15,10 +25,18 @@ namespace ambient{ namespace groups{
         packet_manager& operator=(packet_manager const&){};  // assignment operator is private
         MPI_Comm* comm;
     public:
+        std::vector<ambient_request*> send_requests;
+        std::vector<ambient_request*> recv_requests;
         packet_manager(MPI_Comm* comm);
         void send(packet* pack, int dest);
         void recv(const packet_t& type, void* memory);
+        ambient_request* isend(packet* pack, int dest);
+        ambient_request* irecv(const packet_t& type, void* memory);
     };
+
+    void wait_on_requests(group* grp);
+    std::vector<ambient_request*>* get_send_requests(group* grp);
+    std::vector<ambient_request*>* get_recv_requests(group* grp);
 
 } }
 #endif
