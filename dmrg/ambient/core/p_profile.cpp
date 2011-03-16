f911fdae2bf1c8f6f5edc5d8faf3f6a5693371a0
akosenko <akosenko@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-03-16 01:58:28 +0000

    requesting for blocks for const profiles now works

--- dmrg/ambient/core/p_profile.cpp
+++ dmrg/ambient/core/p_profile.cpp
@@ -54,7 +54,7 @@ namespace ambient {
     int p_profile::get_xmaster(){ return this->master_relay.first; }
     groups::group* p_profile::get_scope(){ return this->scope; }
     groups::group* p_profile::get_xscope(){ return this->xscope; } 
-    bool p_profile::xinvolved(){ return (this->get_xscope() != NULL && this->get_xscope()->involved()); }
+    bool p_profile::xinvolved(){ return ((this->get_xscope() != NULL && this->get_xscope()->involved()) || this->involved()); } // modified
     bool p_profile::involved(){ return this->get_scope()->involved(); }
 
     p_profile & p_profile::operator>>(dim3 distr_dim) 
