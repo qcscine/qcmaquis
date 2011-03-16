1c268386fb71980f255036732b4606585c8c23cc
dolfim <dolfim@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-03-16 13:10:32 +0000

    Refs #26: interface to ALPS Lattice library

---
+++ dmrg/mpos/alps_adjancency.h
@@ -0,0 +1,43 @@
+
+#ifndef ALPS_ADJACENCY_H
+#define ALPS_ADJACENCY_H
+
+#include "adjancency.h"
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+namespace adj {
+	
+	class ALPSAdj : public Adjacency
+    {
+    public:
+		
+		ALPSAdj (std::string const &);
+		
+        std::vector<int> forward(int p) const
+		{
+			return forward_[p];
+		}
+        std::vector<int> all(int p) const
+		{
+			std::vector<int> ret = forward_[p];
+			std::copy(backward_[p].begin(), backward_[p].end(), std::back_inserter(ret));
+			return ret; 
+		}
+		
+        int size() const
+		{
+			return size_;
+		}
+		
+	private:
+		int size_;
+		std::vector<std::vector<int> > forward_;
+		std::vector<std::vector<int> > backward_;		
+    };	
+	
+}
+
+#endif
