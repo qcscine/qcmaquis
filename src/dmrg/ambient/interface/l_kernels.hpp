cbff97909a2a39e3cc13540b813e2baa8d02baa1
akosenko <akosenko@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-03-21 15:22:42 +0000

    update

--- src/dmrg/ambient/interface/l_kernels.hpp
+++ src/dmrg/ambient/interface/l_kernels.hpp
@@ -7,75 +7,50 @@ void info(T& obj){
     }
 }
 
-void mem_bound_l_kernel(const p_dense_matrix<double>& a, const p_dense_matrix<double>& b, pinned p_dense_matrix<double>& out){
-//    a >> dim3(10,5), dim3(1,1), dim3(10,1); <- kinda non-trivial - need to think
-    scope_select("2 from ambient as work where master is 0");
-    scope_retain("2 from ambient as work_storage");
-    if(!scope.involved()) return; // out of scope quick exit
-
-    info(a); info(b); info(out);
-
-    for(int i=0; i < get_grid_dim(out).y; i++)
-        for(int j=0; j < get_grid_dim(out).x; j++)
-            if(j % scope.get_size() == scope.get_rank()){
-                assign(a,   i, j);
-                assign(b,   i, j);
-                assign(out, i, j);
-            }
+void block_2d_cycle_assign(const p_dense_matrix<double>& target)
+{
+///////////////////////////////////////////// 2D-block-cyclic decomposition
+    int np = 1; // can be a function arg   // process grid's num of rows 
+    int nq = (int)(scope.get_size() / np); // process grid's num of cols 
+    int rank_i = (int)(scope.get_rank() / nq); // process row
+    int rank_j = (int)(scope.get_rank() % nq); // process col
+///////////////////////////////////////////////////////////////////////////
+    for(int i = rank_i; i < get_grid_dim(target).y; i += np){
+        for(int j = rank_j; j < get_grid_dim(target).x; j += nq){
+            assign(target, i, j);
+        }
+    }
 }
 
-void gemm_l_kernel(const p_dense_matrix<double>& a, const p_dense_matrix<double>& b, pinned p_dense_matrix<double>& out)
+void gemm_l_kernel(const p_dense_matrix<double>& a, const p_dense_matrix<double>& b, pinned p_dense_matrix<double>& c)
 {
     scope_select("0.5 from ambient as work where master is 0"); // todo: correct the naming issue
     if(!scope.involved()) return;
 
-    zout << "2d-block-cyclic decomposition kernel in gemm:\n"; info(a); info(b); info(out);
-///////////////////////////////////////////// 2D-block-cyclic decomposition
-    int np = 1; // can be a function arg   // process grid's num of rows 
-    int nq = (int)(scope.get_size() / np); // process grid's num of cols 
-    int rank_i = (int)(scope.get_rank() / nq); // process row
-    int rank_j = (int)(scope.get_rank() % nq); // process col
-///////////////////////////////////////////////////////////////////////////
-    for(int i = rank_i; i < get_grid_dim(out).y; i += np){
-        for(int j = rank_j; j < get_grid_dim(out).x; j += nq){
-            assign(a,   i, 0);
-            assign(b,   0, j);
-            assign(out, i, j);
-        }
-    }
+    zout << "2d-block-cyclic decomposition kernel in gemm:\n"; info(a); info(b); info(c);
+
+    block_2d_cycle_assign(a);
+    block_2d_cycle_assign(b);
+    block_2d_cycle_assign(c);
 }
 
-void scale_l_kernel(const p_dense_matrix<double>& m, const double& t, pinned p_dense_matrix<double>& out){
-// todo
+void mem_bound_l_kernel(const p_dense_matrix<double>& a, const p_dense_matrix<double>& b, pinned p_dense_matrix<double>& c)
+{
+    scope_select("2 from ambient as work where master is 0");
+    scope_retain("2 from ambient as work_storage");
+    if(!scope.involved()) return; // out of scope quick exit
+
+    info(a); info(b); info(c);
+
+    block_2d_cycle_assign(a);
+    block_2d_cycle_assign(b);
+    block_2d_cycle_assign(c);
 }
 
-void pdgemm_l_kernel(p_dense_matrix<double>& a, p_dense_matrix<double>& b, p_dense_matrix<double>& c){
-    scope_select("* from ambient as work_redist where master is 0"); // fix the "same name" problem!
-    if(!scope.involved()) return;
+void scale_l_kernel(const p_dense_matrix<double>& m, const double& t, pinned p_dense_matrix<double>& out){}
 
-    zout << "2d-block-cyclic decomposition kernel:\n"; info(a); info(b); info(c);
-///////////////////////////////////////////// 2D-block-cyclic decomposition
-    int np = 1; // can be a function arg   // process grid's num of rows 
-    int nq = (int)(scope.get_size() / np); // process grid's num of cols 
-    int rank_i = (int)(scope.get_rank() / nq); // process row
-    int rank_j = (int)(scope.get_rank() % nq); // process col
-///////////////////////////////////////////////////////////////////////////
-    for(int i = rank_i; i < get_grid_dim(a).y; i += np){
-        for(int j = rank_j; j < get_grid_dim(a).x; j += nq){
-            assign(a, i, j);
-        }
-    }
-    for(int i = rank_i; i < get_grid_dim(a).y; i += np){
-        for(int j = rank_j; j < get_grid_dim(a).x; j += nq){
-            assign(b, i, j);
-        }
-    }
-    for(int i = rank_i; i < get_grid_dim(a).y; i += np){
-        for(int j = rank_j; j < get_grid_dim(a).x; j += nq){
-            assign(c, i, j);
-        }
-    }
-}
+/////////////////////
+// testing kernels // 
 
 void single_integer_l_kernel(int& input){
     scope_select("* from ambient as single_integer_work where master is 0");
@@ -83,4 +58,3 @@ void single_integer_l_kernel(int& input){
     zout << "single integer kernel: input is " << input << "\n";
     input = 0;
 }
-
