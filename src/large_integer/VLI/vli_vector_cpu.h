df7f731aad2878f83af99bcc4898d1b823a7e428
ewartt <ewartt@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-03-23 23:13:35 +0000

    start the vector implementation of vli

---
+++ src/large_integer/VLI/vli_vector_cpu.h
@@ -0,0 +1,69 @@
+/*
+ *  vli_vector_cpu.h
+ *  Untitled
+ *
+ *  Created by Tim Ewart on 23/03/11.
+ *  Copyright 2011 University of Geneva. All rights reserved.
+ *
+ */
+
+
+#ifndef __VLI_VECTOR_
+#define __VLI_VECTOR_
+
+#include "definition.h"
+#include "vli_number_cpu.h"
+
+namespace vli
+{
+template <class T>
+class vli_vector
+{
+public:
+
+	vli_vector(std::size_t size);
+	~vli_vector();
+	void clean();
+	std::size_t const GetSize() const;
+
+	/**
+	 access operator
+	*/
+	vli_cpu<T>* operator[](std::size_t i);
+	/**
+	 multiply and addition operators
+	 */															
+	vli_vector<T>& operator += (vli_vector<T> const& vli); 
+	vli_vector<T>& operator *= (vli_vector<T> const& vli); 
+	
+private:
+	std::size_t size_;
+	std::vector<vli_cpu<T>* > vector_vli_;
+};
+	
+	/**
+	 multiply and addition operators, suite ...
+	 */
+	template <class T>
+	const vli_vector<T> operator+(vli_vector<T> vli_a, vli_vector<T> const& vli_b);
+	
+	template <class T>
+	void plus_assign(vli_vector<T> & vli_a, vli_vector<T> const& vli_b );
+	
+	template <class T>
+	const vli_vector<T> operator*(vli_vector<T> vli_a, vli_vector<T> const& vli_b);
+	
+	template <class T>
+	void multiply_assign(vli_vector<T> & vli_a, vli_vector<T> const& vli_b );
+	
+	/**
+	 stream 
+	 */
+	template<typename T>
+	std::ostream& operator<< (std::ostream& os,  vli_vector<T> & vli);
+	
+	
+
+}
+
+#endif
---
+++ src/large_integer/VLI/vli_vector_cpu.hpp
@@ -0,0 +1,99 @@
+/*
+ *  vli_vector_cpu.hpp
+ *  Untitled
+ *
+ *  Created by Tim Ewart on 23/03/11.
+ *  Copyright 2011 University of Geneva. All rights reserved.
+ *
+ */
+
+#include "vli_vector_cpu.h"
+
+namespace vli
+{
+	template<class T>
+	vli_vector<T>::vli_vector(std::size_t size = 0):size_(size)
+	{
+		vector_vli_.resize(size);
+		typename std::vector< vli_cpu<T> * >::iterator it;
+		for(it = vector_vli_.begin() ; it != vector_vli_.end() ; it++)
+		{
+			*it = new vli_cpu<T>();
+		}
+	}
+	
+	template<class T>
+	vli_vector<T>::~vli_vector()
+	{
+		vector_vli_.clear(); //Useless I think
+	};
+
+
+	template<class T>
+	void vli_vector<T>::clean()
+	{
+		vector_vli_.clear(); //should be enough
+	}
+	
+	template<class T>
+	const std::size_t vli_vector<T>::GetSize() const
+	{
+		return size_;
+	}
+	
+	/**
+	 access operator
+	 */
+	template<class T>
+	vli_cpu<T>*  vli_vector<T>::operator[](std::size_t i )
+	{
+		return vector_vli_[i];
+	}
+	
+	
+	template<class T>
+	vli_vector<T>& vli_vector<T>::operator += (vli_vector<T> const &  vli)
+	{
+		plus_assign(*this, vli);
+		return *this;
+	}
+	
+	/**
+	 multiply and addition operators, suite ...
+	 */
+	template <class T>
+	const vli_vector<T> operator+(vli_vector<T> vli_a, vli_vector<T> const& vli_b)
+	{
+		vli_a += vli_b;
+		return vli_a;
+	}
+																				   																				   																		
+	template <class T>
+	void plus_assign(vli_vector<T> & vli_a, vli_vector<T> const& vli_b )
+	{
+		/**
+		  the vectors have the same size
+		*/
+		#pragma omp parallel for private(i)
+		for(std::size_t i = 0; i < vli_b.GetSize(); i++)
+		{
+	//		addition_classic_cpu( vli_a[i], vli_b[i]);
+		}
+		
+			
+	}
+	
+	template <class T>
+	const vli_vector<T> operator*(vli_vector<T> vli_a, vli_vector<T> const& vli_b);
+	
+	template <class T>
+	void multiply_assign(vli_vector<T> & vli_a, vli_vector<T> const& vli_b );
+	
+	/**
+	 stream 
+	 */
+	template<typename T>
+	std::ostream& operator<< (std::ostream& os,  vli_vector<T> & vli);
+	
+
+}
