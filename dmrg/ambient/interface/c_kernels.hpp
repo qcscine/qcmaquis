663cedcfdcde81f987a57db14d7a2f96aff31bee
ewartt <ewartt@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-04-04 12:41:42 +0000

    fix bug resize_kernel

--- dmrg/ambient/interface/c_kernels.hpp
+++ dmrg/ambient/interface/c_kernels.hpp
@@ -6,8 +6,8 @@ extern "C" {
     void Cblacs_get( int, int, int* );
     void Cblacs_gridinit( int*, const char*, int, int );
     void Cblacs_gridinfo( int, int*, int*, int*, int* );
-	void Cblacs_gridexit(int *);
-	void Cblacs_exit(int *);
+    void Cblacs_gridexit(int *);
+    void Cblacs_exit(int *);
     int Csys2blacs_handle( MPI_Comm );
     int numroc_( int*, int*, int*, int*, int* );
     void descinit_( int*, int*, int*, int*, int*, int*, int*, int*, int*, int* );
@@ -227,11 +227,14 @@ void remove_cols_c_kernel(pinned p_dense_matrix<double>& a, const size_t& j_mark
 }
 
 void resize_c_kernel(p_dense_matrix<double>& a, const size_t& rows, const size_t& cols)
-{ 
+{
     for(int i = 0; i < get_grid_dim(a).y; i++)
     if(current(a).group(i, get_grid_dim(a).x-1)->available()){
         size_t cutoff = get_grid_dim(a).x*get_group_t_dim(a).x - get_dim(a).x;
-        if(cutoff > 0) memset((double*)current(a)(i, get_grid_dim(a).x-1), 0, cutoff*sizeof(double));
+        if(cutoff > 0){
+            double* ad = current(a)(i, get_grid_dim(a).x-1);
+            memset(&ad[(get_group_t_dim(a).x - cutoff)*get_group_t_dim(a).y], 0, cutoff*sizeof(double));
+        }
     }
     for(int j = 0; j < get_grid_dim(a).x; j++)
     if(current(a).group(get_grid_dim(a).y-1, j)->available()){
@@ -249,8 +252,13 @@ void sqrt_diagonal_c_kernel(pinned p_dense_matrix<double>& a){
         ad[i] = sqrt(ad[i]);
 }
 
-void null_c_kernel(const p_dense_matrix<double>& a, const p_dense_matrix<double>& b, pinned p_dense_matrix<double>& c){
-}
+void one_null_c_kernel(const p_dense_matrix<double>& a){ }
+
+void two_null_c_kernel(const p_dense_matrix<double>& a, const p_dense_matrix<double>& b){ }
+
+void three_null_c_kernel(const p_dense_matrix<double>& a, const p_dense_matrix<double>& b, const p_dense_matrix<double>& c){ }
+
+
 void add_c_kernel(const p_dense_matrix<double>& a, const p_dense_matrix<double>& b, pinned p_dense_matrix<double>& c){
     printf("Executing add kernel for %d %d\n", get_group_id(c).y, get_group_id(c).x); 
     double* ad = current(a)(get_group_id(c).y, get_group_id(c).x);
@@ -264,110 +272,110 @@ void scale_c_kernel(const p_dense_matrix<double>& a, const double& alfa, pinned 
 
 void gemm_c_scalapack_kernel(const p_dense_matrix<double>  &  A, const p_dense_matrix<double>  & B, p_dense_matrix<double> & C){
 #ifdef SCALAPACK
-	int nmyidBLACS,nnumprocsBLACS,nContinue;
-	int nContxt,nVal;  
-	int i, j, k;
-	int bhandle, ictxt, nprow, npcol, myrow, mycol,nb;
-	int nN = get_grid_dim(C).x*get_group_dim(C).x*get_item_dim(C).x; 
-	int nM = get_grid_dim(C).y*get_group_dim(C).y*get_item_dim(C).y;
-  							 
-	int info,itemp;
-	int ZERO=0,ONE=1;
+    int nmyidBLACS,nnumprocsBLACS,nContinue;
+    int nContxt,nVal;  
+    int i, j, k;
+    int bhandle, ictxt, nprow, npcol, myrow, mycol,nb;
+    int nN = get_grid_dim(C).x*get_group_dim(C).x*get_item_dim(C).x; 
+    int nM = get_grid_dim(C).y*get_group_dim(C).y*get_item_dim(C).y;
 
-	nprow = scope.np;
-	npcol = scope.nq; 
-	nb = get_group_dim(C).x*get_item_dim(C).x;
+    int info,itemp;
+    int ZERO=0,ONE=1;
 
-	ictxt =Csys2blacs_handle(scope.get_group()->mpi_comm);
-	Cblacs_get( -1, 0, &ictxt );
-	Cblacs_gridinit( &ictxt, "Row", nprow, npcol );
-	Cblacs_gridinfo( ictxt, &nprow, &npcol, &myrow, &mycol );
-	
-	int descA[9],descB[9],descC[9];
-	
-	int mA = numroc_( &nM, &nb, &myrow, &ZERO, &nprow );	
-	int nA = numroc_( &nN, &nb, &mycol, &ZERO, &npcol );
-	int mB = numroc_( &nM, &nb, &myrow, &ZERO, &nprow );
-	int nB = numroc_( &nN, &nb, &mycol, &ZERO, &npcol );
-	int mC = numroc_( &nM, &nb, &myrow, &ZERO, &nprow );
-	int nC = numroc_( &nN, &nb, &mycol, &ZERO, &npcol );
-		
-	descinit_(descA, &nM,   &nN,   &nb,  &nb,  &ZERO, &ZERO, &ictxt, &mA,  &info);
-	descinit_(descB, &nM,   &nN,   &nb,  &nb,  &ZERO, &ZERO, &ictxt, &mB,  &info);
-	descinit_(descC, &nM,   &nN,   &nb,  &nb,  &ZERO, &ZERO, &ictxt, &mC,  &info);
+    nprow = scope.np;
+    npcol = scope.nq; 
+    nb = get_group_dim(C).x*get_item_dim(C).x;
 
-	breakdown(A).solidify();
-	breakdown(B).solidify();
-	breakdown(C).solidify();
-	
-	double alpha = 1.0; double beta = 0.0;
+    ictxt =Csys2blacs_handle(scope.get_group()->mpi_comm);
+    Cblacs_get( -1, 0, &ictxt );
+    Cblacs_gridinit( &ictxt, "Row", nprow, npcol );
+    Cblacs_gridinfo( ictxt, &nprow, &npcol, &myrow, &mycol );
 
-	pdgemm_("N","N",&nM,&nN,&nM,&alpha,(double*)breakdown(A).data,&ONE,&ONE,descA,(double*)breakdown(B).data,&ONE,&ONE,descB,&beta,(double*)breakdown(C).data,&ONE,&ONE,descC);
+    int descA[9],descB[9],descC[9];
 
-	breakdown(A).disperse();
-	breakdown(B).disperse();
-	breakdown(C).disperse();		
-#endif							 
+    int mA = numroc_( &nM, &nb, &myrow, &ZERO, &nprow );
+    int nA = numroc_( &nN, &nb, &mycol, &ZERO, &npcol );
+    int mB = numroc_( &nM, &nb, &myrow, &ZERO, &nprow );
+    int nB = numroc_( &nN, &nb, &mycol, &ZERO, &npcol );
+    int mC = numroc_( &nM, &nb, &myrow, &ZERO, &nprow );
+    int nC = numroc_( &nN, &nb, &mycol, &ZERO, &npcol );
+
+    descinit_(descA, &nM,   &nN,   &nb,  &nb,  &ZERO, &ZERO, &ictxt, &mA,  &info);
+    descinit_(descB, &nM,   &nN,   &nb,  &nb,  &ZERO, &ZERO, &ictxt, &mB,  &info);
+    descinit_(descC, &nM,   &nN,   &nb,  &nb,  &ZERO, &ZERO, &ictxt, &mC,  &info);
+
+    breakdown(A).solidify();
+    breakdown(B).solidify();
+    breakdown(C).solidify();
+
+    double alpha = 1.0; double beta = 0.0;
+
+    pdgemm_("N","N",&nM,&nN,&nM,&alpha,(double*)breakdown(A).data,&ONE,&ONE,descA,(double*)breakdown(B).data,&ONE,&ONE,descB,&beta,(double*)breakdown(C).data,&ONE,&ONE,descC);
+
+    breakdown(A).disperse();
+    breakdown(B).disperse();
+    breakdown(C).disperse();
+#endif
 }
 
 void svd_c_scalapack_kernel(const p_dense_matrix<double>  &  M, p_dense_matrix<double>  & U, p_dense_matrix<double> & V, double* & S)
 {
 #ifdef SCALAPACK
 
-	int descA[9],descV[9],descU[9];
-	int nmyidBLACS,nnumprocsBLACS,nContinue;
-	int nContxt,nVal;  
-	int i, j, k;
-	int bhandle, ictxt, nprow, npcol, myrow, mycol,nb;
-	int nN = get_grid_dim(M).x*get_group_t_dim(M).x; 
-	int nM = get_grid_dim(M).y*get_group_t_dim(M).y;
+    int descA[9],descV[9],descU[9];
+    int nmyidBLACS,nnumprocsBLACS,nContinue;
+    int nContxt,nVal;  
+    int i, j, k;
+    int bhandle, ictxt, nprow, npcol, myrow, mycol,nb;
+    int nN = get_grid_dim(M).x*get_group_t_dim(M).x; 
+    int nM = get_grid_dim(M).y*get_group_t_dim(M).y;
 
-	int info,itemp;
-	int ZERO=0,ONE=1;
+    int info,itemp;
+    int ZERO=0,ONE=1;
 
-	nprow = scope.np;
-	npcol = scope.nq; 
-	nb = get_group_dim(M).x*get_item_dim(M).x;
+    nprow = scope.np;
+    npcol = scope.nq; 
+    nb = get_group_dim(M).x*get_item_dim(M).x;
 
-	ictxt =Csys2blacs_handle(scope.get_group()->mpi_comm);
-	Cblacs_get( -1, 0, &ictxt );
-	Cblacs_gridinit( &ictxt, "Row", nprow, npcol );
-	Cblacs_gridinfo( ictxt, &nprow, &npcol, &myrow, &mycol );
-	
-	int mA = numroc_( &nM, &nb, &myrow, &ZERO, &nprow );	
-	int nA = numroc_( &nN, &nb, &mycol, &ZERO, &npcol );
-	int mU = numroc_( &nM, &nb, &myrow, &ZERO, &nprow );
-	int nU = numroc_( &nM, &nb, &mycol, &ZERO, &npcol );
-	int mV = numroc_( &nN, &nb, &myrow, &ZERO, &nprow );
-	int nV = numroc_( &nN, &nb, &mycol, &ZERO, &npcol );
-	
-	descinit_(descA, &nM,   &nN,   &nb,  &nb,  &ZERO, &ZERO, &ictxt, &mA,  &info);
-	descinit_(descU, &nM,   &nM,   &nb,  &nb,  &ZERO, &ZERO, &ictxt, &mU,  &info);
-	descinit_(descV, &nN,   &nN,   &nb,  &nb,  &ZERO, &ZERO, &ictxt, &mV,  &info);
-	
-	breakdown(M).solidify();
-	breakdown(U).solidify();
-	breakdown(V).solidify();
-	
-	char JOBU[1] ={'V'};
-	char JOBV[1] ={'V'};
-	int lwork = -1;
-	double wkopt;
+    ictxt =Csys2blacs_handle(scope.get_group()->mpi_comm);
+    Cblacs_get( -1, 0, &ictxt );
+    Cblacs_gridinit( &ictxt, "Row", nprow, npcol );
+    Cblacs_gridinfo( ictxt, &nprow, &npcol, &myrow, &mycol );
 
-	//SCALAPACK, firsti, dry run to allocate buffer	
-	pdgesvd_(JOBU, JOBV, &nM, &nN, (double*)breakdown(M).data,&ONE,&ONE,descA, S, (double*)breakdown(U).data,&ONE,&ONE,descU, (double*)breakdown(V).data,&ONE,&ONE,descV, &wkopt, &lwork, &info);
+    int mA = numroc_( &nM, &nb, &myrow, &ZERO, &nprow );
+    int nA = numroc_( &nN, &nb, &mycol, &ZERO, &npcol );
+    int mU = numroc_( &nM, &nb, &myrow, &ZERO, &nprow );
+    int nU = numroc_( &nM, &nb, &mycol, &ZERO, &npcol );
+    int mV = numroc_( &nN, &nb, &myrow, &ZERO, &nprow );
+    int nV = numroc_( &nN, &nb, &mycol, &ZERO, &npcol );
 
-	lwork = static_cast<int> (wkopt);
-	double *work = new double[lwork];
+    descinit_(descA, &nM,   &nN,   &nb,  &nb,  &ZERO, &ZERO, &ictxt, &mA,  &info);
+    descinit_(descU, &nM,   &nM,   &nb,  &nb,  &ZERO, &ZERO, &ictxt, &mU,  &info);
+    descinit_(descV, &nN,   &nN,   &nb,  &nb,  &ZERO, &ZERO, &ictxt, &mV,  &info);
 
-	//COMPUTATIONAL run	
-	pdgesvd_(JOBU, JOBV, &nM, &nN, (double*)breakdown(M).data,&ONE,&ONE,descA, S, (double*)breakdown(U).data,&ONE,&ONE,descU, (double*)breakdown(V).data,&ONE,&ONE,descV, work, &lwork, &info);
-	
-	breakdown(M).disperse();
-	breakdown(U).disperse();
-	breakdown(V).disperse();		
-	
-	delete[] work; //clean the working buffer
+    breakdown(M).solidify();
+    breakdown(U).solidify();
+    breakdown(V).solidify();
+
+    char JOBU[1] ={'V'};
+    char JOBV[1] ={'V'};
+    int lwork = -1;
+    double wkopt;
+
+    //SCALAPACK, firsti, dry run to allocate buffer
+    pdgesvd_(JOBU, JOBV, &nM, &nN, (double*)breakdown(M).data,&ONE,&ONE,descA, S, (double*)breakdown(U).data,&ONE,&ONE,descU, (double*)breakdown(V).data,&ONE,&ONE,descV, &wkopt, &lwork, &info);
+
+    lwork = static_cast<int> (wkopt);
+    double *work = new double[lwork];
+
+    //COMPUTATIONAL run
+    pdgesvd_(JOBU, JOBV, &nM, &nN, (double*)breakdown(M).data,&ONE,&ONE,descA, S, (double*)breakdown(U).data,&ONE,&ONE,descU, (double*)breakdown(V).data,&ONE,&ONE,descV, work, &lwork, &info);
+
+    breakdown(M).disperse();
+    breakdown(U).disperse();
+    breakdown(V).disperse();
+
+    delete[] work; //clean the working buffer
 
 #endif
 }
