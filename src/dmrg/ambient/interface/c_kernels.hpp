9bed8f3cd90a52fe04d205f0c06c58a2590f4651
ewartt <ewartt@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-04-05 16:21:52 +0000

    bug ambient

--- src/dmrg/ambient/interface/c_kernels.hpp
+++ src/dmrg/ambient/interface/c_kernels.hpp
@@ -46,61 +46,6 @@ void gemm_c_kernel(pinned const p_dense_matrix<double>& a, const p_dense_matrix<
     }
 }
 
-
-void pdgemm_c_kernel(p_dense_matrix<double>& a, p_dense_matrix<double>& b, p_dense_matrix<double>& c){
-//    printf("R%d: Executing ScaLAPACK PDGEMM kernel\n", scope.get_rank());
-
-/*    int i, j, k;
-    int bhandle, ictxt, nprow, npcol, myrow, mycol,nb;
-    nprow = NODE_COUNT; npcol = (int)(scope.get_size()/NODE_COUNT); 
-    nb = get_group_dim(c).x*get_item_dim(c).x;
-    int M = get_grid_dim(c)*get_group_dim(c).x*get_item_dim(c).x;
-    int info,itemp;
-    int ZERO=0,ONE=1;
- 
-    bhandle = Csys2blacs_handle(scope.get_group()->mpi_comm);
-    ictxt = bhandle;
-    //Cblacs_get( -1, 0, &ictxt );
-    Cblacs_gridinit( &ictxt, "Row", nprow, npcol );
-    Cblacs_gridinfo( ictxt, &nprow, &npcol, &myrow, &mycol );
-    //printf("Number of rows: %d\nNumber of cols: %d\nMy row: %d\nMy col: %d\n", nprow, npcol, myrow, mycol);
-
-    int descA[9],descB[9],descC[9];
-    int mA = numroc_( &M, &nb, &myrow, &ZERO, &nprow );
-    int nA = numroc_( &M, &nb, &mycol, &ZERO, &npcol );
-    int mB = numroc_( &M, &nb, &myrow, &ZERO, &nprow );
-    int nB = numroc_( &M, &nb, &mycol, &ZERO, &npcol );
-    int mC = numroc_( &M, &nb, &myrow, &ZERO, &nprow );
-    int nC = numroc_( &M, &nb, &mycol, &ZERO, &npcol );
-    descinit_(descA, &M,   &M,   &nb,  &nb,  &ZERO, &ZERO, &ictxt, &mA,  &info);
-    descinit_(descB, &M,   &M,   &nb,  &nb,  &ZERO, &ZERO, &ictxt, &mB,  &info);
-    descinit_(descC, &M,   &M,   &nb,  &nb,  &ZERO, &ZERO, &ictxt, &mC,  &info);
-    
-   // breakdown(a).solidify();
-   // breakdown(b).solidify();
-   // breakdown(c).solidify();
-    double *A = (double*) malloc(mA*nA*sizeof(double));
-    double *B = (double*) malloc(mB*nB*sizeof(double));
-    double *C = (double*) malloc(mC*nC*sizeof(double));
-    for(i=0;i<mA;i++) for(j=0;j<nA;j++){
-                 A[j*mA+i]=0.01;
-         }
-    for(i=0;i<mB;i++) for(j=0;j<nB;j++){
-                 B[j*mB+i]=0.01;
-         }
-    for(i=0;i<mC;i++) for(j=0;j<nC;j++){
-                 C[j*mC+i]=0.01;
-         }
-    printf("M: %d; mA: %d; nA: %d\n", M, mA, nA);
-    double alpha = 1.0; double beta = 0.0;
-    
-//    for(i=0;i<VECTOR_SIZE;i++)
-    //pdgemm_("N","N",&M,&M,&M,&alpha,(double*)breakdown(a).data,&ONE,&ONE,descA,(double*)breakdown(b).data,&ONE,&ONE,descB,&beta,(double*)breakdown(c).data,&ONE,&ONE,descC);
-    pdgemm_("N","N",&M,&M,&M,&alpha,A,&ONE,&ONE,descA,B,&ONE,&ONE,descB,&beta,C,&ONE,&ONE,descC);
-*/
-}
-
-
 void copy_c_kernel(p_dense_matrix<double>& ac, pinned const p_dense_matrix<double>& a)
 {    
     int i = get_group_id(a).y;
@@ -248,7 +193,6 @@ void two_null_c_kernel(const p_dense_matrix<double>& a, const p_dense_matrix<dou
 
 void three_null_c_kernel(const p_dense_matrix<double>& a, const p_dense_matrix<double>& b, const p_dense_matrix<double>& c){ }
 
-
 void add_c_kernel(const p_dense_matrix<double>& a, const p_dense_matrix<double>& b, pinned p_dense_matrix<double>& c){
     double* ad = current(a)(get_group_id(c).y, get_group_id(c).x);
     double* cd = current(c)(get_group_id(c).y, get_group_id(c).x);
@@ -257,7 +201,35 @@ void add_c_kernel(const p_dense_matrix<double>& a, const p_dense_matrix<double>&
     printf("Executed add kernel for %d %d\n", get_group_id(c).y, get_group_id(c).x); 
 }
 void sub_c_kernel(const p_dense_matrix<double>& a, const p_dense_matrix<double>& b, pinned p_dense_matrix<double>& c){}
+
 void scale_c_kernel(const p_dense_matrix<double>& a, const double& alfa, pinned p_dense_matrix<double>& out){}
+/**
+void gemm_lhs_diagonal_c_kernel(const p_dense_matrix<double> & a_diag, pinned const p_dense_matrix<double>& b, p_dense_matrix<double>& c)
+{
+
+}*/
+
+//void gemm_rhs_diagonal_c_kernel(pinned const p_dense_matrix<double> & a, const p_dense_matrix<double>& b_diag, p_dense_matrix<double>& c)
+void gemm_rhs_diagonal_c_kernel(const p_dense_matrix<double> & a, const p_dense_matrix<double>& c)
+{
+	std::cout << " computation gemm diag " << std::endl;  
+ //  int j = get_group_id(a).x*get_group_t_dim(a).x;
+/*
+    int size = get_group_t_dim(a).y;
+    int ONE = 1;
+    double* ad = current(a)(get_group_id(a).y, get_group_id(a).x);
+    double* cd = current(c)(get_group_id(a).y, get_group_id(a).x);
+
+    memset(cd, 0, get_group_t_dim(c).x*get_group_t_dim(c).y*sizeof(double));
+    for(int jj = 0 ; jj < get_group_t_dim(a).x ; jj++)
+    {
+	 double * alpha = current(b_diag)(j/get_group_t_dim(b_diag).y);
+	 daxpy(&size,&alpha[j%get_group_t_dim(b_diag).y],&ad[jj*get_group_t_dim(a).y], &ONE,&cd[jj*get_group_t_dim(c).y],&ONE);
+    } 
+*/
+}
+
+
 
 void single_integer_c_kernel(int*& input){
     zout << "single integer kernel: output is " << input[2] << "\n";
