f2a0d583daa4946b3957fd16ff5cbe6273fa4bab
dolfim <dolfim@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-04-07 22:01:36 +0000

    refs #26: interface to the alps lattice returning site/bond types, labels and wraps_pbc

--- dmrg/app/lattice.h
+++ dmrg/app/lattice.h
@@ -10,6 +10,11 @@
 #ifndef LATTICE_H
 #define LATTICE_H
 
+#include <vector>
+#include <string>
+#include <fstream>
+#include <boost/any.hpp>
+
 class Lattice
 {
 public:
@@ -19,30 +24,32 @@ public:
     virtual std::vector<pos_t> all(pos_t) const = 0;
     
     // non-virtual!
-    template<class T> T get_prop(std::string property = std::string("type"),
+    template<class T> T get_prop(std::string property,
                                  pos_t site) const
     {
-        return boost::any_cast<T>(get_prop_(property), std::vector<pos_t>(1, site));
+        return boost::any_cast<T>(get_prop_(property, std::vector<pos_t>(1, site)));
     }
     
-    template<class T> T get_prop(std::string property = std::string("type"),
+    template<class T> T get_prop(std::string property,
                                  pos_t bond1, pos_t bond2) const
     {
         std::vector<pos_t> v(2);
         v[0] = bond1; v[1] = bond2;
-        return boost::any_cast<T>(get_prop_(property), v);
+        return boost::any_cast<T>(get_prop_(property, v));
     }
     
-    template<class T> T get_prop(std::string property = std::string("type"),
+    template<class T> T get_prop(std::string property,
                                  std::vector<pos_t> const & positions) const
     {
-        return boost::any_cast<T>(get_prop_(property), positions);
+        return boost::any_cast<T>(get_prop_(property, positions));
     }
     
     // virtual!
-    virtual boost::any get_prop_(std::string property, std::vector<pos_t> const & positions) const = 0;
+    virtual boost::any get_prop_(std::string const &, std::vector<pos_t> const &) const = 0;
     
-    virtual std::size_t size() const = 0;
+    virtual pos_t size() const = 0;
 };
 
+
+Lattice * lattice_factory (std::string const & lattice, std::ifstream & ifs);
 #endif
