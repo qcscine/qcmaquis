b92fab98b81e34447fbf05dc882dea231348171d
maquis <maquis@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-05-10 11:58:53 +0000

    cleaned test

--- src/dmrg/regression/p_block_matrix/p_block_matrix_unit_tests.cpp
+++ src/dmrg/regression/p_block_matrix/p_block_matrix_unit_tests.cpp
@@ -47,10 +47,6 @@ struct AmbientConfig {
     ~AmbientConfig()  { ambient::finalize(); }
 };
 
-
-//
-// List of type pairs <T,U> for which the mixed type matrix vector multiplication is tested.
-//
 typedef boost::mpl::list<type_pairs::IntDouble, type_pairs::DoubleInt> test_type_pairs;
 
 BOOST_GLOBAL_FIXTURE( AmbientConfig );
@@ -67,13 +63,12 @@ BOOST_AUTO_TEST_CASE_TEMPLATE( p_block_matrix_test, T, test_types )
     Matrix f(0,0);
     ambient::playout();
 
-    resize(d, (size_t)4, (size_t)4);
+    resize(d, 4, 4);
     std::cout << d;
-    resize(e, (size_t)4, (size_t)4);
+    resize(e, 4, 4);
     std::cout << e;
-    resize(f, (size_t)4, (size_t)4);
+    resize(f, 4, 4);
     std::cout << f;
-    printf("0-------------\n");
         
     block_matrix<Matrix, grp> m1, m2, m3;
     
@@ -81,44 +76,14 @@ BOOST_AUTO_TEST_CASE_TEMPLATE( p_block_matrix_test, T, test_types )
     m1.insert_block(e, 0, 1);
     m2.insert_block(d, 1, 1);
 
-    printf("-1---------------\n"); 
     m2.reserve(2,2,6, 6);
-    printf("--2--------------\n"); 
     m2.allocate_blocks();
-    printf("---3-------------\n"); 
 
-    //m1 = m2;
-    //std::cout << m1;
-    //std::cout << m2;
     resize(d, 2, 2);
-    printf("----4------------\n"); 
     std::cout << d;
     std::cout << e;
     std::cout << f;
     std::cout << m1;
     std::cout << m2;
-    /*
-    zout << m1.description() << endl << m2.description() << endl;
-    zout << m1.description() << endl << m2.description() << endl;
-    
-    for (std::size_t k = 0; k < m3.n_blocks(); ++k)
-        for (std::size_t l = 0; l < num_rows(m3[k]); ++l)
-            for (std::size_t r = 0; r < num_columns(m3[k]); ++r)
-                m3[k](l,r) = drand48();
-    
-    zout << m3 << endl;
-    
-    block_matrix<Matrix, grp> U, V;
-    block_matrix<DiagMatrix, grp> S;
-    svd(m3, U, V, S);
-    
-    //zout << S << endl;
-    //zout << U << endl;
-    
-    block_matrix<Matrix, grp> t;
-    gemm(U, S, t);
-    gemm(t, V, U);
-        
-    zout << U << endl;*/
 }
 
