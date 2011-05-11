53e00fef96c7164bcdf642d2fbe18cdfe921bb25
ewartt <ewartt@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-05-11 11:02:02 +0000

    new boost test for VLI

--- src/large_integer/VLI/main.cpp
+++ src/large_integer/VLI/main.cpp
@@ -14,11 +14,118 @@
 #include "vli_vector_cpu.hpp"
 #include "vli_vector_gpu.hpp"
 
+#define BOOST_TEST_MODULE gpu
+
+//#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp> //faster to compile
 
 #include "timings.h"
 
 typedef int TYPE; 
 
+BOOST_AUTO_TEST_CASE(gpu_manager)
+{
+	gpu::gpu_manager* GPU;
+	GPU->instance();
+	TYPE FreqGPU = GPU->instance().GetDeviceProperties().clockRate;
+    printf(" FreqGPU %d, Hz", FreqGPU);
+    GPU->instance().destructor();
+}
+
+BOOST_AUTO_TEST_CASE(copy_construction)
+{
+	gpu::gpu_manager* GPU;
+	GPU->instance();
+	
+	vli::vli_cpu<TYPE> A(255);
+
+	A[1] = 255;
+	A[2] = 255;
+	A[3] = 255;
+	
+    vli::vli_gpu<TYPE> B(A);
+		
+	BOOST_CHECK_EQUAL(A,B);
+	
+	GPU->instance().destructor();
+}
+
+BOOST_AUTO_TEST_CASE(serial_addition)
+{
+	gpu::gpu_manager* GPU;
+	GPU->instance();
+	
+	vli::vli_cpu<TYPE> A(255);
+	vli::vli_cpu<TYPE> B(255);
+	vli::vli_cpu<TYPE> C(0);
+
+	A[1] = 255;
+	A[2] = 255;
+	A[3] = 255;
+	/** this number is the result by hand */
+	vli::vli_cpu<TYPE> Res;
+
+	Res[0] = 254;
+	Res[1] = 0;
+	Res[2] = 0;
+	Res[3] = 0;
+	Res[4] = 1;
+	/** end calculation by hand */
+	
+    vli::vli_gpu<TYPE> D(A);
+	vli::vli_gpu<TYPE> E(B);
+	vli::vli_gpu<TYPE> F(0);
+
+	C = A+B;
+	F = D+E;
+	
+	BOOST_CHECK_EQUAL(C,F);
+	BOOST_CHECK_EQUAL(C,Res);
+	BOOST_CHECK_EQUAL(Res,F);
+
+	GPU->instance().destructor();
+}
+
+BOOST_AUTO_TEST_CASE(serial_multiplication)
+{
+	gpu::gpu_manager* GPU;
+	GPU->instance();
+	
+	vli::vli_cpu<TYPE> A(255);
+	vli::vli_cpu<TYPE> B(255);
+	vli::vli_cpu<TYPE> C(0);
+	
+	A[1] = 255;
+	A[2] = 255;
+	A[3] = 255;
+	/** this number is the result by hand */
+	vli::vli_cpu<TYPE> Res;
+	
+	Res[0] = 254;
+	Res[1] = 0;
+	Res[2] = 0;
+	Res[3] = 0;
+	Res[4] = 1;
+	
+	
+	
+    vli::vli_gpu<TYPE> D(A);
+	vli::vli_gpu<TYPE> E(B);
+	vli::vli_gpu<TYPE> F(0);
+	
+	C = A*B;
+	F = D*E;
+	
+	BOOST_CHECK_EQUAL(C,F);
+//	BOOST_CHECK_EQUAL(C,Res);
+//	BOOST_CHECK_EQUAL(Res,F);
+	
+	GPU->instance().destructor();
+
+}
+
+
+/*
 int main (int argc, char * const argv[]) 
 {
 
@@ -28,36 +135,47 @@ int main (int argc, char * const argv[])
 	TYPE FreqGPU = GPU->instance().GetDeviceProperties().clockRate;
 	std::cout << " FreqGPU : " << FreqGPU << std::endl;
 
-	vli::vli_cpu<TYPE> A(1);
-	vli::vli_cpu<TYPE> B(1);	
-	vli::vli_cpu<TYPE> C;
+	vli::vli_cpu<TYPE> A(255);
+	vli::vli_cpu<TYPE> B(255);	
+	vli::vli_cpu<TYPE> C1;
+	vli::vli_cpu<TYPE> C2;
+
+	A[1] = 255;
 
 
 	vli::vli_gpu<TYPE> D(A);
 	std::cout << D << std::endl;
 	vli::vli_gpu<TYPE> E(B);
 	std::cout << E << std::endl;
-	vli::vli_gpu<TYPE> F(0);
+	vli::vli_gpu<TYPE> F1(0);
+	vli::vli_gpu<TYPE> F2(0);
+	
 
 	
-	A+=B;	
-   vli::detail::multiply_gpu(D.p(),E.p(),F.p(),1,vli::vli_gpu<TYPE>::size);
+
+	
+	C1 = B+A;	
+ //  vli::detail::multiply_gpu(D.p(),E.p(),F.p(),1,vli::vli_gpu<TYPE>::size);
 	
 //	D*=E;
 
-    F = D+E;    
-	std::cout << " cpu "<< A << std::endl;
-	std::cout << " gpu "<< F << std::endl;	
+    F1 = D+E;    
+	
+	std::cout << " cpu + "<< C1 << std::endl;
+	std::cout << " gpu + "<< F1 << std::endl;	
 
-    std::cout<< E<<std::endl;    
-/*	
+	std::cout << " cpu * "<< C2 << std::endl;
+	std::cout << " gpu * "<< F2 << std::endl;	
+	
+	
+
 	A = E*B;
 
 	C = A;
 	std::cout << A << std::endl;	
 
 //	std::cout << B << std::endl;
-*/
+
 	vli::vli_vector<vli::vli_cpu<TYPE> > U(1000);
 	vli::vli_vector<vli::vli_cpu<TYPE> > V(1000);
 	vli::vli_vector<vli::vli_cpu<TYPE> > W(1000);
@@ -87,9 +205,11 @@ int main (int argc, char * const argv[])
     for(unsigned int i=0; i< 100000; ++i)    
         W = U + W;
     cpu_timer.end();
-    
+
 
 	GPU->instance().destructor();
 	
     return 0;
 }
+
+*/
