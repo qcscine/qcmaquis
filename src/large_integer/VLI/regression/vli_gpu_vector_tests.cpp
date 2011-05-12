239879ea1bf46323c1235dcefba7a601c24d0a69
ewartt <ewartt@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-05-12 08:29:58 +0000

    vector vli cpu/gpu addition, it works !

--- src/large_integer/VLI/regression/vli_gpu_vector_tests.cpp
+++ src/large_integer/VLI/regression/vli_gpu_vector_tests.cpp
@@ -11,6 +11,8 @@
 #include "vli_vector_cpu.hpp"
 #include "vli_vector_gpu.hpp"
 
+using vli::vli_cpu;
+using vli::vli_vector;
 using vli::vli_vector_gpu;
 
 BOOST_AUTO_TEST_CASE( constructors_test )
@@ -23,16 +25,14 @@ BOOST_AUTO_TEST_CASE( constructors_test )
     vli_vector_gpu<int> c(10);
 	
 	c=a+b;
+	
+//	BOOST_CHECK_EQUAL(a,b);
 
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << c << std::endl;	
-//    BOOST_CHECK_EQUAL(a,b);
 	GPU->instance().destructor();
 }
 
-/*
-BOOST_AUTO_TEST_CASE( addition )
+
+BOOST_AUTO_TEST_CASE( addition_vector )
 {
 	gpu::gpu_manager* GPU;
 	GPU->instance();
@@ -44,14 +44,26 @@ BOOST_AUTO_TEST_CASE( addition )
 	
 	for (int i =0; i < 10; i++)
 	{
+	    a[i][0] = 255;
+	    a[i][1] = 255;
+	    a[i][2] = 255;
 		
+		b[i][0] = 255;
+	    b[i][1] = 255;
+	    b[i][2] = 255;
 	}
 	
-	c=a+b;
+	vli_vector_gpu<int> a_gpu(a);
+	vli_vector_gpu<int> b_gpu(b);
+	vli_vector_gpu<int> c_gpu(c);
 	
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
+	
+	c = a+b;
+	c_gpu=a_gpu+b_gpu;
+	
 	std::cout << c << std::endl;	
+	std::cout << c_gpu << std::endl;	
+
 	//    BOOST_CHECK_EQUAL(a,b);
 	GPU->instance().destructor();
-}*/
+}
