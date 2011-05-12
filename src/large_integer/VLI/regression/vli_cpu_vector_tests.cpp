e29d9bc3374832d95a0fe24dbf1c091fa3985361
ewartt <ewartt@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-05-12 07:36:10 +0000

    VLI minor compil fixes

---
+++ src/large_integer/VLI/regression/vli_cpu_vector_tests.cpp
@@ -0,0 +1,33 @@
+#define BOOST_TEST_MODULE vli_cpu
+#include <iostream>
+#include <boost/test/unit_test.hpp>
+
+
+#define SIZE_BITS 256
+#include "vli_number_cpu.hpp"
+#include "vli_vector_cpu.hpp"
+
+
+using vli::vli_cpu;
+using vli::vli_vector;
+
+BOOST_AUTO_TEST_CASE( constructors_test )
+{
+    vli_vector< vli_cpu<int> >  a(10);
+    vli_vector<vli_cpu< int > > b(a);
+	
+    BOOST_CHECK_EQUAL(a,b);
+}
+
+
+BOOST_AUTO_TEST_CASE( copy_constructor_and_assignment )
+{
+    vli_vector< vli_cpu<int> >  a(10);
+    vli_vector<vli_cpu< int > > b(a);
+
+    BOOST_CHECK_EQUAL(a,b);
+	
+	vli_vector<vli_cpu< int > > c(5);
+    c = b;
+    BOOST_CHECK_EQUAL(c,b);
+}
