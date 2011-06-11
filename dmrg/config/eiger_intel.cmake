346664885d53b4c1704845d0871d6da4856340f7
dolfim <dolfim@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-06-11 15:43:37 +0000

    Machine config for Eiger

---
+++ dmrg/config/eiger_intel.cmake
@@ -0,0 +1,20 @@
+
+set(CMAKE_C_COMPILER icc)
+set(CMAKE_CXX_COMPILER icpc)
+
+set(MPI_FOUND TRUE)
+set(MPI_COMPILE_FLAGS "")
+set(MPI_INCLUDE_PATH "/apps/eiger/Intel-MPI-4.0/intel64/include ")
+set(MPI_LINK_FLAGS "-L/apps/eiger/Intel-MPI-4.0/intel64/lib -Xlinker --enable-new-dtags -Xlinker -rpath -Xlinker /apps/eiger/Intel-MPI-4.0/intel64/lib -Xlinker -rpath -Xlinker /opt/intel/mpi-rt/4.0.0")
+set(MPI_LIBRARY "mpigc4 mpi mpigf mpigi")
+set(MPI_EXTRA_LIBRARY "dl pthread rt")
+set(MPI_LIBRARIES "dl mpigc4 mpi mpigf mpigi pthread rt")
+set(MPIEXEC "/apps/eiger/Intel-MPI-4.0/intel64/bin/mpirun")
+set(MPIEXEC_NUMPROC_FLAG -np)
+set(MPIEXEC_PREFLAGS )
+set(MPIEXEC_POSTFLAGS )
+
+set(DEFAULT_BLAS_LAPACK manual)
+set(BLAS_LAPACK_MANUAL_LIBS mkl_scalapack_lp64 mkl_intel_lp64 mkl_sequential mkl_core mkl_blacs_intelmpi_lp64 pthread)
+set(BLAS_LAPACK_MANUAL_LIBS_DIR /apps/eiger/Intel-Composer-XE-2011/mkl/lib/intel64)
+set(BLAS_LAPACK_MANUAL_INCLUDES /apps/eiger/Intel-Composer-XE-2011/mkl/include)
