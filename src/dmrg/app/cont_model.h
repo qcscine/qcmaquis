c962417c5b9d760a066cf99795de6cec6b88a1d5
dolfim <dolfim@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-07-05 11:04:40 +0000

    some work on multigrid & cont_model

--- src/dmrg/app/cont_model.h
+++ src/dmrg/app/cont_model.h
@@ -18,7 +18,7 @@
 
 namespace app {
     
-    /* ****************** OPTICAL LATTICE */
+    /* ****************** OPTICAL LATTICE (with symmetry) */
     template<class Matrix>
     class OpticalLattice {
         typedef Hamiltonian<Matrix, U1> ham;        
@@ -123,6 +123,15 @@ namespace app {
                 mterm_t term;
                 term.fill_operator = ident;
                 term.name = "Density";
+                term.type = mterm_t::Average;
+                term.operators.push_back( std::make_pair(count, false) );
+                
+                meas.add_term(term);
+            }
+            {
+                mterm_t term;
+                term.fill_operator = ident;
+                term.name = "Local density";
                 term.type = mterm_t::Local;
                 term.operators.push_back( std::make_pair(count, false) );
                 
@@ -140,6 +149,142 @@ namespace app {
         op_t count, interaction;
         Index<U1> phys;
     };
+    
+    
+    /* ****************** OPTICAL LATTICE (without symmetry) */
+    template<class Matrix>
+    class OpticalLatticeNull {
+        typedef Hamiltonian<Matrix, NullGroup> ham;        
+        typedef typename ham::hamterm_t hamterm_t;        
+        typedef Measurement_Term<Matrix, NullGroup> mterm_t;
+        typedef typename ham::op_t op_t;
+    public:
+        OpticalLatticeNull (const Lattice& lat_, ModelParameters & model_)
+        : lat(lat_)
+        , model(model_)
+        {
+            int phys_size = model.get<int>("Nmax")+1;
+            NullGroup::charge c = NullGroup::SingletCharge;
+            
+            phys.insert(std::make_pair(c, phys_size));
+            ident.insert_block(Matrix::identity_matrix(phys_size), c, c);
+            count.insert_block(Matrix(phys_size, phys_size, 0), c, c);
+            interaction.insert_block(Matrix(phys_size, phys_size, c), c, c);
+            create.insert_block(Matrix(phys_size, phys_size, 0), c, c);
+            destroy.insert_block(Matrix(phys_size, phys_size, 0), c, c);
+            
+            for (int n=1; n<=model.get<int>("Nmax"); ++n)
+            {                                
+                count[0](n, n) = n;
+
+                interaction[0](n, n) = n*n-n;
+
+                create[0](n-1, n) = std::sqrt(n);
+                destroy[0](n, n-1) = std::sqrt(n);
+            }
+        }
+        
+        Hamiltonian<Matrix, NullGroup> H () const
+        {
+            NullGroup::charge c = NullGroup::SingletCharge;
+
+            std::vector<hamterm_t> terms;
+            for (int p=0; p<lat.size(); ++p)
+            {
+                double exp_potential = model.get<double>("V0")*std::pow( std::sin(model.get<double>("k")*lat.get_prop<double>("x", p)), 2 );
+                
+                double U = model.get<double>("c")/lat.get_prop<double>("dx", p);
+                double mu = ( -model.get<double>("mu")
+                             + exp_potential
+                             + 2*model.get<double>("h")/(lat.get_prop<double>("dx", p)*lat.get_prop<double>("dx", p)) );
+                
+                op_t site_op;
+                site_op.insert_block(U*interaction[0]+mu*count[0], c, c);
+                
+                { // site term
+                    hamterm_t term;
+                    term.fill_operator = ident;
+                    term.operators.push_back( std::make_pair(p, site_op) );
+                    terms.push_back(term);
+                }
+                
+                //                if (mu != 0)
+                //                { // density term
+                //                    hamterm_t term;
+                //                    term.fill_operator = ident;
+                //                    term.operators.push_back( std::make_pair(p, mu*count) );
+                //                    terms.push_back(term);
+                //                }
+                //                
+                //                if (U != 0)
+                //                { // interaction term
+                //                    hamterm_t term;
+                //                    term.fill_operator = ident;
+                //                    term.operators.push_back( std::make_pair(p, U*interaction) );
+                //                    terms.push_back(term);
+                //                }
+                
+                std::vector<int> neighs = lat.forward(p);
+                for (int n=0; n<neighs.size(); ++n) { // hopping
+                    
+                    double t = model.get<double>("h") / (lat.get_prop<double>("dx", p, neighs[n])*lat.get_prop<double>("dx", p, neighs[n]));
+                    
+                    {
+                        hamterm_t term;
+                        term.fill_operator = ident;
+                        term.operators.push_back( std::make_pair(p, -t*create) );
+                        term.operators.push_back( std::make_pair(neighs[n], destroy) );
+                        terms.push_back(term);
+                    }
+                    {
+                        hamterm_t term;
+                        term.fill_operator = ident;
+                        term.operators.push_back( std::make_pair(p, -t*destroy) );
+                        term.operators.push_back( std::make_pair(neighs[n], create) );
+                        terms.push_back(term);
+                    }
+                }
+            }
+            
+            return ham(phys, ident, terms);
+        }
+        
+        Measurements<Matrix, NullGroup> measurements () const
+        {
+            Measurements<Matrix, NullGroup> meas;
+            meas.set_identity(ident);
+            
+            {
+                mterm_t term;
+                term.fill_operator = ident;
+                term.name = "Density";
+                term.type = mterm_t::Average;
+                term.operators.push_back( std::make_pair(count, false) );
+                
+                meas.add_term(term);
+            }
+            {
+                mterm_t term;
+                term.fill_operator = ident;
+                term.name = "Local density";
+                term.type = mterm_t::Local;
+                term.operators.push_back( std::make_pair(count, false) );
+                
+                meas.add_term(term);
+            }
+            return meas;
+        }
+        
+    private:
+        const Lattice & lat;
+        ModelParameters & model;
+        
+        op_t ident;
+        op_t create, destroy;
+        op_t count, interaction;
+        Index<NullGroup> phys;
+    };
+
     
 } // namespace
 
