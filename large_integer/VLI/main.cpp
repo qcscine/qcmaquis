309bc86dff428c38ab9db55197f75f749bf9a7d1
ewartt <ewartt@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-07-21 12:04:50 +0000

    test

--- large_integer/VLI/main.cpp
+++ large_integer/VLI/main.cpp
@@ -14,8 +14,8 @@
 #include "vli_cpu/vli_number_cpu.hpp"
 #include "vli_gpu/vli_number_gpu.hpp"
 #include "utils/timings.h"
-#define SIZE_POLY 4
-#define SIZE_VECTOR 16
+#define SIZE_POLY 10
+#define SIZE_VECTOR 256
 
 #define TYPE unsigned int 
 
@@ -33,24 +33,47 @@ int main (int argc, char * const argv[])
 	GPU->instance();
     
     polynomial_cpu<vli_cpu<TYPE,8>, SIZE_POLY> pa;
-    
+/*
+    polynomial_cpu<vli_cpu<TYPE,8>, SIZE_POLY> pb;
+    polynomial_cpu<vli_cpu<TYPE,8>, SIZE_POLY> pc;
+  */  
     for(int i=0; i<3; i++){
         for(int j = 0; j < SIZE_POLY; j++ ){
             for(int k= 0; k< SIZE_POLY; k++){
                 pa(j,k)[i]= MAX_VALUE-1;                
+    //            pb(j,k)[i]= MAX_VALUE-2;                
             }
         }
     }
-    
+    /*
     polynomial_gpu<vli_gpu<TYPE,8>, SIZE_POLY> pagpu(pa);
+    polynomial_gpu<vli_gpu<TYPE,8>, SIZE_POLY> pbgpu(pb);
+    polynomial_gpu<vli_gpu<TYPE,8>, SIZE_POLY> pcgpu(pa);
+
+    Timer A("CPU");
+    A.begin();
+    pc = pa*pb;
+    A.end();
+    
+    TimerCuda B("GPU");
+    B.begin();
+    pcgpu = pagpu*pbgpu;
+    B.end();
+
+
+    if(pc == pcgpu){ printf("ok \n");}else{printf("no ok \n");
+    std::cout << pc << std::endl;
+    std::cout << pcgpu << std::endl;
+    }
+*/
     
     vector_polynomial_gpu< polynomial_gpu<vli_gpu<TYPE, 8>,SIZE_POLY> > VaGPU(SIZE_VECTOR);
     vector_polynomial_gpu< polynomial_gpu<vli_gpu<TYPE, 8>,SIZE_POLY> > VbGPU(SIZE_VECTOR);
-    vector_polynomial_gpu< polynomial_gpu<vli_gpu<TYPE, 8>,SIZE_POLY> > VcGPU;
+    vector_polynomial_gpu< polynomial_gpu<vli_gpu<TYPE, 8>,SIZE_POLY> > VcGPU(1);
     
     vector_polynomial_cpu< polynomial_cpu<vli_cpu<TYPE, 8>,SIZE_POLY> > VaCPU(SIZE_VECTOR);
     vector_polynomial_cpu< polynomial_cpu<vli_cpu<TYPE, 8>,SIZE_POLY> > VbCPU(SIZE_VECTOR);
-    vector_polynomial_cpu< polynomial_cpu<vli_cpu<TYPE, 8>,SIZE_POLY> > VcCPU;
+    vector_polynomial_cpu< polynomial_cpu<vli_cpu<TYPE, 8>,SIZE_POLY> > VcCPU(1);
     
     for(int i=0;i < SIZE_VECTOR;i++){
         VaCPU[i]=pa;
@@ -70,11 +93,15 @@ int main (int argc, char * const argv[])
     VcGPU =  inner_product(VaGPU,VbGPU);
     B.end();
     
-  //  std::cout << VcCPU << std::endl;
-  //  std::cout << VcGPU << std::endl;
  
-    if(VcGPU == VcCPU){ printf("ok \n");}else{printf("no ok \n");}
-    
+    if(VcGPU == VcCPU){ 
+        printf("ok \n");
+    }else{
+        std::cout << VcCPU << std::endl;
+        std::cout << VcGPU << std::endl;
+        printf("no ok \n");
+    }
+  
     GPU->instance().destructor();
     return 0;
 }
