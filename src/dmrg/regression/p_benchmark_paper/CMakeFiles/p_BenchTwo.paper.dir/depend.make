b149b0cbf3f3a42e778e6676a71d06cdfde64e1e
ewartt <ewartt@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-08-02 15:57:11 +0000

    new regression test dmrg-ambient for the paper

---
+++ src/dmrg/regression/p_benchmark_paper/CMakeFiles/p_BenchTwo.paper.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for p_BenchTwo.paper.
+# This may be replaced when dependencies are built.
---
+++ src/dmrg/regression/p_benchmark_paper/opari.tab.c
@@ -0,0 +1,9 @@
+#include "pomp_lib.h"
+
+
+
+int POMP_MAX_ID = 1;
+
+struct ompregdescr* pomp_rd_table[1] = {
+  0,
+};
