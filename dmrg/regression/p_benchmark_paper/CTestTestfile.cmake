5715c3706e8f4d49df2e6517570cbcb98545e258
maquis <maquis@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-08-06 01:03:39 +0000

    removing trash from svn

--- dmrg/regression/p_benchmark_paper/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /users/ewartt/DMRG_DEV/regression/p_benchmark_paper
-# Build directory: /users/ewartt/DMRG_DEV/regression/p_benchmark_paper
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
