9ca2ea5746538c36785ea4f9c7ca05c5970e8733
dolfim <dolfim@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-08-25 11:38:02 +0000

    Upgrading multigrid dmrg with optimization in fine-graining loops

--- src/dmrg/app/cont_model.h
+++ src/dmrg/app/cont_model.h
@@ -54,20 +54,73 @@ namespace app {
             std::vector<hamterm_t> terms;
             for (int p=0; p<lat.size(); ++p)
             {
+                std::vector<int> neighs = lat.all(p);
+                // normal, b0, b1:
+                // (nothing)
+                
+                // b2:
+//                if (lat.get_prop<bool>("at_open_left_boundary", p))
+//                    neighs.push_back(p+2);
+//                if (lat.get_prop<bool>("at_open_right_boundary", p))
+//                    neighs.push_back(p-2);
+                
+
                 double exp_potential = model.get<double>("V0")*std::pow( std::sin(model.get<double>("k")*lat.get_prop<double>("x", p)), 2 );
                 
-                double U = model.get<double>("c")/lat.get_prop<double>("dx", p);
-                double mu = ( -model.get<double>("mu")
-                             + exp_potential
-                             + model.get<double>("h")/(lat.get_prop<double>("dx", p, p+1)*lat.get_prop<double>("dx", p, p+1)) );
-                if (!lat.get_prop<bool>("at_open_boundary", p))
-                    mu += model.get<double>("h")/(lat.get_prop<double>("dx", p, p+1)*lat.get_prop<double>("dx", p, p+1));
+//              double dx = std::min(lat.get_prop<double>("dx", p, p+1), lat.get_prop<double>("dx", p-1, p));
+                double dx1 = lat.get_prop<double>("dx", p, neighs[0]);
+                double dx2;
+                if (neighs.size() == 1 && lat.get_prop<bool>("at_open_left_boundary", p))
+                    dx2 = lat.get_prop<double>("dx", p, p-1);
+                else if (neighs.size() == 1 && lat.get_prop<bool>("at_open_right_boundary", p))
+                    dx2 = lat.get_prop<double>("dx", p, p+1);
+                else
+                    dx2 = lat.get_prop<double>("dx", p, neighs[1]);
+                
+//                double dx0 = (std::abs(dx1) + std::abs(dx2)) / 2.;
+//                dx0 = std::min(std::abs(dx1), std::abs(dx2));
 
+                double dx0 = lat.get_prop<double>("dx", p);
+
+                // Psi''(x) = coeff1 * Psi(x+dx1) + coeff0 * Psi(x) + coeff2 * Psi(x+dx2)
+                double coeff1 = 2. / (dx1*dx1 - dx1*dx2);
+                double coeff2 = 2. / (dx2*dx2 - dx1*dx2);
+                double coeff0 = -(coeff1 + coeff2);
+                
+                if (lat.get_prop<bool>("at_open_boundary", p)) {
+//                    dx0 = std::min(std::abs(dx1),std::abs(dx2));
+                    // b0, b2:
+                    // (nothing)
+                    
+                    // normal:
+//                    coeff0 = -1./(dx0*dx0);
+                    
+                    // b1:
+//                    coeff0 = -coeff1;
+                }
+                
+                double U = model.get<double>("c") / dx0;
+                double mu = -model.get<double>("mu") + exp_potential;
+                mu += -coeff0 * model.get<double>("h");
+                
+                /*
+                 if (!lat.get_prop<bool>("at_open_boundary", p) && equal_grid)
+                 mu += 2 * model.get<double>("h") / (dx*dx);
+                 else if (lat.get_prop<bool>("at_open_boundary", p) && equal_grid)
+                 mu += model.get<double>("h") / (dx*dx);
+                 else if (!lat.get_prop<bool>("at_open_boundary", p) && !equal_grid)
+                 mu += model.get<double>("h") / (dx*dx);
+                 else if (lat.get_prop<bool>("at_open_right_boundary", p))
+                 mu += 2./3. * model.get<double>("h") / (dx*dx);
+                 else if (lat.get_prop<bool>("at_open_left_boundary", p))
+                 mu += 1./3. * model.get<double>("h") / (dx*dx);
+                 */
+                
                 op_t site_op;
                 for (int n=1; n<=model.get<int>("Nmax"); ++n)
                     if (U*(n*n-n)+mu*n != 0)
                         site_op.insert_block(Matrix(1, 1, U*(n*n-n)+mu*n), n, n);
-
+                
                 { // site term
                     hamterm_t term;
                     term.fill_operator = ident;
@@ -91,23 +144,30 @@ namespace app {
 //                    terms.push_back(term);
 //                }
                 
-                std::vector<int> neighs = lat.forward(p);
                 for (int n=0; n<neighs.size(); ++n) { // hopping
                     
-                    double t = model.get<double>("h") / (lat.get_prop<double>("dx", p, neighs[n])*lat.get_prop<double>("dx", p, neighs[n]));
+                    double t;
+                    /*
+                     // if (equal_grid || lat.get_prop<bool>("at_open_boundary", p))
+                     if (equal_grid)
+                     t = model.get<double>("h") / (dx*dx);
+                     else if (lat.get_prop<double>("dx", p, neighs[n]) == dx)
+                     t = 2./3. * model.get<double>("h") / (dx*dx);
+                     else if (lat.get_prop<double>("dx", p, neighs[n]) == 2*dx)
+                     t = 1./3. * model.get<double>("h") / (dx*dx);
+                     else
+                     throw std::runtime_error("I don't know the Laplacian operator in this kind of lattice!");
+                     */
+                    if (lat.get_prop<double>("dx", p, neighs[n]) == dx1)
+                        t = coeff1 * model.get<double>("h");
+                    else
+                        t = coeff2 * model.get<double>("h");
                     
                     {
                         hamterm_t term;
                         term.fill_operator = ident;
                         term.operators.push_back( std::make_pair(p, -t*create) );
                         term.operators.push_back( std::make_pair(neighs[n], destroy) );
-                        terms.push_back(term);
-                    }
-                    {
-                        hamterm_t term;
-                        term.fill_operator = ident;
-                        term.operators.push_back( std::make_pair(p, -t*destroy) );
-                        term.operators.push_back( std::make_pair(neighs[n], create) );
                         terms.push_back(term);
                     }
                 }
@@ -120,7 +180,7 @@ namespace app {
         {
             Measurements<Matrix, U1> meas;
             meas.set_identity(ident);
-
+            
             {
                 mterm_t term;
                 term.fill_operator = ident;
@@ -165,6 +225,9 @@ namespace app {
         : lat(lat_)
         , model(model_)
         {
+            
+            throw std::runtime_error("This model is broken, it has to be adapted to the one above...");
+            
             int phys_size = model.get<int>("Nmax")+1;
             NullGroup::charge c = NullGroup::SingletCharge;
             
@@ -178,9 +241,9 @@ namespace app {
             for (int n=1; n<=model.get<int>("Nmax"); ++n)
             {                                
                 count[0](n, n) = n;
-
+                
                 interaction[0](n, n) = n*n-n;
-
+                
                 create[0](n-1, n) = std::sqrt(n);
                 destroy[0](n, n-1) = std::sqrt(n);
             }
@@ -189,7 +252,7 @@ namespace app {
         Hamiltonian<Matrix, NullGroup> H () const
         {
             NullGroup::charge c = NullGroup::SingletCharge;
-
+            
             std::vector<hamterm_t> terms;
             for (int p=0; p<lat.size(); ++p)
             {
@@ -212,21 +275,21 @@ namespace app {
                     terms.push_back(term);
                 }
                 
-                //                if (mu != 0)
-                //                { // density term
-                //                    hamterm_t term;
-                //                    term.fill_operator = ident;
-                //                    term.operators.push_back( std::make_pair(p, mu*count) );
-                //                    terms.push_back(term);
-                //                }
-                //                
-                //                if (U != 0)
-                //                { // interaction term
-                //                    hamterm_t term;
-                //                    term.fill_operator = ident;
-                //                    term.operators.push_back( std::make_pair(p, U*interaction) );
-                //                    terms.push_back(term);
-                //                }
+//                if (mu != 0)
+//                { // density term
+//                    hamterm_t term;
+//                    term.fill_operator = ident;
+//                    term.operators.push_back( std::make_pair(p, mu*count) );
+//                    terms.push_back(term);
+//                }
+//                
+//                if (U != 0)
+//                { // interaction term
+//                    hamterm_t term;
+//                    term.fill_operator = ident;
+//                    term.operators.push_back( std::make_pair(p, U*interaction) );
+//                    terms.push_back(term);
+//                }
                 
                 std::vector<int> neighs = lat.forward(p);
                 for (int n=0; n<neighs.size(); ++n) { // hopping
@@ -288,7 +351,7 @@ namespace app {
         op_t count, interaction;
         Index<NullGroup> phys;
     };
-
+    
     
 } // namespace
 
