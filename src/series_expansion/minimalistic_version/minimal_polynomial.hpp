c1167a275a7111637ec99abe0086c408f527ee9d
hehn <hehn@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-09-13 08:22:58 +0000

    undo r1275 corrected includes

--- src/series_expansion/minimalistic_version/minimal_polynomial.hpp
+++ src/series_expansion/minimalistic_version/minimal_polynomial.hpp
@@ -11,7 +11,6 @@
 
 #include <vector>
 #include <iostream>
-#include <omp.h>
 
 namespace hp2c
 {
@@ -319,28 +318,17 @@ template <typename CoeffType>
 polynomial<CoeffType>  inner_product(std::vector<polynomial<CoeffType> > const& a, std::vector<polynomial<CoeffType> >const& b)
 {
     assert( a.size() == b.size() );
-    polynomial<CoeffType> result[omp_get_max_threads()];
-//    typename std::vector<polynomial<CoeffType> >::const_iterator it = a.begin();
-//    typename std::vector<polynomial<CoeffType> >::const_iterator it_b = b.begin();
+    polynomial<CoeffType> result;
+    typename std::vector<polynomial<CoeffType> >::const_iterator it = a.begin();
+    typename std::vector<polynomial<CoeffType> >::const_iterator it_b = b.begin();
     //TO DO transfor into loop for openmp
-    
-    #pragma omp parallel for
-    for(int i=0; i < a.size();++i){
-        result[omp_get_thread_num()] += a[i]*b[i];
+    while( it != a.end() )
+    {
+        result += *it * *it_b;
+        ++it;
+        ++it_b;
     }
-    
-    for(int i=1; i < omp_get_max_threads();++i){
-        result[0] += result[i];
-    }
-    
-//    while( it != a.end() )
-//    {
-//        result += *it * *it_b;
-//        ++it;
-//        ++it_b;
-//    }
-    
-    return result[0];
+    return result;
 }
 
 }
