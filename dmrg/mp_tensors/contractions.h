6a247737dfe33facf5aea2a30d35ad4bca4829ae
dolfim <dolfim@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-11-18 08:44:08 +0000

    computing density matrix

--- dmrg/mp_tensors/contractions.h
+++ dmrg/mp_tensors/contractions.h
@@ -792,6 +792,156 @@ struct contraction {
         
         return ret;
     }
+    
+    // tested only for diagonal operator
+    template<class Matrix, class SymmGroup>
+    static MPSTensor<Matrix, SymmGroup>
+    local_op(MPSTensor<Matrix, SymmGroup> const & mps,
+             block_matrix<Matrix, SymmGroup> const & op)
+    {
+        using std::size_t;
+        typedef typename SymmGroup::charge charge;
+        
+        mps.make_left_paired();
+        block_matrix<Matrix, SymmGroup> const & vec = mps.data();
+        
+        Index<SymmGroup> phys_i = mps.site_dim();
+        Index<SymmGroup> left_i = mps.row_dim();
+        Index<SymmGroup> right_i = mps.col_dim();
+        
+        ProductBasis<SymmGroup> left_pb(phys_i, left_i);
+        
+        MPSTensor<Matrix, SymmGroup> ret(phys_i, left_i, right_i, false);
+        block_matrix<Matrix, SymmGroup> & ret_vec = ret.data();
+        
+        for (size_t ls = 0; ls < left_i.size(); ++ls)
+            for (size_t rs = 0; rs < right_i.size(); ++rs)
+                for (size_t s1 = 0; s1 < phys_i.size(); ++s1)
+                    for (size_t s2 = 0; s2 < phys_i.size(); ++s2) {
+                        
+                        charge lc = left_i[ls].first, rc = right_i[rs].first;
+                        charge s1c = phys_i[s1].first, s2c = phys_i[s2].first;
+                        
+                        charge left_vec_charge = SymmGroup::fuse(s1c, lc);
+                        charge right_vec_charge = rc;
+                        charge left_out_charge = SymmGroup::fuse(s2c, lc);
+                        charge right_out_charge = right_vec_charge;
+                        
+                        if (! vec.has_block(left_vec_charge, right_vec_charge) )
+                            continue;
+                        if (! op.has_block(s1c, s2c) )
+                            continue;
+                        if (! ret_vec.has_block(left_out_charge, right_out_charge) )
+                            ret_vec.insert_block(Matrix(left_pb.size(s2c, lc), right_i[rs].second, 0), left_out_charge, right_out_charge);
+                        
+                        Matrix & oblock = ret_vec(left_out_charge, right_out_charge);
+                        Matrix const & iblock = vec(left_vec_charge, right_vec_charge);
+                        Matrix const & op_block = op(s1c, s2c);
+                        
+                        size_t i_l_offset = left_pb(s1c, lc);
+                        size_t i_r_offset = 0;
+                        size_t l_offset = left_pb(s2c, lc);
+                        size_t r_offset = 0;
+                        size_t i_op_offset = 0;
+                        size_t op_offset = 0;
+                        
+                        for (size_t ll = 0; ll < left_i[ls].second; ++ll)
+                            for (size_t rr = 0; rr < right_i[rs].second; ++rr)
+                                for (size_t ss1 = 0; ss1 < phys_i[s1].second; ++ss1)
+                                    for (size_t ss2 = 0; ss2 < phys_i[s2].second; ++ss2) {
+                                        
+                                        oblock(l_offset + ss2*left_i[ls].second + ll,
+                                               r_offset + rr);
+                                        iblock(i_l_offset + ss1*left_i[ls].second + ll,
+                                               i_r_offset + rr);
+                                        op_block(i_op_offset + ss1,
+                                                 op_offset + ss2);
+                                        oblock(l_offset + ss2*left_i[ls].second + ll,
+                                               r_offset + rr) += 
+                                        iblock(i_l_offset + ss1*left_i[ls].second + ll,
+                                               i_r_offset + rr) *
+                                        op_block(i_op_offset + ss1,
+                                                 op_offset + ss2);
+                                        
+                                    }
+                    }
+        
+        
+        ret.cur_storage = LeftPaired;
+        ret.phys_i = phys_i;
+        ret.left_i = left_i;
+        ret.right_i = right_i;     
+        
+        assert( ret.reasonable() );
+        return ret;
+    }
+
+    template<class Matrix, class SymmGroup>
+    static block_matrix<Matrix, SymmGroup>
+    density_matrix(MPSTensor<Matrix, SymmGroup> const & bra_tensor,
+                   MPSTensor<Matrix, SymmGroup> const & ket_tensor)
+    {
+        using std::size_t;
+        typedef typename SymmGroup::charge charge;
+        
+        assert( bra_tensor.row_dim() == ket_tensor.row_dim() );
+        assert( bra_tensor.col_dim() == ket_tensor.col_dim() );
+        assert( bra_tensor.site_dim() == ket_tensor.site_dim() );
+        
+        bra_tensor.make_left_paired();
+        ket_tensor.make_left_paired();
+        
+        Index<SymmGroup> phys_i = ket_tensor.site_dim();
+        Index<SymmGroup> left_i = ket_tensor.row_dim();
+        Index<SymmGroup> right_i = ket_tensor.col_dim();
+        
+        ProductBasis<SymmGroup> left_pb(phys_i, left_i);
+        
+        block_matrix<Matrix, SymmGroup> const & bra_vec = bra_tensor.data();
+        block_matrix<Matrix, SymmGroup> const & ket_vec = ket_tensor.data();
+        
+        block_matrix<Matrix, SymmGroup> ret(phys_i, phys_i);
+        
+        for (size_t ls = 0; ls < left_i.size(); ++ls)
+            for (size_t rs = 0; rs < right_i.size(); ++rs)
+                for (size_t s1 = 0; s1 < phys_i.size(); ++s1)
+                    for (size_t s2 = 0; s2 < phys_i.size(); ++s2) {
+                        
+                        charge lc = left_i[ls].first, rc = right_i[rs].first;
+                        charge s1c = phys_i[s1].first, s2c = phys_i[s2].first;
+                        
+                        charge left_ket_charge = SymmGroup::fuse(s1c, lc);
+                        charge left_bra_charge = SymmGroup::fuse(s2c, lc);
+                        charge right_charge = rc;
+                        
+                        if (! ket_vec.has_block(left_ket_charge, right_charge) )
+                            continue;
+                        if (! bra_vec.has_block(left_bra_charge, right_charge) )
+                            continue;
+                        if (! ret.has_block(s1c, s2c) )
+                            ret.insert_block(Matrix(phys_i[s1].second, phys_i[s2].second, 0), s1c, s2c);
+                        
+                        Matrix & oblock = ret(s1c, s2c);
+                        Matrix const & ket_block = ket_vec(left_ket_charge, right_charge);
+                        Matrix const & bra_block = bra_vec(left_bra_charge, right_charge);
+                        
+                        size_t l_ket_offset = left_pb(s1c, lc);
+                        size_t l_bra_offset = left_pb(s2c, lc);
+                        
+                        for (size_t ll = 0; ll < left_i[ls].second; ++ll)
+                            for (size_t rr = 0; rr < right_i[rs].second; ++rr)
+                                for (size_t ss1 = 0; ss1 < phys_i[s1].second; ++ss1)
+                                    for (size_t ss2 = 0; ss2 < phys_i[s2].second; ++ss2) {
+                                        
+                                        oblock(ss1, ss2) +=
+                                        utils::conj(bra_block(l_bra_offset + ss2*left_i[ls].second + ll, rr)) *
+                                        ket_block(l_ket_offset + ss1*left_i[ls].second + ll, rr);
+                                        
+                                    }
+                    }
+        return ret;
+    }
+
 };
 
 #endif
