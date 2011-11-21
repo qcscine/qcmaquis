84c42eaa976381799c859e00a586cae99327c3fa
ewartt <ewartt@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-11-21 17:35:19 +0000

    adapt cmake to the new tree

---
+++ src/dmrg/config_machines/eiger_mkl.cmake
@@ -0,0 +1,5 @@
+
+set(DEFAULT_BLAS_LAPACK manual)
+set(BLAS_LAPACK_MANUAL_LIBS -lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lpthread)
+set(BLAS_LAPACK_MANUAL_LIBS_DIR /apps/eiger/Intel-Composer-XE-2011/mkl/lib/intel64)
+set(BLAS_LAPACK_MANUAL_INCLUDES /apps/eiger/Intel-Composer-XE-2011/mkl/include)
