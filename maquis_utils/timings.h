fc6ed1cfb6c31c0eb044df1c9ab84746729e85c9
ewartt <ewartt@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-11-23 15:23:00 +0000

    timer power970 should be compatible BG

--- maquis_utils/timings.h
+++ maquis_utils/timings.h
@@ -44,6 +44,27 @@ inline unsigned long getcpuclocks()
  return( lo | (hi << 32) );
 }
 
+#elif (defined(__powerpc64__) || defined(__powerpc__))
+
+inline unsigned long getcpuclocks()
+{
+  unsigned long long int result=0;
+  unsigned long int upper, lower,tmp;
+  __asm__ volatile(
+                "0:                  \n"
+                "\tmftbu   %0           \n"
+                "\tmftb    %1           \n"
+                "\tmftbu   %2           \n"
+                "\tcmpw    %2,%0        \n"
+                "\tbne     0b         \n"
+                : "=r"(upper),"=r"(lower),"=r"(tmp)
+                );
+  result = upper;
+  result = result<<32;
+  result = result|lower;
+  return(result);
+}
+
 #else
 
 inline unsigned long getcpuclocks() { return 0; }
