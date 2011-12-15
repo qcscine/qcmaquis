8417a646c0d3150cd0f1f8291ec27bf356e37b45
ewartt <ewartt@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2011-12-15 10:30:23 +0000

    some modifs vli

--- large_integer/VLI/main/main.cpp
+++ large_integer/VLI/main/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <cstdio>
-
+/*
+#include "use_gmp_integers.hpp"
+#include "minimal_polynomial.hpp"
+*/
 #include <boost/lexical_cast.hpp>
 #ifdef VLI_USE_GPU
 #include "vli/utils/gpu_manager.h"
@@ -33,8 +36,12 @@ typedef vli::monomial<vli_type_cpu> monomial_type_cpu;
 typedef vli::polynomial_cpu< vli_type_cpu, 21 > polynomial_type_cpu;
 
 typedef vli::vector_polynomial_cpu<polynomial_type_cpu> vector_type_cpu;
-
-
+/*
+typedef mpz_class large_int;
+typedef hp2c::monomial<large_int> monomial_type;
+typedef hp2c::polynomial<large_int> polynomial_type;
+typedef std::vector<polynomial_type> polynomial_vector_type;
+*/
 int main (int argc, char * const argv[]) 
 {
    
@@ -45,38 +52,59 @@ int main (int argc, char * const argv[])
 
     vector_type_cpu v1(16384);
     vector_type_cpu v2(16384);
+/*
+    polynomial_vector_type v1gmp(1024);
+    polynomial_vector_type v2gmp(1024);
+*/
+
     polynomial_type_cpu result;
+    polynomial_type_cpu result_acc;
+    polynomial_type_cpu result_plain;
     polynomial_type_cpu result_pure_gpu;
     polynomial_type_cpu result_pure_cpu; 
+
+
+
 
     fill_vector_random(v1,1);
     fill_vector_random(v2,1);
 
 
-    Timer t3("CPU openmp");
-    t3.begin();
+    TimerOMP t1("CPU openmp");
+    t1.begin();
     result_pure_cpu = vli::detail::inner_product_openmp(v1,v2);
+    t1.end();
+
+    TimerOMP t2("MIX CPU/GPU");
+    t2.begin();
+    result = inner_product(v1,v2);
+    t2.end();
+/*
+    TimerOMP t3("oldsyte");
+    t3.begin();
+    result_acc=  vli::detail::inner_product_accp(v1,v2);
     t3.end();
 
-    Timer t1("MIX CPU/GPU");
-    t1.begin();
-    result = inner_product(v1,v2);
-    t1.end();
+    TimerOMP t4("plain");
+    t4.begin();
+    result_plain=  vli::detail::inner_product_plain(v1,v2);
+    t4.end();
+
 #ifdef VLI_USE_GPU
-    Timer t2("GPU");
+    TimerOMP t2("GPU");
     t2.begin();
     vli::detail::inner_product_gpu_booster<vector_type_cpu> gpu_boost(v1,v2,v1.size());
     result_pure_gpu = polynomial_type_cpu(gpu_boost);
     t2.end();
 #endif //VLI_USE_GPU
 
-/*
+
     std::cout << result << std::endl << std::endl <<std::endl;
 #ifdef VLI_USE_GPU
     std::cout << result_pure_gpu << std::endl << std::endl << std::endl;
 #endif //VLI_USE_GPU
     std::cout << result_pure_cpu << std::endl << std::endl << std::endl;
-  */  
+    
     if(
        result == result_pure_cpu
 #ifdef VLI_USE_GPU
@@ -87,6 +115,7 @@ int main (int argc, char * const argv[])
     else{
         printf( "NOT OK \n"); 
     }
+*/
     return 0;
 }
 
