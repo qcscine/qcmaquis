426a358b187307bb7f15c9cd6716674c1b934f46
ewartt <ewartt@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-01-19 19:46:49 +0000

    start to adapt the new  poly block algo to GPU

--- large_integer/VLI/src/vli/detail/kernels_gpu.cu
+++ large_integer/VLI/src/vli/detail/kernels_gpu.cu
@@ -9,46 +9,60 @@
 #include "vli/detail/kernels_cpu_gpu.hpp"
 #include "vli/detail/kernels_gpu.h"
 
+
 #include "utils/cuPrintf.cu"
 
 namespace vli {
-namespace detail {
+    namespace detail {
     
 /**
 * local declaration of the commun kernels
 */
 
-template <typename BaseInt, std::size_t Size>
-__host__ __device__ void kernels_addition_classic(BaseInt* x, BaseInt const* y);
+    template <typename BaseInt, std::size_t Size> 
+    __host__ __device__ void kernels_addition_classic(BaseInt* x, BaseInt const* y);
 
-template <typename BaseInt, std::size_t Size>
-__host__ __device__ void kernel_negate_device(BaseInt* x);
+    template <typename BaseInt, std::size_t Size>
+    __host__ __device__ void kernel_negate_device(BaseInt* x);
  
-template <typename BaseInt>
-__host__ __device__ void kernels_addition_block(BaseInt* x, BaseInt const* y); 
+    template <typename BaseInt>
+    __host__ __device__ void kernels_addition_block(BaseInt* x, BaseInt const* y); 
 
-template <typename BaseInt, std::size_t Size>
-__host__ __device__ void kernels_multiplication_classic_truncate(BaseInt * res, BaseInt const* x, BaseInt const* y);	
+    template <typename BaseInt, std::size_t Size>
+    __host__ __device__ void kernels_multiplication_classic_truncate(BaseInt * res, BaseInt const* x, BaseInt const* y);	
 
-template <typename BaseInt>
-__host__ __device__ void kernels_multiplication_block(BaseInt const* x, BaseInt const* y, BaseInt* r);
+    template <typename BaseInt>
+    __host__ __device__ void kernels_multiplication_block(BaseInt const* x, BaseInt const* y, BaseInt* r);
+ 
+    template <typename BaseInt>
+    __host__ __device__ void kernels_multiplication_block_down(BaseInt const* x, BaseInt const*  y, BaseInt* r);
 
-template <typename BaseInt>
-__host__ __device__ void kernels_multiplication_block_down(BaseInt const* x, BaseInt const*  y, BaseInt* r);
+    template <typename BaseInt>
+    __host__ __device__ void kernels_multiplication_block_up(BaseInt const* x, BaseInt const*  y, BaseInt* r);	
 
-template <typename BaseInt>
-__host__ __device__ void kernels_multiplication_block_up(BaseInt const* x, BaseInt const*  y, BaseInt* r);	
+    template <typename BaseInt>
+    __host__ __device__ void kernels_multiplication_base_reshaping(BaseInt const* x, BaseInt  const*  y, BaseInt* r);	
 
-template <typename BaseInt>
-__host__ __device__ void kernels_multiplication_base_reshaping(BaseInt const* x, BaseInt  const*  y, BaseInt* r);	
+    template <typename BaseInt, std::size_t Size>
+    __device__ void single_multiplication_device(BaseInt const* x, BaseInt const* y, BaseInt* z);  
 
-template <typename BaseInt, std::size_t Size>
-__device__ void single_multiplication_device(BaseInt const* x, BaseInt const* y, BaseInt* z);  
+    template <typename BaseInt, std::size_t Size>
+    __device__ void polynome_polynome_multiplication_device(BaseInt const* p1, BaseInt const* p2, BaseInt* res);
 
-template <typename BaseInt, std::size_t Size>
-__device__ void polynome_polynome_multiplication_device(BaseInt const* p1, BaseInt const* p2, BaseInt* res);
+// functions for the blocks algos
 
+    template <typename BaseInt, std::size_t Size>
+    __device__ void algo_triangle_up(int block_ai, int block_bj,unsigned int Order, BaseInt const* a,  BaseInt const* b, BaseInt* c);
 
+    template <typename BaseInt, std::size_t Size>
+    __device__ void algo_triangle_down(int block_ai, int block_bj,unsigned int Order, BaseInt const* a,  BaseInt const* b, BaseInt* c);
+        
+    template <typename BaseInt, std::size_t Size>
+    __device__ void algo_diag(int block_ai, int block_bj,unsigned int Order, BaseInt const* a, BaseInt const* b, BaseInt* c);
+    
+    template <typename BaseInt, std::size_t Size>
+    __device__ void algo_block_algo(int i, int j,unsigned int Order, BaseInt const* a, BaseInt const* b, BaseInt* c);
+        
 /**
 * a kind of hook function with a little bit of arithmetic in case of signed int (multiplication)
 */
@@ -73,30 +87,6 @@ __device__  void single_multiplication_device(BaseInt const* x, BaseInt const* y
     {
         kernels_multiplication_classic_truncate<BaseInt,Size>(z,x,y); 
     }
-
-/*    
-    if( static_cast<bool>((x[Size-1]) >> data_bits<BaseInt>::value)){
-        kernel_negate_device<BaseInt,Size>(const_cast<BaseInt* >(x));
-        na = -1;
-    }
-
-    if( static_cast<bool>((y[Size-1]) >> data_bits<BaseInt>::value)){
-        kernel_negate_device<BaseInt,Size>(const_cast<BaseInt* >(y));
-        nb = -1;
-    }
-
-    kernels_multiplication_classic_truncate<BaseInt,Size>(z,x,y);
-
-    if(nb*na == -1)
-        kernel_negate_device<BaseInt,Size>(z);
-       
-    if(na == -1){
-        kernel_negate_device<BaseInt,Size>(const_cast<BaseInt* >(x));
-    }
-
-    if(nb == -1){
-        kernel_negate_device<BaseInt,Size>(const_cast<BaseInt* >(y));
-    }*/
 }
 /**
 I try to implement this stuff into the commun kernel impossible ... (3 days of trying)
@@ -168,6 +158,72 @@ __device__ void polynome_polynome_multiplication_device(unsigned int max_order, 
          } 
     }
 } 
+        
+        
+        template <typename BaseInt, std::size_t Size>
+        void algo_triangle_up(int block_ai, int block_bj, unsigned int Order, BaseInt const* a,  BaseInt const* b, BaseInt *c){
+            int n(0);
+            int offset_block_ai = block_ai*Order;
+            int offset_block_bj = block_bj*Order;            
+            int offset_a, offset_b, offset_c;
+            
+            for(int i = 0; i < Order-1; ++i){
+                for(int j = n; j < Order-1; ++j){
+                    BaseInt inter[Size];
+                    #pragma unroll
+                    for(std::size_t i=0 ; i < Size ;++i)
+                        inter[i] = 0;
+                    
+                    offset_a = (j-n+offset_block_ai)*Size;
+                    offset_b = (n+offset_block_bj)*Size;
+                    offset_c = ((offset_block_ai+offset_block_bj)*2+j)*Size;
+                    
+                    single_multiplication_device<BaseInt,Size>(&a[offset_a],&b[offset_b],inter);
+                    kernels_addition_classic<BaseInt,Size>(&c[offset_c],inter);
+
+                    //*(c+(offset_block_ai+offset_block_bj)*2+j) +=  *(a+j-n+offset_block_ai) * *(b+n+offset_block_bj); 
+                }    
+                n++;
+            }        
+        }
+        
+        template <typename BaseInt, std::size_t Size>
+        void algo_diag(int block_ai, int block_bj,  unsigned int Order, BaseInt const* a, BaseInt const* b, BaseInt *c){
+            int OrderMinusOne = Order-1;
+            int offset_block_ai = block_ai*Order;
+            int offset_block_bj = block_bj*Order;    
+            
+            for(int i = 0; i < Order; ++i)
+                *(c+(offset_block_ai+offset_block_bj)*2+OrderMinusOne) += *(a+offset_block_bj+Order-1-i) * *(b+offset_block_ai+i);
+        }
+        
+        template <typename BaseInt, std::size_t Size>
+        void algo_triangle_down(int block_ai, int block_bj, unsigned int Order, BaseInt const* a, BaseInt const* b, BaseInt *c){
+            int n(0);
+            int offset_block_ai = (block_ai+1)*(Order)-1;
+            int offset_block_bj = (block_bj+1)*(Order)-1;
+            
+            for(int i = 0; i < Order-1; ++i){
+                for(int j = n; j < Order-1; ++j){
+                    *(c+(offset_block_ai+offset_block_bj)*2-2*Order+2-j) +=  *(a+offset_block_ai+n-j) * *(b+offset_block_bj-n); // will be VLI *
+                }    
+                n++;
+            }         
+        }
+        
+        template <typename BaseInt, std::size_t Size>
+        void algo_block_algo(int i, int j, unsigned int Order, BaseInt const* a, BaseInt const* b, BaseInt *c)
+        {
+            algo_triangle_up<BaseInt,Size>(i,j,Order,a,b,c);
+            //       algo_diag<BaseInt,Size>(i,j,Order,a,b,c);
+            //       algo_triangle_down<BaseInt,Size>(i,j,Order,a,b,c);
+        }
+
+        
+        
+        
+        
+        
 
 /**
 * VLI_GPU_VECTOR functions
@@ -198,11 +254,46 @@ __global__ void reduction_polynome(unsigned int max_order, std::size_t vector_si
         }
     }
 }
-   
+    
+/**
+* New algo 
+*/
+    
+template <typename BaseInt, std::size_t Size>
+__global__ void inner_prod_vector_blocks(unsigned int Order, std::size_t vector_size, BaseInt const* A, BaseInt const* B, BaseInt* C)
+{
+    // remove the loops  
+    unsigned int xIndex = blockIdx.x; // get poly one by one
+    const std::size_t size_multiplicant = Size*Order*Order;
+    const std::size_t size_product = Size*2*Order*2*Order;
+    //multiplication between polynomial
+    std::size_t offset_m = xIndex*size_multiplicant;
+    std::size_t offset_p = xIndex*size_product;
+    // first PASS, half top right corner, 
+    int n(0);
+    for(int i=0;i< Order;++i){ // i is a thread here
+        for(int j=0; j<=n; ++j){
+            algo_block_algo<BaseInt, Size>(j,n-j,Order,&A[offset_m],&B[offset_m],&C[offset_p]);
+        }
+        n++; // thread num
+    }
+    
+    // second PASS, half button right corner, 
+    n=1;
+    for(int i=1; i<Order;++i){ // i is a thread here
+        for(int j=n; j<Order; ++j){
+            algo_block_algo<BaseInt, Size>(j,n-j,Order,&A[offset_m],&B[offset_m],&C[offset_p]);          
+        }
+        n++; // thread num
+    }    
+        
+}
+            
+     
 /**
   * The C++ functions that call the kernels
   */
-
+    
 template <typename BaseInt, std::size_t Size>
 void inner_product_vector(unsigned int max_order, std::size_t vector_size, BaseInt const* A, BaseInt const* B, BaseInt* C, std::size_t threads_per_block) 
 {
@@ -225,12 +316,26 @@ void vector_reduction_inplace(unsigned int max_order, std::size_t vector_size, B
     reduction_polynome<BaseInt, Size> <<< dimgrid, dimblock >>>(max_order, vector_size, A);
 }
 
+template <typename BaseInt, std::size_t Size>
+void inner_product_vector_blocks(unsigned int Order, std::size_t vector_size, BaseInt const* A, BaseInt const* B, BaseInt *C)
+{
+    // start easy
+    dim3 dimgrid(1,1,1);
+    dim3 dimblock(1,1,1);
+    inner_prod_vector_blocks<BaseInt,Size><<<dimgrid,dimblock>>>(Order,vector_size,A,B,C);
+       
+}
+    
+
 #define VLI_IMPLEMENT_GPU_FUNCTIONS(TYPE, VLI_SIZE) \
     void inner_product_vector(vli_size_tag<VLI_SIZE>, unsigned int max_order, std::size_t vector_size, TYPE const* A, TYPE const* B, TYPE* C, std::size_t threads_per_block) \
         {inner_product_vector<TYPE,VLI_SIZE>(max_order,vector_size,A,B,C,threads_per_block);} \
     void vector_reduction_inplace(vli_size_tag<VLI_SIZE>, unsigned int max_order, std::size_t vector_size, TYPE* A) \
-        {vector_reduction_inplace<TYPE,VLI_SIZE>(max_order,vector_size,A);}
+        {vector_reduction_inplace<TYPE,VLI_SIZE>(max_order,vector_size,A);} \
+    void inner_product_vector_blocks(vli_size_tag<VLI_SIZE>, unsigned int Order, std::size_t vector_size, TYPE const* A, TYPE const* B, TYPE* C) \
+        {inner_product_vector_blocks<TYPE,VLI_SIZE>(Order, vector_size, A, B, C);} 
 
+    
 #define VLI_IMPLEMENT_GPU_FUNCTIONS_FOR(r, data, BASEINT_SIZE_PAIR) \
     VLI_IMPLEMENT_GPU_FUNCTIONS( BOOST_PP_TUPLE_ELEM(2,0,BASEINT_SIZE_PAIR), BOOST_PP_TUPLE_ELEM(2,1,BASEINT_SIZE_PAIR) )
 
--- large_integer/VLI/src/vli/detail/kernels_gpu.h
+++ large_integer/VLI/src/vli/detail/kernels_gpu.h
@@ -18,7 +18,8 @@ struct vli_size_tag
 
 #define VLI_DECLARE_GPU_FUNCTIONS(TYPE, VLI_SIZE) \
     void inner_product_vector(vli_size_tag<VLI_SIZE>, unsigned int max_order, std::size_t vector_size, TYPE const* A, TYPE const* B, TYPE* C, std::size_t threads_per_block); \
-    void vector_reduction_inplace(vli_size_tag<VLI_SIZE>, unsigned int max_order, std::size_t vector_size, TYPE* A); 
+    void vector_reduction_inplace(vli_size_tag<VLI_SIZE>, unsigned int max_order, std::size_t vector_size, TYPE* A); \
+    void inner_product_vector_blocks(vli_size_tag<VLI_SIZE>, unsigned int Order, std::size_t vector_size, TYPE const* A, TYPE const* B, TYPE* C);
 
 #define VLI_DECLARE_GPU_FUNCTIONS_FOR(r, data, BASEINT_SIZE_PAIR) \
     VLI_DECLARE_GPU_FUNCTIONS( BOOST_PP_TUPLE_ELEM(2,0,BASEINT_SIZE_PAIR), BOOST_PP_TUPLE_ELEM(2,1,BASEINT_SIZE_PAIR) )
