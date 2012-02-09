d14b3bb30841dd146cf870c7dfa6ecb6d20865d2
maquis <maquis@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-02-09 18:13:31 +0000

    added tasklists

---
+++ ambient/src/ambient/utils/tasklist.h
@@ -0,0 +1,33 @@
+#ifndef AMBIENT_TASKLIST_H
+#define AMBIENT_TASKLIST_H
+
+#include <pthread.h>
+
+#define STACK_CONTENT_RESERVATION 10
+namespace ambient{
+
+    class tasklist {
+    public:
+        class task {
+        public:
+            task(void* data);
+            void set_next(task* t);
+            task* next();
+            void* content;
+            task* pair;
+        };
+
+        tasklist();
+       ~tasklist();
+        void add_task(void* content);
+        void* get_task();
+
+        task* seed;
+        task* tail;
+        pthread_mutex_t mutex;
+        bool active;
+    };
+
+}
+
+#endif
