d1caa60f9ec1eb227222f11104b6d5f94ba60f25
maquis <maquis@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-03-12 13:58:13 +0000

    adding reudction file

---
+++ src/ambient/src/ambient/models/reduction.cpp
@@ -0,0 +1,37 @@
+#include "ambient/ambient.h"
+#include "ambient/models/v_model.h"
+
+namespace ambient { namespace models {
+
+    v_model::reduction::reductionq::reductionq(){
+
+    }
+
+    void v_model::reduction::reductionq::push(imodel::layout::entry* e){
+
+    }
+
+    v_model::reduction::reduction(imodel::revision* r)
+    : revision(r)
+    {
+        dim2 dim = this->revision->get_layout().get_mem_grid_dim();
+        for(size_t i = 0; i < dim.y; i++){
+            entries.push_back(std::vector<reductionq*>());
+            for(size_t j = 0; j < dim.x; j++){
+                entries[i].push_back(NULL);
+            }
+        }
+    };
+
+    v_model::reduction::~reduction(){
+    }
+
+    imodel::layout::entry& v_model::reduction::operator()(size_t i, size_t j){
+        if(this->entries[i][j] == NULL) this->entries[i][j] = new reductionq();
+        models::imodel::layout::entry* block = controller.alloc_block(*this->revision);
+        this->entries[i][j]->push(block);
+        return *block;
+    }
+
+} }
+
