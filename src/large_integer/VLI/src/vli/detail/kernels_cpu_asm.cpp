ff9021f41d0cbea218585f367ea07e51bd48307b
ewartt <ewartt@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-03-28 23:34:37 +0000

    fix sign VLI*VLI=2VLI

--- src/large_integer/VLI/src/vli/detail/kernels_cpu_asm.cpp
+++ src/large_integer/VLI/src/vli/detail/kernels_cpu_asm.cpp
@@ -354,71 +354,125 @@ void mul192_192(unsigned long int* x/* %%rdi */, unsigned long int const* y/* %%
 // Vli (384) = VLI (192) * VLI (192) 
 #define HELPER_ASM_MUL384_192_192(n) \
 void mul384_192_192(unsigned long int* x/* %%rdi */, unsigned long int const* y/* %%rsi */, unsigned long int const* z/* %%rdx -> rbx */){ \
-    asm( \
+   asm( \
 /*00*/  "movq %%rdx            ,%%rbx             \n" /* rdx uses by mul             */   \
-/*01*/  "xorq %%r9             ,%%r9              \n" /* r9 = 0  due to carry effect */   \
-/*01*/  "xorq %%r10            ,%%r10             \n" /* r10 = 0 due to carry effect */   \
-/*02*/  "xorq %%r11            ,%%r11             \n" /* r11 = 0 due to carry effect */   \
-/*03*/  "xorq %%r12            ,%%r12             \n" /* r12 = 0 due to carry effect */   \
-/*04*/  "movq (%%rsi)          ,%%rax             \n" /* a0 into rax */                   \
-/*06*/  "movq %%rbp            ,-0x08(%%rsp)      \n" /* alloc the stack */               \
-/*07*/  "movq %%rax            ,%%rbp             \n" /* keep a stack copy of rax/a0 */   \
+/*01*/  "xorq %%r11            ,%%r11             \n" /* r10 = 0 due to carry effect */   \
+/*02*/  "xorq %%r12            ,%%r12             \n" /* r11 = 0 due to carry effect */   \
+/*03*/  "xorq %%r13            ,%%r13             \n" /* r12 = 0 due to carry effect */   \
+/*03*/  "xorq %%r14            ,%%r14             \n" /* r13 = 0 it is the sign 0+ 1-*/   \
+/*03*/  "xorq %%r15            ,%%r15             \n" /* r13 = 0 it is the sign 0+ 1-*/   \
+/* negate a if negative and store into stack, in reverse order due to universal access */ \
+/*04*/  "movq "PPS(2,n)"(%%rsi),%%rax             \n" /* load a3 into r8, for the sign */ \
+/*05*/  "cmpq $0               ,%%rax             \n" /* test a is negative(sign on a3)*/ \
+/*06*/  "jns _Negativea_384_192_                  \n" /* number is negative, we negate */ \
+/*07*/  "movq (%%rsi)          ,%%r8              \n" /* load a0 */                       \
+/*08*/  "movq "PPS(1,n)"(%%rsi),%%r9              \n" /* load a1 */                       \
+/*09*/  "notq %%r8                                \n" /* C2M, ~a0 */                      \
+/*10*/  "notq %%r9                                \n" /* C2M, ~a1 */                      \
+/*11*/  "notq %%rax                               \n" /* C2M, ~a2 */                      \
+/*12*/  "addq $0x1             ,%%r8              \n" /* C2M, ~a0+1 */                    \
+/*13*/  "adcq $0x0             ,%%r9              \n" /* C2M, ~a1+CB */                   \
+/*14*/  "adcq $0x0             ,%%rax             \n" /* C2M, ~a2+CB */                   \
+/*15*/  "movq %%r8             ,-24(%%rsp)        \n" /* a0 into the stack -24 rsp */     \
+/*16*/  "movq %%r9             ,-16(%%rsp)        \n" /* a1 into the stack -16 rsp */     \
+/*17*/  "movq %%rax            ,-8(%%rsp)         \n" /* a2 into the stack -8 rsp */      \
+/*18*/  "leaq  -24(%%rsp)      ,%%rsi             \n" /* rsi points to stack a0 > 0 */    \
+/*19*/  "movq $1               ,%%r14             \n" /* r13 = 0 it is the sign 0+ 1-*/   \
+/*20*/  "_Negativea_384_192_ :                    \n" /* end if structure */              \
+/* negate a if negative and store into stack, in reverse order due to universal access */ \
+/*21*/  "movq "PPS(2,n)"(%%rbx),%%rax             \n" /* load a3 into r8, for the sign */ \
+/*22*/  "cmpq $0               ,%%rax             \n" /* test a is negative(sign on a3)*/ \
+/*23*/  "jns _Negativeb_384_192_                  \n" /* number is negative, we negate */ \
+/*24*/  "movq (%%rbx)          ,%%r8              \n" /* load b0 */                       \
+/*25*/  "movq "PPS(1,n)"(%%rbx),%%r9              \n" /* load b1 */                       \
+/*26*/  "notq %%r8                                \n" /* C2M, ~b0 */                      \
+/*27*/  "notq %%r9                                \n" /* C2M, ~b1 */                      \
+/*28*/  "notq %%rax                               \n" /* C2M, ~b2 */                      \
+/*29*/  "addq $0x1             ,%%r8              \n" /* C2M, ~b0+1 */                    \
+/*30*/  "adcq $0x0             ,%%r9              \n" /* C2M, ~b1+CB */                   \
+/*31*/  "adcq $0x0             ,%%rax             \n" /* C2M, ~b2+CB */                   \
+/*32*/  "movq %%r8             ,-48(%%rsp)        \n" /* b0 into the stack -48 rsp */     \
+/*33*/  "movq %%r9             ,-40(%%rsp)        \n" /* b1 into the stack -40 rsp */     \
+/*34*/  "movq %%rax            ,-32(%%rsp)        \n" /* b2 into the stack -32 rsp */     \
+/*35*/  "leaq  -48(%%rsp)      ,%%rbx             \n" /* rsi points to stack b0 > 0 */    \
+/*36*/  "movq $1               ,%%r15             \n" /* r13 = 0 it is the sign 0+ 1-*/   \
+/*37*/  "_Negativeb_384_192_ :                    \n" /* end if structure */              \
+/*38*/  "xorq %%r10            ,%%r10             \n" /* r9 = 0  due to carry effect */   \
 /* --------------------------- a0 * b0, a0 * b1, a0 * b2 start ------------------------*/ \
-/*08*/  "mulq (%%rbx)                             \n" /* lo rax, hi rdx   a0*b0 */        \
-/*09*/  "movq %%rax            ,(%%rdi)           \n" /* only one term, write into c0 */  \
-/*10*/  "movq %%rdx            ,%%r8              \n" /* a0b0hi into r8 */                \
-/*11*/  "movq %%rbp            ,%%rax             \n" /* reload rax(a0) from the stack */ \
-/*12*/  "mulq "PPS(1,n)"(%%rbx)                   \n" /* a0 * b1 */                       \
-/*13*/  "addq %%rax            ,%%r8              \n" /* add a0b0hi + a0b1lo */           \
-/*14*/  "adcq %%rdx            ,%%r9              \n" /* save the a0b1hi into r9 */       \
-/*15*/  "movq %%rbp            ,%%rax             \n" /* reload rax(a0) from the stack */ \
-/*16*/  "mulq "PPS(2,n)"(%%rbx)                   \n" /* a0 * b2 */                       \
-/*17*/  "addq %%rax            ,%%r9              \n" /* add l11 + a0b2lo + c */          \
-/*18*/  "adcq %%rdx            ,%%r10             \n" /* add l01 + a0b2hi + c, end */     \
+/*39*/  "movq (%%rsi)          ,%%rax             \n" /* a0 into rax */                   \
+/*40*/  "movq %%rax            ,%%rcx             \n" /* save a0-rcx faster than stack */ \
+/*41*/  "mulq (%%rbx)                             \n" /* lo rax, hi rdx   a0*b0 */        \
+/*42*/  "movq %%rax            ,%%r8              \n" /* only one term, write into c0 */  \
+/*43*/  "movq %%rdx            ,%%r9              \n" /* a0b0hi into r8 */                \
+/*44*/  "movq %%rcx            ,%%rax             \n" /* reload rax(a0) from the stack */ \
+/*45*/  "mulq "PPS(1,n)"(%%rbx)                   \n" /* a0 * b1 */                       \
+/*46*/  "addq %%rax            ,%%r9              \n" /* add a0b0hi + a0b1lo */           \
+/*47*/  "adcq %%rdx            ,%%r10             \n" /* save the a0b1hi into r9 */       \
+/*48*/  "movq %%rcx            ,%%rax             \n" /* reload rax(a0) from the stack */ \
+/*49*/  "mulq "PPS(2,n)"(%%rbx)                   \n" /* a0 * b2 */                       \
+/*50*/  "addq %%rax            ,%%r10             \n" /* add l11 + a0b2lo + c */          \
+/*51*/  "adcq %%rdx            ,%%r11             \n" /* add l01 + a0b2hi + c, end */     \
 /* --------------------------- a0 * b0, a0 * b1, a0 * b2 end --------------------------*/ \
 /* --------------------------- a1 * b0, a1 * b1, a1 * b2 start ------------------------*/ \
-/*19*/  "movq "PPS(1,n)"(%%rsi),%%rax             \n" /* a1 into rax */                   \
-/*20*/  "movq %%rax            ,%%rbp             \n" /* keep a stack copy of rax/a1 */   \
-/*21*/  "mulq (%%rbx)                             \n" /* a1 * b0 */                       \
-/*22*/  "addq %%rax            ,%%r8              \n" /* l13 + a1b0lo */                  \
-/*23*/  "adcq %%rdx            ,%%r9              \n" /* l17 + a1b0hi + c */              \
-/*AD*/  "adcq $0               ,%%r10             \n" /* possible carry */                \
-/*24*/  "movq %%rbp            ,%%rax             \n" /* reload rax(a1) from the stack */ \
-/*25*/  "mulq "PPS(2,n)"(%%rbx)                   \n" /* a1*b2 */                         \
-/*26*/  "addq %%rax            ,%%r10             \n" /* l18 + a1b2lo + c */              \
-/*27*/  "adcq %%rdx            ,%%r11             \n" /* a1b2hi + c */                    \
-/*28*/  "movq %%rbp            ,%%rax             \n" /* reload rax(a1) from the stack */ \
-/*29*/  "mulq "PPS(1,n)"(%%rbx)                   \n" /* a1*b1 */                         \
-/*30*/  "addq %%rax            ,%%r9              \n" /* a1b2lo to r9 */                  \
-/*31*/  "adcq %%rdx            ,%%r10             \n" /* a1b2hi + c  */                   \
-/*31*/  "adcq $0               ,%%r11             \n" /* r11 + c  */                      \
+/*52*/  "movq "PPS(1,n)"(%%rsi),%%rax             \n" /* a1 into rax */                   \
+/*53*/  "movq %%rax            ,%%rcx             \n" /* save a0-rcx faster than stack */ \
+/*54*/  "mulq (%%rbx)                             \n" /* a1 * b0 */                       \
+/*55*/  "addq %%rax            ,%%r9              \n" /* l13 + a1b0lo */                  \
+/*56*/  "adcq %%rdx            ,%%r10             \n" /* l17 + a1b0hi + c */              \
+/*57*/  "adcq $0               ,%%r11             \n" /* possible carry */                \
+/*58*/  "movq %%rcx            ,%%rax             \n" /* reload rax(a1) from the stack */ \
+/*59*/  "mulq "PPS(2,n)"(%%rbx)                   \n" /* a1*b2 */                         \
+/*60*/  "addq %%rax            ,%%r11             \n" /* l18 + a1b2lo + c */              \
+/*61*/  "adcq %%rdx            ,%%r12             \n" /* a1b2hi + c */                    \
+/*62*/  "movq %%rcx            ,%%rax             \n" /* reload rax(a1) from the stack */ \
+/*63*/  "mulq "PPS(1,n)"(%%rbx)                   \n" /* a1*b1 */                         \
+/*64*/  "addq %%rax            ,%%r10             \n" /* a1b2lo to r9 */                  \
+/*65*/  "adcq %%rdx            ,%%r11             \n" /* a1b2hi + c  */                   \
+/*66*/  "adcq $0               ,%%r12             \n" /* r11 + c  */                      \
 /* --------------------------- a1 * b0, a1 * b1, a1 * b2 end --------------------------*/ \
 /* --------------------------- a2 * b0, a2 * b1, a2 * b2 start ------------------------*/ \
-/*32*/  "movq "PPS(2,n)"(%%rsi),%%rax             \n" /* a2 to rax */                     \
-/*33*/  "movq %%rax            ,%%rbp             \n" /* copy into the stack */           \
-/*34*/  "mulq (%%rbx)                             \n" /* a2*b0 */                         \
-/*35*/  "addq %%rax            ,%%r9              \n" /* l30 + a2b0lo */                  \
-/*36*/  "adcq %%rdx            ,%%r10             \n" /* l31 + a2b0hi + c */              \
-/*AD*/  "adcq $0               ,%%r11             \n" /* possible carry */                \
-/*37*/  "movq %%rbp            ,%%rax             \n" /* reload rax(a2) */                \
-/*38*/  "mulq "PPS(2,n)"(%%rbx)                   \n" /* a2*b2 */                         \
-/*39*/  "addq %%rax            ,%%r11             \n" /* a2b2lo + l31 + c*/               \
-/*40*/  "adcq %%rdx            ,%%r12             \n" /* a2b2hi + l03 + c*/               \
-/*41*/  "movq %%rbp            ,%%rax             \n" /* reload a2 */                     \
-/*42*/  "mulq "PPS(1,n)"(%%rbx)                   \n" /* a2*b1 */                         \
-/*43*/  "addq %%rax            ,%%r10             \n" /* a2b1lo + l36 */                  \
-/*44*/  "adcq %%rdx            ,%%r11             \n" /* a2b1hi + l39 */                  \
-/*45*/  "adcq $0               ,%%r12             \n" /* r12 + c */                       \
+/*67*/  "movq "PPS(2,n)"(%%rsi),%%rax             \n" /* a2 to rax */                     \
+/*68*/  "movq %%rax            ,%%rcx             \n" /* copy into the stack */           \
+/*69*/  "mulq (%%rbx)                             \n" /* a2*b0 */                         \
+/*70*/  "addq %%rax            ,%%r10             \n" /* l30 + a2b0lo */                  \
+/*71*/  "adcq %%rdx            ,%%r11             \n" /* l31 + a2b0hi + c */              \
+/*72*/  "adcq $0               ,%%r12             \n" /* possible carry */                \
+/*73*/  "movq %%rcx            ,%%rax             \n" /* reload rax(a2) */                \
+/*74*/  "mulq "PPS(2,n)"(%%rbx)                   \n" /* a2*b2 */                         \
+/*75*/  "addq %%rax            ,%%r12             \n" /* a2b2lo + l31 + c*/               \
+/*76*/  "adcq %%rdx            ,%%r13             \n" /* a2b2hi + l03 + c*/               \
+/*77*/  "movq %%rcx            ,%%rax             \n" /* reload a2 */                     \
+/*78*/  "mulq "PPS(1,n)"(%%rbx)                   \n" /* a2*b1 */                         \
+/*79*/  "addq %%rax            ,%%r11             \n" /* a2b1lo + l36 */                  \
+/*80*/  "adcq %%rdx            ,%%r12             \n" /* a2b1hi + l39 */                  \
+/*81*/  "adcq $0               ,%%r13             \n" /* r12 + c */                       \
 /* --------------------------- a2 * b0, a2 * b1, a2 * b2 end --------------------------*/ \
-/*46*/  "movq %%r8             ,"PPS(1,n)"(%%rdi) \n" /* r8 -> c1 */                      \
-/*47*/  "movq %%r9             ,"PPS(2,n)"(%%rdi) \n" /* r8 -> c2 */                      \
-/*48*/  "movq %%r10            ,"PPS(3,n)"(%%rdi) \n" /* r8 -> c3 */                      \
-/*49*/  "movq %%r11            ,"PPS(4,n)"(%%rdi) \n" /* r8 -> c4 */                      \
-/*50*/  "movq %%r12            ,"PPS(5,n)"(%%rdi) \n" /* r8 -> c5 */                      \
-/*51*/  "movq -0x08(%%rsp)      ,%%rbp            \n" /* stack clean up */                \
-    : : : "rax","rdx","rbx","r8","r9","r10","r11","r12","memory"  \
-    ); \
-};
+/* ---------------------------           sign                --------------------------*/ \
+/*81*/  "xorq %%r14            ,%%r15             \n"                                     \
+/*82*/  "cmpq $0               ,%%r15             \n" /* r15 = 1 we negate */             \
+/*83*/  "je _IsNegativeResult_384_192_            \n" /* not equal ZF = 0, negate*/       \
+/*84*/   "notq %%r8                               \n" /* start2ComplementMethod negate */ \
+/*85*/  "notq %%r9                                \n" /* 2CM negate */                    \
+/*86*/  "notq %%r10                               \n" /* 2CM negate */                    \
+/*87*/  "notq %%r11                               \n" /* 2CM negate */                    \
+/*88*/  "notq %%r12                               \n" /* 2CM negate */                    \
+/*89*/  "notq %%r13                               \n" /* 2CM negate */                    \
+/*90*/  "addq $0x1             ,%%r8              \n" /* 2CM add 1 */                     \
+/*91*/  "adcq $0x0             ,%%r9              \n" /* 2CM propagate CB */              \
+/*92*/  "adcq $0x0             ,%%r10             \n" /* 2CM propagate CB */              \
+/*93*/  "adcq $0x0             ,%%r11             \n" /* 2CM propagate CB */              \
+/*94*/  "adcq $0x0             ,%%r12             \n" /* 2CM propagate CB */              \
+/*95*/  "adcq $0x0             ,%%r13             \n" /* 2CM propagate CB */              \
+/*96*/  "_IsNegativeResult_384_192_ :             \n" /* end if negative result */        \
+/*97*/  "movq %%r8             ,(%%rdi)           \n" /* r8 -> c1 */                      \
+/*98*/  "movq %%r9             ,"PPS(1,n)"(%%rdi) \n" /* r9 -> c1 */                      \
+/*99*/  "movq %%r10            ,"PPS(2,n)"(%%rdi) \n" /* r10 -> c2 */                     \
+/*100*/ "movq %%r11            ,"PPS(3,n)"(%%rdi) \n" /* r11 -> c3 */                     \
+/*101*/ "movq %%r12            ,"PPS(4,n)"(%%rdi) \n" /* r12 -> c4 */                     \
+/*102*/ "movq %%r13            ,"PPS(5,n)"(%%rdi) \n" /* r13 -> c5 */                     \
+   : : : "rax","rdx","rcx","rbx","r8","r9","r10","r11","r12","r13","r14","r15","memory"   \
+   ); \
+}
 // Vli (384) = VLI (192) * VLI (192) 
 // c += a*b
 #define HELPER_ASM_MUL_ADD384_192_192(n) \
