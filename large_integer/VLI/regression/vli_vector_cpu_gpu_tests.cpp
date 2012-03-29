c316f73bc6261d1cb878e5a98a227ea81aaae084
ewartt <ewartt@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-03-29 12:28:03 +0000

    VLI release 6 ok

--- large_integer/VLI/regression/vli_vector_cpu_gpu_tests.cpp
+++ large_integer/VLI/regression/vli_vector_cpu_gpu_tests.cpp
@@ -30,6 +30,7 @@ using vli::vector_polynomial_cpu;
 using vli::test::fill_random;
 using vli::test::fill_poly_random;
 using vli::test::fill_vector_random;
+using vli::test::fill_vector_negate;
 
 typedef vli::test::vli_cpu_type_list vli_types;
 typedef mpz_class large_int; //mpz_class into gmp
@@ -37,28 +38,9 @@ typedef hp2c::monomial<large_int> monomial_type;
 
 typedef vli::test::vli_cpu_type_list vli_types;
 
-enum { vector_size = 128 };
+enum { vector_size = 64 };
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(vector_inner_product, Vli, vli_types)
-{
-    typedef vli::polynomial_cpu<Vli, 11 > polynomial_type_cpu;
-    typedef vli::vli_cpu<typename Vli::value_type,  2*Vli::size > vli_result_type_cpu;
-    typedef vli::polynomial_cpu<vli_result_type_cpu, 22 > polynomial_result_type_cpu;
-    typedef vli::vector_polynomial_cpu<polynomial_type_cpu> vector_type_cpu;
-
-
-    vector_type_cpu VaCPU(vector_size),VbCPU(vector_size);
-    polynomial_result_type_cpu pcCPU0;
-    fill_vector_random(VaCPU,Vli::size);
-    fill_vector_random(VbCPU,Vli::size);
-    pcCPU0 = inner_product(VaCPU,VbCPU);
-
-    polynomial_result_type_cpu pcCPU1;
-    pcCPU1 = vli::detail::inner_product_plain(VaCPU,VbCPU);
-    BOOST_CHECK_EQUAL(pcCPU0,pcCPU1);
-}
-
-BOOST_AUTO_TEST_CASE_TEMPLATE(vector_inner_product_gmp, Vli, vli_types)
+BOOST_AUTO_TEST_CASE_TEMPLATE(vector_inner_product_gmp_positive, Vli, vli_types)
 {
     // VLI
     typedef vli::polynomial_cpu<Vli, 11 > polynomial_type_cpu;
@@ -81,7 +63,7 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(vector_inner_product_gmp, Vli, vli_types)
    
     fill_vector_random(v1);
     fill_vector_random(v2);
-
+    
     vli::test::InitVecVLItoVecGMP(v1,vgmp1);
     vli::test::InitVecVLItoVecGMP(v2,vgmp2);
     pd = vli::detail::inner_product_plain(v1,v2);
@@ -91,5 +73,37 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(vector_inner_product_gmp, Vli, vli_types)
 }
 
 
-
-
+BOOST_AUTO_TEST_CASE_TEMPLATE(vector_inner_product_gmp_positive_negative, Vli, vli_types)
+{
+    // VLI
+    typedef vli::polynomial_cpu<Vli, 11 > polynomial_type_cpu;
+    typedef vli::vli_cpu<typename Vli::value_type,  2*Vli::size > vli_result_type_cpu;
+    typedef vli::polynomial_cpu<vli_result_type_cpu, 22 > polynomial_result_type_cpu;
+    typedef vli::vector_polynomial_cpu<polynomial_type_cpu> vector_type_cpu;
+    // GMP
+    typedef hp2c::polynomial<large_int,11> poly_gmp;
+    typedef hp2c::polynomial<large_int,2*11> poly_gmp_double;
+    typedef std::vector<poly_gmp> vector_poly_gmp;
+    
+    poly_gmp_double pgmpd;
+    polynomial_result_type_cpu pd; 
+    
+    vector_type_cpu v1(vector_size);
+    vector_type_cpu v2(vector_size);
+    
+    vector_poly_gmp vgmp1(vector_size);
+    vector_poly_gmp vgmp2(vector_size);
+    
+    fill_vector_random(v1,2);
+    fill_vector_random(v2,3);
+    
+    fill_vector_negate(v1,2);
+    fill_vector_negate(v2,3);
+        
+    vli::test::InitVecVLItoVecGMP(v1,vgmp1);
+    vli::test::InitVecVLItoVecGMP(v2,vgmp2);
+    pd = vli::detail::inner_product_plain(v1,v2);
+    pgmpd = inner_product(vgmp1,vgmp2);
+        
+    BOOST_CHECK_EQUAL(vli::test::ValidatePolyVLI_PolyGMP(pd,pgmpd), true );
+}
