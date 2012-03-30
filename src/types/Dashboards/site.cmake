dfb02095e7d84c2eef50ebc42bdcf69ea646aa27
maquis <maquis@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-03-30 02:10:16 +0000

    auto-generating site.cmake for the install

--- src/types/Dashboards/site.cmake
+++ src/types/Dashboards/site.cmake
@@ -1,15 +1,11 @@
-# Client maintainer: alex.kosenkov@gmail.com
-set(CTEST_SITE "castor")
-set(CTEST_BUILD_NAME "Linux x64-icc-mvapich")
+set(CTEST_SITE "${PREDEFINED_CTEST_SITE}")
+set(CTEST_BUILD_NAME "${PREDEFINED_CTEST_BUILD_NAME}")
 set(CTEST_BUILD_CONFIGURATION Debug)
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
-set(CTEST_SOURCE_DIRECTORY "/users/maquis/maquis2012/src/types")
-set(CTEST_BINARY_DIRECTORY "/users/maquis/maquis2012/src/types/build_intel_mvapich2")
-#set(CTEST_CHECKOUT_COMMAND "svn up")
+set(CTEST_SOURCE_DIRECTORY "${PREDEFINED_CTEST_SOURCE_DIRECTORY}")
+set(CTEST_BINARY_DIRECTORY "${PREDEFINED_CTEST_BINARY_DIRECTORY}")
 set(CTEST_UPDATE_COMMAND "svn")
 set(CTEST_PROJECT_SUBPROJECTS types)
-
-set(ENABLE_REGRESSION_VALIDATIONS ON)
 
 set(subproject ${CTEST_PROJECT_SUBPROJECTS})
 set_property(GLOBAL PROPERTY SubProject ${subproject})
