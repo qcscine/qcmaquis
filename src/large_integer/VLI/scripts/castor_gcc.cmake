652d9c682f7dea6970e4ec399aefebcc10316fe9
ewartt <ewartt@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-04-02 22:06:16 +0000

    vli cdash

--- src/large_integer/VLI/scripts/castor_gcc.cmake
+++ src/large_integer/VLI/scripts/castor_gcc.cmake
@@ -1,3 +1,4 @@
 set(CMAKE_C_COMPILER gcc)
 set(CMAKE_CXX_COMPILER g++)
-set(CMAKE_CXX_FLAGS "-Wall -Os -funroll-loops -m64")
+set (VLI_COMPILER_FLAGS_RELEASE " -Wall -Os -funroll-loops -m64" CACHE STRING "Compiler flags for a regular compile.")
+set (VLI_COMPILER_FLAGS_DEBUG " -Wall -g -O0 -m64" CACHE STRING "Compiler flags for a debug compile.")
