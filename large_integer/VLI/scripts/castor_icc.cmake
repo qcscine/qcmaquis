703c9b2ec177800003721645837deae6bb552ff0
ewartt <ewartt@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-04-02 22:10:49 +0000

    vli cdash

--- large_integer/VLI/scripts/castor_icc.cmake
+++ large_integer/VLI/scripts/castor_icc.cmake
@@ -1,4 +1,4 @@
 set(CMAKE_C_COMPILER icc)
 set(CMAKE_CXX_COMPILER icpc)
-set (VLI_COMPILER_FLAGS_RELEASE " -Wall -O2 -funroll-loops -m64 -std=c++0" CACHE STRING "Compiler flags for a regular compile.")
+set (VLI_COMPILER_FLAGS_RELEASE " -Wall -O2 -funroll-loops -m64 -std=c++0x" CACHE STRING "Compiler flags for a regular compile.")
 set (VLI_COMPILER_FLAGS_DEBUG " -Wall -g -O0 -m64" CACHE STRING "Compiler flags for a debug compile.")
