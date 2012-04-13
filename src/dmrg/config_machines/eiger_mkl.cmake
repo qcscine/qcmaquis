e0fe0dc2dc879ec9dd332eaadefc39c6b89beaa9
maquis <maquis@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-04-13 20:48:56 +0000

    removing config_machines due to new environment aware build scripts

--- src/dmrg/config_machines/eiger_mkl.cmake
+++
@@ -1,4 +0,0 @@
-
-set(DEFAULT_BLAS_LAPACK manual)
-set(CMAKE_CXX_FLAGS "-Wall -O2 -funroll-loops -std=c++0x -m64 -mkl=parallel")
-set(BLAS_LAPACK_MANUAL_INCLUDES /apps/todi/intel/composerxe-2011.3.174/mkl/include)
