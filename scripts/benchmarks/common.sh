28446a2e014f87d4f62bfcfde452fd61a801e7ec
maquis <maquis@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-04-18 20:49:17 +0000

    adding benchmark functionality

---
+++ scripts/benchmarks/common.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+## path to this script ##
+SCRIPT="$ROOT_DIR/$SCRIPTS_DIR/benchmarks/common.sh"
+
+## execution ##
+COMMAND="( mpiexec -np \$NP \$EXECUTABLE \$NT \$WL | grep GFlops | tr -d 'GFlops ' ) 2> /dev/null"
+[[ -n "$EXECUTABLE" ]] || EXECUTABLE="./$BENCHMARK.out"
+
+## locations ##
+SCRATCH="$ROOT_DIR/$TARGET/$BUILD_DIR/$BENCHMARK_DIR/$NAME.`date +\"%m.%d.%H.%M\"`"
+ORIGIN="$ROOT_DIR/$TARGET/$BUILD_DIR/$BENCHMARK_DIR"
+OUTPUT_LOG="output.log"
+REPRODUCER="reproducer.sh"
+
+## Handle SIGINT (^C) ##
+## Terminate all runs ##
+control_c(){ echo; echo; echo -n " "; kill $$; }
+trap control_c SIGINT
+
+## prepare the environment ##
+pushd . &> /dev/null
+mkdir $SCRATCH
+touch $SCRATCH/$OUTPUT_LOG
+touch $SCRATCH/$REPRODUCER
+cp $ORIGIN/$EXECUTABLE $SCRATCH
+cd $SCRATCH
+
+## generating reproducer ##
+head -n 1 $SCRIPT > $REPRODUCER
+echo "## auto-environment ##" >> $REPRODUCER
+echo "## `uname -a` ##" >> $REPRODUCER
+set >> $REPRODUCER
+echo "## end of auto-environment ##" >> $REPRODUCER
+tail +2 $SCRIPT >> $REPRODUCER
+
+## main execution ## 
+echo | tee $OUTPUT_LOG
+echo " `date +\"%m.%d, %H:%M:%S\"`" | tee -a $OUTPUT_LOG
+
+## loop over process counts ##
+NP_STEP_BOUND=$NP_LOWER_BOUND
+for NP in `seq $NP_LOWER_BOUND $NP_S_INCREMENT $NP_UPPER_BOUND`
+do
+    [[ $NP < $NP_STEP_BOUND ]] && continue
+    NP_STEP_BOUND="$(( $NP*$NP_L_INCREMENT ))"
+    echo | tee -a $OUTPUT_LOG
+    echo " ------------------------------------------------------------------------------------------ " | tee -a $OUTPUT_LOG
+    echo " $NAME: processes: $NP                                                                      " | tee -a $OUTPUT_LOG
+    echo -n " ----" | tee -a $OUTPUT_LOG
+
+    ## loop over thread counts ##
+    NT_STEP_BOUND=$NT_LOWER_BOUND
+    for NT in `seq $NT_LOWER_BOUND $NT_S_INCREMENT $NT_UPPER_BOUND`
+    do
+        [[ $NT < $NT_STEP_BOUND ]] && continue
+        NT_STEP_BOUND="$(( $NT*$NT_L_INCREMENT ))"
+        echo      "-------------------------------------------------------------------------------------- " | tee -a $OUTPUT_LOG
+        echo "     $NAME: threads: $NT                                                                    " | tee -a $OUTPUT_LOG
+        echo "     -------------------------------------------------------------------------------------- " | tee -a $OUTPUT_LOG
+
+        ## loop over workloads ##
+        WL_STEP_BOUND=$WL_LOWER_BOUND
+        for WL in `seq $WL_LOWER_BOUND $WL_S_INCREMENT $WL_UPPER_BOUND`
+        do
+            [[ $WL < $WL_STEP_BOUND ]] && continue
+            WL_STEP_BOUND="$(( $WL*$WL_L_INCREMENT ))"
+        
+            echo -n "     $NAME (${NP}x${NT} on ${WL}): " | tee -a $OUTPUT_LOG
+            eval $COMMAND | tee -a $OUTPUT_LOG
+        done
+        echo -n "     " | tee -a $OUTPUT_LOG
+    done
+done
+
+## exiting / cleaning ##
+echo | tee -a $OUTPUT_LOG
+echo " `date +\"%m.%d, %H:%M:%S\"`" | tee -a $OUTPUT_LOG
+echo | tee -a $OUTPUT_LOG
+popd &> /dev/null
