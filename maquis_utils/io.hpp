c9a8b975e0aa958dd752bb002312bf9dd2ee41b2
maquis <maquis@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-04-24 16:09:27 +0000

    adding cerr

--- maquis_utils/io.hpp
+++ maquis_utils/io.hpp
@@ -5,8 +5,10 @@ namespace maquis {
 
 #ifdef AMBIENT_IO
     extern ambient::io cout;
+    extern ambient::io cerr;
 #else
     std::ostream& cout = std::cout;
+    std::ostream& cerr = std::cerr;
 #endif
 
 }
