3ac1849aad9c5960e45863c314b13eaba4a91454
maquis <maquis@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-04-26 15:03:45 +0000

    changing a bit CMakeLists

--- dmrg/applications/dmrg/dmrg.h
+++ dmrg/applications/dmrg/dmrg.h
@@ -9,24 +9,25 @@
 #ifndef APP_DMRG_H
 #define APP_DMRG_H
 
+#ifdef USE_COMPLEX
+    #define value_type std::complex<double>
+#else
+    #define value_type double
+#endif
+#if defined USE_AMBIENT
+    #include "types/p_dense_matrix/p_dense_matrix.h"
+    typedef maquis::types::p_dense_matrix<value_type> matrix;
+#elif defined USE_MTM
+    #include "types/mt_matrix/mt_matrix.h"
+    typedef maquis::types::mt_matrix<value_type> matrix;
+#else
+    typedef maquis::types::dense_matrix<value_type> matrix;
+#endif
+#undef value_type
+
 #include "dmrg/utils/DmrgParameters2.h"
-
 #include "types/dense_matrix/dense_matrix.h"
-#ifdef USE_MTM
-#include "types/mt_matrix/mt_matrix.h"
-typedef maquis::types::mt_matrix<double> Matrix;
-#else
-
-#ifndef USE_COMPLEX
-typedef maquis::types::dense_matrix<double> Matrix;
-#else
-typedef maquis::types::dense_matrix<std::complex<double> > Matrix;
-#endif
-
-#endif
-
 #include "dmrg/block_matrix/symmetry.h"
-
 
 // def. of run functions
 template <class SymmGroup>
