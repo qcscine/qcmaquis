f2d48ab9989de23d55ef16c7b46ec9424d3eb720
hehn <hehn@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-05-09 11:18:12 +0000

    splitted the vli_cpu tests into several smaller tests

---
+++ src/large_integer/VLI/regression/vli_cpu/gmp_extending_plus.cpp
@@ -0,0 +1,49 @@
+#include <regression/vli_cpu/test_header.hpp>
+#include <gmpxx.h>
+
+using namespace vli::test;
+
+VLI_FUZZABLE_TEST( plus_assign_extend_positive )
+{
+    vli_type a;
+    vli_type b;
+    init(a,max_positive);
+    init(b,max_positive);
+    mpz_class agmp(a.get_str()), bgmp(b.get_str());
+
+    typename extended<vli_type>::type c = plus_extend(a,b);
+    mpz_class cgmp = agmp + bgmp;
+
+    BOOST_CHECK_EQUAL(c.get_str(),cgmp.get_str());
+}
+
+VLI_FUZZABLE_TEST( plus_assign_extend_negative )
+{
+    vli_type a,b;
+    init(a);
+    init(b);
+    negate_inplace(a);
+    negate_inplace(b);
+    mpz_class agmp(a.get_str()), bgmp(b.get_str());
+
+    typename extended<vli_type>::type c = plus_extend(a,b);
+    mpz_class cgmp = agmp + bgmp;
+
+    BOOST_CHECK_EQUAL(c.get_str(),cgmp.get_str());
+}
+
+VLI_FUZZABLE_TEST( plus_assign_extend_mixed_sign )
+{
+    vli_type a,b;
+    init(a,max_positive);
+    init(b);
+    negate_inplace(b);
+    mpz_class agmp(a.get_str()), bgmp(b.get_str());
+
+    typename extended<vli_type>::type c = plus_extend(a,b);
+    typename extended<vli_type>::type d = plus_extend(b,a);
+    mpz_class cgmp = agmp + bgmp;
+
+    BOOST_CHECK_EQUAL(c,d);
+    BOOST_CHECK_EQUAL(c.get_str(),cgmp.get_str());
+}
---
+++ src/large_integer/VLI/regression/vli_cpu/gmp_minus.cpp
@@ -0,0 +1,34 @@
+#include <regression/vli_cpu/test_header.hpp>
+#include<gmpxx.h>
+
+using namespace vli::test;
+
+VLI_FUZZABLE_TEST( gmp_minus )
+{
+    vli_type a;
+    vli_type b;
+    init(a,max_positive);
+    init(b,overflow_safe);
+    
+    mpz_class agmp(a.get_str()), bgmp(b.get_str());
+    
+    vli_type c = a-b;
+    mpz_class cgmp = agmp - bgmp;
+    
+    BOOST_CHECK_EQUAL(c.get_str(),cgmp.get_str());
+}
+
+VLI_FUZZABLE_TEST( gmp_minus_int )
+{
+    vli_type a;
+    init(a,max_positive);
+    int b;
+    init(b);
+    
+    mpz_class agmp(a.get_str());
+    
+    vli_type c = a-b;
+    mpz_class cgmp = agmp - b;
+    
+    BOOST_CHECK_EQUAL(c.get_str(),cgmp.get_str());
+}
