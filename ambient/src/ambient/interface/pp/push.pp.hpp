107f340a358b5dcff9e4c68602b1cd04467cfe51
maquis <maquis@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-05-10 17:57:15 +0000

    future copy constructor

--- ambient/src/ambient/interface/pp/push.pp.hpp
+++ ambient/src/ambient/interface/pp/push.pp.hpp
@@ -1,41 +1,51 @@
 template <typename FP, class T0>
 void push(FP l_kernel, FP c_kernel, T0& arg0){
     ambient::controller.push(new models::operation(l_kernel, c_kernel, models::info<T0>::unfold(arg0)));
+    ambient::playout();
 }
 template <typename FP, class T0, class T1>
 void push(FP l_kernel, FP c_kernel, T0& arg0, T1& arg1){
     ambient::controller.push(new models::operation(l_kernel, c_kernel, models::info<T0>::unfold(arg0), models::info<T1>::unfold(arg1)));
+    ambient::playout();
 }
 template <typename FP, class T0, class T1, class T2>
 void push(FP l_kernel, FP c_kernel, T0& arg0, T1& arg1, T2& arg2){
     ambient::controller.push(new models::operation(l_kernel, c_kernel, models::info<T0>::unfold(arg0), models::info<T1>::unfold(arg1), models::info<T2>::unfold(arg2)));
+    ambient::playout();
 }
 template < typename FP, class T0 , class T1 , class T2 , class T3 >
 void push( FP l_kernel, FP c_kernel, T0 &arg0 , T1 &arg1 , T2 &arg2 , T3 &arg3 ){
     ambient::controller.push(new models::operation( l_kernel, c_kernel, models::info<T0>::unfold(arg0) , models::info<T1>::unfold(arg1) , models::info<T2>::unfold(arg2) , models::info<T3>::unfold(arg3) ));
+    ambient::playout();
 }
 template < typename FP, class T0 , class T1 , class T2 , class T3 , class T4 >
 void push( FP l_kernel, FP c_kernel, T0 &arg0 , T1 &arg1 , T2 &arg2 , T3 &arg3 , T4 &arg4 ){
     ambient::controller.push(new models::operation( l_kernel, c_kernel, models::info<T0>::unfold(arg0) , models::info<T1>::unfold(arg1) , models::info<T2>::unfold(arg2) , models::info<T3>::unfold(arg3) , models::info<T4>::unfold(arg4) ));
+    ambient::playout();
 }
 template < typename FP, class T0 , class T1 , class T2 , class T3 , class T4 , class T5 >
 void push( FP l_kernel, FP c_kernel, T0 &arg0 , T1 &arg1 , T2 &arg2 , T3 &arg3 , T4 &arg4 , T5 &arg5 ){
     ambient::controller.push(new models::operation( l_kernel, c_kernel, models::info<T0>::unfold(arg0) , models::info<T1>::unfold(arg1) , models::info<T2>::unfold(arg2) , models::info<T3>::unfold(arg3) , models::info<T4>::unfold(arg4) , models::info<T5>::unfold(arg5) ));
+    ambient::playout();
 }
 template < typename FP, class T0 , class T1 , class T2 , class T3 , class T4 , class T5 , class T6 >
 void push( FP l_kernel, FP c_kernel, T0 &arg0 , T1 &arg1 , T2 &arg2 , T3 &arg3 , T4 &arg4 , T5 &arg5 , T6 &arg6 ){
     ambient::controller.push(new models::operation( l_kernel, c_kernel, models::info<T0>::unfold(arg0) , models::info<T1>::unfold(arg1) , models::info<T2>::unfold(arg2) , models::info<T3>::unfold(arg3) , models::info<T4>::unfold(arg4) , models::info<T5>::unfold(arg5) , models::info<T6>::unfold(arg6) ));
+    ambient::playout();
 }
 
 template < typename FP, class T0 , class T1 , class T2 , class T3 , class T4 , class T5 , class T6 , class T7 >
 void push( FP l_kernel, FP c_kernel, T0 &arg0 , T1 &arg1 , T2 &arg2 , T3 &arg3 , T4 &arg4 , T5 &arg5 , T6 &arg6 , T7 &arg7 ){
     ambient::controller.push(new models::operation( l_kernel, c_kernel, models::info<T0>::unfold(arg0) , models::info<T1>::unfold(arg1) , models::info<T2>::unfold(arg2) , models::info<T3>::unfold(arg3) , models::info<T4>::unfold(arg4) , models::info<T5>::unfold(arg5) , models::info<T6>::unfold(arg6) , models::info<T7>::unfold(arg7) ));
+    ambient::playout();
 }
 template < typename FP, class T0 , class T1 , class T2 , class T3 , class T4 , class T5 , class T6 , class T7 , class T8 >
 void push( FP l_kernel, FP c_kernel, T0 &arg0 , T1 &arg1 , T2 &arg2 , T3 &arg3 , T4 &arg4 , T5 &arg5 , T6 &arg6 , T7 &arg7 , T8 &arg8 ){
     ambient::controller.push(new models::operation( l_kernel, c_kernel, models::info<T0>::unfold(arg0) , models::info<T1>::unfold(arg1) , models::info<T2>::unfold(arg2) , models::info<T3>::unfold(arg3) , models::info<T4>::unfold(arg4) , models::info<T5>::unfold(arg5) , models::info<T6>::unfold(arg6) , models::info<T7>::unfold(arg7) , models::info<T8>::unfold(arg8) ));
+    ambient::playout();
 }
 template < typename FP, class T0 , class T1 , class T2 , class T3 , class T4 , class T5 , class T6 , class T7 , class T8 , class T9 >
 void push( FP l_kernel, FP c_kernel, T0 &arg0 , T1 &arg1 , T2 &arg2 , T3 &arg3 , T4 &arg4 , T5 &arg5 , T6 &arg6 , T7 &arg7 , T8 &arg8 , T9 &arg9 ){
     ambient::controller.push(new models::operation( l_kernel, c_kernel, models::info<T0>::unfold(arg0) , models::info<T1>::unfold(arg1) , models::info<T2>::unfold(arg2) , models::info<T3>::unfold(arg3) , models::info<T4>::unfold(arg4) , models::info<T5>::unfold(arg5) , models::info<T6>::unfold(arg6) , models::info<T7>::unfold(arg7) , models::info<T8>::unfold(arg8) , models::info<T9>::unfold(arg9) ));
+    ambient::playout();
 }
