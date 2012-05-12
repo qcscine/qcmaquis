99c924048708562f1c78528a7138d1b8ed4ec255
dolfim <dolfim@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-05-12 07:10:20 +0000

    fixing mg-dmrg in trunk (for icpc)

--- dmrg/applications/multigrid/mg_meas.h
+++ dmrg/applications/multigrid/mg_meas.h
@@ -12,6 +12,9 @@
 #include "dmrg/utils/DmrgParameters2.h"
 
 #include "types/dense_matrix/dense_matrix.h"
+#include "types/dense_matrix/matrix_interface.hpp"
+#include "types/dense_matrix/resizable_matrix_interface.hpp"
+#include "types/dense_matrix/algorithms.hpp"
 #ifdef USE_MTM
 #include "types/mt_matrix/mt_matrix.h"
 typedef maquis::types::mt_matrix<double> Matrix;
