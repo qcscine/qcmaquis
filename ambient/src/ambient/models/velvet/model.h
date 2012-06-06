56dcd7e9959803d0e02313563c9c0fcc560d8dd2
maquis <maquis@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-06-06 08:57:02 +0000

    some speed up

--- ambient/src/ambient/models/velvet/model.h
+++ ambient/src/ambient/models/velvet/model.h
@@ -16,7 +16,7 @@ namespace ambient { namespace models { namespace velvet {
     public:
         inline model();
         inline void insert(layout* l);
-        inline void add_revision(history* o);
+        inline revision& add_revision(history* o);
         inline bool is_atomic(const history* o);
         inline size_t get_block_lda(history* o);
         inline dim2 get_current_dim(const history* o);
