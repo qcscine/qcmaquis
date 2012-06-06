95cfb3ec0721ab76568301b2cef81f6b1a41f043
maquis <maquis@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-06-06 14:46:03 +0000

    simplification of kernel pp

--- ambient/src/ambient/interface/pp/kernel_inliner.pp.hpp.template.h
+++ ambient/src/ambient/interface/pp/kernel_inliner.pp.hpp.template.h
@@ -2,17 +2,16 @@
 #define ARGS_MAX_LEN 10
 
 #define cleanup_object(z, n, unused)                                                                                 \
-    info<T ## n>::typed::deallocate(o->arguments[n]);
+    info<T ## n>::typed::deallocate<n>(o);
 
 #define creditup_object(z, n, unused)                                                                                \
-    info<T ## n>::typed::weight(o->arguments[n], o); 
+    info<T ## n>::typed::weight<n>(o); 
 
 #define place_revision(z, n, unused)                                                                                 \
-    info<T ## n>::typed::place(o->arguments[n], o);                                                           
+    info<T ## n>::typed::place<n>(o);                                                           
 
 #define extract_arguments(z, n, unused)                                                                              \
-    o->arguments[n] = (void*)info<T ## n>::typed::pointer(arg ## n);                                                 \
-    o->revisions[n] = info<T ## n>::typed::modify(const_cast<T ## n&>(info<T ## n>::typed::dereference(o->arguments[n])), o);
+    info<T ## n>::typed::modify<n>(arg ## n, o);
 
 #define type_arg_list(z, n, pn)                                                                                      \
     BOOST_PP_COMMA_IF(n)                                                                                             \
@@ -20,50 +19,12 @@
 
 #define type_list(z, n, pn)                                                                                          \
     BOOST_PP_COMMA_IF(n)                                                                                             \
-    BOOST_PP_IF(BOOST_PP_EQUAL(n,pn), pinned,)                                                                       \
     T ## n&                                                                                                          
 
 #define arg_list(z, n, pn)                                                                                           \
     BOOST_PP_COMMA_IF(n)                                                                                             \
-    BOOST_PP_IF(BOOST_PP_EQUAL(n,pn), marked,)                                                                       \
-    info<T ## n>::typed::revised(o->arguments[n], o->revisions[n])
+    info<T ## n>::typed::revised<n>(o)
                                                                                                                      
-#define body_tn(z, n, text)                                                                                          \
-template< BOOST_PP_ENUM_PARAMS(TYPES_NUMBER, typename T) , void(*fp)( BOOST_PP_REPEAT(TYPES_NUMBER, type_list, n) )> \
-struct kernel_inliner<void(*)( BOOST_PP_REPEAT(TYPES_NUMBER, type_list, n) ), fp> {                                  \
-                                                                                                                     \
-    static const size_t argc = TYPES_NUMBER;                                                                         \
-                                                                                                                     \
-    template<typename K>                                                                                             \
-    static inline void latch(kernel_dispatch<K>* o, BOOST_PP_REPEAT(TYPES_NUMBER, type_arg_list, n) ){               \
-        BOOST_PP_REPEAT(TYPES_NUMBER, extract_arguments, ~)                                                          \
-        ambient::controller.push(o);                                                                                 \
-    }                                                                                                                \
-    static inline void invoke(sfunctor* o){                                                                          \
-        fp( BOOST_PP_REPEAT(TYPES_NUMBER, arg_list, n) );                                                            \
-    }                                                                                                                \
-    static inline void cleanup(sfunctor* o){                                                                         \
-        BOOST_PP_REPEAT(TYPES_NUMBER, cleanup_object, ~)                                                             \
-    }                                                                                                                \
-    static inline void weight(cfunctor* o){                                                                          \
-        BOOST_PP_REPEAT(TYPES_NUMBER, creditup_object, ~)                                                            \
-    }                                                                                                                \
-    static inline void place(sfunctor* o){                                                                           \
-        BOOST_PP_REPEAT(TYPES_NUMBER, place_revision, ~)                                                             \
-    }                                                                                                                \
-    static inline bool pretend(cfunctor* o){                                                                         \
-        return false;                                                                                                \
-    }                                                                                                                \
-    static inline void l_check_complete(cfunctor* o){                                                                \
-        o->check_complete();                                                                                         \
-    }                                                                                                                \
-    static inline void c_check_complete(cfunctor* o){                                                                \
-        o->check_complete();                                                                                         \
-    }                                                                                                                \
-                                                                                                                     \
-};
-
-
 #ifndef BOOST_PP_IS_ITERATING
 #ifndef AMBIENT_INTERFACE_KERNEL_INLINER_PP
 #define AMBIENT_INTERFACE_KERNEL_INLINER_PP
@@ -80,12 +41,8 @@ struct kernel_inliner<void(*)( BOOST_PP_REPEAT(TYPES_NUMBER, type_list, n) ), fp
 template< BOOST_PP_ENUM_PARAMS(TYPES_NUMBER, typename T) , void(*fp)( BOOST_PP_REPEAT(TYPES_NUMBER, type_list, BOOST_PP_ADD(n,1)) )>
 struct kernel_inliner<void(*)( BOOST_PP_REPEAT(TYPES_NUMBER, type_list, BOOST_PP_ADD(n,1)) ), fp> {
     
-    static const size_t argc = TYPES_NUMBER;
-
-    template<typename K>
-    static inline void latch(kernel_dispatch<K>* o, BOOST_PP_REPEAT(TYPES_NUMBER, type_arg_list, n) ){
+    static inline void latch(cfunctor* o, BOOST_PP_REPEAT(TYPES_NUMBER, type_arg_list, n) ){
         BOOST_PP_REPEAT(TYPES_NUMBER, extract_arguments, ~) 
-        ambient::controller.push(o); 
     }
     static inline void invoke(sfunctor* o){
         fp( BOOST_PP_REPEAT(TYPES_NUMBER, arg_list, BOOST_PP_ADD(n,1)) );
@@ -99,25 +56,6 @@ struct kernel_inliner<void(*)( BOOST_PP_REPEAT(TYPES_NUMBER, type_list, BOOST_PP
     static inline void place(sfunctor* o){
         BOOST_PP_REPEAT(TYPES_NUMBER, place_revision, ~) 
     }
-    static inline bool pretend(cfunctor* o){
-        o->lock();
-        o->workload--;
-        o->unlock();
-        if(!o->workload) return false;
-        return true;
-    }
-    static inline void l_check_complete(cfunctor* o){
-        o->lock();
-        if(o->workload == 1) controller.execute_free_mod(o);
-        else o->workload--;
-        o->unlock();
-    }
-    static inline void c_check_complete(cfunctor* o){
-        o->check_complete();
-    }
-
 };
-
-BOOST_PP_REPEAT(n, body_tn, ~) 
 
 #endif
