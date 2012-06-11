408dbfa75a9cd6720f0e4e4d83446395a7131273
dolfim <dolfim@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-06-11 19:54:44 +0000

    restoring blas in all targets

--- src/dmrg/applications/time_evolution/dmrg_tevol.h
+++ src/dmrg/applications/time_evolution/dmrg_tevol.h
@@ -21,6 +21,7 @@
     #include "types/dense_matrix/dense_matrix.h"
     #include "types/dense_matrix/matrix_interface.hpp"
     #include "types/dense_matrix/resizable_matrix_interface.hpp"
+    #include "types/dense_matrix/dense_matrix_blas.hpp"
     #include "types/dense_matrix/algorithms.hpp"
     typedef maquis::types::dense_matrix<double> matrix;
     typedef maquis::types::dense_matrix<std::complex<double> > cmatrix;
