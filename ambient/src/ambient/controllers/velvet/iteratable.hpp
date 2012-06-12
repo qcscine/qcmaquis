898e8e40b0219d01635e286d8e509b651a93f285
maquis <maquis@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-06-12 17:36:59 +0000

    removing fast_revision

--- ambient/src/ambient/controllers/velvet/iteratable.hpp
+++ ambient/src/ambient/controllers/velvet/iteratable.hpp
@@ -2,24 +2,15 @@ namespace ambient { namespace controllers { namespace velvet {
 
 
     inline layout::entry& slow_revision::operator()(size_t x, size_t y){
-        layout::entry* e = ((revision*)this)->block(x,y);
-        if(e->header == NULL) return ambient::controller.alloc_block(this->get_layout(), x, y);
-        return *e;
+        layout::entry& e = ((revision*)this)->block(x,y);
+        if(e.header == NULL) return ambient::controller.alloc_block(this->get_layout(), x, y);
+        return e;
     }
 
     inline layout& slow_revision::get_layout(){
         return ((revision*)this)->get_layout();
     }
     
-    inline layout::entry& fast_revision::operator()(size_t x, size_t y){
-        // while(!this->valid()) pthread_yield(); // can be done if MPI enabled
-        return *((revision*)this)->block(x,y);
-    }
-
-    inline layout& fast_revision::get_layout(){
-        return ((revision*)this)->get_layout();
-    }
-
     template<class T>
     inline iteratable<T>::iteratable(){
         this->thread_revision_base = (size_t*)calloc(NUM_THREADS, sizeof(size_t));
