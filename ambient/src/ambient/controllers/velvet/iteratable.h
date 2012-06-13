4b693c0c2e2b1d9985b5c5e7674bdb167894556d
maquis <maquis@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-06-13 18:20:10 +0000

    slow_revision -> check_revision

--- ambient/src/ambient/controllers/velvet/iteratable.h
+++ ambient/src/ambient/controllers/velvet/iteratable.h
@@ -9,14 +9,14 @@ namespace ambient { namespace controllers { namespace velvet {
     using ambient::models::velvet::revision;
     using ambient::models::velvet::layout;
 
-    class slow_revision
+    class check_revision
     {
     public:
         inline layout::entry& operator()(size_t x, size_t y); // should be operator
         inline layout& get_layout();
     };
 
-    class reusable_revision
+    class reuse_revision
     {
     public:
         inline layout::entry& operator()(size_t x, size_t y);
@@ -38,11 +38,14 @@ namespace ambient { namespace controllers { namespace velvet {
         inline revision& ui_l_revision_1() const { // only for debug for now
             return *this->content[this->thread_revision_base[GET_TID] + 1]; 
         }
-        inline slow_revision& ui_c_revision_0() const {
-            return *(slow_revision*)this->content[this->thread_revision_base[GET_TID]]; 
+        inline check_revision& ui_c_revision_0() const {
+            return *(check_revision*)this->content[this->thread_revision_base[GET_TID]]; 
         }
-        inline slow_revision& ui_c_revision_1() const { 
-            return *(slow_revision*)this->content[this->thread_revision_base[GET_TID] + 1]; 
+        inline check_revision& ui_c_revision_1() const { 
+            return *(check_revision*)this->content[this->thread_revision_base[GET_TID] + 1]; 
+        }
+        inline reuse_revision& ui_r_revision_1() const { 
+            return *(reuse_revision*)this->content[this->thread_revision_base[GET_TID] + 1]; 
         }
         inline size_t get_thread_revision_base() const;
         inline void set_thread_revision_base(size_t);
--- ambient/src/ambient/controllers/velvet/iteratable.hpp
+++ ambient/src/ambient/controllers/velvet/iteratable.hpp
@@ -1,17 +1,17 @@
 namespace ambient { namespace controllers { namespace velvet {
 
 
-    inline layout::entry& slow_revision::operator()(size_t x, size_t y){
+    inline layout::entry& check_revision::operator()(size_t x, size_t y){
         layout::entry& e = ((revision*)this)->block(x,y);
         if(!e.valid()) return ambient::controller.alloc_block(this->get_layout(), x, y);
         return e;
     }
 
-    inline layout& slow_revision::get_layout(){
+    inline layout& check_revision::get_layout(){
         return ((revision*)this)->get_layout();
     }
 
-    inline layout::entry& reusable_revision::operator()(size_t x, size_t y){
+    inline layout::entry& reuse_revision::operator()(size_t x, size_t y){
         layout::entry& e = ((revision*)this)->block(x,y);
         if(!e.valid()){
             layout::entry& parent = this->get_parent()->block(x,y);
@@ -21,11 +21,11 @@ namespace ambient { namespace controllers { namespace velvet {
         return e;
     }
 
-    inline layout& reusable_revision::get_layout(){
+    inline layout& reuse_revision::get_layout(){
         return ((revision*)this)->get_layout();
     }
 
-    inline revision* reusable_revision::get_parent(){
+    inline revision* reuse_revision::get_parent(){
         return ((revision*)this)->get_parent();
     }
     
