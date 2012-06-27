87602c7c15b4844fb6b1efef893d4f34f5227e42
dolfim <dolfim@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-06-27 14:48:43 +0000

    missing commit (should go together with r2281)

--- maquis_utils/function_objects.h
+++ maquis_utils/function_objects.h
@@ -9,23 +9,32 @@
 #ifndef MAQUIS_FUNCTION_OBJECTS_H
 #define MAQUIS_FUNCTION_OBJECTS_H
 
+#include <alps/numeric/conj.hpp>
+
 namespace utils {
-    inline float conj(float v) { return v; }
-    inline double conj(double v) { return v; }
-    using std::conj;
+    using alps::numeric::conj;
     
 #define DEFINE_FUNCTION_OBJECT(name, return_type, arg_type) \
 struct functor_##name { template<class T> return_type operator() (arg_type t) { return name(t); } };
+
+#define DEFINE_VOID_FUNCTION_OBJECT(name, arg_type) \
+struct functor_##name { template<class T> void operator() (arg_type t) { name(t); } };
     
     DEFINE_FUNCTION_OBJECT(trace, typename maquis::traits::scalar_type<T>::type, T const &)
     DEFINE_FUNCTION_OBJECT(transpose, T, T const &)
+    DEFINE_VOID_FUNCTION_OBJECT(transpose_inplace, T &)
     DEFINE_FUNCTION_OBJECT(conj, T, T)
+    DEFINE_VOID_FUNCTION_OBJECT(conj_inplace, T&)
     DEFINE_FUNCTION_OBJECT(conjugate, T, T const &)
+    DEFINE_FUNCTION_OBJECT(adjoint, T, T const &)
+    DEFINE_VOID_FUNCTION_OBJECT(adjoint_inplace, T &)
     DEFINE_FUNCTION_OBJECT(sqrt, T, T const &)
+    DEFINE_VOID_FUNCTION_OBJECT(sqrt_inplace, T &)
     DEFINE_FUNCTION_OBJECT(exp, T, T const &)
     DEFINE_FUNCTION_OBJECT(size_of, std::size_t, T const &)
     
 #undef DEFINE_FUNCTION_OBJECT
+#undef DEFINE_VOID_FUNCTION_OBJECT
     
     template<class T>
     struct constant
