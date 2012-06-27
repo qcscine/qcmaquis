1599821afcb81a439957e2bf5bfd491690138e5f
dolfim <dolfim@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-06-27 17:31:40 +0000

    missing files in prev

--- maquis_utils/data_collector.cpp
+++ maquis_utils/data_collector.cpp
@@ -4,4 +4,5 @@
 DCOLLECTOR_CREATE(gemm_collector, "GEMM sizes", 100)
 DCOLLECTOR_CREATE(svd_collector, "SVD sizes", 100)
 DCOLLECTOR_CREATE(num_blocks_gemm_collector, "GEMM num blocks", 50)
+DCOLLECTOR_CREATE(num_blocks_svd_collector, "SVD num blocks", 50)
 
