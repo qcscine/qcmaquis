10131d58051c4d38a8d405a72fd3c6d62bfa3a62
akosenko <akosenko@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-07-11 14:15:43 +0000

    cleanup #2

--- src/dmrg/framework/dmrg/block_matrix/detail/ambient.hpp
+++ src/dmrg/framework/dmrg/block_matrix/detail/ambient.hpp
@@ -75,4 +75,43 @@ namespace maquis { namespace dmrg { namespace detail {
 } } }
 
 #undef ATOMIC
+
+#include "types/utils/traits.hpp"
+
+namespace maquis { namespace traits {
+
+    template<class T> struct scalar_type <ambient::numeric::matrix<T> > { typedef typename ambient::numeric::matrix<T>::scalar_type type; };
+    template<class T> struct scalar_type <ambient::numeric::diagonal_matrix<T> > { typedef typename ambient::numeric::matrix<T>::scalar_type type; };
+    template<class T> struct real_type<ambient::numeric::matrix<T> > { typedef typename ambient::numeric::matrix<T>::real_type type; };
+    template<class T> struct real_type<ambient::numeric::diagonal_matrix<T> > { typedef typename ambient::numeric::matrix<T>::real_type type; };
+
+    template <typename T>
+    struct transpose_view< ambient::numeric::matrix<T> > { typedef ambient::numeric::matrix<T> type; }; // to fix
+
+    template <typename T>
+    struct transpose_view< ambient::numeric::diagonal_matrix<T> > { typedef ambient::numeric::diagonal_matrix<T> type; };
+
+} }
+
+namespace alps { namespace numeric {
+
+    template<typename T> 
+    struct associated_diagonal_matrix< ambient::numeric::matrix<T> > {
+        typedef ambient::numeric::diagonal_matrix<T> type;
+    };
+    template<typename T> 
+    struct associated_real_diagonal_matrix< ambient::numeric::matrix<T> > {
+        typedef ambient::numeric::diagonal_matrix<typename maquis::traits::real_type<T>::type> type;
+    };
+    template<typename T> 
+    struct associated_vector< ambient::numeric::matrix<T> > {
+        typedef std::vector<T> type;
+    };
+    template<typename T> 
+    struct associated_real_vector< ambient::numeric::matrix<T> > {
+        typedef std::vector<typename maquis::traits::real_type<T>::type> type;
+    };
+    
+} }
+
 #endif
