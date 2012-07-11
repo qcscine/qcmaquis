f1099ffb42e138a4599251c8e2b431a832ce2e39
akosenko <akosenko@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-07-11 14:19:42 +0000

    traits

--- src/types/src/types/utils/traits.hpp
+++ src/types/src/types/utils/traits.hpp
@@ -3,47 +3,13 @@
 
 #include <alps/numeric/real.hpp>
 #include <alps/numeric/matrix/matrix_traits.hpp>
-#include <boost/numeric/bindings/tag.hpp>
-
-namespace ambient { namespace numeric {
-
-    template<typename T> class matrix;
-    template<typename T> class diagonal_matrix;
-
-} }
-
-
-namespace maquis { namespace types {
-
-    struct Transpose   { 
-        typedef boost::numeric::bindings::tag::transpose type;
-        template<class Matrix> static typename Matrix::size_type first(Matrix const & m){ return m.num_cols(); }
-        template<class Matrix> static typename Matrix::size_type second(Matrix const & m){ return m.num_rows(); }
-        template<class Matrix> static Matrix eval(const Matrix& m){ return transpose(m); }
-        static const char* code(){ return "T"; } 
-    };
-    struct NoTranspose { 
-        typedef boost::numeric::bindings::tag::no_transpose type;
-        template<class Matrix> static typename Matrix::size_type first(Matrix const & m){ return m.num_rows(); }
-        template<class Matrix> static typename Matrix::size_type second(Matrix const & m){ return m.num_cols(); }
-        template<class Matrix> static const Matrix& eval(const Matrix& m){ return m; }
-        static const char* code(){ return "N"; } 
-    };
-
-} }
 
 namespace maquis { namespace traits {
 
     template<class T> struct scalar_type { typedef typename T::value_type type; };
-    template<class T> struct scalar_type <ambient::numeric::matrix<T> > { typedef typename ambient::numeric::matrix<T>::scalar_type type; };
-    template<class T> struct scalar_type <ambient::numeric::diagonal_matrix<T> > { typedef typename ambient::numeric::matrix<T>::scalar_type type; };
-
     template<class T> struct real_type { typedef typename real_type<typename T::value_type>::type type; };
     template<>        struct real_type<double> { typedef double type; };
     template<class T> struct real_type<std::complex<T> > { typedef T type; };
-    template<class T> struct real_type<ambient::numeric::matrix<T> > { typedef typename ambient::numeric::matrix<T>::real_type type; };
-    template<class T> struct real_type<ambient::numeric::diagonal_matrix<T> > { typedef typename ambient::numeric::matrix<T>::real_type type; };
-
     template<class T> struct real_identity { static const T value; };
     template<class T> struct imag_identity { static const T value; };
     template<class T> struct real_identity<std::complex<T> > { static const std::complex<T> value; };
@@ -54,42 +20,8 @@ namespace maquis { namespace traits {
     template<class T> const std::complex<T> imag_identity<std::complex<T> >::value = std::complex<T>(0,1);
 
     template <class Matrix> struct transpose_view { typedef Matrix type; };
-
-    template <class Matrix> struct transpose{ typedef typename transpose_view<Matrix>::type type; };
+    template <class Matrix> struct transpose { typedef typename transpose_view<Matrix>::type type; };
     template <class Matrix> struct transpose< typename transpose_view<Matrix>::type > { typedef Matrix type; };
-    template <typename T>   struct transpose< alps::numeric::diagonal_matrix<T> > { typedef alps::numeric::diagonal_matrix<T> type; };
-    template <typename T>   struct transpose< ambient::numeric::diagonal_matrix<T> > { typedef ambient::numeric::diagonal_matrix<T> type; };
-    
-    template<class Matrix> struct is_transpose {
-        typedef boost::false_type type;
-        static bool const value = false;
-    };
-    template<class Matrix> struct is_transpose<transpose<Matrix> > {
-        typedef boost::true_type type;
-        static bool const value = true;
-    };
-    
-} }
-
-namespace alps { namespace numeric {
-
-    template<typename T> 
-    struct associated_diagonal_matrix< ambient::numeric::matrix<T> > {
-        typedef ambient::numeric::diagonal_matrix<T> type;
-    };
-    template<typename T> 
-    struct associated_real_diagonal_matrix< ambient::numeric::matrix<T> > {
-        typedef ambient::numeric::diagonal_matrix<typename maquis::traits::real_type<T>::type> type;
-    };
-    template<typename T> 
-    struct associated_vector< ambient::numeric::matrix<T> > {
-        typedef std::vector<T> type;
-    };
-    template<typename T> 
-    struct associated_real_vector< ambient::numeric::matrix<T> > {
-        typedef std::vector<typename maquis::traits::real_type<T>::type> type;
-    };
-    
 } }
 
 #endif
