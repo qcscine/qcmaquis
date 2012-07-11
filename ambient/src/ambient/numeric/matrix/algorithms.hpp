3c6a8a928f63b61021e4b36c711f1f6c17b3069e
akosenko <akosenko@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-07-11 20:53:31 +0000

    transposed_view in ambient::numeric

--- ambient/src/ambient/numeric/matrix/algorithms.hpp
+++ ambient/src/ambient/numeric/matrix/algorithms.hpp
@@ -12,20 +12,27 @@
 #define scalar_type     typename matrix<T>::scalar_type
 #define difference_type typename matrix<T>::difference_type
 
+// Functions used by the ground state algorithm: //
+//
+// norm_square
+// overlap
+// transpose
+// transpose_inplace
+// trace
+// add_inplace
+// sub_inplace
+// resize
+// mul_inplace (scalar)
+// div_inplace
+// copy
+// gemm (matrix-matrix)
+// gemm (matrix-diagonal)
+// svd
+// heev
+
 namespace ambient { namespace numeric {
 
-    // {{{ matrix advanced algorithms
-
-    template <typename T>
-    inline real_type norm_square(const matrix<T>& a){ real_type norm(0); ATOMIC(a.atomic(), scalar_norm, a, norm); return norm; } // gs
-
-    template <typename T>
-    inline scalar_type overlap(const matrix<T>& a, const matrix<T>& b){ scalar_type overlap(0); ATOMIC(a.atomic(), overlap, a, b, overlap); return overlap; } // gs
-
-    // }}}
-
     // {{{ matrix operators (free functions)
-
     template <typename T>
     std::ostream& operator << (std::ostream& o, matrix<T> const& m){
         for(size_type i=0; i< m.num_rows(); ++i){
@@ -50,12 +57,27 @@ namespace ambient { namespace numeric {
     template<typename T, typename T2> inline const matrix<T> operator * (const T2& lhs, matrix<T> rhs){ return (rhs *= lhs); }
     template<typename T> inline size_type num_rows(const matrix<T>& m){ return m.num_rows(); }
     template<typename T> inline size_type num_cols(const matrix<T>& m){ return m.num_cols(); }
+    template<typename T> inline size_type num_rows(const transpose_view<matrix<T> >& m){ return transpose_view<matrix<T> >::rows(*m.impl); }
+    template<typename T> inline size_type num_cols(const transpose_view<matrix<T> >& m){ return transpose_view<matrix<T> >::cols(*m.impl); }
     template<typename T> inline void resize(matrix<T>& m, size_t rows, size_t cols){ m.resize(rows, cols); }
     template<typename T> inline scalar_type trace(const matrix<T>& m){ return m.trace(); }
 
+    template <typename T>
+    inline real_type norm_square(const matrix<T>& a){ 
+        real_type norm(0); 
+        ATOMIC(a.atomic(), scalar_norm, a, norm); 
+        return norm; 
+    }
+
+    template <typename T>
+    inline scalar_type overlap(const matrix<T>& a, const matrix<T>& b){ 
+        scalar_type overlap(0); 
+        ATOMIC(a.atomic(), overlap, a, b, overlap); 
+        return overlap; 
+    }
+
     template<typename T>
     inline void transpose_inplace(matrix<T>& a){
-        // gs
         matrix<T> t(a.num_cols(), a.num_rows());
         ATOMIC(a.atomic(), transpose_out, a, t);
         a.swap(t);
@@ -63,7 +85,6 @@ namespace ambient { namespace numeric {
 
     template<typename T>
     inline matrix<T> transpose(const matrix<T>& a){
-        // gs
         matrix<T> t(a.num_cols(), a.num_rows());
         ATOMIC(a.atomic(), transpose_out, a, t);
         return t;
@@ -95,14 +116,157 @@ namespace ambient { namespace numeric {
     template<typename T>
     inline matrix<T> exp(const matrix<T>& m, const T& alfa = 1.){
         printf("Error: Not checked <- EXP\n");
-        typename alps::numeric::associated_real_diagonal_matrix< matrix<T> >::type evals(m.num_rows());
+        diagonal_matrix<double> evals(m.num_rows());
         matrix<T> evecs = matrix<T>();
         heev(m, evecs, evals);
         return (evecs * exp(evals, alfa))*conj(transpose(evecs));
     }
 
-    // {{{ strassen matrix multiplication algorithm
+    template<class MatrixViewA, class MatrixViewB, typename T>
+    inline void gemm(const MatrixViewA& a, const MatrixViewB& b, matrix<T>& c){ 
+        ambient::push< kernels::gemm_general_atomic<MatrixViewA,MatrixViewB,T> >(a, b, c); 
+    }
 
+    template<class MatrixViewA, typename T, typename D>
+    inline void gemm(const MatrixViewA& a, const diagonal_matrix<D>& b, matrix<T>& c){ 
+        ambient::push< kernels::gemm_diagonal_rhs<MatrixViewA,T,D> >(a, b, c); 
+    }
+
+    template<class MatrixViewB, typename T, typename D>
+    inline void gemm(const diagonal_matrix<D>& a, const MatrixViewB& b, matrix<T>& c){ 
+        ambient::push< kernels::gemm_diagonal_lhs<MatrixViewB,T,D> >(a, b, c); 
+    }
+
+    template<typename T>
+    inline void svd(matrix<T> a, matrix<T>& u, matrix<T>& vt, diagonal_matrix<double>& s){
+        int m = num_rows(a);
+        int n = num_cols(a);
+        int k = std::min(m,n);
+        u.resize(m, k);
+        vt.resize(k, n);
+        s.resize(k, k);
+        ATOMIC(a.atomic(), svd, a, u, vt, s);
+    }
+
+    template<typename T>
+    inline void heev(matrix<T> a, matrix<T>& evecs, diagonal_matrix<double>& evals){
+        assert(num_rows(a) == num_cols(a) && num_rows(evals) == num_rows(a));
+        ATOMIC(a.atomic(), heev, a, evals); // destoys U triangle of M
+        evecs.swap(a);
+    }
+
+    template<typename T>
+    inline void syev(const matrix<T>& a, matrix<T>& evecs, diagonal_matrix<double>& evals){
+        heev(a, evecs, evals);
+    }
+
+    template<typename T>
+    inline void qr(const matrix<T>& m, matrix<T>& q, matrix<T>& r){
+        assert(false); printf("Error: Not implemented <- QR\n");
+    }
+
+    template<typename T, class G>
+    inline void generate(matrix<T>& m, G g){ m.fill_random(); } // warning: G isn't used
+    // }}}
+
+// {{{ implementation-specific type-nested algorithms //
+
+namespace algorithms {
+
+    template<typename T>
+    inline void fill_identity(matrix_impl<T>& m){
+        ATOMIC(m.atomic(), init_identity, m);
+    }
+
+    template<typename T>
+    inline void fill_random(matrix_impl<T>& m){
+        ATOMIC(m.atomic(), init_random, m);
+    }
+
+    template<typename T>
+    inline void fill_value(matrix_impl<T>& m, T value){
+        if(value == 0.) return; // matrices are 0s by default
+        ATOMIC(m.atomic(), init_value, m, value);
+    }
+
+    template<typename T>
+    inline void remove_rows(matrix_impl<T>& m, size_type i, difference_type k){
+        assert(false); printf("Error: Not checked <- REMOVE ROWS\n");
+        ambient::push< kernels::remove_rows<T> >(m, i, k);
+    }
+
+    template<typename T>
+    inline void remove_cols(matrix_impl<T>& m, size_type j, difference_type k){
+        assert(false); printf("Error: Not checked <- REMOVE COLS\n");
+        ambient::push< kernels::remove_cols<T> >(m, j, k);
+    }
+
+    template<typename T>
+    inline void resize(matrix_impl<T>& r, size_type rows, size_type cols, matrix_impl<T>& m, size_type orows, size_type ocols){
+        ATOMIC(m.atomic(), resize, r, m, std::min(rows, orows), std::min(cols, ocols));
+    }
+
+    template<typename T>
+    inline void conj_inplace(matrix_impl<T>& m){
+        // gs (doubles)
+        // does nothing for now
+    }
+
+    template<typename T>
+    inline void transpose_inplace(matrix_impl<T>& m){
+        assert(false); printf("Error: Not checked <- INPLACE TRANSPOSE\n");
+        assert(m.num_rows() == m.num_cols()); // current limitation
+        ambient::push< kernels::transpose<T> >(m);
+    }
+
+    template <typename T>
+    inline scalar_type trace(const matrix_impl<T>& m){
+        scalar_type trace(0);
+        ATOMIC(m.atomic(), trace, m, trace);
+        return trace;
+    }
+
+    template <typename T>
+    inline void add_inplace(matrix_impl<T>& m, const matrix_impl<T>& rhs){ 
+        ATOMIC(m.atomic(), add, m, rhs); 
+    }
+
+    template <typename T>
+    inline void sub_inplace(matrix_impl<T>& m, const matrix_impl<T>& rhs){ 
+        ATOMIC(m.atomic(), sub, m, rhs); 
+    }
+
+    template <typename T>
+    inline void mul_inplace(matrix_impl<T>& m, const matrix<T>& rhs){
+        assert(false); printf("Error: Not checked <- GEMM INPLACE\n");
+        ambient::push< kernels::gemm_inplace<T> >(m, rhs);
+    }
+
+    template <typename T>
+    inline void mul_inplace(matrix_impl<T>& m, const diagonal_matrix<T>& rhs){
+        assert(false); printf("Error: Not checked <- GEMM DIAG INPLACE\n");
+        //ambient::push< kernels::gemm_diag_inplace<T> >(m, rhs);
+    }
+
+    template <typename T>
+    inline void mul_inplace(matrix_impl<T>& a, const scalar_type& rhs) { 
+        ATOMIC(a.atomic(), scale, a, rhs); 
+    }
+
+    template <typename T>
+    inline void div_inplace(matrix_impl<T>& a, const scalar_type& rhs) { 
+        ATOMIC(a.atomic(), scale_inverse, a, rhs); 
+    }
+
+    template<typename T>
+    inline void copy(matrix_impl<T>& ac, const matrix_impl<T>& a){ 
+        ATOMIC(a.atomic(), copy, ac, a); 
+    }
+
+} 
+// }}} end of implementation specific type-nested algorithms //
+
+    // {{{ strassen matrix multiplication algorithm
     // {{{ strassen multiplication supplementary functions
     /*
     template<typename T>
@@ -204,134 +368,6 @@ namespace ambient { namespace numeric {
         gemm_strassen(a, 0, 0, b, 0, 0, c, 0, 0, n);
     }*/
     // }}}
-
-    template<class Tag1, class Tag2, typename T>
-    inline void gemm(const matrix<T>& a, const matrix<T>& b, matrix<T>& c){ assert(num_cols(a) == num_rows(b)); ambient::push< kernels::gemm_general_atomic<Tag1,Tag2,T> >(a, b, c); } // gs
-
-    template<class Tag1, class Tag2, typename T, typename D>
-    inline void gemm(const matrix<T>& a, const diagonal_matrix<D>& b, matrix<T>& c){ assert(num_cols(a) == num_rows(b)); ambient::push< kernels::gemm_diagonal_rhs<Tag1,Tag2,T,D> >(a, b, c); } // gs
-
-    template<class Tag1, class Tag2, typename T, typename D>
-    inline void gemm(const diagonal_matrix<D>& a, const matrix<T>& b, matrix<T>& c){ assert(num_cols(a) == num_rows(b)); ambient::push< kernels::gemm_diagonal_lhs<Tag1,Tag2,T,D> >(a, b, c); } // gs
-
-    template<typename T>
-    inline void svd(matrix<T> a, matrix<T>& u, matrix<T>& vt, typename alps::numeric::associated_real_diagonal_matrix<matrix<T> >::type& s){ // gs
-        int m = num_rows(a);
-        int n = num_cols(a);
-        int k = std::min(m,n);
-        u.resize(m, k);
-        vt.resize(k, n);
-        s.resize(k, k);
-        ATOMIC(a.atomic(), svd, a, u, vt, s);
-    }
-
-    template<typename T>
-    inline void heev(matrix<T> a, matrix<T>& evecs, typename alps::numeric::associated_real_diagonal_matrix< matrix<T> >::type& evals){ // gs
-        assert(num_rows(a) == num_cols(a) && num_rows(evals) == num_rows(a));
-        ATOMIC(a.atomic(), heev, a, evals); // destoys U triangle of M
-        evecs.swap(a);
-    }
-
-    template<typename T>
-    inline void syev(const matrix<T>& a, matrix<T>& evecs, typename alps::numeric::associated_real_diagonal_matrix< matrix<T> >::type& evals){
-        heev(a, evecs, evals);
-    }
-
-    template<typename T>
-    inline void qr(const matrix<T>& m, matrix<T>& q, matrix<T>& r){
-        assert(false); printf("Error: Not implemented <- QR\n");
-    }
-
-    template<typename T, class G>
-    inline void generate(matrix<T>& m, G g){ m.fill_random(); } // warning: G isn't used
-    // }}}
-
-// {{{ implementation-specific type-nested algorithms //
-
-namespace algorithms {
-
-    template<typename T>
-    inline void fill_identity(matrix_impl<T>& m){
-        ATOMIC(m.atomic(), init_identity, m);
-    }
-
-    template<typename T>
-    inline void fill_random(matrix_impl<T>& m){
-        ATOMIC(m.atomic(), init_random, m);
-    }
-
-    template<typename T>
-    inline void fill_value(matrix_impl<T>& m, T value){
-        if(value == 0.) return; // matrices are 0s by default
-        ATOMIC(m.atomic(), init_value, m, value);
-    }
-
-    template<typename T>
-    inline void remove_rows(matrix_impl<T>& m, size_type i, difference_type k){
-        assert(false); printf("Error: Not checked <- REMOVE ROWS\n");
-        ambient::push< kernels::remove_rows<T> >(m, i, k);
-    }
-
-    template<typename T>
-    inline void remove_cols(matrix_impl<T>& m, size_type j, difference_type k){
-        assert(false); printf("Error: Not checked <- REMOVE COLS\n");
-        ambient::push< kernels::remove_cols<T> >(m, j, k);
-    }
-
-    template<typename T>
-    inline void resize(matrix_impl<T>& r, size_type rows, size_type cols, matrix_impl<T>& m, size_type orows, size_type ocols){ // gs
-        ATOMIC(m.atomic(), resize, r, m, std::min(rows, orows), std::min(cols, ocols));
-    }
-
-    template<typename T>
-    inline void conj_inplace(matrix_impl<T>& m){
-        // gs (doubles)
-        // does nothing for now
-    }
-
-    template<typename T>
-    inline void transpose_inplace(matrix_impl<T>& m){
-        assert(false); printf("Error: Not checked <- INPLACE TRANSPOSE\n");
-        assert(m.num_rows() == m.num_cols()); // current limitation
-        ambient::push< kernels::transpose<T> >(m);
-    }
-
-    template <typename T>
-    inline scalar_type trace(const matrix_impl<T>& m){
-        // gs
-        scalar_type trace(0);
-        ATOMIC(m.atomic(), trace, m, trace);
-        return trace;
-    }
-
-    template <typename T>
-    inline void add_inplace(matrix_impl<T>& m, const matrix_impl<T>& rhs){ ATOMIC(m.atomic(), add, m, rhs); } // gs
-
-    template <typename T>
-    inline void sub_inplace(matrix_impl<T>& m, const matrix_impl<T>& rhs){ ATOMIC(m.atomic(), sub, m, rhs); } // gs
-
-    template <typename T>
-    inline void scale_inplace(matrix_impl<T>& a, const scalar_type& rhs) { ATOMIC(a.atomic(), scale, a, rhs); } // gs
-
-    template <typename T>
-    inline void scale_inverse_inplace(matrix_impl<T>& a, const scalar_type& rhs) { ATOMIC(a.atomic(), scale_inverse, a, rhs); } // gs
-
-    template<typename T>
-    inline void copy(matrix_impl<T>& ac, const matrix_impl<T>& a){ ATOMIC(a.atomic(), copy, ac, a); } // gs
-
-    template <typename T>
-    inline void gemm_inplace(matrix_impl<T>& m, const matrix_impl<T>& rhs) {
-        assert(false); printf("Error: Not checked <- GEMM INPLACE\n");
-        ambient::push< kernels::gemm_inplace<T> >(m, rhs);
-    }
-
-    template <typename T>
-    inline void gemm_diag_inplace(matrix_impl<T>& m, const matrix_impl<T>& rhs_diag) {
-        assert(false); printf("Error: Not checked <- GEMM DIAG INPLACE\n");
-        //ambient::push< kernels::gemm_diag_inplace<T> >(m, rhs);
-    }
-} 
-// }}} end of implementation specific type-nested algorithms //
 
 } } // namespace ambient::numeric
 
