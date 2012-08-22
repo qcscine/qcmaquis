8b5ecbf9f498b993de9e91ca76765f0728bad122
akosenko <akosenko@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-08-22 14:12:25 +0000

    fixing traits for tiles

--- dmrg/framework/dmrg/block_matrix/detail/ambient.hpp
+++ dmrg/framework/dmrg/block_matrix/detail/ambient.hpp
@@ -16,13 +16,17 @@
 
 namespace maquis { namespace traits {
 
-    template<class T> struct scalar_type <ambient::numeric::matrix<T> > { typedef typename ambient::numeric::matrix<T>::scalar_type type; };
-    template<class T> struct scalar_type <ambient::numeric::diagonal_matrix<T> > { typedef typename ambient::numeric::matrix<T>::scalar_type type; };
-    template<class T> struct real_type <ambient::numeric::matrix<T> > { typedef typename ambient::numeric::matrix<T>::real_type type; };
-    template<class T> struct real_type <ambient::numeric::diagonal_matrix<T> > { typedef typename ambient::numeric::matrix<T>::real_type type; };
+    template<typename T> struct scalar_type <ambient::numeric::matrix<T> > { typedef typename ambient::numeric::matrix<T>::scalar_type type; };
+    template<typename T> struct scalar_type <ambient::numeric::diagonal_matrix<T> > { typedef typename ambient::numeric::matrix<T>::scalar_type type; };
+    template<typename T> struct real_type <ambient::numeric::matrix<T> > { typedef typename ambient::numeric::matrix<T>::real_type type; };
+    template<typename T> struct real_type <ambient::numeric::diagonal_matrix<T> > { typedef typename ambient::numeric::matrix<T>::real_type type; };
 
-    template <typename T> struct transpose_view< ambient::numeric::matrix<T> > { typedef typename ambient::numeric::transpose_view<ambient::numeric::matrix<T> > type; };
-    template <typename T> struct transpose_view< ambient::numeric::diagonal_matrix<T> > { typedef ambient::numeric::diagonal_matrix<T> type; };
+    template<typename T> struct transpose_view< ambient::numeric::matrix<T> > { typedef typename ambient::numeric::transpose_view<ambient::numeric::matrix<T> > type; };
+    template<typename T> struct transpose_view< ambient::numeric::diagonal_matrix<T> > { typedef ambient::numeric::diagonal_matrix<T> type; };
+
+    template<class M> struct scalar_type <ambient::numeric::tiles<M> > { typedef typename scalar_type<M>::type type; };
+    template<class M> struct real_type <ambient::numeric::tiles<M> > { typedef typename real_type<M>::type type; };
+    template<class M> struct transpose_view< ambient::numeric::tiles<M> > { typedef typename ambient::numeric::tiles< typename transpose_view<M>::type > type; };
 
 } }
 
@@ -32,6 +36,11 @@ namespace alps { namespace numeric {
     template<typename T> struct associated_diagonal_matrix< ambient::numeric::matrix<T> > { typedef ambient::numeric::diagonal_matrix<T> type; };
     template<typename T> struct associated_real_diagonal_matrix< ambient::numeric::matrix<T> > { typedef ambient::numeric::diagonal_matrix<typename maquis::traits::real_type<T>::type> type; };
     template<typename T> struct associated_real_vector< ambient::numeric::matrix<T> > { typedef std::vector<typename maquis::traits::real_type<T>::type> type; };
+
+    template<class M> struct associated_vector< ambient::numeric::tiles<M> > { typedef ambient::numeric::tiles< typename associated_vector<M>::type > type; };
+    template<class M> struct associated_real_vector< ambient::numeric::tiles<M> > { typedef ambient::numeric::tiles< typename associated_real_vector<M>::type > type; };
+    template<class M> struct associated_diagonal_matrix< ambient::numeric::tiles<M> > { typedef ambient::numeric::tiles< typename associated_diagonal_matrix<M>::type > type; };
+    template<class M> struct associated_real_diagonal_matrix< ambient::numeric::tiles<M> > { typedef ambient::numeric::tiles< typename associated_real_diagonal_matrix<M>::type > type; };
     
 } }
 
