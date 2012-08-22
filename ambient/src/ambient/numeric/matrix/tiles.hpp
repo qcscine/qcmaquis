36cad631339d020ad38dc575c03b61e4e998e326
akosenko <akosenko@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-08-22 22:25:40 +0000

    diagonal tiles

--- ambient/src/ambient/numeric/matrix/tiles.hpp
+++ ambient/src/ambient/numeric/matrix/tiles.hpp
@@ -48,12 +48,12 @@ namespace ambient { namespace numeric {
 
         for(int j = 1; j < nb; j++){
             for(int i = 1; i < mb; i++) 
-                this->data.push_back(new Matrix(TILE_SIZE, TILE_SIZE));
-            this->data.push_back(new Matrix(tailm, TILE_SIZE));
+                this->data.push_back(new Matrix(TILE_SIZE, TILE_SIZE, init_value));
+            this->data.push_back(new Matrix(tailm, TILE_SIZE, init_value));
         }
         for(int i = 1; i < mb; i++) 
-            this->data.push_back(new Matrix(TILE_SIZE, tailn));
-        this->data.push_back(new Matrix(tailm, tailn));
+            this->data.push_back(new Matrix(TILE_SIZE, tailn, init_value));
+        this->data.push_back(new Matrix(tailm, tailn, init_value));
     }
 
     template <class Matrix>
@@ -187,6 +187,112 @@ namespace ambient { namespace numeric {
     inline const value_type& tiles<Matrix>::operator() (size_type i, size_type j) const {
         int mb = __a_ceil(rows/TILE_SIZE);
         return (*this->data[mb*(int)(j/TILE_SIZE) + (int)(i/TILE_SIZE)])(i % TILE_SIZE, j % TILE_SIZE);
+    }
+
+} }
+
+#undef size_type
+#undef value_type
+#undef scalar_type
+
+#define size_type   typename tiles<diagonal_matrix<T> >::size_type
+#define value_type  typename tiles<diagonal_matrix<T> >::value_type
+#define scalar_type typename tiles<diagonal_matrix<T> >::scalar_type
+
+namespace ambient { namespace numeric {
+
+    template<typename T>
+    inline void* tiles<diagonal_matrix<T> >::operator new (size_t size){
+        return ambient::static_memory::malloc<tiles<diagonal_matrix<T> > >();
+    }
+
+    template<typename T>
+    inline void tiles<diagonal_matrix<T> >::operator delete (void* ptr){
+        ambient::static_memory::free<tiles<diagonal_matrix<T> > >(ptr); 
+    }
+
+    template <typename T>
+    inline tiles<diagonal_matrix<T> >::tiles(size_type size, value_type init_value)
+    : size(size)
+    {
+        int nb = __a_ceil(size/TILE_SIZE);
+        int tailm = __a_mod(size, TILE_SIZE);
+        this->data.reserve(nb);
+
+        for(int i = 1; i < nb; i++) 
+            this->data.push_back(new diagonal_matrix<T>(TILE_SIZE, init_value));
+        this->data.push_back(new diagonal_matrix<T>(tailm, init_value));
+    }
+
+    template <typename T>
+    inline tiles<diagonal_matrix<T> >::tiles(const tiles& m){
+        int nb = __a_ceil(m.num_rows()/TILE_SIZE);
+        for(int k = 0; k < nb; k++) this->data.push_back(new diagonal_matrix<T>(m[k]));
+        this->size = m.num_rows();
+    }
+    
+    template <typename T>
+    tiles<diagonal_matrix<T> >& tiles<diagonal_matrix<T> >::operator = (const tiles& rhs){
+        tiles c(rhs);
+        this->swap(c);
+        return *this;
+    }
+
+    template <typename T>
+    tiles<diagonal_matrix<T> >::~tiles(){
+        int size = this->data.size();
+        for(int i = 0; i < size; i++) delete this->data[i];
+    }
+
+    template<typename T>
+    inline size_type tiles<diagonal_matrix<T> >::num_rows() const {
+        return this->size;
+    }
+
+    template<typename T>
+    inline size_type tiles<diagonal_matrix<T> >::num_cols() const {
+        return this->size;
+    }
+
+    template<typename T>
+    inline void tiles<diagonal_matrix<T> >::swap(tiles& r){
+        std::swap(r.data, this->data);
+        std::swap(r.size, this->size);
+    }
+
+    template<typename T>
+    inline void tiles<diagonal_matrix<T> >::resize(size_type rows, size_type cols){
+        ambient::numeric::resize(*this, rows, cols);
+    }
+
+    template<typename T>
+    inline void tiles<diagonal_matrix<T> >::remove_rows(size_type i, size_type k){
+        remove_rows(*this, i, k);
+    }
+
+    template<typename T>
+    inline void tiles<diagonal_matrix<T> >::remove_cols(size_type j, size_type k){
+        remove_cols(*this, j, k); 
+    }
+
+    template<typename T>
+    inline diagonal_matrix<T>& tiles<diagonal_matrix<T> >::operator[] (size_type k){
+        return *this->data[k];
+    }
+
+    template<typename T>
+    inline const diagonal_matrix<T>& tiles<diagonal_matrix<T> >::operator[] (size_type k) const {
+        return *this->data[k];
+    }
+
+    template<typename T>
+    inline value_type& tiles<diagonal_matrix<T> >::operator() (size_type i, size_type j){
+        return (*this->data[(int)(i/TILE_SIZE)])(i % TILE_SIZE, i % TILE_SIZE);
+    }
+
+    template<typename T>
+    inline const value_type& tiles<diagonal_matrix<T> >::operator() (size_type i, size_type j) const {
+        return (*this->data[(int)(i/TILE_SIZE)])(i % TILE_SIZE, i % TILE_SIZE);
     }
 
 } }
