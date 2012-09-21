af88dc371be42df834e6482e2d95c1456b84c93e
dolfim <dolfim@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-09-21 15:23:02 +0000

    removing CPU_FREQ dependency

--- maquis_utils/timings.h
+++ maquis_utils/timings.h
@@ -4,68 +4,18 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <boost/chrono.hpp>
 #include "utils/io.hpp"
 
 #ifdef _OPENMP
 #include "omp.h"
 #endif
 
-#ifdef __CUBLAS__
-#include "cuda_runtime_api.h" 
-#endif
-
-#define BILLION 0x3B9ACA00
-
-#if defined(__i386__)
- 
-inline unsigned long getcpuclocks()
-{
- unsigned long tsc;
- asm(".byte 0x0f, 0x31" : "=A" (tsc));
- return( tsc );
-}
- 
-#elif (defined(__amd64__) || defined(__x86_64__))
- 
-inline unsigned long getcpuclocks()
-{
- unsigned long lo, hi;
- asm( "rdtsc" : "=a" (lo), "=d" (hi) );
- return( lo | (hi << 32) );
-}
-
-#elif (defined(__powerpc64__) || defined(__powerpc__))
-
-inline unsigned long getcpuclocks()
-{
-  unsigned long long int result=0;
-  unsigned long int upper, lower,tmp;
-  __asm__ volatile(
-                "0:                  \n"
-                "\tmftbu   %0           \n"
-                "\tmftb    %1           \n"
-                "\tmftbu   %2           \n"
-                "\tcmpw    %2,%0        \n"
-                "\tbne     0b         \n"
-                : "=r"(upper),"=r"(lower),"=r"(tmp)
-                );
-  result = upper;
-  result = result<<32;
-  result = result|lower;
-  return(result);
-}
-
-#else
-
-inline unsigned long getcpuclocks() { return 0; }
-
-#endif
-
 class Timer
 {
 public:
     Timer(std::string name_)
-    : val(0.0), name(name_), freq((long long unsigned int)CPU_FREQ), nCounter(0) { }
+    : val(0.0), name(name_), nCounter(0) { }
     
     ~Timer() { maquis::cout << name << " " << val << ", nCounter : " << nCounter << std::endl; }
     
@@ -75,67 +25,67 @@ public:
     }
     
     void begin() {
-        t0 = getcpuclocks();
+        t0 = boost::chrono::system_clock::now();
     }
     
     void end() {
 		nCounter += 1;
-        unsigned long long t1 = getcpuclocks();
-        if (t1 > t0)
-            val += (getcpuclocks()-t0)/freq;
+        boost::chrono::duration<double> sec = boost::chrono::system_clock::now() - t0;
+        val += sec.count();
     }
-
+    
     double get_time() const {
 	    return  val;
-    }    
-  
+    }
+    
     friend std::ostream& operator<< (std::ostream& os, Timer const& timer) {
         os << timer.name << " " << timer.val << ", nCounter : " << timer.nCounter;
         return os;
     }
     
 protected:
-    double val, t0;
-    unsigned long long freq, nCounter;
+    double val;
+    boost::chrono::system_clock::time_point t0;
+    unsigned long long nCounter;
     std::string name;
 };
-
+        
 #ifdef _OPENMP
 class TimerOMP : public Timer {
 public:
-	TimerOMP(std::string name_) : Timer(name_), timer_start(0.0), timer_end(0.0){}
-
-	~TimerOMP(){}
-	
-	void begin() {
-		timer_start = omp_get_wtime(); 
-	}
-	
-	void end() {
-		timer_end = omp_get_wtime();
-		val += timer_end - timer_start;
-	}
+    TimerOMP(std::string name_) : Timer(name_), timer_start(0.0), timer_end(0.0){}
+    
+    ~TimerOMP(){}
+    
+    void begin() {
+        timer_start = omp_get_wtime();
+    }
+    
+    void end() {
+        timer_end = omp_get_wtime();
+        val += timer_end - timer_start;
+    }
 private:
-	double timer_start, timer_end;
+    double timer_start, timer_end;
 };
 #endif
-
+        
 #ifndef WIN32
 #include <sys/time.h>
 #else
-
+        
 #include <ctime>
-
+        
 struct timeval {
-	time_t tv_sec, tv_usec;
+    time_t tv_sec, tv_usec;
 };
 
 void gettimeofday(timeval * tv, void *)
 {
-	tv->tv_sec = time(NULL);
-	tv->tv_usec = 0;
+    tv->tv_sec = time(NULL);
+    tv->tv_usec = 0;
 }
-
+        
 #endif
-
+        
 #endif
