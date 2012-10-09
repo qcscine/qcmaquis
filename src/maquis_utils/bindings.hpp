3dc457ddf9afd1d9aaf1fd4aff0969b0e1ee946e
akosenko <akosenko@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-10-09 18:25:56 +0000

    diagonal tiles casting

--- src/maquis_utils/bindings.hpp
+++ src/maquis_utils/bindings.hpp
@@ -34,6 +34,89 @@ namespace maquis { namespace bindings {
 #ifdef AMBIENT 
 
     template <typename T, typename S, template<class M, class SS> class C>
+    struct binding< std::vector< std::vector<T> >, C<ambient::numeric::diagonal_matrix<T>, S> > {
+        static std::vector< std::vector<T> > convert(const C<ambient::numeric::diagonal_matrix<T>, S>& m){
+            std::vector< std::vector<T> > set;
+            for(size_t k = 0; k < m.n_blocks(); ++k) 
+                set.push_back(std::vector<T>(m[k].num_rows()));
+            size_t num_cols(1);
+            size_t offset(0);
+            size_t num_rows;
+            for(size_t k = 0; k < m.n_blocks(); ++k){
+                num_rows = m[k].num_rows();
+                std::vector<T>* v_ptr = &set[k];
+                ambient::numeric::kernels::cast_to_vector<T>::spawn(v_ptr, m[k], num_rows, num_cols, num_rows, offset);
+            }
+            ambient::sync();
+            return set;
+        }
+    };
+
+    template <typename T>
+    struct binding< ambient::numeric::matrix<T>, alps::numeric::matrix<T> > {
+        static ambient::numeric::matrix<T> convert(const alps::numeric::matrix<T>& m){
+            size_t num_rows = m.num_rows();
+            size_t num_cols = m.num_cols();
+            size_t lda = m.stride2();
+            size_t offset(0);
+            ambient::numeric::matrix<T> pm(num_rows, num_cols);    
+            const std::vector<typename alps::numeric::matrix<T>::value_type>* v_ptr = &m.get_values();
+            ambient::numeric::kernels::cast_from_vector<T>::spawn(v_ptr, pm, num_rows, num_cols, lda, offset);
+            ambient::sync();
+            return pm;
+        }
+    };
+
+    template <typename T>
+    struct binding< alps::numeric::matrix<T>, ambient::numeric::matrix<T> > {
+        static alps::numeric::matrix<T> convert(const ambient::numeric::matrix<T>& pm){
+            size_t num_rows = pm.num_rows();
+            size_t num_cols = pm.num_cols();
+            size_t offset(0);
+            alps::numeric::matrix<T> m(num_rows, num_cols);    
+            std::vector<typename alps::numeric::matrix<T>::value_type>* v_ptr = &m.get_values();
+            ambient::numeric::kernels::cast_to_vector<T>::spawn(v_ptr, pm, num_rows, num_cols, num_rows, offset);
+            ambient::sync();
+            return m;
+        }
+    };
+
+    template <typename T>
+    struct binding< ambient::numeric::diagonal_matrix<T>, alps::numeric::diagonal_matrix<T> > {
+        static ambient::numeric::diagonal_matrix<T> convert(const alps::numeric::diagonal_matrix<T>& m){
+            size_t num_rows(m.num_rows());
+            size_t num_cols(1);
+            size_t offset(0);
+            ambient::numeric::diagonal_matrix<T> pm(num_rows);    
+            const std::vector<typename alps::numeric::diagonal_matrix<T>::value_type>* v_ptr = &m.get_values();
+            ambient::numeric::kernels::cast_from_vector<T>::spawn(v_ptr, pm, num_rows, num_cols, num_rows, offset);
+            ambient::sync();
+            return pm;
+        }
+    };
+
+    template <typename T>
+    struct binding< alps::numeric::diagonal_matrix<T>, ambient::numeric::diagonal_matrix<T> > {
+        static alps::numeric::diagonal_matrix<T> convert(const ambient::numeric::diagonal_matrix<T>& pm){
+            size_t offset(0);
+            size_t num_cols(1);
+            size_t num_rows = pm.num_rows();
+            alps::numeric::diagonal_matrix<T> m((std::size_t)num_rows);
+            std::vector<typename alps::numeric::diagonal_matrix<T>::value_type>* v_ptr = &m.get_values();
+            ambient::numeric::kernels::cast_to_vector<T>::spawn(v_ptr, pm, num_rows, num_cols, num_rows, offset);
+            ambient::sync();
+            return m;
+        }
+    };
+
+    template <typename T>
+    struct binding< std::vector<T>, ambient::numeric::diagonal_matrix<T> > {
+        static std::vector<T> convert(const ambient::numeric::diagonal_matrix<T>& pm){
+            return binding<alps::numeric::diagonal_matrix<T>, ambient::numeric::diagonal_matrix<T> >::convert(pm).get_values();
+        }
+    };
+
+    template <typename T, typename S, template<class M, class SS> class C>
     struct binding< std::vector< std::vector<T> >, C<ambient::numeric::tiles<ambient::numeric::diagonal_matrix<T> >, S> > {
         static std::vector< std::vector<T> > convert(const C<ambient::numeric::tiles<ambient::numeric::diagonal_matrix<T> >, S>& m){
             std::vector< std::vector<T> > set;
@@ -48,25 +131,6 @@ namespace maquis { namespace bindings {
                     ambient::numeric::kernels::cast_to_vector<T>::spawn(v_ptr, m[k][kk], num_rows, num_cols, num_rows, offset);
                     offset += num_rows;
                 }
-            }
-            ambient::sync();
-            return set;
-        }
-    };
-
-    template <typename T, typename S, template<class M, class SS> class C>
-    struct binding< std::vector< std::vector<T> >, C<ambient::numeric::diagonal_matrix<T>, S> > {
-        static std::vector< std::vector<T> > convert(const C<ambient::numeric::diagonal_matrix<T>, S>& m){
-            std::vector< std::vector<T> > set;
-            for(size_t k = 0; k < m.n_blocks(); ++k) 
-                set.push_back(std::vector<T>(m[k].num_rows()));
-            size_t num_cols(1);
-            size_t offset(0);
-            size_t num_rows;
-            for(size_t k = 0; k < m.n_blocks(); ++k){
-                num_rows = m[k].num_rows();
-                std::vector<T>* v_ptr = &set[k];
-                ambient::numeric::kernels::cast_to_vector<T>::spawn(v_ptr, m[k], num_rows, num_cols, num_rows, offset);
             }
             ambient::sync();
             return set;
@@ -99,26 +163,10 @@ namespace maquis { namespace bindings {
     };
 
     template <typename T>
-    struct binding< ambient::numeric::matrix<T>, alps::numeric::matrix<T> > {
-        static ambient::numeric::matrix<T> convert(const alps::numeric::matrix<T>& m){
-            size_t num_rows = m.num_rows();
-            size_t num_cols = m.num_cols();
-            size_t lda = m.stride2();
-            size_t offset(0);
-            ambient::numeric::matrix<T> pm(num_rows, num_cols);    
-            const std::vector<typename alps::numeric::matrix<T>::value_type>* v_ptr = &m.get_values();
-            ambient::numeric::kernels::cast_from_vector<T>::spawn(v_ptr, pm, num_rows, num_cols, lda, offset);
-            ambient::sync();
-            return pm;
-        }
-    };
-
-    template <typename T>
     struct binding< alps::numeric::matrix<T>, ambient::numeric::tiles<ambient::numeric::matrix<T> > > {
         static alps::numeric::matrix<T> convert(const ambient::numeric::tiles<ambient::numeric::matrix<T> >& pm){
             size_t num_rows = pm.num_rows();
             size_t num_cols = pm.num_cols();
-            size_t offset(0);
             alps::numeric::matrix<T> m(num_rows, num_cols);    
             std::vector<typename alps::numeric::matrix<T>::value_type>* v_ptr = &m.get_values();
             size_t lda = m.stride2();
@@ -140,51 +188,43 @@ namespace maquis { namespace bindings {
     };
 
     template <typename T>
-    struct binding< alps::numeric::matrix<T>, ambient::numeric::matrix<T> > {
-        static alps::numeric::matrix<T> convert(const ambient::numeric::matrix<T>& pm){
-            size_t num_rows = pm.num_rows();
-            size_t num_cols = pm.num_cols();
-            size_t offset(0);
-            alps::numeric::matrix<T> m(num_rows, num_cols);    
-            std::vector<typename alps::numeric::matrix<T>::value_type>* v_ptr = &m.get_values();
-            ambient::numeric::kernels::cast_to_vector<T>::spawn(v_ptr, pm, num_rows, num_cols, num_rows, offset);
-            ambient::sync();
-            return m;
-        }
-    };
-
-    template <typename T>
-    struct binding< ambient::numeric::diagonal_matrix<T>, alps::numeric::diagonal_matrix<T> > {
-        static ambient::numeric::diagonal_matrix<T> convert(const alps::numeric::diagonal_matrix<T>& m){
-            size_t num_rows(m.num_rows());
+    struct binding< ambient::numeric::tiles<ambient::numeric::diagonal_matrix<T> >, alps::numeric::diagonal_matrix<T> > {
+        static ambient::numeric::tiles<ambient::numeric::diagonal_matrix<T> > convert(const alps::numeric::diagonal_matrix<T>& m){
+            size_t num_rows = m.num_rows();
             size_t num_cols(1);
-            size_t offset(0);
-            ambient::numeric::diagonal_matrix<T> pm(num_rows, num_rows);    
+            ambient::numeric::tiles<ambient::numeric::diagonal_matrix<T> > pm(num_rows);    
             const std::vector<typename alps::numeric::diagonal_matrix<T>::value_type>* v_ptr = &m.get_values();
-            ambient::numeric::kernels::cast_from_vector<T>::spawn(v_ptr, pm, num_rows, num_cols, num_rows, offset);
+
+            size_t offset(0);
+            for(size_t i = 0; i < pm.nt; ++i){
+                ambient::numeric::diagonal_matrix<T>& tile = pm[i];
+                size_t rows = tile.num_rows();
+                ambient::numeric::kernels::cast_from_vector<T>::spawn(v_ptr, tile, rows, num_cols, num_rows, offset);
+                offset += rows;
+            }
+
             ambient::sync();
             return pm;
         }
     };
 
     template <typename T>
-    struct binding< alps::numeric::diagonal_matrix<T>, ambient::numeric::diagonal_matrix<T> > {
-        static alps::numeric::diagonal_matrix<T> convert(const ambient::numeric::diagonal_matrix<T>& pm){
-            size_t offset(0);
-            size_t num_cols(1);
+    struct binding< alps::numeric::diagonal_matrix<T>, ambient::numeric::tiles<ambient::numeric::diagonal_matrix<T> > > {
+        static alps::numeric::diagonal_matrix<T> convert(const ambient::numeric::tiles<ambient::numeric::diagonal_matrix<T> >& pm){
             size_t num_rows = pm.num_rows();
-            alps::numeric::diagonal_matrix<T> m(num_rows, num_rows);    
+            size_t num_cols(1);
+            size_t offset(0);
+            alps::numeric::diagonal_matrix<T> m((std::size_t)num_rows);    
             std::vector<typename alps::numeric::diagonal_matrix<T>::value_type>* v_ptr = &m.get_values();
-            ambient::numeric::kernels::cast_to_vector<T>::spawn(v_ptr, pm, num_rows, num_cols, num_rows, offset);
+            for(size_t i = 0; i < pm.nt; ++i){
+                const ambient::numeric::diagonal_matrix<T>& tile = pm[i];
+                size_t rows = tile.num_rows();
+                ambient::numeric::kernels::cast_to_vector<T>::spawn(v_ptr, tile, rows, num_cols, num_rows, offset);
+                offset += rows;
+            }
+
             ambient::sync();
             return m;
-        }
-    };
-
-    template <typename T>
-    struct binding< std::vector<T>, ambient::numeric::diagonal_matrix<T> > {
-        static std::vector<T> convert(const ambient::numeric::diagonal_matrix<T>& pm){
-            return binding<alps::numeric::diagonal_matrix<T>, ambient::numeric::diagonal_matrix<T> >::convert(pm).get_values();
         }
     };
 
