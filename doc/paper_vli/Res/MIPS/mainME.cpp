4c84b5f4527a7df0e9f08533ac424fbd369fe3d0
ewartt <ewartt@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-11-18 22:39:31 +0000

    pqper vli

---
+++ doc/paper_vli/Res/MIPS/mainME.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+
+double foo( int Order, int var, int mul, int add){
+    return 4096*pow((Order+1),2*var)*mul; //  +4095*pow((2*Order+1),var)*add;
+}
+
+int main(){
+
+for ( int i=0; i < 15; ++i)
+     std::cout << i << " " <<  foo(i,1,32,0) << " " <<  foo(i,2,32,0) <<  " " <<  foo(i,3,32,0) << std::endl;
+
+
+
+
+
+
+}
