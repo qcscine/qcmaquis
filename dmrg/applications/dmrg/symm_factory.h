dbbce4ae8141608c0192b19c3a83b64dc911ebaa
dolfim <dolfim@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-11-20 08:09:38 +0000

    Cleaning up redundant code between dmrg, dmrg_meas, dmrg_cmeas

---
+++ dmrg/applications/dmrg/symm_factory.h
@@ -0,0 +1,20 @@
+/*****************************************************************************
+ *
+ * MAQUIS DMRG Project
+ *
+ * Copyright (C) 2011-2012 by Michele Dolfi <dolfim@phys.ethz.ch>
+ *
+ *****************************************************************************/
+
+#ifndef MAQUIS_DMRG__APP_SYMM_FACTORY_H
+#define MAQUIS_DMRG__APP_SYMM_FACTORY_H
+
+#include "dmrg/utils/DmrgParameters2.h"
+
+namespace maquis { namespace dmrg {
+
+    void symm_factory(DmrgParameters & parms, ModelParameters & model);
+
+} }
+
+#endif /* defined(MAQUIS_DMRG__APP_SYMM_FACTORY_H) */
