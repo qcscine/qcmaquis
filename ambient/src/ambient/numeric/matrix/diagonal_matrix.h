01ee108ffdd03678b7d48b5c596eb189dd26cdea
akosenko <akosenko@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-11-20 13:51:46 +0000

    rows, cols -> m, n + locate/addr stubs

--- ambient/src/ambient/numeric/matrix/diagonal_matrix.h
+++ ambient/src/ambient/numeric/matrix/diagonal_matrix.h
@@ -24,11 +24,14 @@ namespace ambient { namespace numeric {
         inline value_type& operator[](size_t i); 
         inline const value_type& operator()(size_t i, size_t j) const;
         inline value_type& operator()(size_t i, size_t j);
-        inline void remove_rows(size_t i, size_t k = 1);
-        inline void remove_cols(size_t j, size_t k = 1);
-        inline void resize(size_t rows, size_t cols);
+        inline void remove_rows(size_t i, size_t m = 1);
+        inline void remove_cols(size_t j, size_t n = 1);
+        inline diagonal_matrix& locate(size_t i, size_t j);
+        inline const diagonal_matrix& locate(size_t i, size_t j) const;
+        inline size_t addr(size_t i, size_t j) const;
+        inline void resize(size_t m, size_t n);
         template< class T1 > 
-        friend std::ostream & operator<<(std::ostream& os, const diagonal_matrix<T1>& m);
+        friend std::ostream & operator<<(std::ostream& os, const diagonal_matrix<T1>& a);
         inline const container& get_data() const; 
         inline container& get_data();    
         inline size_type size() const;
