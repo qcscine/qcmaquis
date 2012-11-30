f93934a154fa482d0d8b2126cea0d5687c3ce52d
ewartt <ewartt@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-11-30 23:27:56 +0000

    KA cpu fix bug

--- large_integer/VLI/src/vli/detail/cpu/x86_64/karatsuba_asm.h
+++ large_integer/VLI/src/vli/detail/cpu/x86_64/karatsuba_asm.h
@@ -30,50 +30,103 @@
 #ifndef VLI_KARATSUBA_ASM_H
 #define VLI_KARATSUBA_ASM_H
 
+#include <boost/preprocessor/arithmetic/add.hpp>
+#include <boost/preprocessor/arithmetic/mul.hpp>
+#include <boost/preprocessor/arithmetic/sub.hpp>
+#include <boost/preprocessor/repetition.hpp>
+#include <boost/preprocessor/iteration/local.hpp>
+#include <boost/preprocessor/comparison/equal.hpp>
+
 namespace vli {
     namespace detail {
+
+    template <std::size_t NumBits>
+    inline void KA_add(boost::uint64_t* x, boost::uint64_t const* y);
+   
+    template <std::size_t NumBits>
+    boost::uint64_t KA_sub(boost::uint64_t* x, boost::uint64_t const* y); //return the mask
+   
+    template <std::size_t NumBits>
+    inline void KA_sign(boost::uint64_t* x, boost::uint64_t a);
+   
+    #define addn128_n128_ka_cpu(w, n, unused) \
+    asm(  \
+            BOOST_PP_IF(n,"adcq %1, %0;","addq  %1, %0; ") /* n=0 no CB,  n!=0, second pass needs CB */  \
+            :"+r"(x[n]):"g"(y[n]):"cc" \
+       ); \
+   
+    #define addn64_bit_ka_cpu(w, n, unused) \
+    asm(  \
+           BOOST_PP_IF(n,"adcq $0, %0; ","addq  %1, %0; ")  /* n=0 no CB,  n!=0, second pass needs CB */  \
+           :"+r"(x[n]):BOOST_PP_IF(n,/* nothing */,"g"(mask)):"cc" \
+       ); \
+   
+    #define subn128_n128_ka_cpu(w, n, unused) \
+    asm(  \
+           BOOST_PP_IF(n,"sbbq %1, %0; \n\t","subq  %1, %0; \n\t") /* n=0 no CB,  n!=0, second pass needs CB */  \
+           :"+r"(x[n]):"g"(y[n]):"cc"                                                     \
+       ); \
+   
+    #define FUNCTION_add_nbits_nbits(z, n, unused) \
+    template<> \
+    inline void KA_add<(n+1)*64>(boost::uint64_t* x,boost::uint64_t const* y){ \
+        BOOST_PP_REPEAT(BOOST_PP_ADD(n,1), addn128_n128_ka_cpu, ~) \
+    }; \
+    
+    #define FUNCTION_add_nbits_bit(z, n, unused) \
+    template<> \
+    inline void KA_sign<(n+1)*64>(boost::uint64_t* x,boost::uint64_t mask){ \
+        for(int i(0); i < (n+1); ++i) \
+            x[i] ^= mask; \
+        mask >>= 63; /* transform the mask to CB */ \
+        BOOST_PP_REPEAT(BOOST_PP_ADD(n,1), addn64_bit_ka_cpu, ~) \
+    }; \
+   
+    #define FUNCTION_sub_nbits_nbits(z, n, unused) \
+    template<> \
+        boost::uint64_t KA_sub<(n+1)*64>(boost::uint64_t* x,boost::uint64_t const* y){ \
+        boost::uint64_t sign(0); \
+        BOOST_PP_REPEAT(BOOST_PP_ADD(n,1), subn128_n128_ka_cpu, ~) \
+        asm("setc %0;":"=g"(sign)::"cc"); /* extract the sign = CB */ \
+        sign = -sign; \
+        return sign; \
+    }; \
+   
+    BOOST_PP_REPEAT(4, FUNCTION_add_nbits_nbits, ~)
+    BOOST_PP_REPEAT(4, FUNCTION_add_nbits_bit,   ~)
+    BOOST_PP_REPEAT(4, FUNCTION_sub_nbits_nbits, ~)
     
     template <std::size_t NumBits>
-    void inline KA_add(vli<NumBits> & x, vli<NumBits> const& y){
-        asm("addq  %1, %0;" :"+r"(x[0]):"g"(y[0]):"cc");
-        for(int i(1); i < vli<NumBits>::numwords;++i)
-            asm("adcq  %1, %0;" :"+r"(x[i]):"g"(y[i]):"cc");
-    };
-   
-    template <std::size_t NumBits>
-    void inline KA_sub(vli<NumBits> & x, vli<NumBits>  const& y){
-        asm("subq  %1, %0;" :"+r"(x[0]):"g"(y[0]):"cc");
-        for(int i(1); i < vli<NumBits>::numwords;++i)
-            asm("sbbq  %1, %0;" :"+r"(x[i]):"g"(y[i]):"cc");
-    };
-   
-    template <std::size_t NumBits>
-    struct KA_helper{
+    struct KA_helper{//lsb = partie haute, msb = partie basse
         inline static vli<2*NumBits> KA_algo(vli<NumBits> const&  vli_a, vli<NumBits> const& vli_b){
-            vli<NumBits/2> x1(vli_a,copy_lsb_tag()),x2(vli_a,copy_msb_tag());
-            vli<NumBits/2> y1(vli_b,copy_lsb_tag()),y2(vli_b,copy_msb_tag());
-            vli<NumBits/2> x1x2(x1);
-            vli<NumBits/2> y1y2(y1);
-            vli<NumBits> u,v,w;
-            u = KA_helper<NumBits/2>::KA_algo(x2,y2);
-            v = KA_helper<NumBits/2>::KA_algo(x1,y1);
-            KA_add<NumBits/2>(x1x2,x2);
-            KA_add<NumBits/2>(y1y2,y2);
-            w = KA_helper<NumBits/2>::KA_algo(x1x2,y1y2);
-            KA_sub<NumBits>(w,u);
-            KA_sub<NumBits>(w,v);
-            vli<2*NumBits> tmp_res0(w,copy_right_shift_tag());
-            vli<2*NumBits> tmp_res1(u,v,copy_right_shift_tag());
-            KA_add<2*NumBits>(tmp_res0,tmp_res1);
+            vli<NumBits/2> al(vli_a,copy_msb_tag()),ah(vli_a,copy_lsb_tag());
+            vli<NumBits/2> bl(vli_b,copy_msb_tag()),bh(vli_b,copy_lsb_tag());
+            vli<NumBits/2> am(al); // partie basse
+            vli<NumBits/2> bm(bh); // partie haute
+            vli<NumBits> low,high,medium;
+            boost::uint64_t mask_s1 = KA_sub<NumBits/2>(&am[0],&ah[0]);
+            boost::uint64_t mask_s2 = KA_sub<NumBits/2>(&bm[0],&bl[0]);
+            KA_sign<NumBits/2>(&am[0],mask_s1);
+            KA_sign<NumBits/2>(&bm[0],mask_s2);
+            low = KA_helper<NumBits/2>::KA_algo(al,bl);
+            high = KA_helper<NumBits/2>::KA_algo(ah,bh);
+            medium = KA_helper<NumBits/2>::KA_algo(am,bm);
+            mask_s1 ^= mask_s2;
+            KA_sign<NumBits>(&medium[0],mask_s1);
+            KA_add<NumBits>(&medium[0],&low[0]);
+            KA_add<NumBits>(&medium[0],&high[0]);
+            vli<2*NumBits> tmp_res0(medium,copy_right_shift_tag());
+            vli<2*NumBits> tmp_res1(low,high,copy_right_shift_tag());
+            KA_add<2*NumBits>(&tmp_res0[0],&tmp_res1[0]);
             return tmp_res0;
         };
     };
    
     template <>
     struct KA_helper<64>{
-        inline static vli<128>  KA_algo(vli<64> const& vli_a, vli<64> const& vli_b){//
+        inline static vli<128> KA_algo(vli<64> const& vli_a, vli<64> const& vli_b){//
             vli<128> vli_res;
-            // =a means rax for lower part, =d means rdx for the higher part, = for writing
+            /* =a means rax for lower part, =d means rdx for the higher part, = for writing, %0 directly vli_a[0] ready for mul vli[0] */
             asm("mulq %3;" :"=a"(vli_res[0]), "=d"(vli_res[1]) :"%0" (vli_a[0]), "r"(vli_b[0]): "cc");
             return vli_res;
         }
@@ -82,4 +135,33 @@ namespace vli {
     }
 }
 
+#undef FUNCTION_sub_nbits_nbits
+#undef FUNCTION_add_nbits_bit
+#undef FUNCTION_add_nbits_nbits
+#undef addn128_n128_ka_cpu
+#undef sub_m128_n128_gpu
+
 #endif
+
+//   Karatusba postive version 
+//    template <std::size_t NumBits>
+//    struct KA_helper{
+//        inline static vli<2*NumBits> KA_algo(vli<NumBits> const&  vli_a, vli<NumBits> const& vli_b){
+//            vli<NumBits/2> x1(vli_a,copy_lsb_tag()),x2(vli_a,copy_msb_tag());
+//            vli<NumBits/2> y1(vli_b,copy_lsb_tag()),y2(vli_b,copy_msb_tag());
+//            vli<NumBits/2> x1x2(x1);
+//            vli<NumBits/2> y1y2(y1);
+//            vli<NumBits> u,v,w;
+//            u = KA_helper<NumBits/2>::KA_algo(x2,y2);
+//            v = KA_helper<NumBits/2>::KA_algo(x1,y1);
+//            KA_add<NumBits/2>(x1x2,x2);
+//            KA_add<NumBits/2>(y1y2,y2);
+//            w = KA_helper<NumBits/2>::KA_algo(x1x2,y1y2);
+//            KA_sub<NumBits>(w,u);
+//            KA_sub<NumBits>(w,v);
+//            vli<2*NumBits> tmp_res0(w,copy_right_shift_tag());
+//            vli<2*NumBits> tmp_res1(u,v,copy_right_shift_tag());
+//            KA_add<2*NumBits>(tmp_res0,tmp_res1);
+//            return tmp_res0;
+//        };
+//    };
