a0e1317de1cb580cd2fcea7172383424dd4280c9
dolfim <dolfim@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-12-05 16:20:11 +0000

    Bose Hubbard model without symmetries

--- src/dmrg/framework/dmrg/models/coded/factory_none.hpp
+++ src/dmrg/framework/dmrg/models/coded/factory_none.hpp
@@ -13,7 +13,13 @@ struct model_factory<Matrix, TrivialGroup> {
     static typename model_traits<Matrix, TrivialGroup>::model_ptr parse
     (Lattice const & lattice, BaseParameters & model)
     {
-        throw std::runtime_error("No models with TrivialGroup defined in the factory!");
-        return typename model_traits<Matrix, TrivialGroup>::model_ptr();
+        if (model.get<std::string>("MODEL") == std::string("boson Hubbard"))
+            return typename model_traits<Matrix, TrivialGroup>::model_ptr(
+                                                                          new BoseHubbardNone<Matrix>(lattice, model.get<int>("Nmax"), model.get<double>("t"), model.get<double>("U"), model.get<double>("V"))
+                                                                );
+        else {
+            throw std::runtime_error("Don't know this model with NONE symmetry group!");
+            return typename model_traits<Matrix, TrivialGroup>::model_ptr();
+        }
     }
 };
--- src/dmrg/framework/dmrg/models/coded/models_none.hpp
+++ src/dmrg/framework/dmrg/models/coded/models_none.hpp
@@ -15,4 +15,115 @@
 #include "dmrg/models/model.h"
 #include "dmrg/utils/BaseParameters.h"
 
+/* ****************** BOSE-HUBBARD */
+template<class Matrix>
+class BoseHubbardNone : public Model<Matrix, TrivialGroup>
+{
+    typedef Hamiltonian<Matrix, TrivialGroup> ham;
+    typedef typename ham::hamterm_t hamterm_t;
+    typedef typename ham::op_t op_t;
+    
+public:
+    BoseHubbardNone (const Lattice& lat, int Nmax=2, double t=1., double U=1., double V=1.)
+    {
+        TrivialGroup::charge C = TrivialGroup::IdentityCharge;
+        size_t N = Nmax+1;
+        
+        phys.insert(std::make_pair(C, N));
+        ident.insert_block(Matrix::identity_matrix(N), C, C);
+        
+        Matrix mcount(N,N), minteraction(N,N), mcreate(N,N), mdestroy(N,N);
+        for (int n=1; n<=Nmax; ++n)
+        {
+            mcount(n,n) = n;
+            if ((n*n-n) != 0)
+                minteraction(n,n) = n*n-n;
+            
+            mcreate(n-1,n) = std::sqrt(n);   // input n-1, output n
+            mdestroy(n,n-1) = std::sqrt(n);  // input n,   output n-1
+        }
+        count.insert_block(mcount, C,C);
+        interaction.insert_block(minteraction, C,C);
+        create.insert_block(mcreate, C,C);
+        destroy.insert_block(mdestroy, C,C);
+        
+        for (int p=0; p<lat.size(); ++p) {
+            /* interaction */
+            {
+                hamterm_t term;
+                term.with_sign = false;
+                term.fill_operator = ident;
+                term.operators.push_back( std::make_pair(p, interaction) );
+                terms.push_back(term);
+            }
+            
+            std::vector<int> neighs = lat.forward(p);
+            for (int n=0; n<neighs.size(); ++n) {
+                /* hopping */
+                {
+                    hamterm_t term;
+                    term.fill_operator = ident;
+                    term.operators.push_back( std::make_pair(p, -t*create) );
+                    term.operators.push_back( std::make_pair(neighs[n], destroy) );
+                    terms.push_back(term);
+                }
+                {
+                    hamterm_t term;
+                    term.fill_operator = ident;
+                    term.operators.push_back( std::make_pair(p, -t*destroy) );
+                    term.operators.push_back( std::make_pair(neighs[n], create) );
+                    terms.push_back(term);
+                }
+                /* nearest-neighborn interaction */
+                {
+                    hamterm_t term;
+                    term.fill_operator = ident;
+                    term.operators.push_back( std::make_pair(p, V*count) );
+                    term.operators.push_back( std::make_pair(neighs[n], count) );
+                    terms.push_back(term);
+                }
+            }
+        }
+        
+    }
+    
+    Index<TrivialGroup> get_phys() const
+    {
+        return phys;
+    }
+    
+    Hamiltonian<Matrix, TrivialGroup> H () const
+    {
+        return ham(phys, ident, terms);
+    }
+    
+    Measurements<Matrix, TrivialGroup> measurements () const
+    {
+        return Measurements<Matrix, TrivialGroup>();
+    }
+    
+    op_t get_op(std::string const & op) const
+    {
+        if (op == "n")
+            return count;
+        else if (op == "bdag")
+            return create;
+        else if (op == "b")
+            return destroy;
+        else
+            throw std::runtime_error("Operator not valid for this model.");
+        return op_t();
+    }
+    
+    
+private:
+    op_t ident;
+    op_t create, destroy, count, interaction;
+    Index<TrivialGroup> phys;
+    
+    std::vector<hamterm_t> terms;
+};
+
+
+
 #endif
