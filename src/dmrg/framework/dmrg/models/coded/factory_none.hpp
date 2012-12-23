9c6ab2f15dcf126925d04794c32080ff35f8620d
gukel <gukel@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2012-12-23 21:03:03 +0000

    Bose-Hubbard model for time evolution of the density operator

--- src/dmrg/framework/dmrg/models/coded/factory_none.hpp
+++ src/dmrg/framework/dmrg/models/coded/factory_none.hpp
@@ -2,11 +2,13 @@
  *
  * MAQUIS DMRG Project
  *
- * Copyright (C) 2011-2011 by Michele Dolfi <dolfim@phys.ethz.ch>
+ * Copyright (C) 2011-2012 by Michele Dolfi <dolfim@phys.ethz.ch>
+ *               2012      by Jan Gukelberger <gukelberger@phys.ethz.ch>
  *
  *****************************************************************************/
 
 #include "dmrg/models/coded/models_none.hpp"
+#include "dmrg/models/coded/super_models_none.hpp"
 
 template<class Matrix>
 struct model_factory<Matrix, TrivialGroup> {
@@ -16,6 +18,10 @@ struct model_factory<Matrix, TrivialGroup> {
         if (model.get<std::string>("MODEL") == std::string("boson Hubbard"))
             return typename model_traits<Matrix, TrivialGroup>::model_ptr(
                                                                           new BoseHubbardNone<Matrix>(lattice, model.get<int>("Nmax"), model.get<double>("t"), model.get<double>("U"), model.get<double>("V"))
+                                                                );
+        else if (model.get<std::string>("MODEL") == std::string("super boson Hubbard"))
+            return typename model_traits<Matrix, TrivialGroup>::model_ptr(
+                                                                          new SuperBoseHubbardNone<Matrix>(lattice, model.get<int>("Nmax"), model.get<double>("t"), model.get<double>("U"), model.get<double>("V"), model.get<double>("Delta"), model.get<double>("Gamma1a"), model.get<double>("Gamma1b"), model.get<double>("Gamma2"))
                                                                 );
         else {
             throw std::runtime_error("Don't know this model with NONE symmetry group!");
