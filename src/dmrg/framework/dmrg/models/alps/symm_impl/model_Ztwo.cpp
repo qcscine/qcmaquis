c63ea6813a1e8a5b6e7f1028e1e4afe51772f209
bela <bela@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2013-01-08 01:59:36 +0000

    U1 and Z2 for models with one block > 1

--- src/dmrg/framework/dmrg/models/alps/symm_impl/model_Ztwo.cpp
+++ src/dmrg/framework/dmrg/models/alps/symm_impl/model_Ztwo.cpp
@@ -19,6 +19,8 @@ Ztwo::charge init_charge<Ztwo> (const alps::Parameters& parms, std::vector<std::
     Ztwo::charge c = Ztwo::IdentityCharge;
     if (parms.defined(qn[0].second+"_total")) {
         int tmp = alps::evaluate<double>(static_cast<std::string>(parms[qn[0].second+"_total"]), parms);
+        if (!(tmp == 0 || tmp == 1))
+            throw std::runtime_error("Invalid value for " + qn[0].second + "_total");
         c = (tmp == 1 ? Ztwo::Minus : Ztwo::Plus);
     }
     return c;
@@ -29,7 +31,7 @@ Ztwo::charge convert_alps<Ztwo> (alps::site_state<short> const & state, std::vec
 {
     assert(qn.size() == 1);
     int tmp = detail::to_integer( get_quantumnumber(state, qn[0].first) );
-    return (tmp == 1 ? Ztwo::Minus : Ztwo::Plus);
+    return (tmp % 2 == 0 ? Ztwo::Plus : Ztwo::Minus);
 }
 
 template <>
@@ -39,7 +41,7 @@ std::map<Ztwo::charge,std::size_t> init_qn_charges<Ztwo>
     assert(conserved_qn.size() == 1);
     std::map<Ztwo::charge,std::size_t> ret;
     for (int i=0; i<states.size(); ++i)
-        ret[convert_alps<Ztwo>(states[i], conserved_qn)] = 1;
+        ret[convert_alps<Ztwo>(states[i], conserved_qn)] += 1;
     return ret;
 }
 
@@ -49,8 +51,11 @@ std::map<alps::site_state<short>, std::pair<Ztwo::charge, std::size_t> > init_co
 {
     assert(conserved_qn.size() == 1);
     std::map<alps::site_state<short>, std::pair<Ztwo::charge, std::size_t> > ret;
-    for (std::size_t i=0; i<states.size(); ++i)
-        ret[states[i]] = std::make_pair(convert_alps<Ztwo>(states[i], conserved_qn), 0);
+    std::map<Ztwo::charge, std::size_t> used;
+    for (std::size_t i=0; i<states.size(); ++i) {
+        Ztwo::charge c = convert_alps<Ztwo>(states[i], conserved_qn);
+        ret[states[i]] = std::make_pair(c, used[c]++);
+    }
     return ret;
 }
 
