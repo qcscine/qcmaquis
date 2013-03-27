abc719e00ac1c3bd519e8b2e90e5d59905bf96bf
akosenko <akosenko@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2013-03-27 19:58:12 +0000

    removing unused stuff (Tim, you can add a folder 'vli' and place it here as a final version)

--- src/large_integer/VLI/scripts/castor_gcc.cmake
+++
@@ -1,4 +0,0 @@
-set(CMAKE_C_COMPILER gcc)
-set(CMAKE_CXX_COMPILER g++)
-set (VLI_COMPILER_FLAGS_RELEASE " -Wall -Os -funroll-loops -m64" CACHE STRING "Compiler flags for a regular compile.")
-set (VLI_COMPILER_FLAGS_DEBUG " -Wall -g -O0 -m64" CACHE STRING "Compiler flags for a debug compile.")
