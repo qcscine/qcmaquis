5ed5ce622c86903d764060b7005d3861c701140f
akosenko <akosenko@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2013-04-24 17:21:29 +0000

    omp: guided -> dynamic

--- src/dmrg/framework/dmrg/block_matrix/block_matrix_algorithms.h
+++ src/dmrg/framework/dmrg/block_matrix/block_matrix_algorithms.h
@@ -28,7 +28,7 @@
 #elif defined(MAQUIS_OPENMP)
     typedef std::size_t locale;
     #define parallel_pragma(a) _Pragma( #a )
-    #define parallel_for(constraint, ...) parallel_pragma(omp parallel for schedule(guided)) for(__VA_ARGS__)
+    #define parallel_for(constraint, ...) parallel_pragma(omp parallel for schedule(dynamic, 1)) for(__VA_ARGS__)
     #define semi_parallel_for(constraint, ...) for(__VA_ARGS__)
 #else
     typedef std::size_t locale;
