c4653cc6f30f7726627ce4bcf1daa214b3e1839b
dolfim <dolfim@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2013-08-29 14:34:25 +0000

    fix in mps_converter

--- src/dmrg/applications/misc/mps_converter.cpp
+++ src/dmrg/applications/misc/mps_converter.cpp
@@ -8,11 +8,15 @@ using std::cerr;
 using std::cout;
 using std::endl;
 
-#include "dmrg/block_matrix/detail/alps.hpp"
+#include "ambient/ambient.hpp"
 #include "dmrg/block_matrix/detail/ambient.hpp"
+#include "dmrg/block_matrix/detail/alps.hpp"
 
 typedef alps::numeric::matrix<double> alps_matrix;
-typedef ambient::numeric::matrix<double> ambient_matrix;
+typedef ambient::numeric::tiles<ambient::numeric::matrix<double> > ambient_matrix;
+
+#include "dmrg/block_matrix/block_matrix.h"
+#include "dmrg/block_matrix/block_matrix_algorithms.h"
 
 #include "dmrg/block_matrix/indexing.h"
 #include "dmrg/mp_tensors/mps.h"
@@ -20,6 +24,8 @@ typedef ambient::numeric::matrix<double> ambient_matrix;
 #include "dmrg/mp_tensors/contractions.h"
 #include "dmrg/mp_tensors/mps_mpo_ops.h"
 #include "dmrg/mp_tensors/mpo_ops.h"
+
+#include "utils/bindings.hpp"
 
 #ifdef USE_TWOU1
 typedef TwoU1 grp;
@@ -35,11 +41,11 @@ typedef U1 grp;
 int main(int argc, char ** argv)
 {
     try {
-        if (argc != 2) {
+        if (argc != 3) {
             std::cout << "Usage: " << argv[0] << " <in.h5> <out.h5>" << std::endl;
             return 1;
         }
-        MPS<ambient_atrix, grp> mps_in;
+        MPS<ambient_matrix, grp> mps_in;
         
         {
             storage::archive ar(argv[1]);
@@ -50,13 +56,13 @@ int main(int argc, char ** argv)
         for (int i=0; i<mps_in.length(); ++i) {
             mps_in[i].make_left_paired();
             block_matrix<alps_matrix, grp> m;
-            for (int k=0; k<mps[i].data().n_blocks(); ++k)
-                m.insert_block(maquis::bindings::convert<alps_matrix>(mps[i].data()[k]), mps[i].data().left_basis()[k], mps[i].data().right_basis()[k]);
+            for (int k=0; k<mps_in[i].data().n_blocks(); ++k)
+                m.insert_block(maquis::bindings::matrix_cast<alps_matrix>(mps_in[i].data()[k]), mps_in[i].data().left_basis()[k].first, mps_in[i].data().right_basis()[k].first);
             mps_out[i] = MPSTensor<alps_matrix, grp>(mps_in[i].site_dim(),mps_in[i].row_dim(), mps_in[i].col_dim(), m, LeftPaired);
         }
         
         {
-            storage::archive ar(argv[2]);
+            storage::archive ar(argv[2], "w");
             ar["/state"] << mps_out;
         }
         
