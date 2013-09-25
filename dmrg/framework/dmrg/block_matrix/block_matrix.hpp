109539d04e80eb4643c88334b5a9d66dedaf0eba
sebkelle <sebkelle@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2013-09-25 12:00:05 +0000

    small formatting improvement in print function

--- dmrg/framework/dmrg/block_matrix/block_matrix.hpp
+++ dmrg/framework/dmrg/block_matrix/block_matrix.hpp
@@ -326,7 +326,7 @@ std::ostream& operator<<(std::ostream& os, block_matrix<Matrix, SymmGroup> const
     os << "Left HS: " << m.left_basis() << std::endl;
     os << "Right HS: " << m.right_basis() << std::endl;
     for (std::size_t k = 0; k < m.n_blocks(); ++k)
-        os << "Block (" << m.left_basis()[k].first << "," << m.right_basis()[k].first << "): " << m[k] << std::endl;
+        os << "Block (" << m.left_basis()[k].first << "," << m.right_basis()[k].first << "):\n" << m[k] << std::endl;
     os << std::endl;
     return os;
 }
