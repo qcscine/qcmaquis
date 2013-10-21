078b4e0fa2a286ca9341af79721bbf4f4fb56575
dolfim <dolfim@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2013-10-21 15:44:09 +0000

    review of cmake config

--- ambient/regression/utils/testing.hpp
+++ ambient/regression/utils/testing.hpp
@@ -3,7 +3,7 @@
 
 #include <mpi.h>
 #include <omp.h>
-#include <boost/test/unit_test.hpp>
+#include <boost/test/included/unit_test.hpp>
 #include <boost/test/test_case_template.hpp>
 #include <boost/mpl/list.hpp>
 #include <boost/random.hpp>
