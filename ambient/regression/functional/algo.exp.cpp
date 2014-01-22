8dfb7d57c27eedcf9e894e3b82bd90b77148969c
akosenko <akosenko@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2014-01-22 17:50:43 +0000

    fixing tests

--- ambient/regression/functional/algo.exp.cpp
+++ ambient/regression/functional/algo.exp.cpp
@@ -1,20 +1,8 @@
 #include "params.hpp"
 
-template<typename T>
-struct random_helper{
-    static void randomize(T& a){
-        a = drand48();
-    }
-
-    static void randomize(std::complex<T>& a){
-        a.real(drand48());
-        a.imag(drand48());
-    }
-};
-
 template<class T, std::size_t Size>
 struct helper_test{
-/*nothing this function is only for TE (complex) */
+     /* only for TE (complex) */
      static void test(){ }
 };
 
@@ -25,7 +13,7 @@ struct helper_test<std::complex<T>, Size >{
          typedef ambient::numeric::tiles<ambient::numeric::matrix<std::complex<T> > > pMatrix;
      
          std::complex<T> a; 
-         random_helper<std::complex<T> >::randomize(a);
+         ambient::numeric::kernels::detail::randomize(a);
      
          pMatrix pA(Size, Size), pB(Size, Size);
          sMatrix sA(Size, Size), sB(Size, Size);
@@ -49,14 +37,14 @@ BOOST_AUTO_TEST_CASE_TEMPLATE( EXP_ALGO_HERMITIAN, T, test_types) {
     typedef ambient::numeric::tiles<ambient::numeric::matrix<typename T::value_type> > pMatrix;
 
     typename T::value_type a; 
-    random_helper<typename T::value_type>::randomize(a);
+    ambient::numeric::kernels::detail::randomize(a);
 
     int Size = T::valuex; 
 
     pMatrix pA(Size, Size), pB(Size, Size);
     sMatrix sA(Size, Size), sB(Size, Size);
    
-    generate(pA);
+    generate_hermitian(pA);
     sA = cast<sMatrix>(pA);
 
     sB = exp_hermitian(sA,a);
