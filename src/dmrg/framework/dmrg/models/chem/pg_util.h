0ca7a7333fd9147b9a6b2365fea55b9810fb0509
dolfim <dolfim@31e2c19e-47e4-4696-900e-09aa6ff92a94>
2014-01-30 14:13:29 +0000

    some other std::cout removed and creating maquis::ostream_iterator

--- src/dmrg/framework/dmrg/models/chem/pg_util.h
+++ src/dmrg/framework/dmrg/models/chem/pg_util.h
@@ -131,7 +131,7 @@ parse_symm<TwoU1PG>(int L, BaseParameters& model)
             irreps[p] = symm_vec[order[p]-1];
 
         maquis::cout << "Symmetry string (reordered): ";
-        std::copy(irreps.begin(), irreps.end(), std::ostream_iterator<subcharge>(std::cout, ","));
+        std::copy(irreps.begin(), irreps.end(), maquis::ostream_iterator<subcharge>(maquis::cout, ","));
         maquis::cout << std::endl;
 
         orb_file.close();
