d3ab5171e0a612c23ce018fc44f3155fcec55480
Sebastian Keller <sebkelle@phys.ethz.ch>
2014-07-01 12:51:58 +0200

    added SU2 fermi hubbard model

--- dmrg/framework/dmrg/models/coded/factory_2u1.hpp
+++ dmrg/framework/dmrg/models/coded/factory_2u1.hpp
@@ -25,6 +25,7 @@
  *****************************************************************************/
 
 #include "dmrg/models/coded/models_2u1.hpp"
+#include "dmrg/models/coded/models_su2.hpp"
 #include "dmrg/models/chem/model_qc.h"
 
 template<class Matrix>
@@ -35,8 +36,11 @@ struct coded_model_factory<Matrix, TwoU1> {
         typedef boost::shared_ptr<model_impl<Matrix, TwoU1> > impl_ptr;
         if (parms["MODEL"] == std::string("fermion Hubbard"))
             return impl_ptr( new FermiHubbardTwoU1<Matrix>(lattice, parms) );
-        else
-            if (parms["MODEL"] == std::string("quantum_chemistry"))
+
+        else if (parms["MODEL"] == std::string("fermion Hubbard SU2"))
+            return impl_ptr( new FermiHubbardSU2<Matrix>(lattice, parms) );
+
+        else if (parms["MODEL"] == std::string("quantum_chemistry"))
             return impl_ptr( new qc_model<Matrix, TwoU1>(lattice, parms) );
 
         else {
