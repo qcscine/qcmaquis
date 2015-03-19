22813c506ddd4fc1044a8b6746551a416c60adbe
Sebastian Keller <sebkelle@phys.ethz.ch>
2015-03-19 12:22:30 +0100

    adapted chem_helper.h for site adaptation

--- dmrg/framework/dmrg/models/chem/chem_detail.h
+++ dmrg/framework/dmrg/models/chem/chem_detail.h
@@ -28,83 +28,9 @@
 #ifndef QC_CHEM_DETAIL_H
 #define QC_CHEM_DETAIL_H
 
+#include "dmrg/models/chem/parse_integrals.h"
+
 namespace chem_detail {
-
-    template <class SymmGroup>
-    struct qn_helper
-    {
-        typename SymmGroup::charge total_qn(BaseParameters & parms)
-        {
-            typename SymmGroup::charge ret(0);
-            ret[0] = parms["u1_total_charge1"];
-            ret[1] = parms["u1_total_charge2"];
-            return ret;
-        }
-    };
-
-    template <>
-    struct qn_helper<TwoU1PG>
-    {
-        TwoU1PG::charge total_qn(BaseParameters & parms)
-        {
-            TwoU1PG::charge ret(0);
-            ret[0] = parms["u1_total_charge1"];
-            ret[1] = parms["u1_total_charge2"];
-            ret[2] = parms["irrep_charge"];
-            return ret;
-        }
-    };
-
-    class IndexTuple : public NU1Charge<4>
-    {
-    public:
-        IndexTuple() {}
-        IndexTuple(int i, int j, int k, int l) {
-            (*this)[0] = i; (*this)[1] = j; (*this)[2] = k; (*this)[3] = l;
-        }
-    };
-
-    inline IndexTuple align(int i, int j, int k, int l) {
-        if (i<j) std::swap(i,j);
-        if (k<l) std::swap(k,l);
-        if (i<k) { std::swap(i,k); std::swap(j,l); }
-        if (i==k && j<l) { std::swap(j,l); }
-        return IndexTuple(i,j,k,l);
-    }
-    
-    inline IndexTuple align(IndexTuple const & rhs) {
-        return align(rhs[0], rhs[1], rhs[2], rhs[3]);
-    }
-
-    inline int sign(IndexTuple const & idx)
-    {
-        int inv_count=0, n=4;
-        for(int c1 = 0; c1 < n - 1; c1++)
-            for(int c2 = c1+1; c2 < n; c2++)
-                if(idx[c1] > idx[c2]) inv_count++;  
-
-        return 1 - 2 * (inv_count % 2);
-    }
-
-    inline std::ostream& operator<<(std::ostream & os, IndexTuple const & c) {
-        os << "<";
-        for (int i = 0; i < 4; ++i) {
-            os << c[i];
-            if (i+1 < 4)
-                os << ",";
-        }
-        os << ">";
-        return os;
-    }
-
-    class TermTuple : public NU1Charge<8>
-    {
-    public:
-        TermTuple() {}
-        TermTuple(IndexTuple const & a, IndexTuple const & b) {
-            for (int i=0; i<4; i++) { (*this)[i] = a[i]; (*this)[i+4] = b[i]; }
-        }
-    };
 
     template <typename M, class S>
     class ChemHelper
@@ -120,11 +46,11 @@ namespace chem_detail {
                    , boost::shared_ptr<TagHandler<M, S> > tag_handler_) 
             : lat(lat_), ident(ident_), fill(fill_), tag_handler(tag_handler_)
         {
-            this->parse_integrals(parms);
+            boost::tie(idx_, matrix_elements) = parse_integrals<value_type>(parms, lat);
 
             for (std::size_t m=0; m < matrix_elements.size(); ++m) {
                 IndexTuple pos;
-                std::copy(idx_[m].begin(), idx_[m].end(), pos.begin());
+                std::copy(idx_.row(m).first, idx_.row(m).second, pos.begin());
                 coefficients[pos] = matrix_elements[m];
             }
         }
@@ -132,9 +58,7 @@ namespace chem_detail {
         std::vector<value_type> & getMatrixElements() { return matrix_elements; }
         
         int idx(int m, int pos) const {
-            //int tmp = idx_[m][pos];
-            //return tmp >= 0 ? inv_order[tmp] : tmp;
-            return idx_[m][pos];
+            return idx_(m,pos);
         }
 
         void commit_terms(std::vector<term_descriptor> & tagterms) {
@@ -240,79 +164,6 @@ namespace chem_detail {
         }
     
     private:
-        void parse_integrals(BaseParameters & parms) {
-
-            // load ordering and determine inverse ordering
-            std::vector<pos_t> order(lat.size());
-            if (!parms.is_set("orbital_order"))
-                for (pos_t p = 0; p < lat.size(); ++p)
-                    order[p] = p+1;
-            else
-                order = parms["orbital_order"].template as<std::vector<pos_t> >();
-
-            if (order.size() != lat.size())
-                throw std::runtime_error("orbital_order length is not the same as the number of orbitals\n");
-
-            std::transform(order.begin(), order.end(), order.begin(), boost::lambda::_1-1);
-            inv_order.resize(order.size());
-            for (int p = 0; p < order.size(); ++p)
-                inv_order[p] = std::distance(order.begin(), std::find(order.begin(), order.end(), p));
-
-            std::copy(order.begin(), order.end(), maquis::ostream_iterator<Lattice::pos_t>(std::cout, " "));
-            maquis::cout << std::endl;
-            std::copy(inv_order.begin(), inv_order.end(), maquis::ostream_iterator<Lattice::pos_t>(maquis::cout, " "));
-            maquis::cout << std::endl;
-
-            // ********************************************************************
-            // *** Parse orbital data *********************************************
-            // ********************************************************************
-
-            std::string integral_file = parms["integral_file"];
-            if (!boost::filesystem::exists(integral_file))
-                throw std::runtime_error("integral_file " + integral_file + " does not exist\n");
-
-            std::ifstream orb_file;
-            orb_file.open(integral_file.c_str());
-            for (int i = 0; i < 4; ++i)
-                orb_file.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-
-            std::vector<double> raw;
-            std::copy(std::istream_iterator<double>(orb_file), std::istream_iterator<double>(),
-                        std::back_inserter(raw));
-
-            std::vector<double>::iterator it = raw.begin();
-            while (it != raw.end()) {
-                
-                if (std::abs(*it) > parms["integral_cutoff"]){
-                    matrix_elements.push_back(*it++);
-                    std::vector<int> tmp;
-                    std::transform(it, it+4, std::back_inserter(tmp), boost::lambda::_1-1);
-
-                    IndexTuple aligned = align(reorder(tmp[0]), reorder(tmp[1]), reorder(tmp[2]), reorder(tmp[3]));
-                    tmp[0] = aligned[0];
-                    tmp[1] = aligned[1];
-                    tmp[2] = aligned[2];
-                    tmp[3] = aligned[3];
-
-                    idx_.push_back(tmp);
-                }
-                else { it++; }
-
-                it += 4;
-            }
-
-            #ifndef NDEBUG
-            for (std::size_t m = 0; m < matrix_elements.size(); ++m)
-            {
-                assert( *std::max_element(idx_[m].begin(), idx_[m].end()) <= lat.size() );
-            }
-            #endif
-            
-        }
-
-        int reorder(int p) {
-            return p >= 0 ? inv_order[p] : p;
-        }
 
         std::vector<tag_type> const & ident;
         std::vector<tag_type> const & fill;
@@ -320,15 +171,14 @@ namespace chem_detail {
         Lattice const & lat;
 
         std::vector<value_type> matrix_elements;
-        std::vector<std::vector<int> > idx_;
-        std::vector<int> order;
-        std::vector<int> inv_order;
+        alps::numeric::matrix<Lattice::pos_t> idx_;
+        std::vector<Lattice::pos_t> order;
+        std::vector<Lattice::pos_t> inv_order;
 
         std::map<IndexTuple, value_type> coefficients;
 
         std::map<TermTuple, term_descriptor> three_terms;
         std::map<IndexTuple, term_descriptor> two_terms;
-
     };
 }
 
