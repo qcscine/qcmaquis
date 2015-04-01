c98bb90946db09357e33aa1c74c352278e9a387d
Sebastian Keller <sebkelle@phys.ethz.ch>
2015-04-01 13:51:11 +0200

    deactivated pg_symmetry_converter -- works\!

--- dmrg/framework/dmrg/models/generate_mpo.hpp
+++ dmrg/framework/dmrg/models/generate_mpo.hpp
@@ -48,8 +48,8 @@ MPO<Matrix, SymmGroup> make_mpo(Lattice const& lat, Model<Matrix, SymmGroup> con
     generate_mpo::TaggedMPOMaker<Matrix, SymmGroup> mpom(lat, model);
     MPO<Matrix, SymmGroup> mpo = mpom.create_mpo();
     
-    PGSymmetryConverter<Matrix, SymmGroup> symm_conv(lat);
-    symm_conv.convert_tags_to_symm_tags(mpo);
+    //PGSymmetryConverter<Matrix, SymmGroup> symm_conv(lat);
+    //symm_conv.convert_tags_to_symm_tags(mpo);
     
     return mpo;
 }
