6db0b80c9e39ba25401fc7748dd98cd8cbe08abe
Sebastian Keller <sebkelle@phys.ethz.ch>
2015-04-01 13:15:37 +0200

    use get_operator_tag(name) to access ident_full

--- dmrg/framework/dmrg/models/chem/su2u1/model_su2.h
+++ dmrg/framework/dmrg/models/chem/su2u1/model_su2.h
@@ -82,11 +82,11 @@ public:
     }
     tag_type identity_matrix_tag(size_t type) const
     {
-        return ident[0];
+        return ident[type];
     }
     tag_type filling_matrix_tag(size_t type) const
     {
-        return fill[0];
+        return fill[type];
     }
 
     typename SymmGroup::charge total_quantum_numbers(BaseParameters & parms_) const
@@ -97,9 +97,9 @@ public:
     tag_type get_operator_tag(std::string const & name, size_t type) const
     {
         if (name == "docc")
-            return docc[0];
+            return docc[type];
         else if (name == "ident_full")
-            return ident_full[0];
+            return ident_full[type];
         else
             throw std::runtime_error("Operator not valid for this model.");
         return 0;
