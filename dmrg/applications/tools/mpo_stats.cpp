51a3076d83cf79e4caa836130aefd1cf222d6c63
Sebastian Keller <sebkelle@phys.ethz.ch>
2015-04-02 14:49:13 +0200

    restored mpo_stats to original state

--- dmrg/applications/tools/mpo_stats.cpp
+++ dmrg/applications/tools/mpo_stats.cpp
@@ -89,13 +89,8 @@ int main(int argc, char ** argv)
             std::ofstream ofs(std::string("mpo_stats."+boost::lexical_cast<std::string>(p)+".dat").c_str());
             for (int b1 = 0; b1 < mpo[p].row_dim(); ++b1) {
                 for (int b2 = 0; b2 < mpo[p].col_dim(); ++b2) {
-                    if (mpo[p].has(b1, b2)) {
-						ofs << mpo[p].tag_number(b1,b2) << " ";
-						maquis::cout << "H(" << b1 << "," << b2 << "): " << mpo[p].at(b1,b2).scale << "\n" << mpo[p].at(b1,b2).op.basis() << "\n\n";
-					}
-					else {
-						ofs << ". ";
-					}
+                    if (mpo[p].has(b1, b2)) ofs << mpo[p].tag_number(b1,b2) << " ";
+					else ofs << ". ";
                 }
                 ofs << std::endl;
             }
