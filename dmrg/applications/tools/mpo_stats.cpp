f71b726a4df0a0d0817a867f2d66ea6a4ee5de7c
Sebastian Keller <sebkelle@phys.ethz.ch>
2015-04-02 14:50:13 +0200

    restored mpo_stats to original state + 2 more lines reverted

--- dmrg/applications/tools/mpo_stats.cpp
+++ dmrg/applications/tools/mpo_stats.cpp
@@ -85,12 +85,11 @@ int main(int argc, char ** argv)
         MPO<matrix, symm> mpo = make_mpo(lattice, model);
         
         for (int p = 0; p < lattice.size(); ++p) {
-			maquis::cout << "######## SITE " << p << " ########\n\n";
             std::ofstream ofs(std::string("mpo_stats."+boost::lexical_cast<std::string>(p)+".dat").c_str());
             for (int b1 = 0; b1 < mpo[p].row_dim(); ++b1) {
                 for (int b2 = 0; b2 < mpo[p].col_dim(); ++b2) {
                     if (mpo[p].has(b1, b2)) ofs << mpo[p].tag_number(b1,b2) << " ";
-					else ofs << ". ";
+                    else ofs << ". ";
                 }
                 ofs << std::endl;
             }
