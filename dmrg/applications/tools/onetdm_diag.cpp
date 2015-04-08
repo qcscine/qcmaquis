2571aa7aab69db19803037d3747d9d0038b87bc7
Stefan Knecht <stefan.knecht@phys.chem.ethz.ch>
2015-04-08 16:29:02 +0200

    save progress in 1-TDM diagonal elements tool...

--- dmrg/applications/tools/onetdm_diag.cpp
+++ dmrg/applications/tools/onetdm_diag.cpp
@@ -45,9 +45,14 @@ typedef ambient::numeric::tiles<ambient::numeric::matrix<double> > matrix;
 typedef alps::numeric::matrix<double> matrix;
 #endif
 
+#include <alps/hdf5.hpp>
+
 #include "dmrg/block_matrix/indexing.h"
+#include "dmrg/block_matrix/block_matrix.h"
+
 #include "dmrg/mp_tensors/mps.h"
 #include "dmrg/mp_tensors/mpo.h"
+
 #include "dmrg/models/model.h"
 #include "dmrg/models/generate_mpo.hpp"
 #include "dmrg/mp_tensors/contractions.h"
@@ -71,34 +76,115 @@ typedef U1 symm;
 #include "dmrg/utils/DmrgOptions.h"
 #include "dmrg/utils/DmrgParameters.h"
 
+struct corr_measurement {
+    typedef operator_selector<matrix, symm>::type op_t;
+    
+    std::string name;
+    std::vector<op_t> op1;
+    bool fermionic;
+};
+
 
 int main(int argc, char ** argv)
 {
     try {
-        if (argc != 4) {
-            std::cout << "Usage: " << argv[0] << " dmrg-input <mps1.h5> <mps2.h5>" << std::endl;
+        if (argc != 2) {
+            std::cout << "Usage: " << argv[0] << " inputfile " << std::endl;
             return 1;
         }
-        // load bra and ket MPS
-        MPS<matrix, symm> mps1, mps2;
-        load(argv[2], mps1);
-        load(argv[3], mps2);
 
         DmrgOptions opt(argc, argv);
         if (!opt.valid) return 0;
         DmrgParameters parms = opt.parms;
+
+        std::cout << "parameter " << parms << std::endl;
+
+        typedef operator_selector<matrix, symm>::type op_t;
+        typedef Model<matrix, symm>::table_ptr table_ptr;
+        typedef Model<matrix, symm>::tag_type tag_type;
+        typedef std::vector<op_t> op_vec;
+        typedef std::vector<std::pair<op_vec, bool> > bond_element;
         
-        maquis::cout.precision(15);
+        typedef typename matrix::value_type value_type;
+        value_type result;
+        std::vector<typename MPS<matrix, symm>::scalar_type> vector_results;
+        std::vector<std::string> labels;
+
         
         /// Parsing model
         Lattice lattice = Lattice(parms);
         Model<matrix, symm> model = Model<matrix, symm>(lattice, parms);
-        
-        //MPO<matrix, symm> mpo = make_mpo(lattice, model);
-        
+        int ntypes = lattice.maximum_vertex_type()+1;
+        table_ptr tag_handler = model.operators_table();
+
+        // types are point group irreps...
+        std::cout << "ntypes " << ntypes << std::endl;
+
+        maquis::cout.precision(15);
+        std::size_t L = lattice.size();
+
+        // load bra (chkp_lhs) and ket (chkpfile) MPS
+        MPS<matrix, symm> mps1, mps2;
+        load(parms["chkp_lhs"].str(),mps2);
+        load(parms["chkpfile"].str(),mps1);
+
+        /// identities and fillings
+        std::vector<op_t> identities(ntypes), fillings(ntypes);
+        for (size_t type = 0; type < ntypes; ++type) {
+            identities[type] = model.identity_matrix(type);
+            fillings[type]   = model.filling_matrix(type);
+        }
+
+        // we need to measure for local ops: c+up cup, c+down cdown, c+up cdown, c+down cup
+        for (std::size_t p = 0; p < L; ++p)
+        {
+
+        vector_results.clear();
+        labels.clear();
+        vector_results.reserve(vector_results.size() + L);
+        labels.reserve(labels.size() + L);
+        {
+            for (int i = 0; i < 4; ++i){
+                    op_vec meas_op;
+                    //if (i == 1)
+                        //meas_op = count_up_op;
+                    //else if (i == 1)
+                        //meas_op = count_down_op;
+                    //else if (i == 2)
+                        //meas_op = swap_u2d_op;
+                    //else if (i == 3)
+                        //meas_op = swap_d2u_op;
+                    //else
+                     //   throw std::runtime_error("Invalid observable\n");
+            }
+        }
+
+              int type = lattice.get_prop<int>("type", p);
+              std::cout << "type for site p " << p << ": "<< lattice.get_prop<int>("type", p)  << std::endl;
+              std::cout << "tag type for nup at site p " << p << ": "<< model.get_operator_tag("count_up", type)  << std::endl;
+              std::cout << "tag type for ndown at site p " << p << ": "<< model.get_operator_tag("count_down", type)  << std::endl;
+              std::cout << "tag type for u2d at site p " << p << ": "<< model.get_operator_tag("u2d", type)  << std::endl;
+              std::cout << "tag type for d2u at site p " << p << ": "<< model.get_operator_tag("d2u", type)  << std::endl;
+
+              // generate MPOs
+              //std::map<std::string, MPO<matrix, symm> > mpos;
+
+              //vector_results.push_back(val);
+              std::cout << "label for site p " << p << ": "<< lattice.get_prop<std::string>("label", p)  << std::endl;
+
+              //double val = expval(mps1, mps2, mpo)
+              labels.push_back( lattice.get_prop<std::string>("label", p) );
+
+              // save the data...
+              {
+                  alps::hdf5::archive oh5(parms["resultfile"].str(), "w");
+                  oh5["/spectrum/results/1-TDM diagonal/mean/value"] << vector_results;
+                  oh5["/spectrum/results/1-TDM diagonal/labels"] << labels;
+            }
+        }
         
     } catch (std::exception& e) {
-        std::cerr << "Error:" << std::endl << e.what() << std::endl;
+        std::cerr << "Error:" << e.what() << std::endl;
         return 1;
     }
 }
