3e10292ebe89ee4b3d6e38e1115e13f8cc4b8e7a
Sebastian Keller <sebkelle@phys.ethz.ch>
2015-04-21 17:39:20 +0200

    change include order in generate_mpo.hpp

--- dmrg/framework/dmrg/models/generate_mpo.hpp
+++ dmrg/framework/dmrg/models/generate_mpo.hpp
@@ -27,14 +27,14 @@
 #ifndef GENERATE_MPO_H
 #define GENERATE_MPO_H
 
+#include "dmrg/models/model.h"
+
+#include "dmrg/models/chem/pg_symm_converter.h"
+
 #include "dmrg/models/generate_mpo/mpo_maker.hpp"
 #include "dmrg/models/generate_mpo/tagged_mpo_maker_optim.hpp"
 #include "dmrg/models/generate_mpo/corr_maker.hpp"
 #include "dmrg/models/generate_mpo/bg_corr_maker.hpp"
-
-#include "dmrg/models/chem/pg_symm_converter.h"
-
-#include "dmrg/models/model.h"
 
 
 template<class Matrix, class SymmGroup>
