a7b5e40b8a7b4a2892ec84eb1563018983d627fb
Stefan Knecht <stefan.knecht@phys.chem.ethz.ch>
2015-05-08 12:47:56 +0200

    introduce index restriction in 4-RDM calculation

--- dmrg/framework/dmrg/models/measurements/tagged_nrankrdm.h
+++ dmrg/framework/dmrg/models/measurements/tagged_nrankrdm.h
@@ -424,20 +424,18 @@ namespace measurements {
             bool bra_neq_ket = (dummy_bra_mps.length() > 0);
             MPS<Matrix, SymmGroup> const & bra_mps = (bra_neq_ket) ? dummy_bra_mps : ket_mps;
 
-            // if bra != ket, no transpose symmetry
-            //for (pos_t p6 = ((bra_neq_ket) ? 0 : std::min(p4, p5)); p6 < lattice.size(); ++p6)
             #ifdef MAQUIS_OPENMP
             #pragma omp parallel for collapse(1)
             #endif
             for (pos_t p1 = 0; p1 < 3; ++p1)
-            for (pos_t p2 = 0; p2 < 3; ++p2)
-            for (pos_t p3 = 0; p3 < 3; ++p3)
+            for (pos_t p2 = 0; p2 < p1+1; ++p2)
+            for (pos_t p3 = 0; p3 < p2+1; ++p3)
             { 
                  // third index must be different if p1 == p2 
                  if(p1 == p2 && p3 == p1)
                       continue;
                  
-                 for (pos_t p4 = 0; p4 < 3; ++p4)
+                 for (pos_t p4 = 0; p4 < p3+1; ++p4)
                  {
                      // fourth index must be different if p1 == p2 or p1 == p3 or p2 == p3
                      if((p1 == p2 && p4 == p1) || (p1 == p3 && p4 == p1) || (p2 == p3 && p4 == p2))
