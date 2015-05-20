6c286de7385988dc71eecff942c1ba98555fba51
Sebastian Keller <sebkelle@phys.ethz.ch>
2015-05-20 16:57:18 +0200

    change start values of linfit parameters and disable python2.6 unsupported tick_label_format

--- dmrg/lib/python/pyeval/extrapolate_base.py
+++ dmrg/lib/python/pyeval/extrapolate_base.py
@@ -89,7 +89,7 @@ class extrapolator(object):
         if num_points is None:
             num_points = len(self._ydata)
         ff = lambda self, x, pars: pars[0]()*x + pars[1]()
-        pars = [fw.Parameter(0.), fw.Parameter(0.01)]
+        pars = [fw.Parameter(1.), fw.Parameter(-1.)]
         fw.fit(None, ff, pars, np.array(self._ydata[0:num_points]), np.array(self._xdata[0:num_points]))
         
         fit_func = lambda x: pars[0].get()*x + pars[1].get()
