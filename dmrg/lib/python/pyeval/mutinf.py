b0b2c1c9f324769f7a2e3be2310efd20ffb2479a
Sebastian Keller <sebkelle@phys.ethz.ch>
2015-05-29 15:31:38 +0200

    updated mutinf.py to take ordering into account

--- dmrg/lib/python/pyeval/mutinf.py
+++ dmrg/lib/python/pyeval/mutinf.py
@@ -26,8 +26,102 @@
 #* DEALINGS IN THE SOFTWARE.
 #*
 #*****************************************************************************
+
 import sys                
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import lines
+from pylab import pi
+
 import s2
+import h5props
+
+def plot_mutinf(entanglement_measurement, props):
+    #MUTUALI INFORMATION PLOT:
+
+    #mutual information matrix:
+    mat_I = entanglement_measurement.I()
+    #single entropy vector:
+    vec_s1= entanglement_measurement.s1()
+
+    #orbital ordering
+    order = map(int, props["orbital_order"].split(','))
+
+    N = len(entanglement_measurement.I())
+    theta = np.zeros(N)
+    r = np.zeros(N)
+    labels = np.zeros(N)
+    area = np.zeros(N)
+
+    for i in range(N):
+      theta[i] = -2*pi/N*i+pi/2
+      r[i] = 1.0
+      labels[i] = order[i]
+      area[i] = vec_s1[i]*500
+
+
+    ax = plt.subplot(111, polar=True)
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+    ax.grid(b=False)
+    c = plt.scatter(theta,r,c="Red",s=area)
+
+    plt.title('N = '+str(N/2)+', Results file: '+sys.argv[1], y = 1.2)
+
+    #this is dummy:
+    c1 = plt.scatter(theta,(r+0.1),c="red",s=0)
+
+    # generation of orbital images. If "-i" switch is passed to the script, the script will incorporate orbital pictures into the image. Pictures must be present in the current directory with names #.png where # is the number of each site. Such images can be conveniently generated with gabedit, vmd or any other orbital plotting program you desire.
+
+    # Generation of pictures requires new python and matplotlib versions
+    pics=False
+    if (len(sys.argv) > 2):
+      if (sys.argv[2] == '-i'):
+        pics=True
+
+    legendlines = {}
+
+    for i in range(N):
+    #  plt.annotate(int(labels[i]),xy=(theta[i],(r[i]+0.2)),size='xx-large',)
+      plt.text(theta[i],(r[i]+0.18),int(labels[i]),size='xx-large',ha='center',va='center')
+      
+      if(pics): # generate pictures.
+        #import matplotlib.pyplot as plt
+        #from matplotlib import lines
+        #from pylab import *
+        from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+        from matplotlib.cbook import get_sample_data
+        from matplotlib._png import read_png
+        
+        #img=OffsetImage(read_png(str(i+1)+".png"),zoom=0.08) # The zoom factor should be ideally adjusted to the size of the images
+        print i+1, labels[i]
+        img=OffsetImage(read_png("t" + str(int(labels[i]))+".png"),zoom=0.2) # The zoom factor should be ideally adjusted to the size of the images
+        ab=AnnotationBbox(img,[theta[i],r[i]+0.57], frameon=False) # pass Frameon=False to disable the frames around the images
+        ax.add_artist(ab)
+        
+      for j in range(i,N):
+        x =[theta[i],theta[j]]
+        y =[1,1]
+        if mat_I[i,j] >= 0.1:
+          line = lines.Line2D(x, y, linewidth=2*10*mat_I[i,j], color='black',linestyle='-', alpha=1,label='0.1')
+          legendlines['0.1'] = line
+          ax.add_line(line)
+        elif mat_I[i,j] >=0.01:
+          line = lines.Line2D(x, y, linewidth=2*30*mat_I[i,j], color='gray',linestyle='--', alpha=1,label='0.01')
+          legendlines['0.01'] = line
+          ax.add_line(line)
+        elif mat_I[i,j] >=0.001:
+          line = lines.Line2D(x, y, linewidth=1.5, color='lime',linestyle=':', alpha=1,label='0.001')
+          legendlines['0.001'] = line
+          ax.add_line(line)
+
+
+    #plt.tight_layout(h_pad = 0.5)
+    #plt.subplots_adjust(bottom=0.2)
+
+    ax.legend(legendlines.values(),[l.get_label() for l in legendlines.values()],bbox_to_anchor=(0.00,1.0),fancybox=True,shadow=True)
+    plt.show()
+
 if __name__ == '__main__':
     inputfile = sys.argv[1]
 
@@ -43,84 +137,6 @@ if __name__ == '__main__':
     print "I (mutual information)"
     s2.pretty_print(guinea_pig.I())
 
-import matplotlib.pyplot as plt
-from matplotlib import lines
-from pylab import *
+    props = h5props.loadProperties(inputfile)
 
-#MUTUALI INFORMATION PLOT:
-
-#mutual information matrix:
-mat_I=guinea_pig.I()
-#single entropy vector:
-vec_s1=guinea_pig.s1()
-
-N= len(guinea_pig.I())
-theta = np.zeros(N)
-r=np.zeros(N)
-labels=np.zeros(N)
-area=np.zeros(N)
-
-for i in range(N):
-  theta[i]=-2*pi/N*i+pi/2
-  r[i]=1.0
-  labels[i]=i+1
-  area[i]=vec_s1[i]*500
-
-
-ax = plt.subplot(111,polar=True)
-ax.set_xticklabels([])
-ax.set_yticklabels([])
-ax.grid(b=False)
-c = plt.scatter(theta,r,c="Red",s=area)
-
-plt.title('N = '+str(N/2)+', Results file: '+sys.argv[1])
-
-#this is dummy:
-c1 = plt.scatter(theta,(r+0.1),c="red",s=0)
-
-# generation of orbital images. If "-i" switch is passed to the script, the script will incorporate orbital pictures into the image. Pictures must be present in the current directory with names #.png where # is the number of each site. Such images can be conveniently generated with gabedit, vmd or any other orbital plotting program you desire.
-
-# Generation of pictures requires new python and matplotlib versions
-pics=False
-if (len(sys.argv) > 2):
-  if (sys.argv[2] == '-i'):
-    pics=True
-
-legendlines = {}
-
-for i in range(N):
-#  plt.annotate(int(labels[i]),xy=(theta[i],(r[i]+0.2)),size='xx-large',)
-  plt.text(theta[i],(r[i]+0.18),int(labels[i]),size='xx-large',ha='center',va='center')
-  
-  if(pics): # generate pictures.
-    import matplotlib.pyplot as plt
-    from matplotlib import lines
-    from pylab import *
-    from matplotlib.offsetbox import OffsetImage, AnnotationBbox
-    from matplotlib.cbook import get_sample_data
-    from matplotlib._png import read_png
-    
-    img=OffsetImage(read_png(str(i+1)+".png"),zoom=0.4) # The zoom factor should be ideally adjusted to the size of the images
-    ab=AnnotationBbox(img,[theta[i],r[i]+0.57]) # pass Frameon=False to disable the frames around the images
-    ax.add_artist(ab)
-    
-  for j in range(i,N):
-    x =[theta[i],theta[j]]
-    y =[1,1]
-    if mat_I[i,j] >= 0.1:
-      line = lines.Line2D(x, y, linewidth=2*10*mat_I[i,j], color='black',linestyle='-', alpha=1,label='0.1')
-      legendlines['0.1'] = line
-      ax.add_line(line)
-    elif mat_I[i,j] >=0.01:
-      line = lines.Line2D(x, y, linewidth=2*30*mat_I[i,j], color='gray',linestyle='--', alpha=1,label='0.01')
-      legendlines['0.01'] = line
-      ax.add_line(line)
-    elif mat_I[i,j] >=0.001:
-      line = lines.Line2D(x, y, linewidth=1.5, color='lime',linestyle=':', alpha=1,label='0.001')
-      legendlines['0.001'] = line
-      ax.add_line(line)
-
-
-ax.legend(legendlines.values(),[l.get_label() for l in legendlines.values()],bbox_to_anchor=(0.00,1.0),fancybox=True,shadow=True)
-plt.show()
-
+    plot_mutinf(guinea_pig, props) 
