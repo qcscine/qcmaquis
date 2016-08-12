fe98852963984c44d5d86eb63a0f643fae033373
Sebastian Keller <sebkelle@phys.ethz.ch>
2016-08-12 16:05:54 +0200

    remove the order vector from TaggedNrankRDM now that we have the the lattice

--- dmrg/framework/dmrg/models/measurements/tagged_nrankrdm.h
+++ dmrg/framework/dmrg/models/measurements/tagged_nrankrdm.h
@@ -474,8 +474,7 @@ namespace measurements {
         }
         
         void measure_correlation(MPS<Matrix, SymmGroup> const & dummy_bra_mps,
-                                 MPS<Matrix, SymmGroup> const & ket_mps,
-                                 std::vector<pos_t> const & order = std::vector<pos_t>())
+                                 MPS<Matrix, SymmGroup> const & ket_mps)
         {
             // Test if a separate bra state has been specified
             bool bra_neq_ket = (dummy_bra_mps.length() > 0);
@@ -522,8 +521,9 @@ namespace measurements {
                     num_labels.push_back(positions);
                 }
 
-                std::vector<std::string> lbt = label_strings(lattice,  (order.size() > 0)
-                                            ? detail::resort_labels(num_labels, order, false) : num_labels );
+                // the lattice knows the ordering and provides the correct orbital label for each position
+                std::vector<std::string> lbt = label_strings(lattice,  num_labels);
+
                 // save results and labels
                 #ifdef MAQUIS_OPENMP
                 #pragma omp critical
@@ -539,8 +539,7 @@ namespace measurements {
         }
 
         void measure_2rdm(MPS<Matrix, SymmGroup> const & dummy_bra_mps,
-                          MPS<Matrix, SymmGroup> const & ket_mps,
-                          std::vector<pos_t> const & order = std::vector<pos_t>())
+                          MPS<Matrix, SymmGroup> const & ket_mps)
         {
             // Test if a separate bra state has been specified
             bool bra_neq_ket = (dummy_bra_mps.length() > 0);
@@ -588,8 +587,7 @@ namespace measurements {
 
                 }
 
-                std::vector<std::string> lbt = label_strings(lattice,  (order.size() > 0)
-                                            ? detail::resort_labels(num_labels, order, false) : num_labels );
+                std::vector<std::string> lbt = label_strings(lattice,  num_labels);
 
                 // save results and labels
                 #ifdef MAQUIS_OPENMP
