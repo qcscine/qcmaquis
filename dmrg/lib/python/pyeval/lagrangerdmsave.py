0f8011dedbaa85c2e4e2a0af6e15fe3981d6bf5c
Leon Freitag <lefreita@ethz.ch>
2018-10-14 02:13:11 +0200

    Python scripts to extract local Hamiltonian and Lagrange RDM updates

---
+++ dmrg/lib/python/pyeval/lagrangerdmsave.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python2
+#-*- coding: utf-8 -*-
+#*****************************************************************************
+#*
+#* ALPS MPS DMRG Project
+#*
+#* Copyright (C) 2014 Laboratory for Physical Chemistry, ETH Zurich
+#*               2014-2014 by Sebastian Keller <sebkelle@phys.ethz.ch>
+#*               2014-2015 by Yingjin Ma <yma@ethz.ch>
+#*               2018      by Leon Freitag <lefreita@ethz.ch>                                                      
+#*
+#* 
+#* This software is part of the ALPS Applications, published under the ALPS
+#* Application License; you can use, redistribute it and/or modify it under
+#* the terms of the license, either version 1 or (at your option) any later
+#* version.
+#* 
+#* You should have received a copy of the ALPS Application License along with
+#* the ALPS Applications; see the file LICENSE.txt. If not, the license is also
+#* available from http://alps.comp-phys.org/.
+#*
+#* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+#* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+#* FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT 
+#* SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE 
+#* FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE, 
+#* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+#* DEALINGS IN THE SOFTWARE.
+#*
+#*****************************************************************************
+
+# This scripts extracts the RDM Lagrange updates from QCMaquis HDF5 file and
+# saves them in human-readable ASCII files (one|two)rdmlagrange(L|R)
+# Both left and right RDMs, as well as both 1- and 2-RDMs must be calculated
+# prior to the invocation of this script.
+
+import sys
+import pyalps
+import numpy as np
+
+
+def load_1rdm(inputfile,meas):
+    # load data from the HDF5 result file
+    rdm =  pyalps.loadEigenstateMeasurements([inputfile], what=meas)[0][0]
+    return rdm
+
+def load_2rdm(inputfile,meas):
+    # load data from the HDF5 result file
+    rdm =  pyalps.loadEigenstateMeasurements([inputfile], what=meas)[0][0]
+    rdm.y[0] = 0.5 * rdm.y[0]
+    return rdm
+
+def save_1rdm(rdm,filename):
+    fmt = '%14.14e'
+
+    L = int(rdm.props["L"])
+    mat = np.zeros((L,L))
+
+    for lab, val in zip(rdm.x, rdm.y[0]):
+        i = lab[0]
+        j = lab[1]
+        mat[i,j] = val;
+        mat[j,i] = val;
+
+    f=open(filename,'w')
+    f.write(str(L)+'\n')
+
+    for i in range(L):
+        for j in range(L):
+            f.write(str(i)+' '+str(j)+' '+str(fmt%mat[i,j])+'\n')
+    f.close()
+
+    return L
+
+def save_2rdm(rdm,L,filename):
+    fmt = '%14.14e'
+
+    f=open(filename,'w')
+    f.write(str(L)+'\n')
+    for lab, val in zip(rdm.x, rdm.y[0]):
+        i = lab[0]
+        j = lab[1]
+        k = lab[2]
+        l = lab[3]
+        f.write(str(i)+' '+str(j)+' '+str(k)+' '+str(l)+' '+str(fmt%val)+'\n')
+    f.close()
+
+if __name__ == '__main__':
+    inputfile = sys.argv[1]
+
+    for c in ['L','R']:
+        rdm1=load_1rdm(inputfile,'onerdmlagrange%s'%c)
+        L=save_1rdm(rdm1,'onerdmlagrange%s'%c)
+
+        rdm2 = load_2rdm(inputfile,'twordmlagrange%s'%c)
+        save_2rdm(rdm2,L,'twordmlagrange%s'%c)
+
---
+++ dmrg/lib/python/pyeval/local_hamiltonian.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python2
+#-*- coding: utf-8 -*-
+#*****************************************************************************
+#*
+#* ALPS MPS DMRG Project
+#*
+#* Copyright (C) 2014 Laboratory for Physical Chemistry, ETH Zurich
+#*               2014-2014 by Sebastian Keller <sebkelle@phys.ethz.ch>
+#*               2014-2015 by Yingjin Ma <yma@ethz.ch>
+#*               2018      by Leon Freitag <lefreita@ethz.ch>                                                      
+#*
+#* 
+#* This software is part of the ALPS Applications, published under the ALPS
+#* Application License; you can use, redistribute it and/or modify it under
+#* the terms of the license, either version 1 or (at your option) any later
+#* version.
+#* 
+#* You should have received a copy of the ALPS Application License along with
+#* the ALPS Applications; see the file LICENSE.txt. If not, the license is also
+#* available from http://alps.comp-phys.org/.
+#*
+#* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+#* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+#* FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT 
+#* SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE 
+#* FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE, 
+#* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+#* DEALINGS IN THE SOFTWARE.
+#*
+#*****************************************************************************
+
+# This scripts extracts the local Hamiltonian from QCMaquis HDF5 file and
+# saves it into an ASCII file named local_hamiltonian.txt
+
+import sys
+import pyalps
+import numpy as np
+
+
+def load_meas(inputfile,meas):
+    # load data from the HDF5 result file
+    m = pyalps.loadEigenstateMeasurements([inputfile], what=meas)[0][0]
+    return m
+
+
+def save_hamiltonian(m):
+    #fmt = '%14.14e'
+
+    # get indices
+    
+    # convert the compound MPS indices (i.e. 0-1-2) to a single running index
+    
+    # first, the indices are converted into an intermediate form
+    # itm_index = a*(m+1)^2+b*(m+1)+c where m is the max. index present
+    
+    order = max([ max([ m.x[i][j] for i in range(len(m.x)) ]) for j in range(3) ])
+
+    def itm_index(index_list):
+        return sum([index_list[j]*(order+1)**(2-j) for j in range(3)])
+    
+    # create an array of intermediate indices via a list comprehension
+    # from the first three indices
+    intermediate_index = [ itm_index(i) for i in [ [ m.x[k][j] for j in range(3) ] for k in range(len(m.x)) ] ]
+
+    # now convert intermediate index to a (final) consecutive index with a dictionary
+    
+    idxdict = {}
+    
+    idx = 0
+    for i in intermediate_index:
+        if (idxdict.get(i) == None): # since intermediate indices contain duplicates, we consider only one unique index at a time
+            idxdict[i] = idx
+            idx += 1
+            
+    # now idxdict contains the mapping of the intermediate indices to the final ones        
+    
+    # determine the matrix size from final indices
+    n = max(idxdict.values())+1
+    
+    # create a numpy array
+    mat = np.zeros((n,n))
+    
+    # and fill it with the values
+    # the zip expression returns a tuple, with the first element as the list of two original index lists and the second element as the values
+
+    for lab, val in zip([ [ [ m.x[k][j] for j in range(3) ], [ m.x[k][j] for j in range(3,6) ] ] for k in range(len(m.x)) ], m.y[0]):
+        mat[idxdict[itm_index(lab[0])],idxdict[itm_index(lab[1])]] = val
+
+    # save the array to file
+    
+    filename = "local_hamiltonian.txt"
+    np.savetxt(filename, mat)
+
+
+if __name__ == '__main__':
+    inputfile = sys.argv[1]
+    save_hamiltonian(load_meas(inputfile,"local_hamiltonian"))
+    
+
