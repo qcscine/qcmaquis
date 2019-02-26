bd58918a8ac7e4eea2c50914942278e12dface55
Leon Freitag <lefreita@ethz.ch>
2019-02-26 21:31:33 +0100

    Cache gsl_coupling_9j

--- dmrg/framework/dmrg/block_matrix/symmetry/gsl_coupling.h
+++ dmrg/framework/dmrg/block_matrix/symmetry/gsl_coupling.h
@@ -4,28 +4,33 @@
  *
  * Copyright (C) 2015 Institute for Theoretical Physics, ETH Zurich
  *               2015-2015 by Sebastian Keller <sebkelle@phys.ethz.ch>
- * 
+ *               2019 by Leon Freitag <lefreita@ethz.ch>
+ *
  * This software is part of the ALPS Applications, published under the ALPS
  * Application License; you can use, redistribute it and/or modify it under
  * the terms of the license, either version 1 or (at your option) any later
  * version.
- * 
+ *
  * You should have received a copy of the ALPS Application License along with
  * the ALPS Applications; see the file LICENSE.txt. If not, the license is also
  * available from http://alps.comp-phys.org/.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT 
- * SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE 
- * FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE, 
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE
+ * FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  *
  *****************************************************************************/
 
 #ifndef GSL_COUPLING_H
 #define GSL_COUPLING_H
+
+#include <unordered_map>
+#include <mutex>
+#include "hash_tuple.h"
 
 extern "C" {
     double gsl_sf_coupling_3j(int two_ja, int two_jb, int two_jc, int two_ma, int two_mb, int two_mc);
@@ -35,6 +40,106 @@ extern "C" {
 
 namespace SU2 {
 
+    inline bool triangle(int a, int b, int c)
+    {
+        return std::abs(a-b) <= c && c <= a+b;
+    }
+
+    // Cache for Wigner-9j symbols
+    // Profilers show that over 60% of the time is spent in gsl_sf_coupling_9j
+    // therefore we need a class that caches the calls to this function
+    // uses std::unordered map as a cache
+    // Currently we don't exploit the permutation symmetry of the Wigner 9j symbols yet
+    // but this should be done in the future
+    class GSLCouplingCache
+    {
+        private:
+            typedef std::tuple<int, int, int, int, int, int, int, int, int> gsl_indices;
+            // derive gsl_indices from tuple to override operator== to incorporate symmetry
+
+            // The map that stores the values
+            std::unordered_map<gsl_indices, double, hash_tuple::hash<gsl_indices> > map;
+
+            // for thread-safety of writing
+            std::mutex m_;
+        public:
+            inline double wigner_9j(int a, int b, int c,
+                                    int d, int e, int f,
+                                    int g, int h, int i)
+            {
+                gsl_indices idx=std::make_tuple(a,b,c,d,e,f,g,h,i);
+                double ret;
+                // Search for the value in cache
+                auto map_idx = map.find(idx);
+                // If value not found in cache, calculate and add it
+                if (map_idx == map.end())
+                {
+                    // ret = sqrt( (g+1.) * (h+1.) * (c+1.) * (f+1.) ) *
+                    ret = gsl_sf_coupling_9j(a, b, c,
+                                             d, e, f,
+                                             g, h, i);
+
+                    std::lock_guard<std::mutex> lk(this->m_);
+                    map[idx] = ret;
+                    // map.emplace(idx, ret);
+
+                }
+                else // use the cached value
+                {
+                    ret = map_idx->second;
+                }
+                return ret;
+            }
+
+            inline double mod_coupling(int a, int b, int c,
+                        int d, int e, int f,
+                        int g, int h, int i)
+            {
+                return sqrt( (g+1.) * (h+1.) * (c+1.) * (f+1.) ) *
+                    wigner_9j(a, b, c,
+                              d, e, f,
+                              g, h, i);
+            }
+
+            template <class T>
+            inline void set_coupling(int a, int b, int c,
+                                     int d, int e, int f,
+                                     int g, int h, int i, T init, T couplings[])
+            {
+                T prefactor = T(sqrt((i+1.)*(a+1.)/((g+1.)*(c+1.)))) * init;
+                if (triangle(a,b,c))
+                {
+                    couplings[0] = prefactor * (T)mod_coupling(a, b, c, d, e, f, g, h, i);
+                    couplings[2] = prefactor * (T)mod_coupling(a, b, c, d, e, f, g, 2, i);
+                }
+                if (triangle(a,2,c))
+                {
+                    couplings[1] = prefactor * (T)mod_coupling(a, 2, c, d, e, f, g, h, i);
+                    couplings[3] = prefactor * (T)mod_coupling(a, 2, c, d, e, f, g, 2, i);
+                }
+            }
+
+    };
+
+    static GSLCouplingCache gsl_coupling_cache;
+
+    inline double mod_coupling(int a, int b, int c,
+                        int d, int e, int f,
+                        int g, int h, int i)
+    {
+        return gsl_coupling_cache.mod_coupling(a,b,c,d,e,f,g,h,i);
+    }
+
+    template <class T>
+    inline void set_coupling(int a, int b, int c,
+                             int d, int e, int f,
+                             int g, int h, int i, T init, T couplings[])
+    {
+        gsl_coupling_cache.set_coupling<T>(a,b,c,d,e,f,g,h,i,init,couplings);
+    }
+
+    /*
+    // Non-cache versions
     inline double mod_coupling(int a, int b, int c,
                         int d, int e, int f,
                         int g, int h, int i)
@@ -44,11 +149,6 @@ namespace SU2 {
                                   d, e, f,
                                   g, h, i);
         return ret;
-    }
-
-    inline bool triangle(int a, int b, int c)
-    {
-        return std::abs(a-b) <= c && c <= a+b;
     }
 
     template <class T>
@@ -68,6 +168,7 @@ namespace SU2 {
             couplings[3] = prefactor * (T)::SU2::mod_coupling(a, 2, c, d, e, f, g, 2, i);
         }
     }
+    */
 }
 
 #endif
---
+++ dmrg/framework/dmrg/block_matrix/symmetry/hash_tuple.h
@@ -0,0 +1,95 @@
+/*****************************************************************************
+*
+* ALPS MPS DMRG Project
+*
+* Copyright (C) 2014 Institute for Theoretical Physics, ETH Zurich
+*               2011-2011 by Bela Bauer <bauerb@phys.ethz.ch>
+*               2011-2013    Michele Dolfi <dolfim@phys.ethz.ch>
+*               2014-2014    Sebastian Keller <sebkelle@phys.ethz.ch>
+*               2018         Leon Freitag <lefreita@ethz.ch>
+*
+*         Employs Boost code and public domain code from stackoverflow.com
+* This software is part of the ALPS Applications, published under the ALPS
+* Application License; you can use, redistribute it and/or modify it under
+* the terms of the license, either version 1 or (at your option) any later
+* version.
+*
+* You should have received a copy of the ALPS Application License along with
+* the ALPS Applications; see the file LICENSE.txt. If not, the license is also
+* available from http://alps.comp-phys.org/.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT
+* SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE
+* FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE,
+* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+* DEALINGS IN THE SOFTWARE.
+*
+*****************************************************************************/
+#ifndef HASH_TUPLE_H
+#define HASH_TUPLE_H
+
+#include <tuple>
+
+namespace hash_tuple
+{
+    // Implementation of a hashing function for tuples, required in caching of Wigner 9j symbols
+    // from
+    // https://stackoverflow.com/questions/7110301/generic-hash-for-tuples-in-unordered-map-unordered-set
+    //
+    // Code from boost
+    // Reciprocal of the golden ratio helps spread entropy
+    //     and handles duplicates.
+    // See Mike Seymour in magic-numbers-in-boosthash-combine:
+    //     http://stackoverflow.com/questions/4948780
+
+    template <typename TT>
+    struct hash
+    {
+        size_t operator()(TT const& tt) const
+        {
+            return std::hash<TT>()(tt);
+        }
+    };
+
+    template <class T>
+    inline void hash_combine(std::size_t& seed, T const& v)
+    {
+        seed ^= hash<T>()(v) + 0x9e3779b9 + (seed<<6) + (seed>>2);
+    }
+
+    // Recursive template code derived from Matthieu M.
+    template <class Tuple, size_t Index = std::tuple_size<Tuple>::value - 1>
+    struct HashValueImpl
+    {
+        static void apply(size_t& seed, Tuple const& tuple)
+        {
+            HashValueImpl<Tuple, Index-1>::apply(seed, tuple);
+            hash_combine(seed, std::get<Index>(tuple));
+        }
+    };
+
+    template <class Tuple>
+    struct HashValueImpl<Tuple,0>
+    {
+        static void apply(size_t& seed, Tuple const& tuple)
+        {
+            hash_combine(seed, std::get<0>(tuple));
+        }
+    };
+
+    template <typename ... TT>
+    struct hash<std::tuple<TT...>>
+    {
+        size_t
+        operator()(std::tuple<TT...> const& tt) const
+        {
+            size_t seed = 0;
+            HashValueImpl<std::tuple<TT...> >::apply(seed, tt);
+            return seed;
+        }
+
+    };
+}
+#endif
