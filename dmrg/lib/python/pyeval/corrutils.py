BUGfix for mutual information calculation with complex numbers.

dmrg/lib/python/pyeval/corrutils.py
@@ -63,24 +63,21 @@ def assemble_halfcorr_complex(diag, triang):
 
     L = len(diag)
 
-    ret_real = np.zeros((L,L))
-    ret_cplx = np.zeros((L,L))
+    ret = np.zeros((L,L),dtype=np.complex_)
 
     # set the diagonal
     for i in range(L):
-        ret_real[i,i] = diag[i]
+        ret[i,i] = diag[i]
 
     for lab, val in zip(triang.x, triang.y[0]):
         #print val
         i = lab[0]
         j = lab[1]
-        ret_real[i,j] =  val.real
-        ret_cplx[i,j] =  val.imag
+        ret[i,j] =  val
         # dv(j,i) = dv(i,j)*
-        ret_real[j,i] =  val.real
-        ret_cplx[j,i] = -val.imag
+        ret[j,i] =  np.conjugate(val)
 
-    return (ret_real,ret_cplx)
+    return ret
 
 def assemble_vector(dataset):
     """From the diagonal and upper triangle, construct a symmetric matrix

dmrg/lib/python/pyeval/entropy_dbg.py
@@ -35,7 +35,7 @@ import scipy.linalg as sl
 
 from copy import deepcopy
 
-from corrutils import assemble_halfcorr as assy_hc
+from corrutils import assemble_halfcorr_complex as assy_hc
 from corrutils import pretty_print
 
 class MaquisMeasurement:
@@ -52,19 +52,11 @@ class MaquisMeasurement:
         self.orb_order = inv_order
         empty_diag = np.zeros(self.norb)
 
-        self.corr_cdag_c = assy_hc(empty_diag, pyalps.loadEigenstateMeasurements([inputfile], what='dm')[0][0])
-        self.corr_docc   = assy_hc(empty_diag, pyalps.loadEigenstateMeasurements([inputfile], what='doccdocc')[0][0])
-
-    def one_pt_dm(self):
-        
-        dm = deepcopy(self.corr_cdag_c)
-        for i in range(self.norb):
-            dm[i,i] = self.loc_n[i]
-
-        return dm
+        self.corr_cdag_c   = assy_hc(empty_diag, pyalps.loadEigenstateMeasurements([inputfile], what='dm')[0][0])
+        self.corr_docc     = assy_hc(empty_diag, pyalps.loadEigenstateMeasurements([inputfile], what='doccdocc')[0][0])
 
     def two_orb_rdm(self, p, q):
-        pq_dm_matrix = np.zeros((4,4))
+        pq_dm_matrix = np.zeros((4,4),dtype=np.complex_)
 
         pq_dm_matrix[0,0] = 1 - self.loc_n[p] - self.loc_n[q] + self.corr_docc[p,q]
         pq_dm_matrix[1,1] = self.loc_n[q] - self.corr_docc[p,q]
@@ -91,7 +83,7 @@ class MaquisMeasurement:
 
             ret[i] = -sum(map(lambda x: x*np.log(x), [m11, m22]))
 
-        return ret[self.orb_order]        
+        return ret[self.orb_order]
 
 
     def s2(self):
@@ -123,16 +115,6 @@ class MaquisMeasurement:
 
         return ret
 
-
-    def corr_func(self, f1,f2):
-        n = self.norb
-        cf = np.zeros((n, n))
-
-        for i in range(n):
-            for j in range(i+1,n):
-                cf[i,j] = self.two_orb_rdm(i,j)[f1,f2]
-
-        return cf
 
     def dump_raw(self):
         for k,v in zip(self.__dict__.keys(), self.__dict__.values()):