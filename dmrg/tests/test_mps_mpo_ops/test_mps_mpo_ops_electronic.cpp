176a13baceaa5cdaba1dab16656d59b5030e6f7e
Alberto <baiardialberto91@gmail.com>
2021-09-13 15:46:27 +0200

    Working version of bra != ket

--- dmrg/tests/test_mps_mpo_ops/test_mps_mpo_ops_electronic.cpp
+++ dmrg/tests/test_mps_mpo_ops/test_mps_mpo_ops_electronic.cpp
@@ -51,8 +51,11 @@ TwoU1PG
 #endif
 > symmetries;
 
-/** @brief Checks that the MPO-MPS contraction fulfill the  */
-BOOST_FIXTURE_TEST_CASE_TEMPLATE( Test_MPS_MPO_Hermitian_Electronic, S, symmetries, BenzeneFixture )
+/**
+ * @brief Checks that the MPO-MPS contraction fulfill the Hermitianity of the Hamiltonian 
+ * Note that this test uses the "standard" implementation of the expval function, which uses the left boundary.
+ */
+BOOST_FIXTURE_TEST_CASE_TEMPLATE( Test_MPS_MPO_Hermitian_Left_Electronic, S, symmetries, BenzeneFixture )
 {
     // Generates the HF MPS
     auto lattice = Lattice(parametersBenzene);
@@ -80,5 +83,37 @@ BOOST_FIXTURE_TEST_CASE_TEMPLATE( Test_MPS_MPO_Hermitian_Electronic, S, symmetri
     // Check 3
     expVal1 = expval(mpsDefault, mpsHF, mpo);
     expVal2 = expval(mpsHF, mpsDefault, mpo);
+    BOOST_CHECK_CLOSE(expVal1, expVal2, 1.E-10);
+}
+
+/** @brief Same as above, but with the right boundaries */
+BOOST_FIXTURE_TEST_CASE_TEMPLATE( Test_MPS_MPO_Hermitian_Right_Electronic, S, symmetries, BenzeneFixture )
+{
+    // Generates the HF MPS
+    auto lattice = Lattice(parametersBenzene);
+    auto modelHF = Model<matrix, S>(lattice, parametersBenzene);
+    auto mpsHF = MPS<matrix, S>(lattice.size(), *(modelHF.initializer(lattice, parametersBenzene)));
+    // Generates the const guess. Note that the bond dimension will be != than that of the HF 
+    // guess, so we check that the contraction routines work also for MPS with different bond dimension.
+    parametersBenzene.set("init_state", "const");
+    auto modelConst = Model<matrix, S>(lattice, parametersBenzene);
+    auto mpsConst = MPS<matrix, S>(lattice.size(), *(modelConst.initializer(lattice, parametersBenzene)));
+    // Generates the default guess
+    parametersBenzene.set("init_state", "default");
+    auto modelDefault = Model<matrix, S>(lattice, parametersBenzene);
+    auto mpsDefault = MPS<matrix, S>(lattice.size(), *(modelDefault.initializer(lattice, parametersBenzene)));
+    // Calculates the MPO (any model would work here)
+    auto mpo = make_mpo(lattice, modelConst);
+    // Check 1
+    double expVal1 = expvalFromRight(mpsDefault, mpsConst, mpo);
+    double expVal2 = expvalFromRight(mpsConst, mpsDefault, mpo);
+    BOOST_CHECK_CLOSE(expVal1, expVal2, 1.E-10);
+    // Check 2
+    expVal1 = expvalFromRight(mpsHF, mpsConst, mpo);
+    expVal2 = expvalFromRight(mpsConst, mpsHF, mpo);
+    BOOST_CHECK_CLOSE(expVal1, expVal2, 1.E-10);
+    // Check 3
+    expVal1 = expvalFromRight(mpsDefault, mpsHF, mpo);
+    expVal2 = expvalFromRight(mpsHF, mpsDefault, mpo);
     BOOST_CHECK_CLOSE(expVal1, expVal2, 1.E-10);
 }
