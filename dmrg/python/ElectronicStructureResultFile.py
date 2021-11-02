2bb150bd89cb56ad0dfbc68ba5e7af5dba8c45fd
Alberto <baiardialberto91@gmail.com>
2021-11-02 17:25:44 +0100

    Added python script to extract 1ptrdm from result file

---
+++ dmrg/python/ElectronicStructureResultFile.py
@@ -0,0 +1,27 @@
+import h5py
+import argparse
+import numpy as np
+
+class ElectronicStructureResultFile:
+
+    def __init__(self, resultFileName):
+        self.__fileName = resultFileName
+        self.__hdf5File = h5py.File(self.__fileName, 'r')
+        self.__nOrbitals = self.__hdf5File['parameters']['L'][()]
+
+    def getOneParticleRdm(self):
+        oneParticleRdmMatrix = np.zeros([self.__nOrbitals, self.__nOrbitals])
+        mapContainer = self.__hdf5File['spectrum']['results']['oneptdm']["mean"]["value"]
+        for idx, i in enumerate(self.__hdf5File['spectrum']['results']['oneptdm']["labels_num"]):
+            oneParticleRdmMatrix[i[0], i[1]] = mapContainer[0][idx]
+            oneParticleRdmMatrix[i[1], i[0]] = mapContainer[0][idx]
+        return oneParticleRdmMatrix
+
+if __name__ == "__main__":
+    #
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-r", "--result", type=str, help="Name of the hdf5 file")
+    args = parser.parse_args()
+    resFileName = args.result
+    resultFileObject = ElectronicStructureResultFile(resFileName)
+    rdm = resultFileObject.getOneParticleRdm()
