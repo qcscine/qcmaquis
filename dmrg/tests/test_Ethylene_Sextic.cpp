c37826c9768c5300d93ac750f8beb15d98cc535c
Alberto <baiardialberto91@gmail.com>
2021-11-26 12:49:12 +0100

    Added test for Watson + TS

--- dmrg/tests/test_Ethylene_Sextic.cpp
+++ dmrg/tests/test_Ethylene_Sextic.cpp
@@ -56,9 +56,10 @@ BOOST_FIXTURE_TEST_CASE(Test_vDMRG_Calculation_Ethylene_Harmonic, WatsonFixture)
 /**
  * @brief Watson-based calculation on the ethylene PES
  * The PES has been taken from the vHBCI reference work, which is
- * J. Chem. Phys., 154, 074104 (2021)
+ * J. Chem. Phys., 154, 074104 (2021).
+ * Note that the calculation uses the single-site optimizer.
  */
-BOOST_FIXTURE_TEST_CASE(Test_vDMRG_Calculation_Ethylene_Sextic, WatsonFixture)
+BOOST_FIXTURE_TEST_CASE(Test_vDMRG_Calculation_Ethylene_Sextic_SingleSite, WatsonFixture)
 {
     using InterfaceType = maquis::DMRGInterface<double, Hamiltonian::VibrationalCanonical>;
     // Adds the final input parameters
@@ -73,6 +74,32 @@ BOOST_FIXTURE_TEST_CASE(Test_vDMRG_Calculation_Ethylene_Sextic, WatsonFixture)
     parametersEthyleneWatson.set("alpha_initial", 1.0E-8);
     parametersEthyleneWatson.set("alpha_main", 1.0E-15);
     parametersEthyleneWatson.set("alpha_final", 0.);
+    // Creates the interface
+    InterfaceType interface(parametersEthyleneWatson);
+    interface.optimize();
+    BOOST_CHECK_CLOSE(interface.energy(), 11011.61, 1.0E-2);
+}
+
+/**
+ * @brief Watson-based calculation on the ethylene PES
+ * The PES has been taken from the vHBCI reference work, which is
+ * J. Chem. Phys., 154, 074104 (2021).
+ * Note that the calculation uses the single-site optimizer.
+ */
+BOOST_FIXTURE_TEST_CASE(Test_vDMRG_Calculation_Ethylene_Sextic_TwoSite, WatsonFixture)
+{
+    using InterfaceType = maquis::DMRGInterface<double, Hamiltonian::VibrationalCanonical>;
+    // Adds the final input parameters
+    parametersEthyleneWatson.set("init_state", "basis_state_generic");
+    parametersEthyleneWatson.set("init_basis_state", "0,0,0,0,0,0,0,0,0,0,0,0");
+    parametersEthyleneWatson.set("nsweeps", 10);
+    parametersEthyleneWatson.set("max_bond_dimension", 20);
+    parametersEthyleneWatson.set("MODEL", "watson");
+    parametersEthyleneWatson.set("optimization", "twosite");
+    parametersEthyleneWatson.set("ngrowsweeps", 2);
+    parametersEthyleneWatson.set("nmainsweeps", 2);
+    parametersEthyleneWatson.set("truncation_initial", 1.0E-16);
+    parametersEthyleneWatson.set("truncation_final", 1.0E-10);
     // Creates the interface
     InterfaceType interface(parametersEthyleneWatson);
     interface.optimize();
