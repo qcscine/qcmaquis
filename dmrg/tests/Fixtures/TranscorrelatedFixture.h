e2b391d82a00631f0c56f203d1f85bbea475e886
Alberto <baiardialberto91@gmail.com>
2022-02-10 15:19:39 +0100

    Added tests TI vs iTD

--- dmrg/tests/Fixtures/TranscorrelatedFixture.h
+++ dmrg/tests/Fixtures/TranscorrelatedFixture.h
@@ -36,7 +36,7 @@ struct TranscorrelatedFixture
 {
     /** @brief Constructor for the fixture class */
     TranscorrelatedFixture() {
-        // 2x2 Fermi-Hubbard Hamiltonian, 
+        // 2x2 Real-Space Fermi-Hubbard Hamiltonian.
         parameters2x2_RealSpace_U4_2Alpha1Beta.set("L", 4);
         parameters2x2_RealSpace_U4_2Alpha1Beta.set("width_FermiHubbard", 2);
         parameters2x2_RealSpace_U4_2Alpha1Beta.set("height_FermiHubbard", 2);
@@ -47,10 +47,21 @@ struct TranscorrelatedFixture
         parameters2x2_RealSpace_U4_2Alpha1Beta.set("MODEL", "fermi_hubbard_real");
         parameters2x2_RealSpace_U4_2Alpha1Beta.set("u1_total_charge1", 2);
         parameters2x2_RealSpace_U4_2Alpha1Beta.set("u1_total_charge2", 1);
+        // 2x2 Fermi-Hubbard Hamiltonian.
+        parameters2x2_MomentumSpace_U4_2Alpha1Beta.set("L", 4);
+        parameters2x2_MomentumSpace_U4_2Alpha1Beta.set("width_FermiHubbard", 2);
+        parameters2x2_MomentumSpace_U4_2Alpha1Beta.set("height_FermiHubbard", 2);
+        parameters2x2_MomentumSpace_U4_2Alpha1Beta.set("max_bond_dimension", 50);
+        parameters2x2_MomentumSpace_U4_2Alpha1Beta.set("U_FermiHubbard", 4.);
+        parameters2x2_MomentumSpace_U4_2Alpha1Beta.set("symmetry", "2u1");
+        parameters2x2_MomentumSpace_U4_2Alpha1Beta.set("site_types", "0,0,0,0");
+        parameters2x2_MomentumSpace_U4_2Alpha1Beta.set("MODEL", "fermi_hubbard_momentum");
+        parameters2x2_MomentumSpace_U4_2Alpha1Beta.set("u1_total_charge1", 2);
+        parameters2x2_MomentumSpace_U4_2Alpha1Beta.set("u1_total_charge2", 1);
     }
 
     // Class members
-    DmrgParameters parameters2x2_RealSpace_U4_2Alpha1Beta;
+    DmrgParameters parameters2x2_RealSpace_U4_2Alpha1Beta, parameters2x2_MomentumSpace_U4_2Alpha1Beta;
 };
 
 #endif
