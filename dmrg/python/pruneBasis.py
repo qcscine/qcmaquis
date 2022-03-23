a99e6d55ecd30dbbf8a2d83c286c87b1252a4451
Alberto <baiardialberto91@gmail.com>
2022-03-23 16:37:22 +0100

    Added function to prune an FCIDUMP file

--- dmrg/python/pruneBasis.py
+++ dmrg/python/pruneBasis.py
@@ -28,37 +28,53 @@
 #*****************************************************************************
 
 import argparse
-import math
-import re
-from sys import dont_write_bytecode
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import lines
-from mpl_toolkits import mplot3d
-from matplotlib.ticker import MaxNLocator
 from VibrationalResultFile import ResultFileVibrationaleMeasurement
+from typing import List
+
+def pruneFcidumpFile(listOfListModals: List[ List[int] ], fileName: str, outputFileName: str):
+  with open(fileName, mode="r") as fileObject:
+    with open(outputFileName, mode="w") as outputFile:
+      for iLine in fileObject:
+        splittedLine = iLine.split()
+        splittedLine.pop()
+        accepted = True
+        if len(splittedLine) > 2:
+          for iElement in splittedLine:
+            iSplittedElement = [int(j) for j in iElement.split("-")]
+            assert(len(iSplittedElement) == 2)
+            if (not iSplittedElement[1] in listOfListModals[iSplittedElement[0]-1]):
+              accepted = False
+              break
+        if accepted:
+          outputFile.write(iLine)
+
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
-  parser.add_argument("-r", "--resultfile", type=str, help="Input result file", required=True)
+  parser.add_argument("-r", "--resultfile", type=str, help="Input result file", required=True, nargs="+")
   parser.add_argument("-f", "--fcidump", type=str, action="store", help="If present, prunes directly the FCIDUMP file")
+  parser.add_argument("-o", "--output", type=str, action="store", help="""
+                                                                       Name of the output file.
+                                                                       Required if the [fcidump] option is provided.
+                                                                       """)
   parser.add_argument("-t", "--threshold", type=float, action="store", help="Pruning threshold for the one-particle modals", required=True)
   args = parser.parse_args()
-  inputFileName = args.resultfile
   fcidumpName = args.fcidump
+  outputFile = args.output
   threshold = args.threshold
   # Creates the result file object and does the pruning
-  resultFile = ResultFileVibrationaleMeasurement(inputFileName)
-  oneModalEntropies = resultFile.getSeparatedOneModalEntropy()
-  lstOfAcceptedModes = []
-  for entropyList in oneModalEntropies:
-    acceptedModes = []
-    for idx, iModalEntropy in enumerate(entropyList):
-      if (abs(iModalEntropy) > threshold):
-        acceptedModes.append(idx)
-    lstOfAcceptedModes.append(acceptedModes)
-  if (fcidumpName):
-    pass
+  resultFileList = [ResultFileVibrationaleMeasurement(iFile) for iFile in args.resultfile]
+  assert(all([iRes.getLatticeSize() == resultFileList[0].getLatticeSize() for iRes in resultFileList]))
+  lstOfAcceptedModes = [[] for i in range(resultFileList[0].getNumberOfModes())]
+  for iRes in resultFileList:
+    oneModalEntropies = iRes.getSeparatedOneModalEntropy()
+    for iMode, entropyList in enumerate(oneModalEntropies):
+      for idx, iModalEntropy in enumerate(entropyList):
+        if abs(iModalEntropy) > threshold and idx not in lstOfAcceptedModes[iMode]:
+          lstOfAcceptedModes[iMode].append(idx)
+  #
+  if (fcidumpName and outputFile):
+    pruneFcidumpFile(lstOfAcceptedModes, fcidumpName, outputFile)
   else:
     print(" == LIST OF ACCEPTED MODES == ")
     for iMode, iList in enumerate(lstOfAcceptedModes):
