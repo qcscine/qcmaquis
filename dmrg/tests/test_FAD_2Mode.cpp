3ac67c3cbb1d168ea1b950e20fe43a1b31c919d7
Alberto <baiardialberto91@gmail.com>
2022-06-21 08:42:58 +0200

    Fixed problem with determinant with wrong dimension

--- dmrg/tests/test_FAD_2Mode.cpp
+++ dmrg/tests/test_FAD_2Mode.cpp
@@ -83,7 +83,7 @@ BOOST_FIXTURE_TEST_CASE(Test_Lattice_Size_1ModeHamiltonian_ExcitedState, NModeFi
     auto gsEnergy = interface.energy();
     BOOST_CHECK_CLOSE(gsEnergy, -2.359242429009664e+03, 1.0E-5);
     // Checks that the overlap of the final wave function with the hf determinant is = 1.
-    auto targetOverlap = interface.getCICoefficient("0,0,0");
+    auto targetOverlap = interface.getCICoefficient("0");
     BOOST_CHECK_CLOSE(std::abs(targetOverlap), 1.0, 1.0E-16);
     // Excited-state calculations
     parametersFADOneBodyBinary.set("chkpfile", "ES.checkpoint.h5");
