fbdbb96b08ded65c60d6e5130567aee5218e8bf9
Alberto <baiardialberto91@gmail.com>
2022-08-26 17:12:27 +0200

    Updated vibrational tests

--- dmrg/tests/SweepOptimizationTools/OverlapPropagatorVibrational.cpp
+++ dmrg/tests/SweepOptimizationTools/OverlapPropagatorVibrational.cpp
@@ -92,7 +92,7 @@ BOOST_FIXTURE_TEST_CASE(Test_OverlapPropagator_Vibrational_NU1, NModeFixture)
   auto mpsVector = std::vector<MPSType>({mpsDefault});
   auto overlapPropagator = OverlapPropagatorType(mpsDefault, mpsVector, 0);
   overlapPropagator.propagateLeftOverlapBoundaries(0, 7);
-  auto mpsOrtho = overlapPropagator.getOrthogonalVector(0, 7, 8);
+  auto mpsOrtho = overlapPropagator.template getOrthogonalVector<SweepOptimization::Singlesite>(0, 7, 8);
   auto overlap = ietl::dot(mpsOrtho, mpsDefault[7]);
   BOOST_CHECK_CLOSE(overlap, 1., 1.0E-7);
 }
