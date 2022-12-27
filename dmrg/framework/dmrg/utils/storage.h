ca60c9290fa4f17d1a400174af74472fb687deaf
Alberto Baiardi <alberto.baiardi@phys.chem.ethz.ch>
2022-12-27 11:43:20 +0100

    Removed unused parts of storage class

--- dmrg/framework/dmrg/utils/storage.h
+++ dmrg/framework/dmrg/utils/storage.h
@@ -2,25 +2,26 @@
  *
  * ALPS MPS DMRG Project
  *
- * Copyright (C) 2014 Institute for Theoretical Physics, ETH Zurich
+ * Copyright (C) 2014- Institute for Theoretical Physics, ETH Zurich
  *               2011-2011 by Bela Bauer <bauerb@phys.ethz.ch>
  *               2011-2012 by Michele Dolfi <dolfim@phys.ethz.ch>
- * 
+ *               2022- by Alberto Baiardi <abaiardi@ethz.ch>
+ *
  * This software is part of the ALPS Applications, published under the ALPS
  * Application License; you can use, redistribute it and/or modify it under
  * the terms of the license, either version 1 or (at your option) any later
  * version.
- * 
+ *
  * You should have received a copy of the ALPS Application License along with
  * the ALPS Applications; see the file LICENSE.txt. If not, the license is also
  * available from http://alps.comp-phys.org/.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT 
- * SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE 
- * FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE, 
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE
+ * FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  *
  *****************************************************************************/
@@ -50,36 +51,32 @@ namespace storage {
 }
 #endif
 
+// Forward declaration of all the objects that can be stored to a file.
 template<class Matrix, class SymmGroup> class Boundary;
 template<class Matrix, class SymmGroup> class MPSTensor;
 template<class Matrix, class SymmGroup> class block_matrix;
 template<class Matrix, class SymmGroup> class SiteOperator;
 
-namespace alps { namespace numeric {
-    template <typename T, typename MemoryBlock> class matrix;
-} }
+// Forward declaration of the alps matrix type
+namespace alps {
+namespace numeric {
+
+template <typename T, typename MemoryBlock>
+class matrix;
+
+} // namespace numeric
+} // namespace alps
+
 namespace storage {
-    template<class T> 
-    struct constrained { 
-        typedef T type; 
+    template<class T>
+    struct constrained {
+        typedef T type;
     };
-    template<typename T> 
+    template<typename T>
     struct constrained<alps::numeric::matrix<T, std::vector<T> > > {
         typedef alps::numeric::matrix<T, std::vector<T> > type;
     };
 }
-#ifdef USE_AMBIENT
-namespace ambient { inline namespace numeric {
-    template <typename Matrix, int IB> class tiles;
-    template <typename T, class Allocator> class matrix;
-} }
-namespace storage {
-    template<typename T> 
-    struct constrained<ambient::tiles<ambient::matrix<T> > > {
-        typedef ambient::tiles<ambient::matrix<T> > type;
-    };
-}
-#endif
 
 namespace storage {
 
@@ -108,23 +105,10 @@ namespace storage {
                 assert( o[b].reasonable() );
                 for (std::size_t k = 0; k < o[b].n_blocks(); ++k){
                     Matrix& m = o[b][k];
-#ifdef USE_AMBIENT
-                    for(int j = 0; j < m.nt; ++j)
-                    for(int i = 0; i < m.mt; ++i){
-                        if(ambient::weak(m.tile(i,j))) continue;
-                        if(ambient::ext::naked(m.tile(i,j)).state != ambient::locality::local) continue;
-                        char* data = (char*)ambient::ext::naked(m.tile(i,j));
-                        ofs.write(data, m.tile(i,j).num_cols() * m.tile(i,j).num_rows() *
-                                  sizeof(typename Matrix::value_type)/sizeof(char));
-                        std::free(data);
-                        ambient::ext::naked(m.tile(i,j)).data = NULL;
-                    }
-#else
                     for (std::size_t c = 0; c < num_cols(m); ++c)
                         ofs.write((char*)(&m(0, c)), num_rows(m)*
                                  sizeof(typename Matrix::value_type)/sizeof(char));
                     m = Matrix();
-#endif
                 }
             }
             ofs.close();
@@ -144,23 +128,11 @@ namespace storage {
             size_t loop_max = o.aux_dim();
             for(size_t b = 0; b < loop_max; ++b){
                 for (std::size_t k = 0; k < o[b].n_blocks(); ++k){
-#ifdef USE_AMBIENT
-                    Matrix& m = o[b][k];
-                    for(int j = 0; j < m.nt; ++j)
-                    for(int i = 0; i < m.mt; ++i){
-                        if(ambient::weak(m.tile(i,j))) continue;
-                        if(ambient::ext::naked(m.tile(i,j)).state != ambient::locality::local) continue;
-                        ambient::ext::naked(m.tile(i,j)).data = std::malloc(ambient::ext::naked(m.tile(i,j)).spec.extent);
-                        ifs.read((char*)ambient::ext::naked(m.tile(i,j)), m.tile(i,j).num_cols() * m.tile(i,j).num_rows() *
-                                 sizeof(typename Matrix::value_type)/sizeof(char));
-                    }
-#else
                     o[b][k] = Matrix(o[b].left_basis()[k].second,
                                      o[b].right_basis()[k].second);
                     Matrix& m = o[b][k];
                     ifs.read((char*)(&m(0,0)), num_cols(m)*num_rows(m)*
                              sizeof(typename Matrix::value_type)/sizeof(char));
-#endif
                 }
             }
             ifs.close();
@@ -177,20 +149,8 @@ namespace storage {
         void operator()(){
             Boundary<Matrix, SymmGroup>& o = *ptr;
             for (std::size_t b = 0; b < o.aux_dim(); ++b)
-            for (std::size_t k = 0; k < o[b].n_blocks(); ++k){
-#ifdef USE_AMBIENT
-                    Matrix& m = o[b][k];
-                    for(int j = 0; j < m.nt; ++j)
-                    for(int i = 0; i < m.mt; ++i){
-                        if(ambient::weak(m.tile(i,j))) continue;
-                        if(ambient::ext::naked(m.tile(i,j)).state != ambient::locality::local) continue;
-                        char* data = (char*)ambient::ext::naked(m.tile(i,j)); std::free(data);
-                        ambient::ext::naked(m.tile(i,j)).data = NULL;
-                    }
-#else
+                for (std::size_t k = 0; k < o[b].n_blocks(); ++k)
                     o[b][k] = Matrix();
-#endif
-            }
         }
     private:
         std::string fp;
@@ -225,7 +185,7 @@ namespace storage {
         };
 
         template<class T> class serializable : public descriptor {
-        public: 
+        public:
             ~serializable(){
                 if (dumped) std::remove(disk::fp(sid).c_str()); // only delete existing file, too slow otherwise on NFS or similar
             }
@@ -286,11 +246,11 @@ namespace storage {
         static size_t index(){
             return instance().sid++;
         }
-        static void track(descriptor* d){ 
+        static void track(descriptor* d){
             d->record = instance().queue.size();
             instance().queue.push_back(d);
         }
-        static void untrack(descriptor* d){ 
+        static void untrack(descriptor* d){
             instance().queue[d->record] = NULL;
         }
         static void sync(){
@@ -303,47 +263,21 @@ namespace storage {
         template<class T> static void StoreToFile(serializable<T>& t)   { if(enabled()) t.StoreToFile(); }
         template<class T> static void drop(serializable<T>& t)          { if(enabled()) t.drop();        }
 
-        template<class Matrix, class SymmGroup> 
+        template<class Matrix, class SymmGroup>
         static void StoreToFile(MPSTensor<Matrix, SymmGroup>& t){ }
 
         disk() : active(false), sid(0) {}
         std::vector<descriptor*> queue;
         std::string path;
-        bool active; 
+        bool active;
         size_t sid;
     };
 
-#ifdef USE_AMBIENT
-    template<class Matrix, class SymmGroup, class Scheduler = parallel::scheduler_nop> 
-    static void migrate(const block_matrix<Matrix, SymmGroup>& tc, const Scheduler& scheduler = Scheduler()){
-        block_matrix<Matrix, SymmGroup>& t = const_cast<block_matrix<Matrix, SymmGroup>&>(tc);
-        for(int i = 0; i < t.n_blocks(); ++i){
-            parallel::guard proc(scheduler(i));
-            ambient::migrate(t[i]);
-        }
-    }
-    template<class Matrix, class SymmGroup, class Scheduler = parallel::scheduler_nop> 
-    static void migrate(const SiteOperator<Matrix, SymmGroup>& tc, const Scheduler& scheduler = Scheduler()){
-        SiteOperator<Matrix, SymmGroup>& t = const_cast<SiteOperator<Matrix, SymmGroup>&>(tc);
-        for(int i = 0; i < t.n_blocks(); ++i){
-            parallel::guard proc(scheduler(i));
-            ambient::migrate(t[i]);
-        }
-    }
-    template<class Matrix, class SymmGroup, class Scheduler = parallel::scheduler_nop> 
-    static void migrate(const MPSTensor<Matrix, SymmGroup>& tc, const Scheduler& scheduler = Scheduler()){
-        migrate(tc.data(), scheduler); 
-    }
-#else
     template<typename T, class Scheduler>
     static void migrate(T& t, const Scheduler& scheduler){ }
 
     template<typename T>
-    static void migrate(T& t)
-    {
-        migrate(t, parallel::scheduler_nop());
-    }
-#endif
+    static void migrate(T& t) { migrate(t, parallel::scheduler_nop()); }
 
     inline static void setup(BaseParameters& parms){
         if(!parms["storagedir"].empty()){
@@ -355,7 +289,8 @@ namespace storage {
                 throw;
             }
             storage::disk::init(dp.string());
-        }else{
+        }
+        else{
             maquis::cout << "Temporary storage is disabled\n";
         }
     }
