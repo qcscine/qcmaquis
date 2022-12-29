9156518748d5f7f15a01a5c603c5d4af1aadac65
Alberto Baiardi <alberto.baiardi@phys.chem.ethz.ch>
2022-12-29 10:32:43 +0100

    Added sweep optimization traits + tests

--- dmrg/framework/dmrg/SweepBasedAlgorithms/SweepOptimizationTypeTrait.h
+++ dmrg/framework/dmrg/SweepBasedAlgorithms/SweepOptimizationTypeTrait.h
@@ -31,7 +31,9 @@
 
 enum class SweepOptimizationType {SingleSite, TwoSite};
 
-enum class SweepDirectionType {Forward, Backward};
+enum class SweepDirectionType {Forward, Backward, EndOfLattice};
+
+enum class GrowBoundaryModality {LeftToRight, RightToLeft};
 
 /** @brief Declaration of the trait class */
 template<SweepOptimizationType SweepType>
@@ -42,31 +44,52 @@ template<>
 class SweepOptimizationTypeTrait<SweepOptimizationType::SingleSite> {
 public:
   /** @brief Gets the index of the left boundary for a given site */
-  static int getIndexOfLeftBoundary(int site, SweepDirectionType sweepDirection) { return site; }
+  static int getIndexOfLeftBoundary(int L, int microiteration) {
+    return convertMicroIterationToSite(L, microiteration);
+  }
 
   /** @brief Gets the index of the right boundary for a given site */
-  static int getIndexOfRightBoundary(int site, SweepDirectionType sweepDirection) { return site+1; }
-
-  /** @brief Gets the index of the left boundary associated with the following sweep for a given site */
-  static int getIndexOfNextLeftBoundary(int site, SweepDirectionType sweepDirection) {
-    return (sweepDirection == SweepDirectionType::Forward) ? getIndexOfLeftBoundary(site+1, sweepDirection)
-                                                           : getIndexOfLeftBoundary(site-1, sweepDirection);
+  static int getIndexOfRightBoundary(int L, int microiteration) {
+    return convertMicroIterationToSite(L, microiteration)+1;
   }
 
-  /** @brief Gets the index of the right boundary associated with the following sweep for a given site */
-  static int getIndexOfNextRightBoundary(int site, SweepDirectionType sweepDirection) {
-    return (sweepDirection == SweepDirectionType::Forward) ? getIndexOfLeftBoundary(site+1, sweepDirection)
-                                                           : getIndexOfLeftBoundary(site-1, sweepDirection);
+  /**
+   * @brief Gets the number of microiterations for a given
+   * Note that the case in which L == 1/2 requires a special treatment.
+   */
+  static int getNumberOfMicroiterations(int L) {
+    int numberOfMicroiterations;
+    if (L == 1)
+      numberOfMicroiterations = 1;
+    else if (L == 2)
+      numberOfMicroiterations = 2;
+    else
+      numberOfMicroiterations = 2*(L-1);
+    return numberOfMicroiterations;
   }
 
-  /** @brief Gets the upper boundary of the loop over the microiterations */
-  static int getLastSite(int L) {
-    return L;
+  /** @brief Gets the direction of a sweep */
+  static SweepDirectionType getSweepDirection(int L, int i) {
+    SweepDirectionType ret;
+    if (i < L)
+      ret = SweepDirectionType::Forward;
+    else if (i < getNumberOfMicroiterations(L))
+      ret = SweepDirectionType::Backward;
+    else
+      ret = SweepDirectionType::EndOfLattice;
+    return ret;
+    // return (i < L) ? SweepDirectionType::Forward : SweepDirectionType::Backward;
   }
 
   /** @brief Simple function converting microiteration index to the optimization site */
   static int convertMicroIterationToSite(int L, int i) {
-    return (i < 0) ? 0 : (i < L) ? i : 2*L-1-i;
+    assert(i < getNumberOfMicroiterations(L));
+    return (i < L) ? i : 2*L-2-i;
+  }
+
+  /** @brief Returns true if at the next microiteration the direction will be reversed */
+  static bool changeDirectionNextMicroiteration(int L, int i) {
+    return (i == L-1) || (i == getNumberOfMicroiterations(L)-1);
   }
 
   /** @brief Gets the string identifier of the simulation type */
@@ -83,35 +106,49 @@ template<>
 class SweepOptimizationTypeTrait<SweepOptimizationType::TwoSite> {
 public:
   /** @brief Gets the index of the left boundary for a given site */
-  static inline int getIndexOfLeftBoundary(int site, SweepDirectionType sweepDirection) {
-    return (sweepDirection == SweepDirectionType::Forward) ? site : site-1;
+  static inline int getIndexOfLeftBoundary(int L, int microiteration) {
+    return convertMicroIterationToSite(L, microiteration);
   }
 
   /** @brief Gets the index of the right boundary for a given site */
-  static inline int getIndexOfRightBoundary(int site, SweepDirectionType sweepDirection) {
-    return (sweepDirection == SweepDirectionType::Forward) ? site+2 : site+1;
-  }
-
-  /** @brief Gets the index of the left boundary associated with the following sweep for a given site */
-  static int getIndexOfNextLeftBoundary(int site, SweepDirectionType sweepDirection) {
-    return (sweepDirection == SweepDirectionType::Forward) ? getIndexOfLeftBoundary(site+1, sweepDirection)
-                                                           : getIndexOfLeftBoundary(site-1, sweepDirection);
-  }
-
-  /** @brief Gets the index of the right boundary associated with the following sweep for a given site */
-  static int getIndexOfNextRightBoundary(int site, SweepDirectionType sweepDirection) {
-    return (sweepDirection == SweepDirectionType::Forward) ? getIndexOfLeftBoundary(site+1, sweepDirection)
-                                                           : getIndexOfLeftBoundary(site-1, sweepDirection);
-  }
-
-  /** @brief Gets the upper boundary of the loop over the microiterations */
-  static int getLastSite(int L) {
-    return L-1;
+  static inline int getIndexOfRightBoundary(int L, int microiteration) {
+    return convertMicroIterationToSite(L, microiteration)+2;
   }
 
   /** @brief Simple function converting microiteration index to the optimization site */
   static int convertMicroIterationToSite(int L, int i) {
-    return (i < 0) ? 0 :  (i < L-1) ? i : 2*L - 2 - i;
+    assert(i < getNumberOfMicroiterations(L));
+    return (i < L-1) ? i : 2*L-4 - i;
+  }
+
+  /** @brief Gets the number of microiterations for a given */
+  static int getNumberOfMicroiterations(int L) {
+    int numberOfMicroiterations;
+    if (L == 2)
+      numberOfMicroiterations = 1;
+    else if (L == 1)
+      throw std::runtime_error("TS optimizer not available for 1-site lattices");
+    else
+      numberOfMicroiterations = 2*(L-2);
+    return numberOfMicroiterations;
+  }
+
+  /** @brief Gets the direction of a sweep */
+  static SweepDirectionType getSweepDirection(int L, int i) {
+    SweepDirectionType ret;
+    if (i < L-1)
+      ret = SweepDirectionType::Forward;
+    else if (i < getNumberOfMicroiterations(L))
+      ret = SweepDirectionType::Backward;
+    else
+      ret = SweepDirectionType::EndOfLattice;
+    return ret;
+    // return (i < L-1) ? SweepDirectionType::Forward : SweepDirectionType::Backward;
+  }
+
+  /** @brief Returns true if at the next microiteration the direction will be reversed */
+  static bool changeDirectionNextMicroiteration(int L, int i) {
+    return (i == L-2) || (i == getNumberOfMicroiterations(L)-1);
   }
 
   /** @brief Gets the string identifier of the simulation type */
