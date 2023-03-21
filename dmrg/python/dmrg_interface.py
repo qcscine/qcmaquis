63e5e7cac9b1310fa34a616131e1f3f166c311a5
Alberto Baiardi <alberto.baiardi@phys.chem.ethz.ch>
2023-03-21 10:55:49 +0100

    Fixed bug with second-order terms in DMRG interface

--- dmrg/python/dmrg_interface.py
+++ dmrg/python/dmrg_interface.py
@@ -11,6 +11,7 @@ DESCR = '''
 Interface from GAUSSIAN anharmonic .log file to Vib-DMRG program
 '''
 pattern_harmonic       = r'\sMode\(Quanta\)\s+E\(harm\)\s+E\(anharm\)' 
+pattern_quadratic      = r':\s+QUADRATIC\sFORCE\sCONSTANTS\sIN\sNORMAL\sMODES'
 pattern_cubic          = r':\s+CUBIC\sFORCE\sCONSTANTS\sIN\sNORMAL\sMODES'
 pattern_quartic        = r':\s+QUARTIC\sFORCE\sCONSTANTS\sIN\sNORMAL\sMODES'
 pattern_Coriolis       = r':\s+CORIOLIS COUPLINGS'
@@ -24,6 +25,12 @@ pattern__const = r'''
 \s+(?P<const1>[+-]?\d+\.\d+)
 \s+(?P<const2>[+-]?\d+\.\d+)
 \s+(?P<const3>[+-]?\d+\.\d+)
+'''  
+pattern_quadratic_const = r'''
+\s+(?P<index1>\d+)
+\s+(?P<index2>\d+)
+\s+(?P<const1>[+-]?\d+\.\d+)
+\s+(?P<const2>[+-]?\d+\.\d+)
 '''  
 pattern_cubic_const = r'''
 \s+(?P<index1>\d+)
@@ -105,10 +112,11 @@ Lst_Cub = []
 Lst_Qrt = []
 Lst_Rot = []
 for i in log_anharm:
-    match_Hrm = re.search(pattern_harmonic,i)
-    match_Cub = re.search(pattern_cubic,i)
-    match_Qrt = re.search(pattern_quartic,i)
-    match_Cor = re.search(pattern_Coriolis,i)
+    match_Hrm = re.search(pattern_harmonic, i)
+    match_Qdr = re.search(pattern_quadratic, i)
+    match_Cub = re.search(pattern_cubic, i)
+    match_Qrt = re.search(pattern_quartic, i)
+    match_Cor = re.search(pattern_Coriolis, i)
     # Looks for the relevant section
     # This part is highly dependent on the version of GAUSSIAN. Might be generalized
     if (match_Cor):
@@ -116,7 +124,7 @@ for i in log_anharm:
         XCor = []
         YCor = []
         ZCor = []
-    if (match_Hrm):
+    if (match_Qdr):
         FndHrm = True
     if (match_Cub):
         FndCor = False
@@ -125,13 +133,17 @@ for i in log_anharm:
         FndCub = False
         FndQrt = True
     # Looks for the specific patterns
-    LJunk0 = re.search(pattern_harmonic_const,i,re.DOTALL|re.VERBOSE)
-    LJunk1 = re.search(pattern_cubic_const,i,re.DOTALL|re.VERBOSE)
-    LJunk2 = re.search(pattern_quartic_const,i,re.DOTALL|re.VERBOSE)
-    LJunk3 = re.search(pattern_Coriolis_const,i,re.DOTALL|re.VERBOSE)
-    LJunk4 = re.search(pattern_Rotational_const,i,re.DOTALL|re.VERBOSE)
+    LJunk0  = re.search(pattern_quadratic_const,i,re.DOTALL|re.VERBOSE)
+    LJunk1  = re.search(pattern_cubic_const,i,re.DOTALL|re.VERBOSE)
+    LJunk2  = re.search(pattern_quartic_const,i,re.DOTALL|re.VERBOSE)
+    LJunk3  = re.search(pattern_Coriolis_const,i,re.DOTALL|re.VERBOSE)
+    LJunk4  = re.search(pattern_Rotational_const,i,re.DOTALL|re.VERBOSE)
     if (FndHrm and LJunk0):
-        Lst_Hrm.append([int(LJunk0.group('index')),float(LJunk0.group('harm'))])
+        Indx1   = int(LJunk0.group('index1'))
+        Indx2   = int(LJunk0.group('index2'))
+        LstJnk  = [Indx1, Indx2]
+        LstJnk2 = [LstJnk, float(LJunk1.group('const1'))]
+        Lst_Hrm.append(LstJnk2)
     if (FndCub and LJunk1):
         Indx1   = int(LJunk1.group('index1'))
         Indx2   = int(LJunk1.group('index2'))
@@ -190,8 +202,12 @@ str_qdr = '  {value:19.12E}  {index1:d}  {index2:d}  0  0  0  0 \n'
 str_cub = '  {value:19.12E}  {index1:d}  {index2:d}  {index3:d}  0  0  0  \n'
 str_qrt = '  {value:19.12E}  {index1:d}  {index2:d}  {index3:d}  {index4:d}  0  0 \n'
 for i in range(len(Lst_Hrm)):
-    out_file.write(str_qdr.format(value=Lst_Hrm[i][1]/4.,index1=Lst_Hrm[i][0],index2=Lst_Hrm[i][0]))
-    out_file.write(str_qdr.format(value=-Lst_Hrm[i][1]/4.,index1=-Lst_Hrm[i][0],index2=-Lst_Hrm[i][0]))
+    if(not DoFact):
+        fact = multinomial(Lst_Hrm[i][0])*2.
+    else:
+        fact = 2.
+    out_file.write(str_qdr.format(value=Lst_Hrm[i][1]/fact, index1=Lst_Hrm[i][0], index2=Lst_Hrm[i][0]))
+    out_file.write(str_qdr.format(value=-Lst_Hrm[i][1]/fact, index1=-Lst_Hrm[i][0], index2=-Lst_Hrm[i][0]))
 for i in range(len(Lst_Cub)):
     if(not DoFact):
         fact = multinomial(Lst_Cub[i][0])*2.*math.sqrt(2.)
