a13dc83ab16a612dfb501cc7f735f3043a7e2293
szeneskalman <szenes.kalman@gmail.com>
2023-05-22 09:50:38 +0200

    Add more missing includes

--- dmrg/framework/dmrg/mp_tensors/contractions/common/move_boundary.hpp
+++ dmrg/framework/dmrg/mp_tensors/contractions/common/move_boundary.hpp
@@ -13,6 +13,7 @@
 #include "dmrg/mp_tensors/reshapes.h"
 #include "dmrg/block_matrix/indexing.h"
 #include "dmrg/mp_tensors/contractions/detail/memsave.hpp"
+#include "dmrg/mp_tensors/contractions/common/boundary_times_mps.hpp"
 
 namespace contraction {
 namespace common {
@@ -304,8 +305,9 @@ generate_right_mpo_basis(MPSTensor<Matrix, SymmGroup> const & bra_tensor, MPSTen
     Index<SymmGroup> out_right_i = adjoin(physical_i) * right_i;
     common_subset(out_right_i, left_i);
     ProductBasis<SymmGroup> in_left_pb(physical_i, left_i);
-    ProductBasis<SymmGroup> out_right_pb(physical_i, right_i, boost::lambda::bind(static_cast<charge(*)(charge, charge)>(SymmGroup::fuse),
-                                                             -boost::lambda::_1, boost::lambda::_2));
+    /* ProductBasis<SymmGroup> out_right_pb(physical_i, right_i, boost::lambda::bind(static_cast<charge(*)(charge, charge)>(SymmGroup::fuse), */
+    /*                                                          -boost::lambda::_1, boost::lambda::_2)); */
+    ProductBasis<SymmGroup> out_right_pb(physical_i, right_i, [](const charge a, const charge b) { return SymmGroup::fuse(-a, b); });
     // Prepares output
     Boundary<Matrix, SymmGroup> ret;
     ret.resize(mpo.row_dim());
