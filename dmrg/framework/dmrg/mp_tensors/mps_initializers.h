00ef16699b4dbf79e2bb73b8874b6cd2af030539
Nina Glaser <nina.glaser@phys.chem.ethz.ch>
2023-05-25 17:17:28 +0200

    Added fromFile functionality to coherent initializer

--- dmrg/framework/dmrg/mp_tensors/mps_initializers.h
+++ dmrg/framework/dmrg/mp_tensors/mps_initializers.h
@@ -132,15 +132,39 @@ public:
    */
   coherent_mps_init(BaseParameters & params_, std::vector<Index<SymmGroup> > const& phys_dims_,
                     typename SymmGroup::charge right_end_, std::vector<int> const& site_type_)
-    : coeff(params_["init_coeff"].as<std::vector<double> >()), phys_dims(phys_dims_),
-      site_type(site_type_), right_end(right_end_), params(params_)
+    : phys_dims(phys_dims_), site_type(site_type_), right_end(right_end_), params(params_)
   {
+    if (params["init_file"].str().empty() && params["init_basis_state"].str().empty())
+      throw std::runtime_error("Either init_file or init_basis_state has to be provided for the coherent initializer");
+
     initialBondDim = (params["init_bond_dimension"] > 5) ? params["init_bond_dimension"] : params["max_bond_dimension"];
-    std::vector<std::string> list_sa;
-    std::string input_str = params["init_basis_state"].str();
-    boost::split(list_sa, input_str, boost::is_any_of("|"));
-    for (int i = 0; i < list_sa.size(); i++) {
-      std::stringstream ss(list_sa[i]);
+    fromFile = (params["init_file"].str().empty()) ? false : true;
+
+    std::vector<std::string> list_dets;
+
+    if (fromFile) {
+      std::string fileName = params["init_file"];
+      if (!boost::filesystem::exists(fileName))
+        throw std::runtime_error("Initializer file " + fileName + " does not exist\n");
+      std::ifstream stateFile;
+      stateFile.open(fileName.c_str());
+      std::string line;
+      std::vector< std::string > line_splitted;
+      while (std::getline(stateFile, line)) {
+        boost::trim_left(line);
+        boost::trim_right(line);
+        boost::split(line_splitted, line, boost::is_any_of(" "), boost::token_compress_on);
+        coeffs.push_back(std::stod(line_splitted[0]));
+        list_dets.push_back(line_splitted[1]);
+      }
+      stateFile.close();
+    } else {
+      boost::split(list_dets, params["init_basis_state"].str(), boost::is_any_of("|"));
+      coeffs = params_["init_coeffs"].as<std::vector<double> >();
+    }
+
+    for (int i = 0; i < list_dets.size(); i++) {
+      std::stringstream ss(list_dets[i]);
       int ichar;
       std::vector<int> tmp_vec;
       while (ss >> ichar) {
@@ -166,10 +190,10 @@ public:
       mps_tmp.normalize_right();
       if (i == 0) {
         mps = mps_tmp;
-        mps[0] *= coeff[0];
+        mps[0] *= coeffs[0];
       }
       else {
-        MPSBuffer = joinAndTruncate(mps, mps_tmp, initialBondDim, 1., coeff[i]);
+        MPSBuffer = joinAndTruncate(mps, mps_tmp, initialBondDim, 1., coeffs[i]);
         mps = MPSBuffer;
       }
     }
@@ -182,12 +206,13 @@ public:
 
 private:
   typename SymmGroup::charge right_end;
-  std::vector<double> coeff;
   std::vector<Index<SymmGroup> > phys_dims;
   std::vector<int> site_type;
   std::vector< std::vector<int> > basis_index;
+  std::vector<double> coeffs;
   BaseParameters& params;
   int initialBondDim;
+  bool fromFile;
 };
 
 /**
