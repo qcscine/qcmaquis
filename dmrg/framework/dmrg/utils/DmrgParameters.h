f8c6d58b9a65dd17f1e46b2129cb9845b1eec61c
Nina Glaser <nina.glaser@phys.chem.ethz.ch>
2023-06-12 14:41:39 +0200

    Pure optics

--- dmrg/framework/dmrg/utils/DmrgParameters.h
+++ dmrg/framework/dmrg/utils/DmrgParameters.h
@@ -199,7 +199,7 @@ private:
 
         // Solution of linear systems
         add_option("linsystem_precond", "If set to [diagonal], applies a diagonal preconditioner to the linear system solver", value("no"));
-        add_option("linsystem_init", "Initial guess for the Krylov basis (either [zero] for a zero MPS or [last] for the rhs", value("last"));
+        add_option("linsystem_init", "Initial guess for the Krylov basis (either [zero] for a zero MPS or [last] for the rhs)", value("last"));
         add_option("linsystem_max_it", "Maximum number of times the iterative linear system solver is repeated (if >1, does basically restarted GMRES", value(1));
         add_option("linsystem_tol", "Threshold for the error - if the error falls below [linsystem_tol], the iterative procedure is stopped", value(1.0E-5));
         add_option("linsystem_krylov_dim", "Maximum dimension of the Krylov subspace for the iterative solution of the linear system", value(50));
