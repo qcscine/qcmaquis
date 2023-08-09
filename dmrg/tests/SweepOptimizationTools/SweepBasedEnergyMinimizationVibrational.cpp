64fbcce0f66af68f55774ead4e3d1dae45ceb50e
Alberto Baiardi <baiardialberto91@gmail.com>
2023-08-09 08:47:51 +0200

    MAC-compatible version

--- dmrg/tests/SweepOptimizationTools/SweepBasedEnergyMinimizationVibrational.cpp
+++ dmrg/tests/SweepOptimizationTools/SweepBasedEnergyMinimizationVibrational.cpp
@@ -58,7 +58,7 @@ BOOST_FIXTURE_TEST_CASE(Test_SweepBasedEnergyMinimizerSS_Vibrational_Watson, Wat
   parametersEthyleneWatsonHarmonic.set("seed", 30031989);
   auto mpsDefault = MPS<matrix, TrivialGroup>(lattice.size(), *(watsonModel.initializer(lattice, parametersEthyleneWatsonHarmonic)));
   mpsDefault.normalize_right();
-  auto energyMinimizer = SweepBasedMinimizerSS(mpsDefault, watsonHarmonicMPO, parametersEthyleneWatsonHarmonic, watsonModel, lattice, false);
+  SweepBasedMinimizerSS energyMinimizer(mpsDefault, watsonHarmonicMPO, parametersEthyleneWatsonHarmonic, watsonModel, lattice, false);
   energyMinimizer.runSweepSimulation();
   double optimalEnergyFromSweeper = energyMinimizer.getSpecificResult<double>("Energy");
   // Now does the same with the interface
@@ -87,7 +87,7 @@ BOOST_FIXTURE_TEST_CASE(Test_SweepBasedEnergyMinimizerTS_Vibrational_Bilinearly,
   // TSOptimizer calculation
   auto mpsConst = MPS<matrix, TrivialGroup>(lattice.size(), *(bilinearlyModel.initializer(lattice, parametersBilinearly)));
   mpsConst.normalize_right();
-  auto energyMinimizer = SweepBasedMinimizerTS(mpsConst, bilinearlyMPO, parametersBilinearly, bilinearlyModel, lattice, false);
+  SweepBasedMinimizerTS energyMinimizer(mpsConst, bilinearlyMPO, parametersBilinearly, bilinearlyModel, lattice, false);
   energyMinimizer.runSweepSimulation();
   double optimalEnergyFromSweeper = energyMinimizer.getSpecificResult<double>("Energy");
   // Interface calculation
