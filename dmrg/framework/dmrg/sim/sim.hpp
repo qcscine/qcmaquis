f8f0a55121c990610597ee29b927d7132ca0c61e
Valentin Barandun <vbarandun@tc-ws-010.ethz.ch>
2023-10-11 12:05:30 +0200

    changed parallelization conditions for measurements

--- dmrg/framework/dmrg/sim/sim.hpp
+++ dmrg/framework/dmrg/sim/sim.hpp
@@ -253,13 +253,16 @@ template <class Matrix, class SymmGroup>
 void sim<Matrix, SymmGroup>::measure(std::string archive_path, measurements_type & meas)
 {
     #ifdef MAQUIS_OPENMP
-    #pragma omp parallel for schedule(dynamic) if(parms["parallelize_measurements"])
-    for (typename measurements_type::iterator it = meas.begin(); it < meas.end(); it++) {
+    if(parms["parallelize_measurements"]){
+        #pragma omp parallel for schedule(dynamic)
+        for (typename measurements_type::iterator it = meas.begin(); it < meas.end(); it++) {
         MPS<Matrix, SymmGroup> mpsCopy = mps; // this is required as the measurements might change the pairing of the mps
         // note that omp firstprivate cannot be used since the mps does apparently not fulfill the necessary requirements
         measure_and_save<Matrix, SymmGroup> ms(rfile(), archive_path, mpsCopy);
         ms(*it);
+        }
     }
+    else std::for_each(meas.begin(), meas.end(), measure_and_save<Matrix, SymmGroup>(rfile(), archive_path, mps)); 
     #else
     std::for_each(meas.begin(), meas.end(), measure_and_save<Matrix, SymmGroup>(rfile(), archive_path, mps));
     #endif
