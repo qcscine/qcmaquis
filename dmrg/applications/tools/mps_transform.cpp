31746d4c500ebaa944385d23b260475c0f0589ae
Momax <maximilian.moerchen@phys.chem.ethz.ch>
2023-10-24 22:37:26 +0200

    rearange header and strip whitespaces, convert tabs

--- dmrg/applications/tools/mps_transform.cpp
+++ dmrg/applications/tools/mps_transform.cpp
@@ -4,26 +4,27 @@
  *
  * Copyright (C) 2015 Laboratory of Physical Chemistry, ETH Zurich
  *               2015-2015 by Sebastian Keller <sebkelle@phys.ethz.ch>
- * 
+ *
  * This software is part of the ALPS Applications, published under the ALPS
  * Application License; you can use, redistribute it and/or modify it under
  * the terms of the license, either version 1 or (at your option) any later
  * version.
- * 
+ *
  * You should have received a copy of the ALPS Application License along with
  * the ALPS Applications; see the file LICENSE.txt. If not, the license is also
  * available from http://alps.comp-phys.org/.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT 
- * SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE 
- * FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE, 
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE
+ * FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  *
  *****************************************************************************/
 
+#include "dmrg/models/MolecularHamiltonians/transform_symmetry.hpp"
 #ifdef USE_AMBIENT
 #include <mpi.h>
 #endif
@@ -40,7 +41,6 @@ using std::endl;
 
 #include "dmrg/sim/matrix_types.h"
 #include "dmrg/models/model.h"
-#include "dmrg/models/MolecularHamiltonian/transform_symmetry.hpp"
 
 #if defined(USE_SU2U1)
 typedef SU2U1 grp;
@@ -98,7 +98,7 @@ int main(int argc, char ** argv)
 
         for (int Sz = -TwoS; Sz <= TwoS; Sz += 2)
         {
-    	    // skip loop to next iteration until we hit the target Sz value 
+    	    // skip loop to next iteration until we hit the target Sz value
 	        if((argc == 3) && (Sz != target_sz))
 	            continue;
 
@@ -110,7 +110,7 @@ int main(int argc, char ** argv)
 
             // the output MPS
             MPS<matrix, mapgrp> mps_out = transform_mps<matrix, grp>()(mps, Nup, Ndown);
-            
+
             std::string mps_out_file = mps_in_file;
             std::size_t pos = mps_out_file.find(".h5");
             if (pos != mps_out_file.size())
@@ -133,7 +133,7 @@ int main(int argc, char ** argv)
         }
 
         myfile.close();
-        
+
     } catch (std::exception& e) {
         std::cerr << "Error:" << std::endl << e.what() << std::endl;
         return 1;
