82daf2e438b4a4deefe984a6cc257f9650b65ec5
Nina Glaser <nina.glaser@phys.chem.ethz.ch>
2023-10-31 17:19:57 +0100

    Indented sebastians code

--- dmrg/framework/dmrg/models/tag_detail.h
+++ dmrg/framework/dmrg/models/tag_detail.h
@@ -76,38 +76,43 @@ namespace tag_detail {
      
         // determine scale of matrices
         const Matrix& m1 = reference[0];
-        for (int i = 0; i < num_rows(m1); i++)
-           for(int j = 0; j < num_cols(m1); j++)
-        {
-            if (std::abs(m1(i,j)) > 1.e-50) {
-                invscale1 = value_type(1.)/m1(i,j);
-                break;
+        for (int i = 0; i < num_rows(m1); i++) {
+            for(int j = 0; j < num_cols(m1); j++) {
+                {
+                    if (std::abs(m1(i,j)) > 1.e-50) {
+                        invscale1 = value_type(1.)/m1(i,j);
+                        break;
+                    }
+                    if(i == (num_rows(m1)-1) && j == (num_cols(m1)-1)){ return std::make_pair(false, 0.); }
+                }
             }
-            if(i == (num_rows(m1)-1) && j == (num_cols(m1)-1)){ return std::make_pair(false, 0.); }
         }
 
         const Matrix& m2 = sample[0];
-        for (int i = 0; i < num_rows(m2); i++)
-           for(int j = 0; j < num_cols(m2); j++)
-        {
-            if (std::abs(m2(i,j)) > 1.e-50) {
-                invscale2 = value_type(1.)/m2(i,j);
-                break;
+        for (int i = 0; i < num_rows(m2); i++) {
+            for(int j = 0; j < num_cols(m2); j++) {
+                {
+                    if (std::abs(m2(i,j)) > 1.e-50) {
+                        invscale2 = value_type(1.)/m2(i,j);
+                        break;
+                    }
+                    if(i == (num_rows(m2)-1) && j == (num_cols(m2)-1)){ return std::make_pair(false, 0.); }
+                }
             }
-            if(i == (num_rows(m2)-1) && j == (num_cols(m2)-1)){ return std::make_pair(false, 0.); }
         }
 
         // Check all blocks for equality modulo scale factor
-        for (typename Matrix::size_type b=0; b < reference.n_blocks(); ++b)
-        {
+        for (typename Matrix::size_type b=0; b < reference.n_blocks(); ++b) {
             const Matrix& mb1 = reference[b];
             const Matrix& mb2 = sample[b];
-            for (int i = 0; i < num_rows(mb1); i++)
-               for(int j = 0; j < num_cols(mb1); j++)
-            {
-                typename Matrix::value_type t1 = mb1(i,j) * invscale1, t2 = mb2(i,j) * invscale2;
-                if (std::abs(t1 - t2) > 1e-12)
-                    return std::make_pair(false, 0.);
+            for (int i = 0; i < num_rows(mb1); i++) {
+                for(int j = 0; j < num_cols(mb1); j++) {
+                    {
+                        typename Matrix::value_type t1 = mb1(i,j) * invscale1, t2 = mb2(i,j) * invscale2;
+                        if (std::abs(t1 - t2) > 1e-12)
+                            return std::make_pair(false, 0.);
+                    }
+                }
             }
         }
 
