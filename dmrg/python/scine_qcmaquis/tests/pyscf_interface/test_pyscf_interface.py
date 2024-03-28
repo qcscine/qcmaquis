8c6e4f726e82090b087200bbda6779e0e150d832
Momax <maximilian.moerchen@phys.chem.ethz.ch>
2024-03-28 14:41:29 +0100

    more tests

--- dmrg/python/scine_qcmaquis/tests/pyscf_interface/test_pyscf_interface.py
+++ dmrg/python/scine_qcmaquis/tests/pyscf_interface/test_pyscf_interface.py
@@ -1,9 +1,19 @@
-
+import os
+import shutil
 
 import pytest
 from pyscf import gto, mcscf, scf
 
 from scine_qcmaquis.pyscf_interface.pyscf_interface import QcMaquis
+
+
+def remove_checkpoint_and_results_file():
+    path = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(path)
+    shutil.rmtree("checkpoint_DMRGSCF.h5", ignore_errors=True)
+    shutil.rmtree("checkpoint_gs.h5", ignore_errors=True)
+    os.remove("results_DMRGSCF.h5")
+    os.remove("results_file.h5")
 
 
 def test_dmrgci():
@@ -19,7 +29,8 @@ def test_dmrgci():
     mc = mcscf.CASCI(m, 4, 4)
     mc.fcisolver = QcMaquis(mol)
     mc.fcisolver.file_path = None
-    mc.fcisolver.verbose = 5
+    mc.fcisolver.parameters.set_orbital_optimization()
+    # mc.fcisolver.verbose = 1
     emc_0 = mc.casci()[0]
 
     mol = gto.Mole()
@@ -32,8 +43,8 @@ def test_dmrgci():
     m.scf()
     mc = mcscf.CASCI(m, 4, 4)
     emc_0ref = mc.casci()[0]
-    # print('Maquis-CI  = %.15g CASCI  = %.15g' % (emc_0, emc_0ref))
-    # print('Diff = %.15g' % (emc_0 - emc_0ref))
+    print('Maquis-CI  = %.15g CASCI  = %.15g' % (emc_0, emc_0ref))
+    print('Diff = %.15g' % (emc_0 - emc_0ref))
     assert abs(emc_0ref - emc_0) < 1e-9
 
 
@@ -51,6 +62,7 @@ def test_dmrgscf():
     mc.max_cycle_macro = 20
     mc.fcisolver = QcMaquis(mol)
     mc.fcisolver.file_path = None
+    mc.fcisolver.parameters.set_orbital_optimization()
     emc_1 = mc.mc2step()[0]
 
     mol = gto.Mole()
@@ -64,11 +76,86 @@ def test_dmrgscf():
     mc = mcscf.CASSCF(m, 4, 4)
     mc.max_cycle_macro = 20
     emc_1ref = mc.mc2step()[0]
-    # print('Maquis-SCF = %.15g CASSCF = %.15g' % (emc_1, emc_1ref))
-    # print('Diff = %.15g' % (emc_1 - emc_1ref))
+    print('Maquis-SCF = %.15g CASSCF = %.15g' % (emc_1, emc_1ref))
+    print('Diff = %.15g' % (emc_1 - emc_1ref))
     assert abs(emc_1ref - emc_1) < 1e-9
+
+
+def test_dmrgscf_with_checkpoint():
+    mol = gto.Mole()
+    mol.build(
+        atom=[['N', (0., 0., i)] for i in range(8)],
+        basis={'N': '6-31g'},
+        symmetry=False, spin=2, verbose=1,
+    )
+    m = scf.RHF(mol)
+    m.scf()
+
+    mc = mcscf.CASSCF(m, 6, 6)
+    mc.max_cycle_macro = 20
+    mc.fcisolver = QcMaquis(mol)
+    mc.fcisolver.verbose = 1
+
+    mc.fcisolver.file_path = os.path.dirname(os.path.abspath(__file__))
+    mc.fcisolver.parameters.set_orbital_optimization()
+    mc.fcisolver.parameters.set_entropies()
+    emc_1 = mc.mc2step()[0]
+
+    mol = gto.Mole()
+    mol.build(
+        atom=[['N', (0., 0., i)] for i in range(8)],
+        basis={'N': '6-31g'},
+        symmetry=False, spin=2, verbose=1,
+    )
+    m = scf.RHF(mol)
+    m.scf()
+    mc = mcscf.CASSCF(m, 6, 6)
+    mc.max_cycle_macro = 20
+    emc_1ref = mc.mc2step()[0]
+    print('Maquis-SCF = %.15g CASSCF = %.15g' % (emc_1, emc_1ref))
+    print('Diff = %.15g' % (emc_1 - emc_1ref))
+    assert abs(emc_1ref - emc_1) < 5e-9
+
+
+def test_dmrgscf_with_existing_checkpoint():
+    mol = gto.Mole()
+    mol.build(
+        atom=[['N', (0., 0., i)] for i in range(8)],
+        basis={'N': '6-31g'},
+        symmetry=False, spin=2, verbose=1,
+    )
+    m = scf.RHF(mol)
+    m.scf()
+
+    mc = mcscf.CASSCF(m, 6, 6)
+    mc.max_cycle_macro = 20
+    mc.fcisolver = QcMaquis(mol)
+    mc.fcisolver.verbose = 1
+
+    mc.fcisolver.file_path = os.path.dirname(os.path.abspath(__file__))
+    mc.fcisolver.parameters.set_orbital_optimization()
+    mc.fcisolver.parameters.set_entropies()
+    emc_1 = mc.mc2step()[0]
+
+    mol = gto.Mole()
+    mol.build(
+        atom=[['N', (0., 0., i)] for i in range(8)],
+        basis={'N': '6-31g'},
+        symmetry=False, spin=2, verbose=1,
+    )
+    m = scf.RHF(mol)
+    m.scf()
+    mc = mcscf.CASSCF(m, 6, 6)
+    mc.max_cycle_macro = 20
+    emc_1ref = mc.mc2step()[0]
+    print('Maquis-SCF = %.15g CASSCF = %.15g' % (emc_1, emc_1ref))
+    print('Diff = %.15g' % (emc_1 - emc_1ref))
+    assert abs(emc_1ref - emc_1) < 5e-9
 
 
 if __name__ == "__main__":
     test_dmrgci()
     test_dmrgscf()
+    test_dmrgscf_with_checkpoint()
+    test_dmrgscf_with_existing_checkpoint()
+    remove_checkpoint_and_results_file()
