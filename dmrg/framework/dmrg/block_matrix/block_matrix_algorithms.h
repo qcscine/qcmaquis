e24b30e71dc1f70481ba0be9e13b5baa4b6ac7a2
Nina Glaser <nina.glaser@phys.chem.ethz.ch>
2024-05-15 11:02:50 +0200

    Update block_matrix_algorithms.h removing leftover couts and commented out stuff

--- dmrg/framework/dmrg/block_matrix/block_matrix_algorithms.h
+++ dmrg/framework/dmrg/block_matrix/block_matrix_algorithms.h
@@ -286,23 +286,9 @@ void estimate_truncation(
   std::reverse(allevals.begin(), allevals.end());
 
   real_type evalscut = cutoff * allevals[0];
-  //std::cout << "--- FACTORS DETERMINIG EVALSCUT ---" << std::endl;
-  //std::cout << "cutoff : " << cutoff << std::endl;
-  //std::cout << "highest eval : "  << allevals[0] << std::endl;
-  //std::cout << " ----------------------------------" << std::endl;
 
   if (allevals.size() > Mmax) {
-    //std::cout << "truncated to Max Bond Dim." << std::endl;
     evalscut = std::max(evalscut, allevals[Mmax]);
-    /*
-    if(allevals[Mmax] == 0 && evalscut == 0){
-      auto lastNonZero = std::find_if(allevals.rbegin(), allevals.rend(), [](double val) {
-        return val != 0.0;
-      });
-      evalscut = *lastNonZero;
-      std::cout << "entered if statement 1" << std::endl;
-    }
-    */
   }
   smallest_ev = evalscut;
 
@@ -353,7 +339,6 @@ truncation_results svd_truncate(
 
   Index<SymmGroup> old_basis = S.left_basis();
   size_t* keeps = new size_t[S.n_blocks()];
-  std::cout << "keep before etimate_truncation() : " << keeps[0] << std::endl;
   double truncated_fraction;
   double truncated_weight;
   double smallest_ev;
@@ -365,7 +350,6 @@ truncation_results svd_truncate(
   estimate_truncation(
       S, Mmax, rel_tol, keeps, truncated_fraction, truncated_weight, smallest_ev
   );
-  std::cout << "keep before etimate_truncation() : " << keeps[0] << std::endl;
 
   for (int k = S.n_blocks() - 1; k >= 0;
        --k)  // C - we reverse faster and safer ! we avoid bug if keeps[k] = 0
@@ -392,14 +376,8 @@ truncation_results svd_truncate(
 #endif
 
       if (keep >= num_rows(S[k])) {
-        std::cout << "keep is larger than num_rows(S[k])" << std::endl;
-        std::cout << "keep : " << keep << std::endl;
-        std::cout << "num_rows(S[k]) : " << num_rows(S[k]) << std::endl;
         continue;
       }
-      std::cout << "truncation occured" << std::endl;
-      std::cout << "num_rows(S[k]) : " << num_rows(S[k]) << std::endl; 
-      std::cout << "keep : " << keep << std::endl;
       S.resize_block(
           S.basis().left_charge(k), S.basis().right_charge(k), keep, keep
       );
