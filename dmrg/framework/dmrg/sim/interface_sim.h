bdccd1bd156281966dd5c082761138962cb71d37
Kalman Szenes <szenes.kalman@gmail.com>
2024-05-27 11:00:17 +0200

    Revert "feat: print number of openmp threads"
    This reverts commit 61e2765b2d462c40da1019ea8b1720c8528027c0.

--- dmrg/framework/dmrg/sim/interface_sim.h
+++ dmrg/framework/dmrg/sim/interface_sim.h
@@ -19,7 +19,6 @@
 #include <stdexcept>
 #include <string>
 #include <vector>
-#include "omp.h"
 
 #include "dmrg/models/measurements/chementropy.h"
 #include "dmrg/MetaSweepSimulations/FEASTLauncher.h"
@@ -151,7 +150,6 @@ class interface_sim : public sim<Matrix, SymmGroup>,
 
   /** @brief Runs a DMRG-based optimization */
   void run(const std::string& simulationType) override {
-    std::cout << "Number of OMP threads: " << omp_get_max_threads() << '\n';
     Timer timer(simulationType);
     timer.begin();
     if (simulationType == "optimize") {
