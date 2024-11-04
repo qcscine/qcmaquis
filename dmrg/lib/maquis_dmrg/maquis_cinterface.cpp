9788c79641458534156c2665d904a22d3c2c576a
Kalman Szenes <szenes.kalman@gmail.com>
2024-11-04 11:09:54 +0100

    fix(caspt2)

--- dmrg/lib/maquis_dmrg/maquis_cinterface.cpp
+++ dmrg/lib/maquis_dmrg/maquis_cinterface.cpp
@@ -560,7 +560,7 @@ extern "C"
       printf("Measuring 3RDM between\n  ket=%s\n  bra=%s\n", twou1_chkp_name.c_str(), MPStimesMPOstr.c_str());
       // parms_caspt2.set("MEASURE[4rdm]", 1); // test
       parms_caspt2.set("MEASURE[trans3rdm]", twou1_chkp_name);
-      parms_caspt2.set("chkpfile", twou1_chkp_name);
+      parms_caspt2.set("chkpfile", MPStimesMPOstr);
       // parms_caspt2.set("resultfile", pname2workdir(pname) + "results.h5");
       printf("Building interface\n");
       maquis::DMRGInterface<double> interface_measure(parms_caspt2);
