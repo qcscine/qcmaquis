cefb624497e17eb9bff4c839aae6edd6f12008f5
Kalman Szenes <szenes.kalman@gmail.com>
2024-11-11 14:18:41 +0100

    fix(caspt2)

--- dmrg/lib/maquis_dmrg/maquis_cinterface.cpp
+++ dmrg/lib/maquis_dmrg/maquis_cinterface.cpp
@@ -575,8 +575,7 @@ extern "C"
       // Build integral map
       maquis::integral_map<double> int_map;
       for (int i = 0; i < nasht; ++i) {
-        // Maybe phase factor (-) needed for OpenMolcas convention
-        int_map[{i+1, i+1, 0, 0}] = -epsa[i];
+        int_map[{i+1, i+1, 0, 0}] = epsa[i];
       }
 
       // Compute MPO * |MPS>
