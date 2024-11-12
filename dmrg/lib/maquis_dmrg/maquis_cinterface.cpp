6f11152b43fe51cd78f2ea6f3dfac4f000a7c4dd
Kalman Szenes <szenes.kalman@gmail.com>
2024-11-12 10:15:30 +0100

    fix(caspt2)

--- dmrg/lib/maquis_dmrg/maquis_cinterface.cpp
+++ dmrg/lib/maquis_dmrg/maquis_cinterface.cpp
@@ -642,7 +642,7 @@ extern "C"
         indices[6*i+3] = trans3rdm_meas.first[i][3];
         indices[6*i+4] = trans3rdm_meas.first[i][4];
         indices[6*i+5] = trans3rdm_meas.first[i][5];
-        printf("i = %d\n", i);
       }
+      exit(1);
     }
 }
