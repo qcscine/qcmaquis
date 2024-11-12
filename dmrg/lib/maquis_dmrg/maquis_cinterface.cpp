1f8ac11a1e400e86074d6e852fa36ae604078995
Kalman Szenes <szenes.kalman@gmail.com>
2024-11-12 10:13:18 +0100

    fix(caspt2)

--- dmrg/lib/maquis_dmrg/maquis_cinterface.cpp
+++ dmrg/lib/maquis_dmrg/maquis_cinterface.cpp
@@ -630,7 +630,7 @@ extern "C"
       printf("Extracting t-3RDM from measurements\n");
       const typename maquis::meas_with_results_type<V>& trans3rdm_meas = interface_measure.getMeasurement("transition_threeptdm");
       printf("Reording t-3RDM from measurements\n");
-      printf("Size of values = %d", size);
+      printf("Size of values = %d\n", size);
       assert(size >= trans3rdm_meas.first.size());
       assert(size >= trans3rdm_meas.second.size());
       for (int i = 0; i < trans3rdm_meas.first.size(); i++)
@@ -642,6 +642,7 @@ extern "C"
         indices[6*i+3] = trans3rdm_meas.first[i][3];
         indices[6*i+4] = trans3rdm_meas.first[i][4];
         indices[6*i+5] = trans3rdm_meas.first[i][5];
+        printf("i = %d\n", i);
       }
     }
 }
