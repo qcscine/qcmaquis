9cbcd6a8c211150246f712cf176364f30c9892d0
Kalman Szenes <szenes.kalman@gmail.com>
2024-11-21 17:03:04 +0100

    fix(molcas)

--- dmrg/lib/interfaces/openmolcas/qcmaquis_interface.f90
+++ dmrg/lib/interfaces/openmolcas/qcmaquis_interface.f90
@@ -286,9 +286,7 @@ module qcmaquis_interface
 
         call GET_ENVIRONMENT_VARIABLE('Project', qcmaquis_param%project_name)
         call GET_ENVIRONMENT_VARIABLE('CurrDir', qcmaquis_param%currdir)
-        call GET_ENVIRONMENT_VARIABLE('ScratchDir', qcmaquis_param%scratchdir)
-        write(*,*) qcmaquis_param%scratchdir
-        call exit(1)
+        call GET_ENVIRONMENT_VARIABLE('WorkDir', qcmaquis_param%scratchdir)
 
 
         ! save wavefunction parameters
