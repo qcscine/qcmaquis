e82e9a9096274673cfc245a559e5654a9a9c1c79
Kalman Szenes <szenes.kalman@gmail.com>
2024-11-21 16:45:03 +0100

    feat(molcas): add scratchdir to interface

--- dmrg/lib/interfaces/openmolcas/qcmaquis_interface.f90
+++ dmrg/lib/interfaces/openmolcas/qcmaquis_interface.f90
@@ -286,6 +286,7 @@ module qcmaquis_interface
 
         call GET_ENVIRONMENT_VARIABLE('Project', qcmaquis_param%project_name)
         call GET_ENVIRONMENT_VARIABLE('CurrDir', qcmaquis_param%currdir)
+        call GET_ENVIRONMENT_VARIABLE('ScratchDir', qcmaquis_param%scratchdir)
 
         ! save wavefunction parameters
         qcmaquis_param%nactel = nel
