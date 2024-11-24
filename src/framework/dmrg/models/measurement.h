1c27d92a9f20f9794f6479dfdf3f0b14d5451f5c
Kalman Szenes <szenes.kalman@gmail.com>
2024-11-24 19:32:52 +0100

    fix(measuremest): reorder based on orbital_order

--- src/framework/dmrg/models/measurement.h
+++ src/framework/dmrg/models/measurement.h
@@ -256,9 +256,11 @@ template <class T>
 inline std::vector<T> order_labels(
     const Lattice& lat, const std::vector<T>& labels
 ) {
-  // std::vector<T> ret(labels.size());
-  // std::transform(labels.begin(), labels.end(), ret.begin(), [&](T i){return
-  // lat.get_prop<T>("label_int", i);}); return ret;
+  std::vector<T> ret(labels.size());
+  std::transform(labels.begin(), labels.end(), ret.begin(), [&](T i) {
+    return lat.get_prop<T>("label_int", i);
+  });
+  return ret;
   return labels;
 }
 
