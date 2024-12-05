5c58b011f88d1d84f766cdb810eb79d5e14187e9
Kalman Szenes <szenes.kalman@gmail.com>
2024-12-05 13:36:49 +0100

    fix(compress): typo in include

--- src/applications/compress/mps_compress.cpp
+++ src/applications/compress/mps_compress.cpp
@@ -9,7 +9,7 @@
 #include "dmrg/sim/matrix_types.h"
 #include "dmrg/mp_tensors/mps.h"
 #include "dmrg/mp_tensors/compression.h"
-#include "dmrg/models/MolecularHamiltonian/transform_symmetry.hpp"
+#include "dmrg/models/MolecularHamiltonians/transform_symmetry.hpp"
 
 #include <boost/algorithm/string.hpp>
 #include <boost/algorithm/string/replace.hpp>
