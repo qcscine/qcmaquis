d313b9ee8f76e37cba3627d9cf24c7a88f1658d6
Kalman Szenes <szenes.kalman@gmail.com>
2025-01-23 09:32:53 +0100

    Revert "debug: print number of microiters"
    This reverts commit 979767e11b33c38ad1d8339617367af19eb27668.

--- dmrg/framework/dmrg/SweepBasedAlgorithms/SweepOptimizationTypeTrait.h
+++ dmrg/framework/dmrg/SweepBasedAlgorithms/SweepOptimizationTypeTrait.h
@@ -111,8 +111,6 @@ public:
       throw std::runtime_error("TS optimizer not available for 1-site lattices");
     else
       numberOfMicroiterations = 2*(L-2);
-    std::cout << "number of micro iterations = " << numberOfMicroiterations << std::endl;
-    exit(1);
     return numberOfMicroiterations;
   }
 
